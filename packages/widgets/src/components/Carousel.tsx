9f773381ccbe02a03442688175b4bb2d72f08eb1
Steve Sewell <sewell.steve@gmail.com>
2019-02-13 13:54:02 -0800

    Dev version bump

--- packages/widgets/src/components/Carousel.tsx
+++ packages/widgets/src/components/Carousel.tsx
@@ -1,18 +1,16 @@
-
 import {
   BuilderBlock,
   BuilderBlocks,
   BuilderBlockComponent,
   BuilderElement,
-  BuilderStoreContext,
-} from '@builder.io/react';
-import React from 'react';
+  BuilderStoreContext
+} from '@builder.io/react'
+import React from 'react'
 import get from 'lodash-es/get'
 import isArray from 'lodash-es/isArray'
 import last from 'lodash-es/last'
 
-import Slider from 'react-slick';
-
+import Slider from 'react-slick'
 
 const defaultElement: BuilderElement = {
   '@type': '@builder.io/sdk:Element',
@@ -20,8 +18,8 @@ const defaultElement: BuilderElement = {
     large: {
       height: '400px',
       display: 'flex',
-      flexDirection: 'column',
-    },
+      flexDirection: 'column'
+    }
   },
   children: [
     {
@@ -31,42 +29,42 @@ const defaultElement: BuilderElement = {
           marginTop: '50px',
           textAlign: 'center',
           display: 'flex',
-          flexDirection: 'column',
-        },
+          flexDirection: 'column'
+        }
       },
       component: {
         name: 'Text',
         options: {
-          text: 'I am a slide',
-        },
-      },
-    },
-  ],
-};
+          text: 'I am a slide'
+        }
+      }
+    }
+  ]
+}
 const defaultButton: BuilderElement = {
   '@type': '@builder.io/sdk:Element',
   responsiveStyles: {
     large: {
       display: 'flex',
       height: '30px',
-      flexDirection: 'column',
-    },
-  },
-};
+      flexDirection: 'column'
+    }
+  }
+}
 
-type BuilderBlockType = BuilderElement;
+type BuilderBlockType = BuilderElement
 
 interface CarouselProps {
   slides: Array<
     React.ReactNode | { content: BuilderBlockType[] } /* BuilderBlock <- export this type */
-  >;
-  builderBlock: BuilderBlockType;
-  nextButton?: BuilderBlockType[];
-  prevButton?: BuilderBlockType[];
-  autoplay?: boolean;
-  autoplaySpeed?: number;
-  hideDots?: boolean;
-  useChildrenForSlides?: boolean;
+  >
+  builderBlock: BuilderBlockType
+  nextButton?: BuilderBlockType[]
+  prevButton?: BuilderBlockType[]
+  autoplay?: boolean
+  autoplaySpeed?: number
+  hideDots?: boolean
+  useChildrenForSlides?: boolean
 }
 
 // TODO: change to slick grid
@@ -83,31 +81,31 @@ interface CarouselProps {
           name: 'content',
           type: 'uiBlocks',
           hideFromUI: true,
-          defaultValue: [defaultElement],
-        },
+          defaultValue: [defaultElement]
+        }
       ],
       defaultValue: [
         {
-          content: [defaultElement],
+          content: [defaultElement]
         },
         {
-          content: [defaultElement],
-        },
+          content: [defaultElement]
+        }
       ],
       // showIf: (options: Map<string, any>) => !options.get('useChildrenForSlides'),
-      showIf: `!options.get('useChildrenForSlides')`,
+      showIf: `!options.get('useChildrenForSlides')`
     },
     {
       name: 'hideDots',
       helperText: 'Show pagination dots',
       type: 'boolean',
-      defaultValue: false,
+      defaultValue: false
     },
     {
       name: 'autoplay',
       helperText: 'Automatically rotate to the next slide every few seconds',
       type: 'boolean',
-      defaultValue: false,
+      defaultValue: false
     },
     {
       name: 'autoplaySpeed',
@@ -118,7 +116,7 @@ interface CarouselProps {
       // TODO: showIf option
       // showIf: (options: Map<string, any>) => options.get('autoplay'),
       // TODO: why fn not working?
-      showIf: `options.get('autoplay')`,
+      showIf: `options.get('autoplay')`
       // showIf: (options) => options.get('autoPlay')
     },
     // TODO: on add new duplicate the prior or expect use templates
@@ -133,11 +131,12 @@ interface CarouselProps {
           component: {
             name: 'Image',
             options: {
-              image: 'https://cdn.builder.codes/api/v1/image/assets%2FagZ9n5CUKRfbL9t6CaJOyVSK4Es2%2Fdb2a9827561249aea3817b539aacdcdc',
-            },
-          },
-        },
-      ],
+              image:
+                'https://cdn.builder.codes/api/v1/image/assets%2FagZ9n5CUKRfbL9t6CaJOyVSK4Es2%2Fdb2a9827561249aea3817b539aacdcdc'
+            }
+          }
+        }
+      ]
     },
     {
       name: 'nextButton',
@@ -149,11 +148,12 @@ interface CarouselProps {
           component: {
             name: 'Image',
             options: {
-              image: 'https://cdn.builder.codes/api/v1/image/assets%2FagZ9n5CUKRfbL9t6CaJOyVSK4Es2%2Fd909a5b91650499c9e0524cc904eeb77',
-            },
-          },
-        },
-      ],
+              image:
+                'https://cdn.builder.codes/api/v1/image/assets%2FagZ9n5CUKRfbL9t6CaJOyVSK4Es2%2Fd909a5b91650499c9e0524cc904eeb77'
+            }
+          }
+        }
+      ]
     },
     // {
     //   name: 'initialHeight',
@@ -171,23 +171,60 @@ interface CarouselProps {
       defaultValue: false,
       onChange: (options: Map<string, any>) => {
         if (options.get('useChildrenForSlides') === true) {
-          const slides = options.get('slides');
+          const slides = options.get('slides')
           if (slides && Array.isArray(slides)) {
-            options.set('slides', []);
+            options.set('slides', [])
           }
         }
-      },
-    },
-  ],
+      }
+    }
+  ]
 })
 export class BuilderCarousel extends React.Component<CarouselProps> {
+  divRef: HTMLElement | null = null
+  sliderRef: Slider | null = null
+
+  componentDidMount() {
+    setTimeout(() => {
+      if (this.divRef) {
+        this.divRef.dispatchEvent(
+          new CustomEvent('builder:carousel:load', {
+            bubbles: true,
+            cancelable: false,
+            detail: {
+              block: this.props.builderBlock,
+              carousel: this.sliderRef
+            }
+          })
+        )
+      }
+    })
+  }
+
   render() {
     return (
       <BuilderStoreContext.Consumer>
         {state => (
-          <React.Fragment>
+          <div ref={ref => (this.divRef = ref)} className="builder-carousel">
             <style type="text/css">${slickStyles}</style>
             <Slider
+              ref={ref => (this.sliderRef = ref)}
+              afterChange={slide => {
+                // TODO; callbacks
+                if (this.divRef) {
+                  this.divRef.dispatchEvent(
+                    new CustomEvent('builder:carousel:change', {
+                      bubbles: true,
+                      cancelable: false,
+                      detail: {
+                        slide,
+                        block: this.props.builderBlock,
+                        carousel: this.sliderRef
+                      }
+                    })
+                  )
+                }
+              }}
               autoplay={this.props.autoplay}
               autoplaySpeed={this.props.autoplaySpeed ? this.props.autoplaySpeed * 1000 : undefined}
               dots={!this.props.hideDots}
@@ -218,12 +255,11 @@ export class BuilderCarousel extends React.Component<CarouselProps> {
                   this.props.builderBlock.children &&
                   this.props.builderBlock.children.map((block: BuilderElement, index: number) => {
                     if (block.repeat && block.repeat.collection) {
-                      const collectionPath = block.repeat.collection;
-                      const collectionName = last((collectionPath || '').trim().split('.'));
+                      const collectionPath = block.repeat.collection
+                      const collectionName = last((collectionPath || '').trim().split('.'))
                       const itemName =
-                        block.repeat.itemName ||
-                        (collectionName ? collectionName + 'Item' : 'item');
-                      const array = get(state.state, collectionPath);
+                        block.repeat.itemName || (collectionName ? collectionName + 'Item' : 'item')
+                      const array = get(state.state, collectionPath)
                       if (isArray(array)) {
                         return array.map((data, index) => {
                           // TODO: Builder state produce the data
@@ -231,26 +267,25 @@ export class BuilderCarousel extends React.Component<CarouselProps> {
                             ...state.state,
                             $index: index,
                             $item: data,
-                            [itemName]: data,
-                          };
+                            [itemName]: data
+                          }
 
                           return (
                             <BuilderStoreContext.Provider
-                              key={block.id + '-provider'}
+                              key={block.id}
                               value={{ ...state, state: childState } as any}
                             >
                               <BuilderBlockComponent
-                                key={block.id}
                                 block={{
                                   ...block,
-                                  repeat: null,
+                                  repeat: null
                                 }}
                                 index={index}
                                 child={true} /* TODO: fieldname? */
                               />
                             </BuilderStoreContext.Provider>
-                          );
-                        });
+                          )
+                        })
                       }
                     }
                     return (
@@ -260,7 +295,7 @@ export class BuilderCarousel extends React.Component<CarouselProps> {
                         index={index}
                         child={true} /* TODO: fieldname? */
                       />
-                    );
+                    )
                   })
                 : this.props.slides &&
                   this.props.slides.map((slide, index) => (
@@ -275,14 +310,14 @@ export class BuilderCarousel extends React.Component<CarouselProps> {
                     />
                   ))}
             </Slider>
-          </React.Fragment>
+          </div>
         )}
       </BuilderStoreContext.Consumer>
-    );
+    )
   }
 }
 
 const slickStyles = `
   .slick-list,.slick-slider,.slick-track{position:relative;display:block}.slick-loading .slick-slide,.slick-loading .slick-track{visibility:hidden}.slick-slider{box-sizing:border-box;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;-webkit-touch-callout:none;-khtml-user-select:none;-ms-touch-action:pan-y;touch-action:pan-y;-webkit-tap-highlight-color:transparent}.slick-list{overflow:hidden;margin:0;padding:0}.slick-list:focus{outline:0}.slick-list.dragging{cursor:pointer;cursor:hand}.slick-slider .slick-list,.slick-slider .slick-track{-webkit-transform:translate3d(0,0,0);-moz-transform:translate3d(0,0,0);-ms-transform:translate3d(0,0,0);-o-transform:translate3d(0,0,0);transform:translate3d(0,0,0)}.slick-track{top:0;left:0}.slick-track:after,.slick-track:before{display:table;content:''}.slick-track:after{clear:both}.slick-slide{display:none;float:left;height:100%;min-height:1px}[dir=rtl] .slick-slide{float:right}.slick-slide img{display:block}.slick-slide.slick-loading img{display:none}.slick-slide.dragging img{pointer-events:none}.slick-initialized .slick-slide{display:block}.slick-vertical .slick-slide{display:block;height:auto;border:1px solid transparent}.slick-arrow.slick-hidden{display:none}
   .slick-dots,.slick-next,.slick-prev{position:absolute;display:block;padding:0}.slick-dots li button:before,.slick-next:before,.slick-prev:before{font-family:slick;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.slick-loading .slick-list{background:url(ajax-loader.gif) center center no-repeat #fff}@font-face{font-family:slick;font-weight:400;font-style:normal;src:url('https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/fonts/slick.eot');src:url('https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/fonts/slick.eot?#iefix') format('embedded-opentype'),url('https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/fonts/slick.woff') format('woff'),url('https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/fonts/slick.ttf') format('truetype'),url('https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/fonts/slick.svg#slick') format('svg')}.slick-next,.slick-prev{font-size:0;line-height:0;top:50%;width:20px;height:20px;-webkit-transform:translate(0,-50%);-ms-transform:translate(0,-50%);transform:translate(0,-50%);cursor:pointer;color:transparent;border:none;outline:0;background:0 0}.slick-next:focus,.slick-next:hover,.slick-prev:focus,.slick-prev:hover{color:transparent;outline:0;background:0 0}.slick-next:focus:before,.slick-next:hover:before,.slick-prev:focus:before,.slick-prev:hover:before{opacity:1}.slick-next.slick-disabled:before,.slick-prev.slick-disabled:before{opacity:.25}.slick-next:before,.slick-prev:before{font-size:20px;line-height:1;opacity:.75;color:#fff}.slick-prev{left:-25px}[dir=rtl] .slick-prev{right:-25px;left:auto}.slick-prev:before{content:'â†'}.slick-next:before,[dir=rtl] .slick-prev:before{content:'â†’'}.slick-next{right:-25px}[dir=rtl] .slick-next{right:auto;left:-25px}[dir=rtl] .slick-next:before{content:'â†'}.slick-dotted.slick-slider{margin-bottom:30px}.slick-dots{bottom:-25px;width:100%;margin:0;list-style:none;text-align:center}.slick-dots li{position:relative;display:inline-block;width:20px;height:20px;margin:0 5px;padding:0;cursor:pointer}.slick-dots li button{font-size:0;line-height:0;display:block;width:20px;height:20px;padding:5px;cursor:pointer;color:transparent;border:0;outline:0;background:0 0}.slick-dots li button:focus,.slick-dots li button:hover{outline:0}.slick-dots li button:focus:before,.slick-dots li button:hover:before{opacity:1}.slick-dots li button:before{font-size:6px;line-height:20px;position:absolute;top:0;left:0;width:20px;height:20px;content:'â€¢';text-align:center;opacity:.25;color:#000}.slick-dots li.slick-active button:before{opacity:.75;color:#000}
-`;
+`
