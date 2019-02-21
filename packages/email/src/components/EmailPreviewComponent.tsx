bb9e719466415f06e03b13f13c7a73262607954f
Steve Sewell <sewell.steve@gmail.com>
2019-02-21 13:45:45 -0800

    fix

--- packages/email/src/components/EmailPreviewComponent.tsx
+++ packages/email/src/components/EmailPreviewComponent.tsx
@@ -1,7 +1,7 @@
 import { resetStyles } from '../constants/reset-styles'
 import React from 'react'
 import { BuilderComponent } from '@builder.io/react'
-import { BuilderPageProps } from '@builder.io/react/dist/types/components/builder-page.component'
+import { BuilderPageProps } from '@builder.io/react/dist/types/src/components/builder-page.component'
 import { EmailPreview } from './EmailPreview'
 
 interface EmailPreviewComponentProps extends BuilderPageProps {}
