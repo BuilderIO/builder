cca6364146c2f2b7e5bd4422d5ae3c7f30e02483
Steve Sewell <sewell.steve@gmail.com>
2019-03-04 09:21:30 -0800

    add back log

--- packages/angular/src/public_api.ts
+++ packages/angular/src/public_api.ts
@@ -1,3 +1,6 @@
 export * from './app/modules/builder/builder.module';
 export * from './app/modules/builder/decorators/builder-component.dectorator';
 export * from './app/modules/builder/services/builder.service';
+export {
+  RouteEvent,
+} from './app/modules/builder/components/builder-component/builder-component.component';
