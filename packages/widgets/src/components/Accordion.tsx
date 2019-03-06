d797fb30b8b2fbe67b81b1eca32defd23153ff10
Steve Sewell <sewell.steve@gmail.com>
2019-03-05 17:35:30 -0800

    accordion with children option

--- packages/widgets/src/components/Accordion.tsx
+++ packages/widgets/src/components/Accordion.tsx
@@ -1,5 +1,15 @@
-import { BuilderBlock, BuilderElement, BuilderBlocks } from '@builder.io/react'
+import {
+  BuilderBlock,
+  BuilderElement,
+  BuilderBlocks,
+  BuilderStoreContext,
+  BuilderBlockComponent
+} from '@builder.io/react'
 import React from 'react'
+import get from 'lodash-es/get'
+import isArray from 'lodash-es/isArray'
+import last from 'lodash-es/last'
+// import { get, isArray, last } from 'lodash';
 
 const defaultTitle: BuilderElement = {
   '@type': '@builder.io/sdk:Element',
@@ -79,15 +89,18 @@ interface AccordionProps {
   grid?: boolean
   defaultOpen?: number
   animate?: boolean
+  builderBlock?: BuilderElement
   // TODO: gridRowWidth
   gridRowWidth?: number
+  useChildrenForItems?: boolean
 }
 
 // TODO: change to slick grid
 @BuilderBlock({
   name: 'Builder:Accordion',
+  canHaveChildren: true,
   image:
-    'https://cdn.builder.io/api/v1/image/assets%2FagZ9n5CUKRfbL9t6CaJOyVSK4Es2%2Ffab6c1fd3fe542408cbdec078bca7f35?width=2000&height=1200',
+    'https://cdn.builder.io/api/v1/image/assets%2FagZ9n5CUKRfbL9t6CaJOyVSK4Es2%2Ffab6c1fd3fe542408cbdec078bca7f35',
   defaultStyles: {
     display: 'flex',
     flexDirection: 'column',
@@ -120,7 +133,8 @@ interface AccordionProps {
           title: [defaultTitle],
           detail: [defaultDetail]
         }
-      ]
+      ],
+      showIf: (options: Map<string, any>) => !options.get('useChildrenForItems')
     },
     {
       name: 'oneAtATime',
@@ -144,8 +158,21 @@ interface AccordionProps {
       name: 'gridRowWidth',
       helperText: 'Display as a grid',
       type: 'string',
-      showIf: options => options.get('grid'),
+      showIf: (options: Map<string, any>) => options.get('grid'),
       defaultValue: '25%'
+    },
+    {
+      name: 'useChildrenForItems',
+      type: 'boolean',
+      helperText:
+        'Use child elements for each slide, instead of the array. Useful for dynamically repeating items',
+      advanced: true,
+      defaultValue: false,
+      onChange: (options: Map<string, any>) => {
+        if (options.get('useChildrenForItems') === true) {
+          options.set('items', [])
+        }
+      }
     }
     // TODO: best way to do this? how multiple, comma? All with "*"? Have as a per item option?
     // {
@@ -177,10 +204,105 @@ export class BuilderAccordion extends React.Component<AccordionProps> {
     })
   }
 
+  getAccordionItem(
+    titleBlocks: BuilderElement[],
+    detailBlocks: BuilderElement[],
+    index: number,
+    openGridItemOrder: number | null,
+    onlyOneAtATime: boolean,
+    fromChildren = false
+  ) {
+    const open = this.state.open.indexOf(index) !== -1
+    const { grid } = this.props
+
+    return (
+      // This will not work as expected with react 15
+      // Did preact get the span replacmenet too?
+      <React.Fragment key={index}>
+        <div
+          className={`builder-accordion-title builder-accordion-title-${open ? 'open' : 'closed'}`}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'stretch',
+            ...(grid && {
+              width: this.props.gridRowWidth,
+              ...(typeof openGridItemOrder === 'number' && {
+                order: index < openGridItemOrder ? index : index + 1
+              })
+            })
+          }}
+          data-index={index}
+          onClick={() => {
+            if (open) {
+              this.setState({
+                ...this.state,
+                open: onlyOneAtATime ? [] : this.state.open.filter(item => item !== index)
+              })
+            } else {
+              this.setState({
+                ...this.state,
+                open: onlyOneAtATime ? [index] : this.state.open.concat(index)
+              })
+            }
+          }}
+        >
+          {fromChildren ? (
+            titleBlocks.map((block, index) => (
+              <BuilderBlockComponent
+                key={index}
+                block={{
+                  ...block,
+                  repeat: null
+                }}
+                index={index}
+                child={true} /* TODO: fieldname? */
+              />
+            ))
+          ) : (
+            <BuilderBlocks blocks={titleBlocks} dataPath={`items.${index}.title`} />
+          )}
+        </div>
+        {open && (
+          <div
+            className={`builder-accordion-detail builder-accordion-detail-${
+              open ? 'open' : 'closed'
+            }`}
+            style={{
+              order: typeof openGridItemOrder === 'number' ? openGridItemOrder : undefined,
+              maxHeight: this.props.animate ? (open ? '100vh' : 0) : undefined,
+              transition: this.props.animate ? 'max-height 0.5s' : undefined,
+              ...(grid && {
+                width: '100%'
+              })
+            }}
+          >
+            {open &&
+              (fromChildren ? (
+                detailBlocks.map((block, index) => (
+                  <BuilderBlockComponent
+                    key={index}
+                    block={{
+                      ...block,
+                      repeat: null
+                    }}
+                    index={index}
+                    child={true} /* TODO: fieldname? */
+                  />
+                ))
+              ) : (
+                <BuilderBlocks blocks={detailBlocks} dataPath={`items.${index}.detail`} />
+              ))}
+          </div>
+        )}
+      </React.Fragment>
+    )
+  }
+
   render() {
     const { grid, oneAtATime } = this.props
 
-    const onlyOneAtATime = grid || oneAtATime
+    const onlyOneAtATime = Boolean(grid || oneAtATime)
 
     const getOpenGridItemPosition = grid && this.state.open.length
     let openGridItemOrder: number | null = null
@@ -221,81 +343,83 @@ export class BuilderAccordion extends React.Component<AccordionProps> {
     }
 
     return (
-      <div
-        ref={ref => (this.divRef = ref)}
-        className="builder-accordion"
-        style={{
-          display: 'flex',
-          alignItems: 'stretch',
-          flexDirection: 'column',
-          ...(grid && {
-            flexDirection: 'row',
-            alignItems: 'flex-start',
-            flexWrap: 'wrap'
-          })
-        }}
-      >
-        {this.props.items && this.props.items.map((item, index) => {
-          const open = this.state.open.indexOf(index) !== -1
-          return (
-            // This will not work as expected with react 15
-            // Did preact get the span replacmenet too?
-            <React.Fragment key={index}>
-              <div
-                className={`builder-accordion-title builder-accordion-title-${
-                  open ? 'open' : 'closed'
-                }`}
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'stretch',
-                  ...(grid && {
-                    width: this.props.gridRowWidth,
-                    ...(typeof openGridItemOrder === 'number' && {
-                      order: index < openGridItemOrder ? index : index + 1
-                    })
-                  })
-                }}
-                data-index={index}
-                onClick={() => {
-                  if (open) {
-                    this.setState({
-                      ...this.state,
-                      open: onlyOneAtATime ? [] : this.state.open.filter(item => item !== index)
-                    })
-                  } else {
-                    this.setState({
-                      ...this.state,
-                      open: onlyOneAtATime ? [index] : this.state.open.concat(index)
-                    })
+      <BuilderStoreContext.Consumer>
+        {state => (
+          <div
+            ref={ref => (this.divRef = ref)}
+            className="builder-accordion"
+            style={{
+              display: 'flex',
+              alignItems: 'stretch',
+              flexDirection: 'column',
+              ...(grid && {
+                flexDirection: 'row',
+                alignItems: 'flex-start',
+                flexWrap: 'wrap'
+              })
+            }}
+          >
+            {/* TODO: helper static method for builder blocks to do this stuff */}
+            {this.props.useChildrenForItems
+              ? this.props.builderBlock &&
+                this.props.builderBlock.children &&
+                this.props.builderBlock.children.map((block: BuilderElement, index: number) => {
+                  if (block.repeat && block.repeat.collection) {
+                    const collectionPath = block.repeat.collection
+                    const collectionName = last((collectionPath || '').trim().split('.'))
+                    const itemName =
+                      block.repeat.itemName || (collectionName ? collectionName + 'Item' : 'item')
+                    const array = get(state.state, collectionPath)
+                    if (isArray(array)) {
+                      return array.map((data, index) => {
+                        // TODO: Builder state produce the data
+                        const childState = {
+                          ...state.state,
+                          $index: index,
+                          $item: data,
+                          [itemName]: data
+                        }
+
+                        return (
+                          <BuilderStoreContext.Provider
+                            key={block.id}
+                            value={{ ...state, state: childState } as any}
+                          >
+                            {this.getAccordionItem(
+                              block.children ? [block.children[0]] : [],
+                              block.children ? [block.children[1]] : [],
+                              index,
+                              openGridItemOrder,
+                              onlyOneAtATime,
+                              true
+                            )}
+                          </BuilderStoreContext.Provider>
+                        )
+                      })
+                    }
                   }
-                }}
-              >
-                <BuilderBlocks blocks={item.title} dataPath={`items.${index}.title`} />
-              </div>
-              {open && (
-                <div
-                  className={`builder-accordion-detail builder-accordion-detail-${
-                    open ? 'open' : 'closed'
-                  }`}
-                  style={{
-                    order: typeof openGridItemOrder === 'number' ? openGridItemOrder : undefined,
-                    maxHeight: this.props.animate ? (open ? '100vh' : 0) : undefined,
-                    transition: this.props.animate ? 'max-height 0.5s' : undefined,
-                    ...(grid && {
-                      width: '100%'
-                    })
-                  }}
-                >
-                  {open && (
-                    <BuilderBlocks blocks={item.detail} dataPath={`items.${index}.detail`} />
-                  )}
-                </div>
-              )}
-            </React.Fragment>
-          )
-        })}
-      </div>
+                  return this.getAccordionItem(
+                    block.children ? [block.children[0]] : [],
+                    block.children ? [block.children[1]] : [],
+                    index,
+                    openGridItemOrder,
+                    onlyOneAtATime,
+                    true
+                  )
+                })
+              : this.props.items &&
+                this.props.items.map((item, index) => {
+                  return this.getAccordionItem(
+                    item.title,
+                    item.detail,
+                    index,
+                    openGridItemOrder,
+                    onlyOneAtATime
+                  )
+                })}
+          </div>
+        )}
+      </BuilderStoreContext.Consumer>
     )
   }
 }
