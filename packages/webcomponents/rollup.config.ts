a6ed14823689ad22fd50ee02e04044ea7a403d5f
Steve Sewell <sewell.steve@gmail.com>
2019-04-08 12:24:23 -0700

    change react build

--- packages/webcomponents/rollup.config.ts
+++ packages/webcomponents/rollup.config.ts
@@ -27,47 +27,6 @@ const options = {
     }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs({
-      namedExports: {
-        'node_modules/d3-ease/dist/d3-ease.js': [
-          'easeLinear',
-          'easeQuad',
-          'easeQuadIn',
-          'easeQuadOut',
-          'easeQuadInOut',
-          'easeCubic',
-          'easeCubicIn',
-          'easeCubicOut',
-          'easeCubicInOut',
-          'easePoly',
-          'easePolyIn',
-          'easePolyOut',
-          'easePolyInOut',
-          'easeSin',
-          'easeSinIn',
-          'easeSinOut',
-          'easeSinInOut',
-          'easeExp',
-          'easeExpIn',
-          'easeExpOut',
-          'easeExpInOut',
-          'easeCircle',
-          'easeCircleIn',
-          'easeCircleOut',
-          'easeCircleInOut',
-          'easeBounce',
-          'easeBounceIn',
-          'easeBounceOut',
-          'easeBounceInOut',
-          'easeBack',
-          'easeBackIn',
-          'easeBackOut',
-          'easeBackInOut',
-          'easeElastic',
-          'easeElasticIn',
-          'easeElasticOut',
-          'easeElasticInOut'
-        ]
-      }
     }),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
