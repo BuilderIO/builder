61cbdd27b67d135858c1f60c0761f3a79ddd4626
Steve Sewell <sewell.steve@gmail.com>
2019-04-29 14:12:39 -0700

    fix unpkg dist file creation

--- packages/webcomponents/tools/make-unpkg-dist.ts
+++ packages/webcomponents/tools/make-unpkg-dist.ts
@@ -8,15 +8,14 @@ async function main() {
   ])
   // TODO: bootstrap script goes here... hmmm...
   // TODO: export basic builder stuff from here so people can use js, or System.import name it hmm
-  const newFileStr = [
-    // Don't load System.js multiple times...
-    `if (typeof System === 'undefined') {${sjs}}`,
-    // TODO: get the version of this and load - how does the others do
-    /*systemMain*/ `
-    if (typeof window === 'undefined' || !window.builderWebcomponentsLoaded) {
-      if (typeof window !== 'undefined') {
-        window.builderWebcomponentsLoaded = true;
-      }
+  const newFileStr =
+    `if (typeof window !== 'undefined' && !window.builderWebcomponentsLoaded) {` +
+    [
+      // Don't load System.js multiple times...
+      sjs,
+      // TODO: get the version of this and load - how does the others do
+      /*systemMain*/ `
+      window.builderWebcomponentsLoaded = true;
       function getQueryParam(url, variable) {
         var query = url.split('?')[1] || '';
         var vars = query.split('&');
@@ -28,18 +27,21 @@ async function main() {
         }
         return null;
       }
-      var version = typeof location !== 'undefined' && location.href && getQueryParam(location.href, 'builder.wcVersion') ||  "${pkg.version}";
+      var version = typeof location !== 'undefined' && location.href && getQueryParam(location.href, 'builder.wcVersion') ||  "${
+        pkg.version
+      }";
       System.import('https://cdn.builder.io/js/webcomponents@' + version + '/dist/system/builder-webcomponents.js')
     }
     `.replace(/\s+/g, ' ')
-    // `setTimeout(function () { if (typeof location !== 'undefined' && location.hostname === 'shop.galmeetsglam.com') {
-    //   var elements = document.getElementsByTagName('builder-component');
-    //   for (var i = 0; i < elements.length; i++) {
-    //     var element = elements[i];
-    //     elements[i].innerHTML = '<div style="text-align:center"><img src="https://static.galmeetsglamcollection.com/static/version1548098907/frontend/MaggyLondon/galmeetsglam/en_US/MaggyLondon_GalMeetsGlam/images/loader.svg" alt="Loading..." style="margin: 30vh auto;"></div>'
-    //   }
-    // } })`
-  ].join(';') // May need to import to initialize: + ';System.import("...")'
+      // `setTimeout(function () { if (typeof location !== 'undefined' && location.hostname === 'shop.galmeetsglam.com') {
+      //   var elements = document.getElementsByTagName('builder-component');
+      //   for (var i = 0; i < elements.length; i++) {
+      //     var element = elements[i];
+      //     elements[i].innerHTML = '<div style="text-align:center"><img src="https://static.galmeetsglamcollection.com/static/version1548098907/frontend/MaggyLondon/galmeetsglam/en_US/MaggyLondon_GalMeetsGlam/images/loader.svg" alt="Loading..." style="margin: 30vh auto;"></div>'
+      //   }
+      // } })`
+    ].join(';') +
+    '}' // May need to import to initialize: + ';System.import("...")'
   await outputFileAsync('./dist/system/builder-webcomponents-async.js', newFileStr)
 }
 
