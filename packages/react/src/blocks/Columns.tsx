fc27b578425c4105e18caf48f59e425bd91d76be
Steve Sewell <sewell.steve@gmail.com>
2019-05-21 10:06:02 -0700

    nth child fix

--- packages/react/src/blocks/Columns.tsx
+++ packages/react/src/blocks/Columns.tsx
@@ -198,8 +198,7 @@ export class Columns extends React.Component<any> {
               flex-grow: 1;
             }${columns.map(
               (col, index) => `
-            .${this.props.builderBlock.id} > .builder-columns > .builder-column:nth-child(${index +
-                1}) {
+            .${this.props.builderBlock.id} > .builder-columns > .builder-column:nth-child(${index}) {
               width: ${this.getColumnWidth(index)};
               margin-left: ${index === 0 ? 0 : gutterSize}px;
             }`
