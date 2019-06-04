803f7f8e320f8ceff4d168f58428c31608badfb7
Steve Sewell <sewell.steve@gmail.com>
2019-06-03 20:22:00 -0700

    inline text editing

--- packages/react/src/blocks/Text.tsx
+++ packages/react/src/blocks/Text.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BuilderBlock } from '../decorators/builder-block.decorator'
+import { Builder, builder, BuilderElement } from '@builder.io/sdk'
 const iconUrl =
   'https://firebasestorage.googleapis.com/v0/b/builder-3b0a2.appspot.com/o/images%2Fbaseline-text_fields-24px%20(1).svg?alt=media&token=12177b73-0ee3-42ca-98c6-0dd003de1929'
 
@@ -21,12 +22,25 @@ const iconUrl =
     textAlign: 'center'
   }
 })
-export class Text extends React.Component<{ text: string }> {
+export class Text extends React.Component<{ text: string; builderBlock?: BuilderElement }> {
   render() {
     return (
       <React.Fragment>
         <style>{`.builder-text p:first-child, .builder-paragraph:first-child { margin: 0 } .builder-text > p, .builder-paragraph { color: inherit; line-height: inherit; letter-spacing: inherit; font-weight: inherit; font-size: inherit; text-align: inherit; font-family: inherit; }`}</style>
+        {/* TODO: <BuilderText component that wraps this for other components with text */}
         <span
+          contentEditable={Builder.isEditing}
+          onChange={e => {
+            if (Builder.isEditing) {
+              window.parent.postMessage({
+                type: 'builder.textEdited',
+                data: {
+                  id: this.props.builderBlock && this.props.builderBlock.id,
+                  value: e.currentTarget.innerHTML
+                }
+              }, '*')
+            }
+          }}
           className="builder-text"
           dangerouslySetInnerHTML={{ __html: this.props.text || (this.props as any).content || '' }}
         />
