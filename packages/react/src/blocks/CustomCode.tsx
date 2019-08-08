d7022b45b689476d181c664dadc8c0dc7b46a24c
Steve Sewell <sewell.steve@gmail.com>
2019-08-07 18:15:39 -0700

    update script handling

--- packages/react/src/blocks/CustomCode.tsx
+++ packages/react/src/blocks/CustomCode.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { BuilderBlock } from '../decorators/builder-block.decorator'
 
+interface Props {
+  code: string
+}
+
 @BuilderBlock({
   name: 'Custom Code',
   inputs: [
@@ -12,19 +16,46 @@ import { BuilderBlock } from '../decorators/builder-block.decorator'
     }
   ]
 })
-export class CustomCode extends React.Component<{ code: string }> {
+export class CustomCode extends React.Component<Props> {
   elementRef: HTMLElement | null = null
 
+  scriptsInserted = new Set()
+  scriptsRun = new Set()
+
+  componentDidUpdate(prevProps: Props) {
+    if (this.props.code !== prevProps.code) {
+      this.findAndRunScripts()
+    }
+  }
+
   componentDidMount() {
+    this.findAndRunScripts()
+  }
+
+  findAndRunScripts() {
     if (this.elementRef && typeof window !== 'undefined') {
       const scripts = this.elementRef.getElementsByTagName('script')
       for (let i = 0; i < scripts.length; i++) {
         const script = scripts[i]
-        // Innertext?
-        try {
-          new Function(script.innerText)()
-        } catch (error) {
-          console.warn('Builder custom code component error:', error)
+        if (script.src) {
+          if (this.scriptsInserted.has(script.src)) {
+            continue
+          }
+          this.scriptsInserted.add(script.src)
+          const newScript = document.createElement('script')
+          newScript.async = true
+          newScript.src = script.src
+          document.head.appendChild(newScript)
+        } else {
+          if (this.scriptsRun.has(script.innerText)) {
+            continue
+          }
+          try {
+            this.scriptsRun.add(script.innerText)
+            new Function(script.innerText)()
+          } catch (error) {
+            console.warn('Builder custom code component error:', error)
+          }
         }
       }
     }
--- packages/react/src/blocks/Embed.tsx
+++ packages/react/src/blocks/Embed.tsx
@@ -47,9 +47,57 @@ import { BuilderBlock } from '../decorators/builder-block.decorator'
   ]
 })
 export class Embed extends React.Component<any> {
+  elementRef: HTMLElement | null = null
+
+  scriptsInserted = new Set()
+  scriptsRun = new Set()
+
+  componentDidUpdate(prevProps: any) {
+    if (this.props.content !== prevProps.content) {
+      this.findAndRunScripts()
+    }
+  }
+
+  componentDidMount() {
+    this.findAndRunScripts()
+  }
+
+  findAndRunScripts() {
+    if (this.elementRef && typeof window !== 'undefined') {
+      const scripts = this.elementRef.getElementsByTagName('script')
+      for (let i = 0; i < scripts.length; i++) {
+        const script = scripts[i]
+        if (script.src) {
+          if (this.scriptsInserted.has(script.src)) {
+            continue
+          }
+          this.scriptsInserted.add(script.src)
+          const newScript = document.createElement('script')
+          newScript.async = true
+          newScript.src = script.src
+          document.head.appendChild(newScript)
+        } else {
+          if (this.scriptsRun.has(script.innerText)) {
+            continue
+          }
+          try {
+            this.scriptsRun.add(script.innerText)
+            new Function(script.innerText)()
+          } catch (error) {
+            console.warn('Builder custom code component error:', error)
+          }
+        }
+      }
+    }
+  }
+
   render() {
     return (
-      <div className="builder-embed" dangerouslySetInnerHTML={{ __html: this.props.content }} />
+      <div
+        ref={ref => (this.elementRef = ref)}
+        className="builder-embed"
+        dangerouslySetInnerHTML={{ __html: this.props.content }}
+      />
     )
   }
 }
