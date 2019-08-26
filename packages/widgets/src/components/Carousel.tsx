e67e0ead339ae23cac70e1b50c2c1eb0c21d7775
Steve Sewell <sewell.steve@gmail.com>
2019-08-26 15:10:53 -0700

    comment

--- packages/widgets/src/components/Carousel.tsx
+++ packages/widgets/src/components/Carousel.tsx
@@ -9,7 +9,6 @@ import {
   Builder
 } from '@builder.io/react'
 import React from 'react'
-import get from 'lodash-es/get'
 import isArray from 'lodash-es/isArray'
 import last from 'lodash-es/last'
 
