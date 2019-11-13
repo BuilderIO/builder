66c2e398255d3f2af00f1ac09acb77acdc8b95f2
Steve Sewell <sewell.steve@gmail.com>
2019-11-13 14:12:00 -0800

    plugin development docs

--- plugins/cloudinary/rollup.config.ts
+++ plugins/cloudinary/rollup.config.ts
@@ -4,6 +4,9 @@ import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
+import serve from 'rollup-plugin-serve'
+
+const SERVE = process.env.SERVE === 'true'
 
 const pkg = require('./package.json')
 
@@ -13,14 +16,14 @@ export default {
   input: `src/${libraryName}.tsx`,
   // Important! We need to have shared references to 'react' and '@builder.io/sdk'
   // for builder plugins to run properly
-  external: ['react', '@builder.io/sdk', '@material-ui/core', '@emotion/core',  '@emotion/styled'],
+  external: ['react', '@builder.io/sdk', '@material-ui/core', '@emotion/core', '@emotion/styled'],
   output: [
     { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true },
-    { file: pkg.unpkg, format: 'system', sourcemap: true },
+    { file: pkg.unpkg, format: 'system', sourcemap: true }
   ],
   watch: {
-    include: 'src/**',
+    include: 'src/**'
   },
   plugins: [
     // Allow json resolution
@@ -36,5 +39,16 @@ export default {
 
     // Resolve source maps to the original source
     sourceMaps(),
-  ],
+    ...(SERVE
+      ? [
+          serve({
+            contentBase: 'dist',
+            port: 1268,
+            headers: {
+              'Access-Control-Allow-Origin': '*',
+            }
+          })
+        ]
+      : [])
+  ]
 }
