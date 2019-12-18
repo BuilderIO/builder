1be5197d51e515ce9986adec37fbff28d5fdd8e1
Steve Sewell <steve@Steves-MacBook-Pro-2.local>
2019-12-17 19:53:14 -0800

    updates

--- packages/shopify/test/import-playground.test.ts
+++ packages/shopify/test/import-playground.test.ts
@@ -9,12 +9,9 @@ import { debugLog, debugFile } from './modules/helpers';
 import * as stringify from 'json-stringify-safe';
 
 let str = `
-<div>
-<div>Hello</div>
-{% if foo == 'bar' %}
-<div>Yo</div>
-{% endif %}
-</div>
+  {% if foo == 'bar' %}
+    <div>hello</div>
+  {% endif %}
 `;
 
 test('Playground', async () => {
@@ -27,6 +24,7 @@ test('Playground', async () => {
   const ignoreKeys = new Set(['liquid', 'input']);
   await debugFile(
     'playground.json',
+  
     stringify(everything, (key, value) => (ignoreKeys.has(key) ? undefined : value), 2)
   );
   expect(blocks).toBeTruthy();
