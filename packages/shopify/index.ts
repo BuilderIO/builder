8392ce2a0ed373cb404e9d1da6456b1cc1bf01d2
Steve Sewell <steve@Steves-MacBook-Pro-2.local>
2019-12-18 14:12:37 -0800

    cleanup

--- packages/shopify/index.ts
+++ packages/shopify/index.ts
@@ -1,4 +1,3 @@
 export { contentToLiquid } from './src/functions/content-to-liquid';
-export { liquidToBuilder, LiquidToBuilderOptions } from './src/functions/liquid-to-builder';
 export { blockToLiquid } from './src/functions/block-to-liquid';
 export { Options } from './src/interfaces/options'
