dc30fcd8ca945e1dbe0ad3c81da5d47796c3a7e2
Steve Sewell <steve@Steves-MacBook-Pro-2.local>
2020-01-21 17:07:17 -0800

    ws

--- packages/shopify/react/react.ts
+++ packages/shopify/react/react.ts
@@ -2,3 +2,4 @@ export { AssignBlock } from './components/Assign';
 export { IfElseBlock } from './components/IfElse';
 export { UnlessBlock } from './components/Unless';
 export { ProductBox } from './components/ProductBox';
+// 
