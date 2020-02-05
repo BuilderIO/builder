ce305f741c23a48c39687ee8c14b2063bdcae89b
Caleb Amsden <caleb.amsden@gmail.com>
2020-02-05 09:38:28 -0800

    add custom event polyfill for older browsers

---
+++ packages/webcomponents/src/polyfills/custom-event-polyfill.js
@@ -0,0 +1,12 @@
+;(function() {
+  if (typeof window.CustomEvent === 'function') return false
+
+  function CustomEvent(event, params) {
+    params = params || { bubbles: false, cancelable: false, detail: null }
+    var evt = document.createEvent('CustomEvent')
+    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail)
+    return evt
+  }
+
+  window.CustomEvent = CustomEvent
+})()
