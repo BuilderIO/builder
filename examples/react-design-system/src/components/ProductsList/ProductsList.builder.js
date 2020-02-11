d1dc3b52943a6460dae2a35c7090b65ea9db40c8
Steve Sewell <steve@Steves-MacBook-Pro-2.local>
2020-02-11 15:54:46 -0800

    product styling and higher level props

--- examples/react-design-system/src/components/ProductsList/ProductsList.builder.js
+++ examples/react-design-system/src/components/ProductsList/ProductsList.builder.js
@@ -1,43 +1,39 @@
-import { Builder } from '@builder.io/react'
-import { ProductsList } from './ProductsList'
+import { Builder } from '@builder.io/react';
+import { ProductsList } from './ProductsList';
 
 Builder.registerComponent(ProductsList, {
-    name: 'Products List',
-    inputs: [
-      {
-        name: 'url',
-        type: 'string',
-        defaultValue: 'https://api.shopstyle.com/api/v2/products',
-      },
-      {
-        name: 'category',
-        type: 'string',
-        defaultValue: 'mens',
-        enum: ['mens', 'womens', {
+  name: 'Products List',
+  inputs: [
+    {
+      name: 'url',
+      type: 'string',
+      advanced: true,
+      defaultValue: 'https://api.shopstyle.com/api/v2/products',
+    },
+    {
+      name: 'category',
+      type: 'string',
+      defaultValue: 'mens',
+      enum: [
+        'mens',
+        'womens',
+        'kids',
+        {
           label: 'home',
-          value: 'living'
-        }]
-      },
-      {
-          name: 'limit',
-          type: 'number',
-          defaultValue: 20
-      },
-      {
-        name: 'spacing',
-        type: 'number',
-        defaultValue: 3
-      },
-      {
-        name: 'columns',
-        type: 'number',
-        defaultValue: 3
-      },
-      {
-        name: 'image',
-        type: 'string',
-        defaultValue: 'Large',
-        enum: ['Small', 'Medium', 'Large', 'XLarge']
-      }
-    ]
-})
+          value: 'living',
+        },
+      ],
+    },
+    {
+      name: 'amount',
+      type: 'number',
+      defaultValue: 20,
+    },
+    {
+      name: 'size',
+      type: 'string',
+      defaultValue: 'Medium',
+      enum: ['Small', 'Medium', 'Large'],
+    },
+  ],
+});
--- examples/react-design-system/src/components/ProductsList/ProductsList.jsx
+++ examples/react-design-system/src/components/ProductsList/ProductsList.jsx
@@ -1,53 +1,54 @@
-import React, { useState, useEffect} from 'react'
-import queryString from 'query-string'
-import { Product } from '../Product/Product'
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import { Product } from '../Product/Product';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-  }
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
 }));
 
 const defaultParams = {
-    abbreviatedCategoryHistogram: true,
-    limit: 20,
-    cat: 'mens',
-    view: 'web',
-    useElasticsearch: true,
-    sorts: 'PriceHiLo',
-    pid: 'shopstyle'
-}
+  abbreviatedCategoryHistogram: true,
+  limit: 20,
+  cat: 'mens',
+  view: 'web',
+  useElasticsearch: true,
+  sorts: 'PriceHiLo',
+  pid: 'shopstyle',
+};
 
-export const ProductsList = (props) => {
-    const { url, limit, category, spacing, columns, image } = props
-    const [data, setData] = useState({ products: [] });
-    const classes = useStyles();
-    useEffect(() => {
-        async function fetchProducts() {
-            const qs = queryString.stringify({
-                ...defaultParams,
-                limit,
-                cat: category,
-            })
-            const result = await fetch(`${url}?${qs}`).then((res) => res.json())
-            setData(result);      
-        }
-        fetchProducts()
-    }, [limit, category, url]);
-  
-    return (
-        <div className={classes.root}>
-            <Grid container spacing={spacing}>
-                {
-                    data.products.map(product =>(
-                      <Grid key={product.id} item md={(12/columns)} sm={(12/(columns / 2))}>
-                          <Product sizeName={image} {...product} />
-                      </Grid>))
-                }
-            </Grid>
-        </div>
-    )
-}
+export const ProductsList = props => {
+  const { url, amount, category, size } = props;
+  const [data, setData] = useState({ products: [] });
+  const classes = useStyles();
+  useEffect(() => {
+    async function fetchProducts() {
+      const qs = queryString.stringify({
+        ...defaultParams,
+        limit: amount,
+        cat: category,
+      });
+      const result = await fetch(`${url}?${qs}`).then(res => res.json());
+      setData(result);
+    }
+    fetchProducts();
+  }, [amount, category, url]);
 
+  return (
+    <div className={classes.root}>
+      <div className={classes.container}>
+        {data.products.map(product => (
+          <Product key={product.id} sizeName={size} {...product} />
+        ))}
+      </div>
+    </div>
+  );
+};
