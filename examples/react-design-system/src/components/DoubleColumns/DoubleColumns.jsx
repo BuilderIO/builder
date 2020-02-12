a8b2009fdc83c48e7872eb97ed2902133b7e7770
Aziz Abbas <aziz@builder.io>
2020-02-12 13:22:25 -0800

    fixes

--- examples/react-design-system/src/components/DoubleColumns/DoubleColumns.jsx
+++ examples/react-design-system/src/components/DoubleColumns/DoubleColumns.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
@@ -16,14 +16,14 @@ const useStyles = makeStyles({
     padding: '30px',
   },
   col: {
-    minWidth: 400,
+    minWidth: 500,
   },
   media: {
-    height: 300,
+    height: 600,
   },
   sectionTitle: {
     textAlign: 'center',
-    padding: '20px',
+    padding: '5px',
   },
 });
 
@@ -35,7 +35,7 @@ export const DoubleColumns = props => {
       <Typography className={classes.sectionTitle} variant="h3" component="p">
         {sectionTitle}
       </Typography>
-      <Grid container justify="center" spacing={2}>
+      <Grid container justify="center" spacing={3}>
         {[
           { img: image1, text: text1 },
           { img: image2, text: text2 },
@@ -44,9 +44,12 @@ export const DoubleColumns = props => {
             <Card className={classes.card}>
               <CardMedia className={classes.media} image={col.img} title={col.text} />
               <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
+                <Typography variant="h5" color="textSecondary" component="p">
                   {col.text}
                 </Typography>
+                <Button size="small" color="textSecondary">
+                  Learn More >
+                </Button>
               </CardContent>
             </Card>
           </Grid>
--- examples/react-design-system/src/components/TripleColumns/TripleColumns.jsx
+++ examples/react-design-system/src/components/TripleColumns/TripleColumns.jsx
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
@@ -36,14 +35,12 @@ export const TripleColumns = props => {
         ].map((col, index) => (
           <Grid key={index} className={classes.col} item md={4}>
             <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia className={classes.media} image={col.img} title={col.text} />
-                <CardContent>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    {col.text}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
+              <CardMedia className={classes.media} image={col.img} title={col.text} />
+              <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {col.text}
+                </Typography>
+              </CardContent>
             </Card>
           </Grid>
         ))}
