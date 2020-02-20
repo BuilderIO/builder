b8cae984b21eb7a2718658da6ff747f49b66e56e
Steve Sewell <steve@Steves-MacBook-Pro-2.local>
2020-02-19 19:10:29 -0800

    gatsby comments

--- examples/gatsby/gatsby-node.js
+++ examples/gatsby/gatsby-node.js
@@ -5,6 +5,10 @@ const { config } = require('./builder-config');
 
 // Create dynamic pages created in Builder.io for
 // every "page" model by it's URL
+// 
+// See https://builder.io/c/docs/graphql-api for more info on our
+// GraphQL API and our explorer
+// 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
   const models = Object.keys(config.templates);
