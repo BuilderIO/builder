a62e4a25d683b1a730753d4cee4ee95d43f8eeb4
caleb amsden <caleb.amsden@gmail.com>
2020-03-05 12:14:02 -0800

    add a comment to Link (#92)

--- packages/react/src/components/Link.tsx
+++ packages/react/src/components/Link.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { BuilderStoreContext } from '../store/builder-store'
-
+/**
+ * Link component should be used instead of an anchor tag in our components,
+ * this is to allow our users to override anchor tags in
+ * case they're using a routing Lib that requires using their
+ * custom Link component (e.g Next, Gatsby, React Router)
+ * <BuilderComponent renderLink=(props) => <myCustomLink {...props} /> />
+ */
 export const Link: React.SFC<React.AnchorHTMLAttributes<
   HTMLAnchorElement
 >> = props => (
