2dd09baa233f07d8f94b7e252585c63c88e31433
Aziz Abbas <aziz@builder.io>
2020-03-17 16:49:41 -0700

    remove global from window to fix realm context error

--- plugins/dynamic-dropdown/src/mapperEvaluator.ts
+++ plugins/dynamic-dropdown/src/mapperEvaluator.ts
@@ -2,6 +2,13 @@
 import SES from 'ses'
 
 const safeEvaluate = (code: String, context: any = {}) => {
+  if (typeof window !== 'undefined') {
+    // workaround realm-shems thinking it's a node context
+    // Remove once this line updated
+    // testing isNode = (this===global)
+    // https://github.com/Agoric/realms-shim/blob/master/src/unsafeRec.js#L28
+    (window as any).global = null
+  }
   const realm = SES.makeSESRootRealm({
     consoleMode: 'allow'
   })
