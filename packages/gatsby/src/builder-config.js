6a9a40c7b08cff6c778220654acd8bb07cda1267
Aziz Abbas <aziz@builder.io>
2020-03-23 09:51:50 -0700

    override custom 404 in development mode to allow dynamic page creating and editing

--- packages/gatsby/src/builder-config.js
+++ packages/gatsby/src/builder-config.js
@@ -1,27 +1,28 @@
-const invariant = require(`invariant`)
+const invariant = require(`invariant`);
 
 module.exports.defaultOptions = {
   // Arbitrary name for the remote schema Query type
   fieldName: `allBuilderModels`,
   typeName: `builder`,
   limit: 30,
-  baseURL: `https://cdn.builder.io/api/v1/graphql`
-}
+  baseURL: `https://cdn.builder.io/api/v1/graphql`,
+  overrideDev404: true,
+};
 
 module.exports.getGQLOptions = options => {
   const config = {
     ...module.exports.defaultOptions,
     ...options,
-  }
+  };
   invariant(
     config.publicAPIKey && config.publicAPIKey.length > 0,
     `@builder.io/gatsby requires a public API Key`
-  )
+  );
   return {
     typeName: config.typeName,
     // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
     fieldName: config.fieldName,
     // Url to query from 30
     url: `${config.baseURL}/${config.publicAPIKey}?cachebust=true`,
-  }
-}
+  };
+};
--- packages/gatsby/src/gatsby-node.js
+++ packages/gatsby/src/gatsby-node.js
@@ -1,64 +1,54 @@
-const uuidv4 = require(`uuid/v4`)
-const fs = require(`fs`)
-const { buildSchema, printSchema } = require(`gatsby/graphql`)
-const {
-  transformSchema,
-  introspectSchema,
-  RenameTypes,
-} = require(`graphql-tools-fork`)
-const { createHttpLink } = require(`apollo-link-http`)
-const fetch = require(`node-fetch`)
-const invariant = require(`invariant`)
+const uuidv4 = require(`uuid/v4`);
+const fs = require(`fs`);
+const { buildSchema, printSchema } = require(`gatsby/graphql`);
+const { transformSchema, introspectSchema, RenameTypes } = require(`graphql-tools-fork`);
+const { createHttpLink } = require(`apollo-link-http`);
+const fetch = require(`node-fetch`);
+const invariant = require(`invariant`);
 
-const {
-  NamespaceUnderFieldTransform,
-  StripNonQueryTransform,
-} = require(`./transforms`)
-const { getGQLOptions, defaultOptions } = require(`./builder-config`)
+const { NamespaceUnderFieldTransform, StripNonQueryTransform } = require(`./transforms`);
+const { getGQLOptions, defaultOptions } = require(`./builder-config`);
 
-exports.sourceNodes = async (
-  { actions, createNodeId, cache, createContentDigest },
-  options
-) => {
-  const { addThirdPartySchema, createNode } = actions
-  const config = getGQLOptions(options)
-  const { url, typeName, fieldName } = config
+exports.sourceNodes = async ({ actions, createNodeId, cache, createContentDigest }, options) => {
+  const { addThirdPartySchema, createNode } = actions;
+  const config = getGQLOptions(options);
+  const { url, typeName, fieldName } = config;
 
   const link = createHttpLink({
     uri: url,
     fetch,
-  })
+  });
 
-  const cacheKey = `@builder.io/gatsby-schema-${typeName}-${fieldName}`
-  let sdl = await cache.get(cacheKey)
-  let introspectionSchema
+  const cacheKey = `@builder.io/gatsby-schema-${typeName}-${fieldName}`;
+  let sdl = await cache.get(cacheKey);
+  let introspectionSchema;
 
   if (!sdl) {
-    introspectionSchema = await introspectSchema(link)
-    sdl = printSchema(introspectionSchema)
+    introspectionSchema = await introspectSchema(link);
+    sdl = printSchema(introspectionSchema);
   } else {
-    introspectionSchema = buildSchema(sdl)
+    introspectionSchema = buildSchema(sdl);
   }
 
-  await cache.set(cacheKey, sdl)
+  await cache.set(cacheKey, sdl);
 
-  const nodeId = createNodeId(`@builder.io/gatsby-${typeName}`)
+  const nodeId = createNodeId(`@builder.io/gatsby-${typeName}`);
   const node = createSchemaNode({
     id: nodeId,
     typeName,
     fieldName,
     createContentDigest,
-  })
-  createNode(node)
+  });
+  createNode(node);
 
   const resolver = (_, __, context) => {
-    const { path, nodeModel } = context
+    const { path, nodeModel } = context;
     nodeModel.createPageDependency({
       path,
       nodeId,
-    })
-    return {}
-  }
+    });
+    return {};
+  };
 
   const schema = transformSchema(
     {
@@ -74,13 +64,13 @@ exports.sourceNodes = async (
         resolver,
       }),
     ]
-  )
+  );
 
-  addThirdPartySchema({ schema })
-}
+  addThirdPartySchema({ schema });
+};
 
 function createSchemaNode({ id, typeName, fieldName, createContentDigest }) {
-  const contentDigest = createContentDigest(uuidv4())
+  const contentDigest = createContentDigest(uuidv4());
   return {
     id,
     typeName,
@@ -92,19 +82,19 @@ function createSchemaNode({ id, typeName, fieldName, createContentDigest }) {
       contentDigest,
       ignoreType: true,
     },
-  }
+  };
 }
 
 exports.createPages = async ({ graphql, actions }, options) => {
   const config = {
     ...defaultOptions,
     ...options,
-  }
-  const { createPage } = actions
-  const models = Object.keys(config.templates)
-  const offsets = models.map(() => 0)
+  };
+  const { createPage } = actions;
+  const models = Object.keys(config.templates);
+  const offsets = models.map(() => 0);
   await createPagesAsync(config, createPage, graphql, models, offsets);
-}
+};
 
 const createPagesAsync = async (config, createPage, graphql, models, offsets) => {
   const result = await graphql(`
@@ -112,40 +102,59 @@ const createPagesAsync = async (config, createPage, graphql, models, offsets) =>
       ${config.fieldName} {
         ${models
           .map(
-            (model, index) => `${model}(limit: ${config.limit}, offset: ${offsets[index]}, options: { cachebust: true }) {
+            (
+              model,
+              index
+            ) => `${model}(limit: ${config.limit}, offset: ${offsets[index]}, options: { cachebust: true }) {
             content
           }`
           )
           .join(` `)}
       }
     }
-  `)
-  let hasMore = false
+  `);
+  let hasMore = false;
 
   models.forEach((modelName, index) => {
-    const component = config.templates[modelName]
+    const component = config.templates[modelName];
     invariant(
       fs.existsSync(component),
       `@builder.io/gatsby requires a valid template path for each model`
-    )
-    const entries = result.data[config.fieldName][modelName]
-    offsets[index] = offsets[index] + entries.length
+    );
+    lastEditingComponent = component;
+    const entries = result.data[config.fieldName][modelName];
+    offsets[index] = offsets[index] + entries.length;
     if (entries.length === config.limit) {
-      hasMore = true
+      hasMore = true;
     }
     entries.forEach(entry => {
-      if (
-        entry.content.data.url &&
-        entry.content.published === `published`
-      ) {
+      if (entry.content.data.url && entry.content.published === `published`) {
         createPage({
           path: entry.content.data.url,
           component,
-        })
+        });
       }
-    })
-  })
+    });
+  });
   if (hasMore) {
     await createPagesAsync(config, createPage, graphql, models, offsets);
   }
-}
+};
+
+exports.onCreatePage = ({ page, actions }, options) => {
+  const { deletePage, createPage } = actions;
+  const config = {
+    ...defaultOptions,
+    ...options,
+  };
+  const modelName = Object.keys(config.templates || {})[0];
+  // override gatsby's custom 404 dev to allow development on Builder.io
+  // with new pages without having to regenerate (restart gatsby develop)
+  if (modelName && config.overrideDev404 && page.path === '/dev-404-page/') {
+    deletePage(page);
+    createPage({
+      ...page,
+      component: config.templates[modelName],
+    });
+  }
+};
