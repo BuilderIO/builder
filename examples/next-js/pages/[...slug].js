6b7b80a762c45c86f7f09cccd54f3ed97fef85a3
Kevin Mårtensson <kevinmartensson@gmail.com>
2020-05-04 19:16:48 +0200

    Update Next.js example to use `getServerSideProps` (#158)
    * Update Next.js example to use `getServerSideProps`
    
    * Small tweaks
    
    * Code style tweak

--- examples/next-js/pages/[...slug].js
+++ examples/next-js/pages/[...slug].js
@@ -8,43 +8,46 @@ import Nav from '../components/nav';
 const BUILDER_API_KEY = 'YOUR_KEY';
 builder.init(BUILDER_API_KEY);
 
-class CatchallPage extends React.Component {
-  static async getInitialProps({ res, req, asPath }) {
-    // Get the upcoming route full location path and set that for Builder.io page targeting
-    const path = asPath.split('?')[0];
+const getServerSideProps = async ({req, res}) => {
+  // Get the upcoming route full location path and set that for Builder.io page targeting
+  const [path] = req.url.split('?');
 
-    // 'page' is the model name for your pages. If you made a new model with a different name,
-    // such as 'my-page', use `builder.get('my-page', ...)
-    const page = await builder
-      .get('page', { req, res, userAttributes: { urlPath: path } })
-      .promise();
+  // 'page' is the model name for your pages. If you made a new model with a different name,
+  // such as 'my-page', use `builder.get('my-page', ...)
+  const page = await builder
+    .get('page', { req, res, userAttributes: { urlPath: path } })
+    .promise();
 
-    if (res && !page) {
-      res.statusCode = 404;
+  if (!page) {
+    res.statusCode = 404;
+  }
+
+  return {
+    props: {
+      builderPage: page ?
+        { ...page, testVariationName: page.testVariationName || null } :
+        null
     }
-    return { builderPage: page };
-  }
+  };
+};
 
-  render() {
-    const page = this.props.builderPage;
-    return (
-      <>
-        <Nav />
-        <div>
-          {page ? (
-            <>
-              <Head>
-                <title>{page.data.title}</title>
-              </Head>
-              <BuilderComponent model="page" content={page} />
-            </>
-          ) : (
-            <div>Page not found!</div>
-          )}
-        </div>
-      </>
-    );
-  }
-}
+const CatchallPage = ({builderPage: page}) => (
+  <>
+    <Nav />
+    <div>
+      {page ? (
+        <>
+          <Head>
+            <title>{page.data.title}</title>
+          </Head>
+          <BuilderComponent model="page" content={page} />
+        </>
+      ) : (
+        <div>Page not found!</div>
+      )}
+    </div>
+  </>
+);
 
 export default CatchallPage;
+export {getServerSideProps};
