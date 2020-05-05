8037696638c325b73371047c5210ec403d55c345
Steve Sewell <steve@builder.io>
2020-05-05 09:01:12 -0700

    whitespace

--- packages/core/src/builder.class.ts
+++ packages/core/src/builder.class.ts
@@ -313,6 +313,7 @@ export interface Component {
     message: string;
     /** Simple way to say a parent must be a specific component name, e.g. 'Product box' */
     component?: string;
+    
     /**
      * More advanced - specify a MongoDB-style query (using sift.js github.com/crcn/sift.js)
      * of what at least one parent in the parents hierarchy should match, e.g.
