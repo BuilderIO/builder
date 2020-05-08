dd5bca86b9fc28a33609b3f21dcdb45335d11452
Aziz Abbas <aziz@builder.io>
2020-05-08 08:39:59 -0700

    update gatsby plugin and add the ability to provide custom development 404 component, add a sample 404.tsx (#166)

---
+++ examples/gatsby/src/pages/404.tsx
@@ -0,0 +1,34 @@
+
+import * as React from 'react';
+import { BuilderComponent, builder } from '@builder.io/react';
+import '@builder.io/widgets';
+/**
+ * Hero is an example of a custom component that you can use in the builder.io editor
+ * https://www.builder.io/c/docs/custom-react-components
+ */
+import '../components/Hero/Hero.builder';
+
+// TODO: enter your public API key
+builder.init('jdGaMusrVpYgdcAnAtgn');
+
+const  Dev404 : React.SFC = () => {
+  const [ notFound, setNotFound ] = React.useState(false);
+  return notFound ? (
+    <NotFound /> // Your 404 content
+  ) : (
+    <BuilderComponent
+      model="page"
+      contentLoaded={content => {
+        if (!content) {
+          setNotFound(true);
+        }
+      }}
+    >
+      <div className="loading">No matching page generated, checking Builder.io ...</div>
+    </BuilderComponent>
+  )
+}
+
+const NotFound = () => <h1>No page found for this URL, did you publish it?</h1>;
+
+export default Dev404;
