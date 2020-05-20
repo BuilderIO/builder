0350123756b9d3da969c874dc728678040e80e57
Steve Sewell <steve@builder.io>
2020-05-20 12:37:44 -0700

    update shopify tracking

--- packages/shopify/track/track.ts
+++ packages/shopify/track/track.ts
@@ -62,29 +62,8 @@ if (!_window[TRACKED_KEY]) {
 
         builder.track('conversion', {
           meta: checkout,
-          amount: parseFloat(checkout.payment_due),
+          amount: parseFloat(checkout.payment_due), // TODO: track or normalize currency
         });
-        for (const item of checkout.line_items) {
-          const id = item.product_id;
-          const cookieValue = builder.getCookie('builder.addToCart.' + id);
-          if (cookieValue) {
-            // Remove the cookie by setting a cooke to a date in the past
-            builder.setCookie('builder.addToCart.' + id, '', new Date(0));
-
-            const [contentId, variationId] = cookieValue.split(',');
-
-            // Send a conversion event to Builder.io upon purchasing a product that was added to cart by a Builder.io page.
-            // On the add to cart we set a cookie, and read it here to attribute conversions to specific content entries
-            // and test variations. We include a couple more custom attributes for order value (`amount`) and some
-            // metadata (the full shopify checkout object for this line item)
-            builder.track('conversion', {
-              contentId,
-              variationId,
-              meta: item,
-              amount: parseFloat(item.price), // x item.quantity?
-            });
-          }
-        }
       }
     }
   }
