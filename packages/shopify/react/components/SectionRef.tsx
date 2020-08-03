b54f8a598308b7fce4b76abfceb2685000922375
Caleb Amsden <caleb.amsden@gmail.com>
2020-08-03 12:28:57 -0700

    ensure we fetch shopify sections if missing contents

--- packages/shopify/react/components/SectionRef.tsx
+++ packages/shopify/react/components/SectionRef.tsx
@@ -12,7 +12,9 @@ const cache: { [key: string]: string } = {};
 const refs =
   Builder.isBrowser &&
   Array.from(document.querySelectorAll('[builder-shopify-section]')).reduce((memo, item) => {
-    memo[item.getAttribute('builder-shopify-section')!] = item;
+    if (item.innerHTML) {
+      memo[item.getAttribute('builder-shopify-section')!] = item;
+    }
     return memo;
   }, {} as { [key: string]: Element });
 
@@ -43,9 +45,11 @@ export const SectionRef = (props: SectionRefProps) => {
       // specifically with /shopify/v1/data etc
       fetch(
         `https://cdn.builder.io/api/v1/proxy-api?url=${encodeURIComponent(
-          `https://${
-            location.host + location.pathname.replace('/apps/builder/preview', '')
-          }?section_id=${sectionName}&${location.search.replace('?', '')}`
+          `https://${location.host +
+            location.pathname.replace(
+              '/apps/builder/preview',
+              ''
+            )}?section_id=${sectionName}&${location.search.replace('?', '')}`
         )}`
       )
         .then(res => res.text())
