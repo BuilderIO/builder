758c0d80f690f7e48cf3ee59852a5a744f8553e6
Aziz Abbas <aziz@builder.io>
2020-08-10 12:38:59 -0700

    remove subscription to user attributes (#236)
    * remove subscription to user attributes
    
    * trigger service

--- packages/angular/src/app/modules/builder/components/builder-component/builder-component.component.ts
+++ packages/angular/src/app/modules/builder/components/builder-component/builder-component.component.ts
@@ -149,7 +149,7 @@ export class BuilderComponentComponent implements OnDestroy, OnInit {
         builder.apiKey = this.builderService.apiKey;
         builder.canTrack = this.builderService.canTrack;
         builder.setUserAttributes(omit(this.builderService.getUserAttributes(), 'urlPath'));
-        builder.userAttributesChanged.subscribe((attrs) => builder.setUserAttributes(attrs));
+        this.builderService.userAttributesChanged.subscribe((attrs) => builder.setUserAttributes(attrs));
       });
     }
   }
