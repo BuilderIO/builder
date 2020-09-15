37cca5259a1815f590d89a85d88def537fd01a30
Armando Perez <gmandx@gmail.com>
2020-09-15 09:50:26 -0700

    Add Builder.io <=> Next.js example demonstrating SSR AMP content

---
+++ examples/next-js-amp-ssr/components/BuilderPageWrapper.js
@@ -0,0 +1,54 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import { renderToStaticMarkup } from 'react-dom/server';
+import { BuilderPage } from '@builder.io/react';
+
+function toAmp(htmlStr) {
+  return htmlStr
+    .replace(/\sbuilder-[\w-]+="[^"]+"/g, ' ')
+    .replace(/\sclassName="(?<values>.*?)"/g, 'class="$<values>"');
+}
+
+function extractHtmlCss(htmlStr) {
+  const cssSet = new Set();
+  const newHtml = htmlStr.replace(
+    /<style.*?>(?<css>[\s\S]*?)<\/style>/g,
+    (_match, css) => {
+      cssSet.add(css.replace(/&gt;/g, '>'));
+      return '';
+    }
+  );
+
+  return {
+    css: [...cssSet.values()].join(' '),
+    html: newHtml,
+  };
+}
+
+function useSsr() {
+  const [isSsr, setSsr] = useState(true);
+
+  useEffect(() => {
+    setSsr(false);
+  }, []);
+
+  return isSsr;
+}
+
+export default function BuilderPageWrapper(props) {
+  const isSsr = useSsr();
+
+  if (!isSsr || !props.ampMode) {
+    return <BuilderPage {...props} />;
+  }
+
+  const { css, html } = extractHtmlCss(
+    renderToStaticMarkup(<BuilderPage {...props} />)
+  );
+
+  return (
+    <Fragment>
+      <style jsx global>{css}</style>
+      <div dangerouslySetInnerHTML={{ __html: toAmp(html) }} />
+    </Fragment>
+  );
+}
---
+++ examples/next-js-amp-ssr/pages/index.js
@@ -0,0 +1,7 @@
+import Page, { getServerSideProps as getPageServerSideProps } from './[...slug]';
+
+export const getServerSideProps = getPageServerSideProps;
+
+export const config = { amp: 'hybrid' };
+
+export default Page;
