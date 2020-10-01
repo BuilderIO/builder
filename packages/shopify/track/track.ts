56347b3c8b5472b78a8e4da2d7dc3492ae5b44df
Steve Sewell <steve@builder.io>
2020-10-01 15:09:31 -0700

    updates

--- packages/shopify/track/track.ts
+++ packages/shopify/track/track.ts
@@ -1,10 +1,12 @@
 import { builder } from '@builder.io/sdk';
 import { Checkout } from './interfaces/checkout';
 
+const currentScript = document.currentScript as HTMLScriptElement | null;
+
 // Ensure our code runs *after* the code that sets window.Shopify = { ... }
 setTimeout(() => {
   const TRACKED_KEY = 'builder.tracked';
-  
+
   function getQueryParam(url: string, variable: string): string | null {
     const query = url.split('?')[1] || '';
     const vars = query.split('&');
@@ -16,52 +18,51 @@ setTimeout(() => {
     }
     return null;
   }
-  
+
   const _window = window as any;
-  
+
   function datePlusMinutes(minutes = 30) {
     return new Date(Date.now() + minutes * 60000);
   }
-  
+
   if (!_window[TRACKED_KEY]) {
     const apiKey =
-      getQueryParam((document.currentScript as HTMLScriptElement)?.src || '', 'apiKey') ||
-      builder.apiKey;
-  
+      getQueryParam((currentScript as HTMLScriptElement)?.src || '', 'apiKey') || builder.apiKey;
+
     if (apiKey && !builder.apiKey) {
       builder.apiKey = apiKey;
     }
-  
+
     // Allow passing a session ID to the tracking script to support cross domain converison tracking
     // AKA: <script src="https://cdn.builder.io/js/shopify/track?apiKey=YOUR_KEY&sessionId={{checkout.attributes._builderSessionId}}">
     const sessionIdParam = getQueryParam(
-      (document.currentScript as HTMLScriptElement)?.src || '',
+      (currentScript as HTMLScriptElement)?.src || '',
       'sessionId'
     );
     if (sessionIdParam) {
       builder.sessionId = sessionIdParam;
     }
-  
+
     builder.track('pageview');
     _window[TRACKED_KEY] = true;
-  
+
     const { Shopify } = _window;
-  
+
     if (!apiKey) {
-      console.debug('No apiKey for Builder JS', document.currentScript);
+      console.debug('No apiKey for Builder JS', currentScript);
     } else if (!Shopify) {
       console.debug('No Shopify object');
     } else if (Shopify.checkout?.order_id) {
       // rely on updated_at since created_at is not accurate
       const orderUpdatedAt = new Date(Shopify.checkout.updated_at);
-  
+
       const orderUpdatedMinutesAgo = (Date.now() - orderUpdatedAt.getTime()) / 1000 / 60;
-  
+
       // Order is not old
       if (orderUpdatedMinutesAgo < 3) {
         const trackedOrdersCookieKey = `builder.trackedOrders.${Shopify.checkout.order_id}`;
         const orderWasTracked = builder.getCookie(trackedOrdersCookieKey);
-  
+
         if (!orderWasTracked) {
           const checkout: Partial<Checkout> = {
             ...Shopify.checkout,
@@ -70,9 +71,9 @@ setTimeout(() => {
             billing_address: undefined,
             credit_card: undefined,
           };
-  
+
           builder.setCookie(trackedOrdersCookieKey, datePlusMinutes(60));
-  
+
           builder.track('conversion', {
             meta: checkout,
             amount: parseFloat(checkout.subtotal_price!), // TODO: normalize currency
@@ -81,4 +82,4 @@ setTimeout(() => {
       }
     }
   }
-})
+});
