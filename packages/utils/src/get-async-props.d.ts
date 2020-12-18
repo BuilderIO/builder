1b1b0466851c4a6550d5ef3d72d5f7fe39df21cb
Aziz Abbas <aziz@builder.io>
2020-12-17 16:27:17 -0800

    prettier

--- packages/utils/src/get-async-props.d.ts
+++ packages/utils/src/get-async-props.d.ts
@@ -1,7 +1,10 @@
 import { BuilderContent, BuilderElement } from '@builder.io/sdk';
 export declare const isBuilderElement: (item: unknown) => item is BuilderElement;
 declare type PropsMappers = {
-    [key: string]: (props: any, block: BuilderElement) => Promise<any>;
+  [key: string]: (props: any, block: BuilderElement) => Promise<any>;
 };
-export declare function getAsyncProps(content: BuilderContent, mappers: PropsMappers): Promise<BuilderContent>;
+export declare function getAsyncProps(
+  content: BuilderContent,
+  mappers: PropsMappers
+): Promise<BuilderContent>;
 export {};
