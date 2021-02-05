23c2d856ca70f5aeff6f6d8a3dbc85dd42fcd4f8
Steve Sewell <steve@builder.io>
2021-02-05 07:11:36 -0800

    Vue and Nuxt improvements (#366)
    * client side routing for Vue and Nuxt updates for 404 handling
    
    * fix 404 handling

--- examples/nuxt/pages/_page.vue
+++ examples/nuxt/pages/_page.vue
@@ -1,10 +1,19 @@
 <template>
-  <RenderContent
-    model="page"
-    :options="{
-      url: $route.path,
-    }"
-  />
+  <div v-if="notFound">
+    <!-- Show your 404 content -->
+    Page not found
+  </div>
+  <div v-else>
+    <RenderContent
+      :key="$route.path"
+      model="page"
+      @contentLoaded="contentLoaded"
+      @contentError="contentError"
+      :options="{
+        url: $route.path,
+      }"
+    />
+  </div>
 </template>
 
 <script>
@@ -15,6 +24,22 @@ import Vue from 'vue'
 builder.init('jdGaMusrVpYgdcAnAtgn')
 
 export default Vue.extend({
+  data: () => ({
+    notFound: false,
+  }),
   components: { RenderContent },
+  methods: {
+    contentLoaded(content) {
+      if (!content) {
+        if (this.$nuxt.context.ssrContext) {
+          this.$nuxt.context.ssrContext.res.statusCode = 404
+        }
+        this.notFound = true
+      }
+    },
+    contentError(err) {
+      // Handle error
+    },
+  },
 })
 </script>
