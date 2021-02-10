2a95ceb6ee69f355a6c55ad3f4284108871ae372
Aziz Abbas <aziz@builder.io>
2021-02-10 09:18:26 -0800

    next-js landing page barebone

---
+++ examples/next-js/landing-page/components/Link/Link.tsx
@@ -0,0 +1,9 @@
+import NextLink from 'next/link'
+
+export const Link: React.FC<React.AnchorHTMLAttributes<any>> = ({ href, children, ...props }) => {
+  return (
+    <NextLink href={href!}>
+      <a {...props}>{children}</a>
+    </NextLink>
+  )
+}
