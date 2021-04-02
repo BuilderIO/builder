3d9d394c049f3a8ccbcb069fb84799749bbfcfd0
Aziz Abbas <aziz@builder.io>
2021-04-02 15:54:36 -0700

    import transfer modules and fix hydration in exmple

--- examples/angular-universal/src/app/app.browser.module.ts
+++ examples/angular-universal/src/app/app.browser.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
-
+import { BrowserTransferStateModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 
 
 @NgModule({
-  imports: [ AppModule ],
+  imports: [
+    AppModule,
+    // Needed for builder.io to hydrate server rendered content
+    BrowserTransferStateModule
+  ],
   bootstrap: [ AppComponent ],
 })
 export class AppBrowserModule { }
