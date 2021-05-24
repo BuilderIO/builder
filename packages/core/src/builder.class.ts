7b52f010e69536b74d1ab650ae051fe3d778cefa
Aziz Abbas <aziz@builder.io>
2021-05-24 10:29:57 -0700

    the correct prettier version

--- packages/core/src/builder.class.ts
+++ packages/core/src/builder.class.ts
@@ -2155,7 +2155,9 @@ export class Builder {
           const data = result[keyName];
           const sorted = data; // sortBy(data, item => item.priority);
           if (data) {
-            const testModifiedResults = Builder.isServer ? sorted : this.processResultsForTests(sorted);
+            const testModifiedResults = Builder.isServer
+              ? sorted
+              : this.processResultsForTests(sorted);
             observer.next(testModifiedResults);
           } else {
             const search = this.getLocation().search;
