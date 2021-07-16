475ce0baedae39c162d73dd2f71408619a1f656a
Steve Sewell <steve@builder.io>
2021-07-16 13:47:12 -0700

    editing in React Native working

--- packages/sdks/src/functions/get-block-properties.ts
+++ packages/sdks/src/functions/get-block-properties.ts
@@ -2,5 +2,9 @@ import { BuilderBlock } from '../types/builder-block';
 
 export function getBlockProperties(block: BuilderBlock) {
   // TODO: bindings
-  return block.properties;
+  return {
+    ...block.properties,
+    'builder-id': block.id,
+    class: [block.id, 'builder-block', block.class].filter(Boolean).join(' '),
+  };
 }
