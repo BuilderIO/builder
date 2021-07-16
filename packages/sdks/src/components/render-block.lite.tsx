ac329e70dcf00bbaae92ac9c6dc8e17c32804886
Steve Sewell <steve@builder.io>
2021-07-15 19:09:49 -0700

    fixie fixie

--- packages/sdks/src/components/render-block.lite.tsx
+++ packages/sdks/src/components/render-block.lite.tsx
@@ -17,7 +17,7 @@ export default function RenderBlock(props: RenderBlockProps) {
     get componentInfo() {
       return state.component?.info;
     },
-    get componentRef() {
+    get √componentRef() {
       return state.component?.ref;
     },
     get tagName() {
