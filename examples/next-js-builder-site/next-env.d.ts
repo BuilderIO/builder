7565820e3959f493f8946e35e57532e0d0ac31b1
Steve Sewell <steve@builder.io>
2021-07-19 14:44:10 -0400

    fix accidental git submodules

--- examples/next-js-builder-site/next-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-/// <reference types="@emotion/react/types/css-prop" />
-
-// Nested types need a fix
-declare module '@builder.io/widgets/*';
