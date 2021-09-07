f877fa9e022d9b7457ef2027c4b58468a3d7ca79
Aziz Abbas <aziz@builder.io>
2021-09-07 01:04:24 -0700

    set up preview url and use private keys for webhook headers

---
+++ plugins/sfcc-sync/src/on-editor-load.ts
@@ -0,0 +1,26 @@
+import appState from '@builder.io/app-context';
+
+interface ContentEditorActions {
+  updatePreviewUrl: (url: string) => void;
+  safeReaction<T>(
+    watchFunction: () => T,
+    reactionFunction: (arg: T) => void,
+    options?: {
+      fireImmediately: true;
+    }
+  ): void;
+}
+
+export const onContentEditorLoad =  ({ safeReaction, updatePreviewUrl }: ContentEditorActions) => {
+  console.log(' here registering ', updatePreviewUrl);
+  safeReaction(() => appState.designerState.editingContentModel?.meta.get('sfccPreviewOptions'), (obj: any) => {
+    if (obj) {
+      const options = JSON.parse(JSON.stringify(obj));
+      const { apiPath, libraryName, assetId } = options;
+      const previewUrl = `${apiPath}/s/${libraryName}/${assetId}.html`.trim();
+      setTimeout(() => updatePreviewUrl(previewUrl), 500);
+      appState.snackBar.show(`Previewing ${previewUrl}`);
+    }
+  });
+}
+
