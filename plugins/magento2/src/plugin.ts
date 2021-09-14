4d4c5f0ec67d5bbfc32c94ac92492005e3959182
Aziz Abbas <aziz@builder.io>
2021-09-14 15:08:22 -0700

    category and product resources

---
+++ plugins/magento2/src/plugin.ts
@@ -0,0 +1,25 @@
+import { registerCommercePlugin } from '@builder.io/commerce-plugin-tools';
+import { Resource } from '@builder.io/commerce-plugin-tools/dist/types/interfaces/resource';
+import pkg from '../package.json';
+import { getAPI } from './service';
+
+registerCommercePlugin(
+  {
+    name: 'Magento',
+    id: pkg.name,
+    settings: [
+      {
+        name: 'storeUrl',
+        type: 'string',
+        required: true,
+      },
+    ],
+    ctaText: `Connect your Magento2 Store`,
+  },
+  async settings => {
+
+    const storeUrl = new URL(settings.get('storeUrl')).origin;
+
+    return getAPI(storeUrl);
+  }
+);
