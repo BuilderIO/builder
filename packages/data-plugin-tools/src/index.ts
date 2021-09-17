f62c2fd20a56520f2a96d0f5ab43b52ef5eb1b88
Aziz Abbas <aziz@builder.io>
2021-09-17 14:35:27 -0700

    [data-plugin-tools] add library with helpers to save settings and check if keys are still needed (#570)
    * first pass on data plugin tools lib
    
    * readme

---
+++ packages/data-plugin-tools/src/index.ts
@@ -0,0 +1,90 @@
+import { Builder } from '@builder.io/react';
+import { Input } from '@builder.io/sdk'
+import appState from '@builder.io/app-context';
+import { ReactNode } from 'react';
+
+interface OnSaveActions {
+  updateSettings(partal: Record<string, any>): Promise<void>;
+  addModel(model: Model): Promise<void>;
+}
+
+interface AppActions {
+  triggerSettingsDialog(pluginId: string): Promise<void>;
+}
+
+interface Model {
+  name: string;
+  hideFromUI?: boolean;
+  kind: 'data' | 'page' | 'component' | 'function';
+}
+
+export type ResourceType = {
+  name: string;
+  id: string;
+  icon?: string;
+  description?: string;
+  inputs?: () => Partial<Input>[];
+  toUrl: (options: Record<string, any>) => string | Promise<string>;
+  canPickEntries?: boolean;
+};
+
+export type GetResourceTypes = () => Promise<ResourceType[]>;
+
+export interface DataPluginOptions {
+  id: string; // match npm package name @builder.io/plugin-shopify,
+  name: string; // single word will prefix types with it, e.g Shopify
+  settings: any[]; // list of setting commerce API needs to communicate, e.g storeFrontAccessToken
+  ctaText: string; // what to display on the button to save settings, e.g `Connect your store`
+  onSave?: (actions: OnSaveActions) => Promise<void>; // you can run any action post save here, for example importing ${capitalize(resourceName)} data, registering webhooks
+  icon?: string;
+  getResourceTypes: GetResourceTypes;
+  getEntriesByResourceType?: GetEntriesByResourceType;
+  noResourceTypesFoundMessage?: string | ReactNode;
+}
+
+export type ResourceEntryType = {
+  id: string;
+  name: string;
+  icon?: string;
+  description?: string;
+};
+
+export type GetEntriesByResourceType = (
+  resourceTypeId: string,
+  options?: { searchText?: string; resourceEntryId?: string }
+) => Promise<ResourceEntryType[]>;
+
+export const registerDataPlugin = async (
+  config: DataPluginOptions,
+) => {
+
+  const registerEditors = async () => {
+    appState.registerDataPlugin(config);
+  };
+
+  const onSave = config.onSave;
+  config.onSave = async actions => {
+    await actions.updateSettings({ hasConnected: true });
+    if (onSave) {
+      await onSave(actions);
+    }
+    registerEditors();
+  };
+
+  Builder.register('plugin', { ...config, isDataPlugin: true });
+
+  Builder.register('app.onLoad', async ({ triggerSettingsDialog }: AppActions) => {
+    const pluginSettings = appState.user.organization.value.settings.plugins.get(config.id);
+    const hasConnected = pluginSettings?.get('hasConnected');
+    if (!hasConnected) {
+      await triggerSettingsDialog(config.id);
+    }
+  });
+
+  const savedSettings = appState.user.organization.value.settings.plugins.get(config.id);
+  if (!savedSettings) {
+    return;
+  }
+
+  await registerEditors();
+};
