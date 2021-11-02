6cf9f61377b5e34c9b8ddda4f674c07dc33a78f0
Steve Sewell <steve@builder.io>
2021-11-01 18:27:54 -0700

    clean up simple app plugin example

--- plugins/example-app-simple/src/plugin.ts
+++ plugins/example-app-simple/src/plugin.ts
@@ -1,12 +1,30 @@
 import { Builder } from '@builder.io/sdk'
+import { Header } from './components/editor-header'
 import { SimplePage } from './components/simple-page'
 
 Builder.register('editor.settings', {
-  hideStyleTab: true,
+  hideToolbar: true,
+  hideHeatMap: true,
   hideMainTabs: true,
+  hideFormComponents: true,
   hideDataTab: true,
+  hideStyleTab: true,
+  hideLayersTab: true,
   hideAnimateTab: true,
-  hideTargeting: true,
+  hideABTab: true,
+  hidePageUrlEditor: true,
+  componentsOnlyMode: true,
+  hideTemplates: true,
+  hideSymbols: true,
+  hideOptionsTab: true
+})
+
+Builder.register('editor.header', {
+  component: Header
+})
+
+Builder.register('editor.previewToolbar', {
+  component: () => null
 })
 
 // Register some app settings
@@ -14,27 +32,25 @@ Builder.register('appSettings', {
   settings: {
     hideDefaultTabs: true,
     hideLeftSidebar: true,
-    defaultRoute: '/apps/simple',
+    defaultRoute: '/apps/simple'
   },
   theme: {
     colors: {
-      primary: 'rgb(220 130 86)',
+      primary: 'rgb(220 130 86)'
     },
     // Provide any theme configuration for material UI v3
     // https://v3.material-ui.com/customization/themes/#theme-configuration-variables
     mui: {
       typography: {
-        fontFamily: 'Arial',
-      },
-    },
-  },
+        fontFamily: 'Arial'
+      }
+    }
+  }
 })
 
 // Register a tab in the app called "campaigns" with a custom UI for listing and creating campaigns
 Builder.register('appTab', {
   name: 'Simple',
   path: 'simple',
-  icon:
-    'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F20c994a85a6741b5be6a6ead7316c8d9',
-  component: SimplePage,
+  component: SimplePage
 })
