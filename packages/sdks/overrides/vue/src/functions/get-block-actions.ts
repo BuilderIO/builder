df0ebe3cdf2f4798a8389f12d8bb33612ba726ca
Steve Sewell <steve@builder.io>
2021-12-17 11:47:31 -0800

    lowercase event names for vue

--- packages/sdks/overrides/vue/src/functions/get-block-actions.ts
+++ packages/sdks/overrides/vue/src/functions/get-block-actions.ts
@@ -1,6 +1,10 @@
 import { evaluate } from './evaluate';
 
-export function getBlockActions(options: { block: any; context: any; state: any }): any {
+export function getBlockActions(options: {
+  block: any;
+  context: any;
+  state: any;
+}): any {
   const obj: any = {};
   if (options.block.actions) {
     for (const key in options.block.actions) {
@@ -9,7 +13,7 @@ export function getBlockActions(options: { block: any; context: any; state: any 
       }
 
       const value = options.block.actions[key];
-      obj['v-on:' + key] = (event: Event) =>
+      obj['v-on:' + key.toLowerCase()] = (event: Event) =>
         evaluate({
           code: value,
           context: options.context,
