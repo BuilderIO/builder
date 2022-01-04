fcf14b37beb7c484c78a6b0a7f4e60223cbf3d01
Steve Sewell <steve@builder.io>
2022-01-04 09:52:45 -0800

    fix accidentally removing comment

--- packages/sdks/overrides/react-native/src/functions/transform-block.ts
+++ packages/sdks/overrides/react-native/src/functions/transform-block.ts
@@ -1,7 +1,8 @@
 import type { BuilderBlock } from '../types/builder-block';
 
 export function transformBlock(block: BuilderBlock): BuilderBlock {
-  // Map the DOM-based pixel format to a native compatible one
+  // Map the DOM-based pixel format to a native compatible one.
+  // react-native doesn't support DOM specific fields like `tagName` and `properties`, but any browser framework (e.g. Vue, etc.) do
   if (block.id.startsWith('builder-pixel-')) {
     return {
       ...block,
