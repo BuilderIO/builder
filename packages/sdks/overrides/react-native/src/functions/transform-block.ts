617f3ea6f26fbc2b7ee703b1d513c7f9354a5fe1
Steve Sewell <steve@builder.io>
2022-01-04 09:41:48 -0800

    improve types

--- packages/sdks/overrides/react-native/src/functions/transform-block.ts
+++ packages/sdks/overrides/react-native/src/functions/transform-block.ts
@@ -1,4 +1,6 @@
-export function transformBlock(block: any) {
+import type { BuilderBlock } from '../types/builder-block';
+
+export function transformBlock(block: BuilderBlock): BuilderBlock {
   // Map the DOM-based pixel format to a native compatible one
   if (block.id.startsWith('builder-pixel-')) {
     return {
--- packages/sdks/src/functions/transform-block.ts
+++ packages/sdks/src/functions/transform-block.ts
@@ -1,4 +1,6 @@
+import { BuilderBlock } from '../types/builder-block';
+
 // Noope way for targets to make modifications to the block object if/as needed
-export function transformBlock(block: any) {
+export function transformBlock(block: BuilderBlock): BuilderBlock {
   return block;
 }
