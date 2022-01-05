c6e37d50035a245c0817c05597225f49588cda66
Steve Sewell <steve@builder.io>
2022-01-05 15:56:09 -0800

    make sure that if we send pixels in the new format they work too

--- packages/sdks/overrides/react-native/src/functions/transform-block.ts
+++ packages/sdks/overrides/react-native/src/functions/transform-block.ts
@@ -3,7 +3,7 @@ import type { BuilderBlock } from '../types/builder-block';
 export function transformBlock(block: BuilderBlock): BuilderBlock {
   // Map the DOM-based pixel format to a native compatible one.
   // react-native doesn't support DOM specific fields like `tagName` and `properties`, but any browser framework (e.g. Vue, etc.) do
-  if (block.id.startsWith('builder-pixel-')) {
+  if (block.id.startsWith('builder-pixel-') && !block.component) {
     return {
       ...block,
       component: {
