9d2a207ceca39bf83d5bbdc4bd67351e86105d78
Sami Jaber <jabersami@gmail.com>
2022-01-20 17:06:43 -0500

    fix(sdks): react-native & isBrowser interactions

--- packages/sdks/src/functions/is-browser.ts
+++ packages/sdks/src/functions/is-browser.ts
@@ -1,9 +1,3 @@
-import { isReactNative } from './is-react-native';
-
 export function isBrowser(): boolean {
-  return (
-    typeof window !== 'undefined' &&
-    typeof document !== 'undefined' &&
-    !isReactNative()
-  );
+  return typeof window !== 'undefined' && typeof document !== 'undefined';
 }
