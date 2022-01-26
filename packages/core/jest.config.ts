f83b8569667ff0592dd811e31d0b7f51ebb1942b
Dylan Kendal <dylankendal@gmail.com>
2022-01-25 22:20:13 -0500

    chore(core): add typescript transform for jest

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -172,7 +172,9 @@ export default {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    "\\.ts$": "esbuild-runner/jest"
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
