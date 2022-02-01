0fd375c1be8e3420d77058919b62f02d376352b5
Alex Patterson <alex@codingcat.dev>
2022-02-01 12:00:56 -0500

    initial copy of next-js starter

---
+++ examples/next-js-algolia-plugin/pages/[[...page]].tsx
@@ -0,0 +1,71 @@
+import type { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import { useRouter } from "next/router";
+import { BuilderComponent, Builder, builder } from "@builder.io/react";
+import DefaultErrorPage from "next/error";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+
+export async function getStaticProps({ params }: GetStaticPropsContext<{ page: string[] }>) {
+  const page = await builder
+    .get("page", {
+      userAttributes: {
+        urlPath: "/" + (params?.page?.join("/") || ""),
+      },
+    })
+    .toPromise();
+
+  return {
+    props: {
+      page: page || null,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 5 seconds
+    revalidate: 5,
+  };
+}
+
+export async function getStaticPaths() {
+  const pages = await builder.getAll("page", {
+    options: { noTargeting: true },
+    omit: "data.blocks",
+  });
+
+  return {
+    paths: pages.map(page => `${page.data?.url}`),
+    fallback: true,
+  };
+}
+
+export default function Page({ page }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+  const [isLive, setIsLive] = useState(false);
+  useEffect(() => {
+    setIsLive(!Builder.isEditing && !Builder.isPreviewing);
+  }, []);
+
+  if (router.isFallback) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!page && isLive) {
+    return (
+      <>
+        <Head>
+          <meta name="robots" content="noindex" />
+          <meta name="title"></meta>
+        </Head>
+        <DefaultErrorPage statusCode={404} />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <BuilderComponent model="page" content={page} />
+    </>
+  );
+}
