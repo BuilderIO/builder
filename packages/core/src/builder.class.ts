ef7dfa918a7390ec35d0cea558145da884b6b417
Aziz Abbas <aziz@builder.io>
2022-02-07 16:28:19 -0800

    typo

--- packages/core/src/builder.class.ts
+++ packages/core/src/builder.class.ts
@@ -481,7 +481,7 @@ export interface Component {
    * Link to a screenshot shown when user hovers over the component in Builder's editor
    * use https://builder.io/upload to upload your screeshot, for easier resizing by Builder.
    */
-  screeshot?: string;
+  screenshot?: string;
 
   /**
    * Input schema for your component for users to fill in the options
