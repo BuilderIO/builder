5d5827d605b40a45903fd79f3d28d3d4c5d233e0
Aziz Abbas <aziz@builder.io>
2022-02-07 16:24:50 -0800

    add screenshot to Components interfce

--- packages/core/src/builder.class.ts
+++ packages/core/src/builder.class.ts
@@ -473,7 +473,16 @@ export interface Component {
    * Link to a documentation page for this component
    */
   docsLink?: string;
+  /**
+   * Link to an image to be used as an icon for this component in Builder's editor
+   */
   image?: string;
+  /**
+   * Link to a screenshot to be shown on when user hover's over the component in Builder's editor
+   * use https://builder.io/upload to upload your screeshot, for easier resizing by Builder.
+   */
+  screeshot?: string;
+
   /**
    * Input schema for your component for users to fill in the options
    */
