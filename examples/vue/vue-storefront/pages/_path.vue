aa74cb35f87723b8d7c3818838e84933a58c6bd2
Sami Jaber <jabersami@gmail.com>
2022-02-14 17:00:40 -0500

    example(sdks): add generic path handling in vue-storefront

---
+++ examples/vue/vue-storefront/pages/_path.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="home">
+    <div>Hello world from your Vue project. Below is Builder Content:</div>
+    <div>page: {{ content.data.title }}</div>
+    <div v-if="canShowContent">
+      <builder-render-content model="page" :content="content" />
+    </div>
+  </div>
+</template>
+<script>
+import Vue from 'vue';
+import cacheControl from '../helpers/cacheControl';
+
+import './init-builder';
+import { getContent, isEditing } from '@builder.io/sdk-vue';
+
+// TODO: enter your public API key
+const BUILDER_PUBLIC_API_KEY = 'f1a790f8c3204b3b8c5c1795aeac4660';
+
+export default Vue.extend({
+  name: 'Home',
+  middleware: cacheControl({
+    'max-age': 60,
+    'stale-when-revalidate': 5,
+  }),
+  data: () => ({
+    canShowContent: false,
+    content: null,
+  }),
+  async fetch() {
+    const content = await getContent({
+      model: 'page',
+      apiKey: BUILDER_PUBLIC_API_KEY,
+      userAttributes: {
+        urlPath: this.$route.path,
+      },
+    });
+    if (!content) {
+      if (this.$nuxt.context?.ssrContext?.res) {
+        this.$nuxt.context.ssrContext.res.statusCode = 404;
+      }
+    }
+    this.content = content;
+    this.canShowContent = content || isEditing();
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+#home {
+  box-sizing: border-box;
+  padding: 0 var(--spacer-sm);
+  @include for-desktop {
+    max-width: 1240px;
+    padding: 0;
+    margin: 0 auto;
+  }
+}
+</style>
