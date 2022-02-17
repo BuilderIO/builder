4867e6fcac6c69ec1f8a45168ac222e95cab95a1
Aziz Abbas <aziz@builder.io>
2022-02-17 14:41:33 -0800

    [algolia sync] use private key for webhook headers

--- plugins/algolia/src/create-web-hook.tsx
+++ plugins/algolia/src/create-web-hook.tsx
@@ -5,17 +5,18 @@ export const createWebhook = async (model: any) => {
   const pluginSettings = appState.user.organization.value.settings.plugins.get(pluginId);
   const algoliaKey = pluginSettings.get('algoliaKey');
   const algoliaAppId = pluginSettings.get('algoliaAppId');
-  const customHeaders = [];
-
-  for (const headerName in appState.user.authHeaders) {
-    customHeaders.push({
-      name: headerName,
-      value: appState.user.authHeaders[headerName],
-    });
-  }
+  const pluginPrivateKey = await appState.globalState.getPluginPrivateKey(pluginId);
 
   const newWebhook = {
-    customHeaders,
+    meta: {
+      pluginId
+    },
+    customHeaders: [
+      {
+        name: 'Authorization',
+        value: `Bearer ${pluginPrivateKey}`,
+      },
+    ],
     url: `${appState.config.apiRoot()}/api/v1/algolia-sync/webhook?algoliaKey=${algoliaKey}&algoliaAppId=${algoliaAppId}&modelName=${
       model.name
     }`,
