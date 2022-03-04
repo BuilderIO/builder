c7ce7f800eb148bb4d3f035b5bea55c9b2990d75
Korey Kassir <korey@builder.io>
2022-03-04 10:32:34 -0800

    fix(core): fix impression tracking for content with no id (#929)
    * fix(core): fix impression tracking for content with no id
    
    * chore(changelog)
    
    * fix(sdks): fix false secret

--- examples/vue/nuxt-2/pages/_page.vue
+++ examples/vue/nuxt-2/pages/_page.vue
@@ -16,7 +16,7 @@ import { getContent, isEditing, isPreviewing } from '@builder.io/sdk-vue'
 import '../scripts/register-builder-components'
 
 // TODO: enter your public API key
-const BUILDER_PUBLIC_API_KEY = 'f1a790f8c3204b3b8c5c1795aeac4660'
+const BUILDER_PUBLIC_API_KEY = 'f1a790f8c3204b3b8c5c1795aeac4660' // ggignore
 
 export default Vue.extend({
   data: () => ({
--- packages/core/CHANGELOG.md
+++ packages/core/CHANGELOG.md
@@ -2,6 +2,7 @@
 
 - improved logging during JSON.parse errors
 - deprecation notice on `Builder.VERSION`
+- stop tracking impressions for content with no id
 
 ### 1.1.26
 
