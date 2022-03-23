efe9829faf41bade04ebd0b715938f9898521a4b
Sanyam Kamat <sanyamkamat@gmail.com>
2022-03-23 22:07:41 +0530

    fix(sdks): ENG-2365 pass content id as an unique key (#945)
    fix(sdks): ENG-2365 pass content id as an unique key

--- packages/react/src/components/builder-content.component.tsx
+++ packages/react/src/components/builder-content.component.tsx
@@ -88,6 +88,9 @@ export class BuilderContent<ContentType extends object = any> extends React.Comp
     let options = {
       ...(this.props.options || ({} as GetContentOptions)),
     };
+    if (!options.key && this.props.content?.id) {
+      options.key = this.props.content.id;
+    }
     if (this.props.content && !options.initialContent?.length) {
       options.initialContent = [this.props.content];
     }
