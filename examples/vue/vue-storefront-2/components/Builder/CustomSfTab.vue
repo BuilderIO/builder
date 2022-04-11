59a569f6f725adcbdbf0f74485c34e48b53a3db5
Sami Jaber <jabersami@gmail.com>
2022-04-11 18:47:51 -0400

    examples(sdks): expand on using custom components w/ children in Vue

---
+++ examples/vue/vue-storefront-2/components/Builder/CustomSfTab.vue
@@ -0,0 +1,35 @@
+<template>
+  <sf-tab v-bind="pass">
+    <!--
+      we need to wrap children in render-blocks, so that Builder content can still be properly
+      rendered. This is similar to what Builder does for its Columns components.
+    -->
+    <render-blocks :blocks="builderBlock.children"></render-blocks>
+  </sf-tab>
+</template>
+
+<script>
+// TO-DO: chat w/ Vue Storefront to make importing internals more ergonomic
+import SfTab from '@storefront-ui/vue/src/components/organisms/SfTabs/_internal/SfTab.vue';
+import RenderBlocks from '@builder.io/sdk-vue/nuxt2/src/components/render-blocks.vue';
+
+export default {
+  components: { SfTab, RenderBlocks },
+  name: 'CustomSfTab',
+  props: {
+    builderBlock: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  computed: {
+    pass() {
+      const { attributes, ...other } = this.$attrs;
+      return {
+        ...attributes,
+        ...other,
+      };
+    },
+  },
+};
+</script>
--- examples/vue/vue-storefront-2/pages/init-builder.ts
+++ examples/vue/vue-storefront-2/pages/init-builder.ts
@@ -8,8 +8,93 @@ import '@builder.io/sdk-vue/nuxt2/src/blocks/columns.vue';
 import '@builder.io/sdk-vue/nuxt2/src/blocks/button.vue';
 import '@builder.io/sdk-vue/nuxt2/src/blocks/section.vue';
 
+// Register Vue Storefront UI components
+
+// Atoms
+import { SfButton } from '@storefront-ui/vue';
+registerComponent(SfButton, {
+  name: 'SfButton',
+  canHaveChildren: true,
+  inputs: [
+    {
+      name: 'link',
+      type: 'string',
+      defaultValue: null,
+    },
+    {
+      name: 'disabled',
+      type: 'boolean',
+      defaultValue: false,
+    },
+    {
+      name: 'type',
+      type: 'string',
+      enum: ['button', 'submit', 'reset'],
+      defaultValue: 'button',
+    },
+  ],
+});
+
+// Molecules (multiple slots)
+import { SfCard } from '@storefront-ui/vue';
+registerComponent(SfCard, {
+  name: 'SfCard',
+  canHaveChildren: true,
+  inputs: [
+    { name: 'image', type: 'string' },
+    { name: 'title', type: 'string' },
+    { name: 'titleLevel', type: 'number' },
+    { name: 'description', type: 'string' },
+    { name: 'link', type: 'string' },
+    { name: 'buttonText', type: 'string' },
+  ],
+});
+
+// Organisms (other UI molecules/atoms as custom children)
+import { SfTabs } from '@storefront-ui/vue';
+
+// TO-DO: can you restrict the type of children it receives, to just the `SfTab` component?
+registerComponent(SfTabs, {
+  name: 'SfTabs',
+  canHaveChildren: true,
+  inputs: [
+    {
+      name: 'openTab',
+      type: 'number',
+    },
+    {
+      name: 'tabMaxContentHeight',
+      type: 'string',
+    },
+    {
+      name: 'tabShowText',
+      type: 'string',
+    },
+    {
+      name: 'tabHideText',
+      type: 'string',
+    },
+  ],
+});
+
+// import SfTab from '@storefront-ui/vue/src/components/organisms/SfTabs/_internal/SfTab.vue';
+import SfTab from '../components/Builder/CustomSfTab.vue';
+registerComponent(SfTab, {
+  name: 'SfTab',
+  canHaveChildren: true,
+  // needed to get correct DOM hierarchy with `SfTabs`
+  noWrap: true,
+  inputs: [
+    {
+      name: 'title',
+      type: 'string',
+      default: '',
+    },
+  ],
+});
+
 // Register your Builder components
-import HelloWorldComponent from '../components/HelloWorld.vue';
+import HelloWorldComponent from '../components/Builder/HelloWorld.vue';
 
 registerComponent(HelloWorldComponent, {
   name: 'Hello World',
