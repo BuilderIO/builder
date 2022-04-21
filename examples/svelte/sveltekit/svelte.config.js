bf9ef02523ef80994878f1e95370bf0c4a7881bb
Sami Jaber <jabersami@gmail.com>
2022-04-21 15:55:46 -0400

    examples(sdks): comment out vite ssr disable

--- examples/svelte/sveltekit/svelte.config.js
+++ examples/svelte/sveltekit/svelte.config.js
@@ -1,33 +1,43 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import { esbuildCommonjs, viteCommonjs } from '@originjs/vite-plugin-commonjs';
+// import { esbuildCommonjs, viteCommonjs } from '@originjs/vite-plugin-commonjs';
 
-const CJS_MODULES = ['@builder.io/sdk-svelte'];
+// const CJS_MODULES = ['@builder.io/sdk-svelte'];
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
+	// compilerOptions: {
+	// 	generate: 'dom'
+	// },
 
 	kit: {
+		// prerender: {
+		// 	enabled: false
+		// },
 		adapter: adapter(),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
-		},
+		}
 		// This vite config is needed to build our Builder Svelte SDK, which is a CJS module at the moment.
 		// https://github.com/vitejs/vite/issues/2579#issuecomment-1025058519
-		vite: {
-			plugins: [viteCommonjs()],
-			optimizeDeps: {
-				esbuildOptions: {
-					plugins: [esbuildCommonjs(CJS_MODULES)]
-				},
-				include: CJS_MODULES
-			}
-		}
+		// vite: {
+		// 	plugins: [viteCommonjs()],
+		// 	optimizeDeps: {
+		// 		esbuildOptions: {
+		// 			plugins: [esbuildCommonjs(CJS_MODULES)]
+		// 		},
+		// 		include: CJS_MODULES
+		// 	}
+		// 	// build: {
+		// 	// 	ssr: false,
+		// 	// 	minify: false
+		// 	// }
+		// }
 	}
 };
 
