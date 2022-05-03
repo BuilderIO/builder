9261e3da4dc889459b80fd019722e9229d09427e
Sami Jaber <jabersami@gmail.com>
2022-05-02 20:13:05 -0400

    add missing export

--- packages/sdks/src/blocks/embed/index.ts
+++ packages/sdks/src/blocks/embed/index.ts
@@ -1,4 +1,4 @@
 import component from './embed-internal.lite';
 import { componentInfo } from './component-info';
 import { registerComponent } from '../../functions/register-component';
-registerComponent(component, componentInfo);
+export default registerComponent(component, componentInfo);
--- packages/sdks/src/blocks/form/index.ts
+++ packages/sdks/src/blocks/form/index.ts
@@ -1,4 +1,4 @@
 import component from './form-internal.lite';
 import { componentInfo } from './component-info';
 import { registerComponent } from '../../functions/register-component';
-registerComponent(component, componentInfo);
+export default registerComponent(component, componentInfo);
