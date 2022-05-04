d9d9e8f4ab8c8d082217442b22fc8b7f5caf848b
Sami Jaber <jabersami@gmail.com>
2022-05-04 10:20:21 -0400

    fix blocks exports paths

--- packages/sdks/src/index-helpers/blocks-exports.ts
+++ packages/sdks/src/index-helpers/blocks-exports.ts
@@ -1,10 +1,10 @@
 // TODO: lazy option
-export { default as Columns } from '../blocks/columns/columns.lite';
-export { default as Image } from '../blocks/image/image.lite';
-export { default as Text } from '../blocks/text/text.lite';
-export { default as Video } from '../blocks/video/video.lite';
-export { default as Symbol } from '../blocks/symbol/symbol.lite';
-export { default as Button } from '../blocks/button/button.lite';
-export { default as Section } from '../blocks/section/section.lite';
-export { default as Fragment } from '../blocks/fragment/fragment.lite';
+export { default as Columns } from '../blocks/columns';
+export { default as Image } from '../blocks/image';
+export { default as Text } from '../blocks/text';
+export { default as Video } from '../blocks/video';
+export { default as Symbol } from '../blocks/symbol';
+export { default as Button } from '../blocks/button';
+export { default as Section } from '../blocks/section';
+export { default as Fragment } from '../blocks/fragment';
 export { default as RenderContent } from '../components/render-content/render-content.lite';
