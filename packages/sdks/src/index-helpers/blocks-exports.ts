c566594125adc8ea919535a0339acfa80988b6b1
Sami Jaber <jabersami@gmail.com>
2022-05-06 15:20:19 -0400

    fix bad imports

--- packages/sdks/src/index-helpers/blocks-exports.ts
+++ packages/sdks/src/index-helpers/blocks-exports.ts
@@ -1,10 +1,10 @@
 // TODO: lazy option
-export { default as Columns } from '../blocks/columns.js';
-export { default as Image } from '../blocks/image.js';
-export { default as Text } from '../blocks/text.js';
-export { default as Video } from '../blocks/video.js';
-export { default as Symbol } from '../blocks/symbol.js';
-export { default as Button } from '../blocks/button.js';
-export { default as Section } from '../blocks/section.js';
-export { default as Fragment } from '../blocks/fragment.js';
+export { default as Columns } from '../blocks/columns/index.js';
+export { default as Image } from '../blocks/image/index.js';
+export { default as Text } from '../blocks/text/index.js';
+export { default as Video } from '../blocks/video/index.js';
+export { default as Symbol } from '../blocks/symbol/index.js';
+export { default as Button } from '../blocks/button/index.js';
+export { default as Section } from '../blocks/section/index.js';
+export { default as Fragment } from '../blocks/fragment/index.js';
 export { default as RenderContent } from '../components/render-content/render-content.lite';
