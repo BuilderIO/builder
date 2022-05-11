be4a750e75ecf15021ca3edfe478566133765645
J. Logan Affleck <56612066+LoganAffleck@users.noreply.github.com>
2022-05-11 13:06:40 -0400

    Ran Prettier locally

--- packages/react/src/blocks/forms/TextArea.tsx
+++ packages/react/src/blocks/forms/TextArea.tsx
@@ -18,7 +18,7 @@ class TextAreaComponent extends React.Component<FormTextAreaProps> {
         name={this.props.name}
         value={this.props.value}
         defaultValue={this.props.defaultValue}
-        {...(this.props.required ? { required: "required" } : {})}
+        {...(this.props.required ? { required: 'required' } : {})}
         {...this.props.attributes}
       />
     );
