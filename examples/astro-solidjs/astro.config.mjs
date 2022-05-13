ae78a0e3ab2a4140c855b982b5bb12392cd4cc58
Sami Jaber <jabersami@gmail.com>
2022-05-13 12:25:24 -0700

    attempt to fix astrojs solidjs usage

--- examples/astro-solidjs/astro.config.mjs
+++ examples/astro-solidjs/astro.config.mjs
@@ -13,9 +13,13 @@ export default defineConfig({
       target: 'esnext',
       polyfillDynamicImport: false,
     },
+    ssr: {
+      noExternal: ['@builder. io/sdk-solid'],
+    },
     optimizeDeps: {
       // sub-dependencies of our sym-linked package must be manually included here
       include: ['solid-styled-components'],
+      exclude: ['node-fetch'],
     },
     resolve: {
       // we must preserve symlinks for our sym-linked package to work properly
