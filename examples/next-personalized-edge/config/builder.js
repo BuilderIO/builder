652f24cf5f4db40a8ab73d8a1545c64a0ef4985e
Aziz Abbas <aziz@builder.io>
2022-05-16 10:51:24 -0700

    remove example

--- examples/next-personalized-edge/config/builder.js
+++
@@ -1,7 +0,0 @@
-if (!process.env.BUILDER_PUBLIC_KEY) {
-  throw new Error('Missing env varialbe BUILDER_PUBLIC_KEY');
-}
-
-export default {
-  apiKey: process.env.BUILDER_PUBLIC_KEY,
-};
--- examples/next-personalized-edge/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../assets/index.css';
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <div className="min-h-screen">
-      <Component {...pageProps} />
-    </div>
-  );
-}
