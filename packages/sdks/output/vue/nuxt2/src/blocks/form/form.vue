cbfa57b7a9b9c2ba82a5bb289cb2479eaeea4591
Sami Jaber <jabersami@gmail.com>
2022-05-31 11:04:46 -0400

    chore(sdks): run build

--- packages/sdks/output/vue/nuxt2/src/blocks/form/form.vue
+++ packages/sdks/output/vue/nuxt2/src/blocks/form/form.vue
@@ -27,7 +27,7 @@
     ></builder-blocks>
 
     <pre
-      class="builder-form-error-text pre-4wbls88y960"
+      class="builder-form-error-text pre-1be3j8m9ewb"
       v-if="submissionState === 'error' && responseData"
     >
         {{ JSON.stringify(responseData, null, 2) }}
@@ -302,7 +302,7 @@ export default {
 };
 </script>
 <style scoped>
-.pre-4wbls88y960 {
+.pre-1be3j8m9ewb {
   padding: 10px;
   color: red;
   text-align: center;
