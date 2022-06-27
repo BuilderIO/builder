febb87089f70ae9c3d38465c0010d2083cb1fbdc
Aziz Abbas <aziz@builder.io>
2022-06-27 16:40:14 -0700

    [examples/angular] add example for component with editable sections

--- examples/angular/src/app/app.module.ts
+++ examples/angular/src/app/app.module.ts
@@ -5,10 +5,10 @@ import { BuilderModule } from '@builder.io/angular';
 
 import { AppComponent, CustomThing } from './app.component';
 import { FooComponent } from './foo.component';
-
+import { CustomThingChildren } from './with-children';
 @NgModule({
-  declarations: [AppComponent, FooComponent, CustomThing],
-  entryComponents: [CustomThing],
+  declarations: [AppComponent, FooComponent, CustomThing, CustomThingChildren],
+  entryComponents: [CustomThing, CustomThingChildren],
   imports: [
     BrowserModule,
     BuilderModule.forRoot('1f3bf1d766354f32ba70dde440fcef97'),
