31dab3cec1c5e6dc4c1073765a6d13a93f34417d
Aziz Abbas <aziz@builder.io>
2022-06-27 10:28:45 -0700

    prettier

--- packages/utils/src/translation-helpers.test.ts
+++ packages/utils/src/translation-helpers.test.ts
@@ -7,7 +7,7 @@ test('getTranslateableFields from content to match snapshot', async () => {
       title: {
         '@type': '@builder.io/core:LocalizedValue',
         'en-US': 'Hello',
-        'Default': 'Test'
+        Default: 'Test',
       },
       blocks: [
         {
@@ -27,40 +27,38 @@ test('getTranslateableFields from content to match snapshot', async () => {
   expect(result).toMatchSnapshot();
 });
 
-
 test('applyTranslation from content to match snapshot', async () => {
-    const content: BuilderContent = {
-      data: {
-        title: {
-          '@type': '@builder.io/core:LocalizedValue',
-          'en-US': 'Hello',
-          'Default': 'Test'
-        },
-        blocks: [
-          {
-            id: 'block-id',
-            '@type': '@builder.io/sdk:Element',
-            component: {
-              name: 'Text',
-              options: {
-                text: 'test',
-              },
+  const content: BuilderContent = {
+    data: {
+      title: {
+        '@type': '@builder.io/core:LocalizedValue',
+        'en-US': 'Hello',
+        Default: 'Test',
+      },
+      blocks: [
+        {
+          id: 'block-id',
+          '@type': '@builder.io/sdk:Element',
+          component: {
+            name: 'Text',
+            options: {
+              text: 'test',
             },
           },
-        ],
-      },
-    };
-
-    const translations = {
-        metadata: {
-            title: 'salut'
         },
-        blocks: {
-            'block-id#text': 'block-id'
-        },
-    }
+      ],
+    },
+  };
 
-    const result = appLyTranslation(content, translations, 'fr-FR');
-    expect(result).toMatchSnapshot();
-  });
-  
+  const translations = {
+    metadata: {
+      title: 'salut',
+    },
+    blocks: {
+      'block-id#text': 'block-id',
+    },
+  };
+
+  const result = appLyTranslation(content, translations, 'fr-FR');
+  expect(result).toMatchSnapshot();
+});
