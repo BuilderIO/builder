7704a186a7ea4bf9e4f5a76bbfb98f6b339c5927
Sami Jaber <jabersami@gmail.com>
2022-07-12 20:38:12 -0400

    remove sveltekit example styles

--- examples/svelte/sveltekit/src/routes/__layout.svelte
+++ examples/svelte/sveltekit/src/routes/__layout.svelte
@@ -15,10 +15,7 @@
     flex: 1;
     display: flex;
     flex-direction: column;
-    padding: 1rem;
     width: 100%;
-    max-width: 1024px;
-    margin: 0 auto;
     box-sizing: border-box;
   }
 
