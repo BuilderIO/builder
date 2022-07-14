d5f94433a84604d4cc481c8070b73378723134e2
Aziz Abbas <aziz@builder.io>
2022-07-14 14:36:40 -0700

    release dev

--- packages/react/rollup.config.ts
+++ packages/react/rollup.config.ts
@@ -14,9 +14,8 @@ const resolvePlugin = resolve();
 
 const externalDependencies = Object.keys(pkg.dependencies)
   .concat(Object.keys(pkg.optionalDependencies || {}))
-  .filter(item => item !== 'tslib');
-// TODO: go back to using peerDependencies once fix rollup iife issue
-// .concat(Object.keys(pkg.peerDependencies || {}))
+  .filter(item => item !== 'tslib')
+  .concat(Object.keys(pkg.peerDependencies || {}))
 
 const options = {
   input: `src/${libraryName}.ts`,
