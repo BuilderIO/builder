c8c4c45f973334317a9b83689ec23db63cfd8c77
Sami Jaber <jabersami@gmail.com>
2022-07-19 15:35:25 -0400

    simplify sveltekit example

--- examples/svelte/sveltekit/src/routes/[...catch_all].svelte
+++ examples/svelte/sveltekit/src/routes/[...catch_all].svelte
@@ -12,14 +12,7 @@
 
 <section>
   <h1>
-    <div class="welcome">
-      <picture>
-        <source srcset="svelte-welcome.webp" type="image/webp" />
-        <img src="svelte-welcome.png" alt="Welcome" />
-      </picture>
-    </div>
-
-    to your new<br />SvelteKit app
+    Welcome to your new<br />SvelteKit app
   </h1>
 
   <DynamicallyRenderBuilderPage />
