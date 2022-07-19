f82e79e26118ad61f60ffc5e522ed08e52e91100
Sami Jaber <jabersami@gmail.com>
2022-07-19 17:42:51 -0400

    upgrade sveltekit

---
+++ examples/svelte/sveltekit/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [sveltekit()],
+  server: {
+    port: 3000
+  }
+});
