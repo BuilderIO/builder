9f4f93136575b13953acd0d8e06519030236242c
Sami Jaber <jabersami@gmail.com>
2022-07-19 16:13:19 -0400

    fix fn serialization

--- packages/sdks/src/functions/register-component.ts
+++ packages/sdks/src/functions/register-component.ts
@@ -30,17 +30,29 @@ export const createRegisterComponentMessage = ({
 /**
  * We need to serialize values to a string in case there are Proxy values, as is the case with SolidJS etc.
  */
-const fastClone = (obj: unknown) => JSON.parse(JSON.stringify(obj));
+const fastClone = <T extends object>(obj: T): T =>
+  JSON.parse(JSON.stringify(obj));
 
-const serializeValue = (value: unknown): string =>
+const serializeValue = (value: object): any =>
   typeof value === 'function' ? serializeFn(value) : fastClone(value);
 
 /**
  * Input attributes that are functions must be converted to strings before being serialized to JSON.
  */
 // eslint-disable-next-line @typescript-eslint/ban-types
-const serializeFn = (fn: Function) =>
-  `return (${fn.toString()}).apply(this, arguments)`;
+const serializeFn = (fnValue: Function) => {
+  const fnStr = fnValue.toString().trim();
+  // we need to account for a few different fn syntaxes:
+  // 1. `function name(args) => {code}`
+  // 2. `name(args) => {code}`
+  // 3. `(args) => {}`
+  const appendFunction =
+    !fnStr.startsWith('function') && !fnStr.startsWith('(');
+
+  return `return (${
+    appendFunction ? 'function ' : ''
+  }${fnStr}).apply(this, arguments)`;
+};
 
 const prepareComponentInfoToSend = ({
   inputs,
