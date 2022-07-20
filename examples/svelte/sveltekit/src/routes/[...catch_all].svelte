a48a20852e2e34f2e1f35836f13748a3fa3423e1
Sami Jaber <jabersami@gmail.com>
2022-07-20 10:42:50 -0400

    remove unused css

--- examples/svelte/sveltekit/src/routes/[...catch_all].svelte
+++ examples/svelte/sveltekit/src/routes/[...catch_all].svelte
@@ -30,19 +30,4 @@
   h1 {
     width: 100%;
   }
-
-  .welcome {
-    position: relative;
-    width: 100%;
-    height: 0;
-    padding: 0 0 calc(100% * 495 / 2048) 0;
-  }
-
-  .welcome img {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    display: block;
-  }
 </style>
