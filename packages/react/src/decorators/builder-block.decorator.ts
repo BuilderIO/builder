c30f4e0d9de0ff567bbf905e04329e1a231d313a
ck-tekton <33156025+charkour@users.noreply.github.com>
2022-07-25 14:59:20 -0400

    improve withBuilder HOC types

--- packages/react/src/decorators/builder-block.decorator.ts
+++ packages/react/src/decorators/builder-block.decorator.ts
@@ -1,9 +1,10 @@
-import { Builder, Component } from '@builder.io/sdk';
+import { Builder, Class, Component } from '@builder.io/sdk';
+import React from 'react';
 
 interface ReactComponent extends Component {}
 
-export function BuilderBlock(options: ReactComponent) {
+export function BuilderBlock<P extends object>(options: ReactComponent) {
   options.type = 'react';
 
-  return Builder.Component(options);
+  return Builder.Component(options) as (component: React.ComponentType<P>) => Class;
 }
--- packages/react/src/functions/with-builder.ts
+++ packages/react/src/functions/with-builder.ts
@@ -1,7 +1,8 @@
+import React from 'react';
 import { Component } from '@builder.io/sdk';
 import { BuilderBlock } from '../decorators/builder-block.decorator';
 
-export function withBuilder(component: Function, options: Component) {
-  BuilderBlock(options)(component as any);
+export function withBuilder<P extends object>(component: React.ComponentType<P>, options: Component) {
+  BuilderBlock<P>(options)(component);
   return component;
 }
