1495034995474be145713e8fefbe84e92bedb193
Sami Jaber <jabersami@gmail.com>
2022-08-23 11:57:10 -0400

    udpate

--- packages/sdks/e2e/nextjs-react/playwright.config.ts
+++ packages/sdks/e2e/nextjs-react/playwright.config.ts
@@ -1,4 +1,3 @@
-// import { configFor } from '@builder.io/sdks-e2e-tests/shared.config';
+import { configFor } from '@builder.io/sdks-e2e-tests/shared.config';
 
-// We exclude the nextjs test due to some annoying webpack limitation with symlinking the react SDK.
-// export default configFor('e2e-nextjs-react', 7509);
+export default configFor('e2e-nextjs-react', 7509);
