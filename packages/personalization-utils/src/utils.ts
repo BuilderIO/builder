dd1b8a268434486c86528087a8ad93e3750e6664
Aziz Abbas <aziz@builder.io>
2022-08-29 14:17:42 -0700

    default prefix fix

--- packages/personalization-utils/src/utils.ts
+++ packages/personalization-utils/src/utils.ts
@@ -1,12 +1,13 @@
 export const getUserAttributes = (
   attributes: Record<string, string>,
-  cookiePrefix = 'builder.userAttributes'
+  cookiePrefix?: string
 ) => {
+  const prefix = cookiePrefix || 'builder.userAttributes';
   return Object.keys(attributes)
-    .filter(key => key.startsWith(cookiePrefix))
+    .filter(key => key.startsWith(prefix))
     .reduce((acc, key) => {
       const value = attributes[key];
-      const sanitizedKey = key.split(`${cookiePrefix}.`)[1];
+      const sanitizedKey = key.split(`${prefix}.`)[1];
       return {
         ...acc,
         ...(typeof value === 'string' && { [sanitizedKey]: value }),
