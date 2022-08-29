2d3f81e4bcd96370bdc7edb3b920b51e81ef87d7
Aziz Abbas <aziz@builder.io>
2022-08-29 10:37:17 -0700

    use typesversinos

--- packages/personalization-utils/next.d.ts
+++
@@ -1,3 +0,0 @@
-import type { NextRequest } from 'next/server';
-export declare function getPersonlizedURL(request: NextRequest): import("next/dist/server/web/next-url").NextURL;
-export declare function getUserAttributesFromHash(hash: string): Record<string, string>;
