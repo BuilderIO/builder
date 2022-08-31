7f12c3792853fd6f1c83cb3eb9c27a39b25c72a6
Sami Jaber <jabersami@gmail.com>
2022-08-31 11:28:41 -0300

    add vite build steps for vue sdks

---
+++ packages/sdks/output/vue/vue3/vite.config.ts
@@ -0,0 +1,27 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      fileName: 'sdk',
+      formats: ['cjs', 'es'],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+});
