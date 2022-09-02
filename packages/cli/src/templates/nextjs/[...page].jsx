a513b9ba9155cb21581c7cdc91907b23bc59827b
Steve Sewell <steve@builder.io>
2022-09-02 15:13:39 -0700

    Updates

--- packages/cli/src/templates/nextjs/[...page].jsx
+++ packages/cli/src/templates/nextjs/[...page].jsx
@@ -124,5 +124,5 @@ Builder.registerComponent(MyCustomComponent, {
 // https://www.builder.io/c/docs/custom-components-visual-editor#:~:text=than%20this%20screenshot.-,organizing%20your%20components%20in%20custom%20sections,-You%20can%20create
 Builder.register('insertMenu', {
   name: 'My Components',
-  items: [{ item: 'ExampleCustomComponent', name: 'My Component' }],
+  items: [{ item: 'ExampleCustomComponent', name: 'My React Component' }],
 });
