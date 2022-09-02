a945135fa196c91d4ae7181ab5a4bad0a15b86a7
gustavocodess <gustavohgs13@gmail.com>
2022-09-02 00:53:38 -0300

    added util to get all content with nested refs

--- packages/utils/src/get-all-content-with-refs.ts
+++ packages/utils/src/get-all-content-with-refs.ts
@@ -1,4 +1,4 @@
-import { BuilderContent, BuilderElement, Builder, GetContentOptions, BehaviorSubject } from '@builder.io/sdk';
+import { BuilderContent, BuilderElement, Builder } from '@builder.io/sdk';
 import traverse from 'traverse';
 
 export const isBuilderElement = (item: unknown): item is BuilderElement => {
@@ -7,18 +7,16 @@ export const isBuilderElement = (item: unknown): item is BuilderElement => {
 
 async function resolveRefs(content: any, apiKey: string) {
   const promises: Promise<void>[] = [];
-  const patches: IJsonPatch[] = [];
+  const patches: unknown[] = [];
   const result = traverse(content).map(function (child) {
     const path = this.path.join('/');
     if (child && child['@type'] === '@builder.io/core:Reference') {
       if (child.model) {
         promises.push(
-          fetch(
-            `https://cdn.builder.io/api/v2/content/${child.model}/${child.id}?apiKey=${apiKey}`
-          )
+          fetch(`https://cdn.builder.io/api/v2/content/${child.model}/${child.id}?apiKey=${apiKey}`)
             .then(res => res.json())
-            .then(value => {
-              child.value = resolveRefs(value, apiKey);
+            .then(async value => {
+              child.value = await resolveRefs(value, apiKey);
               patches.push({
                 op: 'replace',
                 path: `/data/${path}/value`,
@@ -26,40 +24,21 @@ async function resolveRefs(content: any, apiKey: string) {
               });
             })
             .catch(err => {
-              console.error(
-                `Error fetching relationship ${child.id} (API key: ${apiKey}):`,
-                err
-              );
+              console.error(`Error fetching relationship ${child.id} (API key: ${apiKey}):`, err);
             })
         );
       }
     }
-
-    if (child && child['@type'] === '@builder.io/core:Request') {
-      promises.push(
-        fetch(child.request.url, { headers: child.request.headers || {} })
-          .then(res => res.json())
-          .then(value => {
-            child.data = value;
-            patches.push({
-              op: 'replace',
-              path: `/data/${path}/data`,
-              value,
-            });
-          })
-      );
-    }
   });
 
   await Promise.all(promises);
-  return { fullContent: result, patches };
+  return { content: result, patches };
 }
 
+export async function getContentWithAllReferences(builderInstance: Builder, modelName: string) {
+  const content: { results: BuilderContent[] } = await fetch(
+    `https://cdn.builder.io/api/v2/content/${modelName}?apiKey=${builderInstance.apiKey}`
+  ).then(res => res.json());
 
-
-export async function getContentWithAllReferences(builderInstance: Builder, modelName: string, options?: GetContentOptions) {
-
-  const content: BehaviorSubject<BuilderContent[], any> = await builderInstance.getContent(modelName, options)
-  
-  return await resolveRefs(content, builderInstance.apiKey!)
+  return await resolveRefs(content?.results, builderInstance.apiKey!);
 }
