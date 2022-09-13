04490bff70e52ae898571cf3f5a328e97abc1161
Misko Hevery <misko@hevery.com>
2022-09-13 14:40:13 -0700

    fixup! fix(qwik): dont pull in `node-fetch` in bundles

--- packages/sdks/src/functions/get-fetch.ts
+++ packages/sdks/src/functions/get-fetch.ts
@@ -4,7 +4,7 @@ export async function getFetch(): Promise<typeof global.fetch> {
   const globalFetch: typeof global.fetch = getGlobalThis().fetch;
 
   if (typeof globalFetch === 'undefined' && typeof global !== 'undefined') {
-    const nodeFetch = import(`node-fetch`).then((d) => d.default) as Promise<
+    const nodeFetch = import('node-fetch').then((d) => d.default) as Promise<
       typeof global.fetch
     >;
     return (nodeFetch as any).default || nodeFetch;
