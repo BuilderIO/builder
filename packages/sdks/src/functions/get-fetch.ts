deaec52b2bec8da7f9344b2c4f0c3d7c7ae3ac3d
Misko Hevery <misko@hevery.com>
2022-09-13 12:29:21 -0700

    fix(qwik): dont pull in `node-fetch` in bundles

--- packages/sdks/src/functions/get-fetch.ts
+++ packages/sdks/src/functions/get-fetch.ts
@@ -1,12 +1,21 @@
 import { getGlobalThis } from './get-global-this.js';
 
+/**
+ * The issue is that when building standalone bundle the bundler includes `node-fetch` as
+ * inlined content. Instead ind those environments we want to use the global `fetch` function
+ * and never include the `node-fetch` package.
+ *
+ * HACK: This hack is here to make sure that the bundler doesn't include `node-fetch` in browser bundle.
+ */
+const NODE_FETCH_URL = () => 'node-fetch';
+
 export async function getFetch(): Promise<typeof global.fetch> {
   const globalFetch: typeof global.fetch = getGlobalThis().fetch;
 
   if (typeof globalFetch === 'undefined' && typeof global !== 'undefined') {
-    const nodeFetch = import('node-fetch').then((d) => d.default) as Promise<
-      typeof global.fetch
-    >;
+    const nodeFetch = import(NODE_FETCH_URL()).then(
+      (d) => d.default
+    ) as Promise<typeof global.fetch>;
     return (nodeFetch as any).default || nodeFetch;
   }
 
