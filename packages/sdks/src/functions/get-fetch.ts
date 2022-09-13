de8f9885ada2a35dfa9f1103a9a65d70f76242e4
Misko Hevery <misko@hevery.com>
2022-09-13 14:40:13 -0700

    fixup! fix(qwik): dont pull in `node-fetch` in bundles

--- packages/sdks/src/functions/get-fetch.ts
+++ packages/sdks/src/functions/get-fetch.ts
@@ -1,24 +1,5 @@
 import { getGlobalThis } from './get-global-this.js';
 
-/**
- * `node-fetch` is a polyfill for fetch.
- *
- * There are two environments:
- * 1. Node - in which case we may need to fetch `node-fetch` and use it.
- * 2. Native (browser/deno/edge-workers) - in which case we can use the native
- *   `fetch` function.
- *
- * PROBLEM:
- * If we write `import('node-fetch')` the bundler will always bundle `node-fetch`
- * because it does not know if it is needed. This is not what we want, because:
- * - in native environments will now get `fetch` and the `node-fetch` polyfill is
- *   not needed.
- * - in Node environment bundling the `node-fetch` polyfill provides no benefit as
- *   we can just `require('node-fetch')` it.
- *
- * ENSURE: that the bundler does not bundle `node-fetch`.
- */
-
 let fetch: typeof global.fetch | undefined = undefined;
 
 export async function getFetch(): Promise<typeof global.fetch> {
