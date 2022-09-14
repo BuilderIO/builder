a55ebe84e275e97474576afc0aafd7705e908717
Sami Jaber <jabersami@gmail.com>
2022-09-14 12:32:39 -0400

    use getTargetPath option

--- packages/sdks/mitosis.config.js
+++ packages/sdks/mitosis.config.js
@@ -9,6 +9,26 @@ const getSeededId = () => {
 
 const isMitosisNode = (x) => x && x['@type'] === '@builder.io/mitosis/node';
 
+const kebabCase = (string) =>
+  string.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase();
+
+/**
+ * @type {import('@builder.io/mitosis'.MitosisConfig['getTargetPath'])}
+ */
+const getTargetPath = (target) => {
+  switch (target) {
+    // we have to workaround a name collision, where the folder can't have the name of the `exports` property in package.json.
+    // crazy, crazy stuff.
+    case 'vue2':
+      return 'vue/packages/_vue2';
+    case 'vue':
+    case 'vue3':
+      return 'vue/packages/_vue3';
+    default:
+      return kebabCase(target);
+  }
+};
+
 /**
  * @type {import('@builder.io/mitosis'.MitosisConfig['options']['vue'])}
  */
@@ -33,6 +53,7 @@ const SRCSET_PLUGIN = () => ({
 module.exports = {
   files: 'src/**',
   targets: ['reactNative', 'vue2', 'vue3', 'solid', 'svelte', 'react', 'qwik'],
+  getTargetPath,
   options: {
     vue2: vueConfig,
     vue3: vueConfig,
