7702693545d43cb5cffba0b6b44981b0b02d61e3
Sami Jaber <jabersami@gmail.com>
2022-09-14 17:19:10 -0400

    add builder to sveltekit example

---
+++ examples/svelte/sveltekit/src/routes/[...catchall]/+page.server.js
@@ -0,0 +1,20 @@
+import { getContent, getBuilderSearchParams } from '@builder.io/sdk-svelte';
+
+// TODO: enter your public API key
+const BUILDER_PUBLIC_API_KEY = 'f1a790f8c3204b3b8c5c1795aeac4660'; // ggignore
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load(event) {
+	console.log(event.url);
+	// fetch your Builder content
+	const content = await getContent({
+		model: 'page',
+		apiKey: BUILDER_PUBLIC_API_KEY,
+		options: getBuilderSearchParams(event.url.searchParams),
+		userAttributes: {
+			urlPath: event.url.pathname || '/'
+		}
+	});
+
+	return { content };
+}
---
+++ examples/svelte/sveltekit/src/routes/[...catchall]/+page.svelte
@@ -0,0 +1,78 @@
+<script>
+	import Counter from '../../lib/Counter.svelte';
+	import { isPreviewing, RenderContent } from '@builder.io/sdk-svelte';
+
+	// Create an array of your custom components and their properties
+	const CUSTOM_COMPONENTS = [
+		{
+			component: Counter,
+			name: 'Counter',
+			inputs: [
+				{
+					name: 'name',
+					type: 'string',
+					defaultValue: 'hello'
+				},
+				{
+					name: 'count',
+					type: 'number',
+					defaultValue: 0
+				}
+			]
+		}
+	];
+
+	// TODO: enter your public API key
+	const BUILDER_PUBLIC_API_KEY = 'f1a790f8c3204b3b8c5c1795aeac4660'; // ggignore
+
+	export let data;
+	const canShowContent = data.content || isPreviewing();
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<main>
+	<h1>Welcome to SvelteKit</h1>
+
+	<div>Below is your Builder Content:</div>
+	{#if canShowContent}
+		<div>page Title: {data.content?.data?.title || 'Unpublished'}</div>
+		<!-- Render builder content with all required props -->
+		<RenderContent
+			model="page"
+			content={data.content}
+			apiKey={BUILDER_PUBLIC_API_KEY}
+			customComponents={CUSTOM_COMPONENTS}
+		/>
+	{:else}
+		Content Not Found
+	{/if}
+</main>
+
+<footer>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</footer>
+
+<style>
+	h1 {
+		width: 100%;
+		font-size: 2rem;
+		text-align: center;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
