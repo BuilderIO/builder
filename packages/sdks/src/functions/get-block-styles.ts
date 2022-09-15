3a6615db26fbdeddc99714ca22bfbaa236d7aa6f
Sami Jaber <jabersami@gmail.com>
2022-09-15 17:42:15 -0400

    fix react-native styles

--- packages/sdks/src/functions/get-block-styles.ts
+++ packages/sdks/src/functions/get-block-styles.ts
@@ -1,27 +1,50 @@
 import { getMaxWidthQueryForSize } from '../constants/device-sizes.js';
+import { TARGET } from '../constants/target.js';
 import { BuilderBlock } from '../types/builder-block.js';
 import { convertStyleObject } from './convert-style-object.js';
 import { sanitizeBlockStyles } from './sanitize-styles.js';
 
-export function getBlockStyles(block: BuilderBlock) {
-  const styles = {
-    ...convertStyleObject(block.responsiveStyles?.large),
-    /**
-     * TO-DO: remove?
-     */
-    ...(block as any).styles,
-  };
+const getStyleForTarget = (
+  styles: NonNullable<BuilderBlock['responsiveStyles']>
+) => {
+  switch (TARGET) {
+    case 'reactNative': {
+      return {
+        ...(styles.large ? convertStyleObject(styles.large) : {}),
+        ...(styles.small ? convertStyleObject(styles.small) : {}),
+      };
+    }
+    default:
+      return {
+        ...(styles.large ? convertStyleObject(styles.large) : {}),
 
-  if (block.responsiveStyles?.medium) {
-    styles[getMaxWidthQueryForSize('medium')] = convertStyleObject(
-      block.responsiveStyles?.medium
-    );
+        ...(styles.medium
+          ? {
+              [getMaxWidthQueryForSize('medium')]: convertStyleObject(
+                styles.medium
+              ),
+            }
+          : {}),
+
+        ...(styles.small
+          ? {
+              [getMaxWidthQueryForSize('small')]: convertStyleObject(
+                styles.small
+              ),
+            }
+          : {}),
+      };
   }
-  if (block.responsiveStyles?.small) {
-    styles[getMaxWidthQueryForSize('small')] = convertStyleObject(
-      block.responsiveStyles?.small
-    );
+};
+
+export function getBlockStyles(block: BuilderBlock) {
+  if (!block.responsiveStyles) {
+    return {};
   }
+
+  const styles = getStyleForTarget(block.responsiveStyles);
+
+  console.log('rn styles', styles);
 
   sanitizeBlockStyles(styles);
 
