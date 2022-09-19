fd8c0ec2c45801f17296cdf146424e3ac4ec85ff
gustavocodess <gustavohgs13@gmail.com>
2022-09-19 13:11:24 -0300

    adding options to query

--- packages/utils/src/get-all-content-with-refs.ts
+++ packages/utils/src/get-all-content-with-refs.ts
@@ -1,19 +1,21 @@
-import { BuilderContent, BuilderElement, Builder } from '@builder.io/sdk';
+import { BuilderContent, BuilderElement, Builder, GetContentOptions } from '@builder.io/sdk';
 import traverse from 'traverse';
 
 export const isBuilderElement = (item: unknown): item is BuilderElement => {
   return Boolean((item as any)?.['@type'] === '@builder.io/sdk:Element');
 };
 
-async function resolveRefs(content: any, apiKey: string, builderInstance: Builder) {
+async function resolveRefs(content: any, builderInstance: Builder, options?: GetContentOptions) {
   const promises: Promise<void>[] = [];
   const result = traverse(content).map(function (child) {
     if (child && child['@type'] === '@builder.io/core:Reference') {
       if (child.model) {
         promises.push(
-          builderInstance.getAll(child.model, { query: { id: child.id } }).then(async value => {
-            child.value = await resolveRefs(value, apiKey, builderInstance);
-          })
+          builderInstance
+            .getAll(child.model, { options: options || { query: { id: child.id } } })
+            .then(async value => {
+              child.value = await resolveRefs(value, builderInstance);
+            })
         );
       }
     }
@@ -23,10 +25,12 @@ async function resolveRefs(content: any, apiKey: string, builderInstance: Builde
   return result;
 }
 
-export async function getContentWithAllReferences(builderInstance: Builder, modelName: string) {
-  const content: BuilderContent[] = await builderInstance.getAll(modelName, {
-    options: { noTargeting: true },
-  });
+export async function getContentWithAllReferences(
+  builderInstance: Builder,
+  modelName: string,
+  options: GetContentOptions
+) {
+  const content: BuilderContent[] = await builderInstance.getAll(modelName, options);
 
-  return await resolveRefs(content, builderInstance.apiKey!, builderInstance);
+  return await resolveRefs(content, builderInstance, options);
 }
