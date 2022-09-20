c40a1b344104582ec0c3713af7103fbdbb523ba7
Sami Jaber <jabersami@gmail.com>
2022-09-20 17:34:14 -0400

    revert changes

--- packages/sdks/e2e/vue-3/src/DynamicallyRenderBuilderPage.vue
+++ packages/sdks/e2e/vue-3/src/DynamicallyRenderBuilderPage.vue
@@ -1,11 +1,10 @@
-vue3-debug
 <template>
   <div v-if="content">
     <builder-render-content model="page" :content="content" />
   </div>
   <div v-else>Content not Found</div>
 </template>
-<script>
+<script lang="ts">
 import { RenderContent } from '@builder.io/sdk-vue/vue3';
 import { getContentForPathname } from '@builder.io/sdks-e2e-tests/specs';
 
@@ -15,7 +14,7 @@ export default {
     'builder-render-content': RenderContent,
   },
   computed: {
-    content() {
+    content(): any {
       return getContentForPathname();
     },
   },
