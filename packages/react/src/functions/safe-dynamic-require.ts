fab7bac4baeb3619b210fad37d2a30db66b41e44
Murtaza Haveliwala <murtaza@builder.io>
2022-09-23 18:15:40 +0530

    Fixing default assingment for safeDynamicRequire

--- packages/react/src/functions/safe-dynamic-require.ts
+++ packages/react/src/functions/safe-dynamic-require.ts
@@ -6,4 +6,7 @@ const noop = () => null;
 // Allow us to require things dynamically safe from webpack bundling
 
 export let safeDynamicRequire: typeof require = noop as any;
-if (typeof globalThis.require === 'function') safeDynamicRequire = eval('require');
+if (typeof globalThis.require === 'function') {
+  safeDynamicRequire = eval('require');
+}
+safeDynamicRequire = safeDynamicRequire ?? (noop as any);
