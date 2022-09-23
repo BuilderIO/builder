8f0728908f0aee4489a4a442e2a9c54c0d88928e
kibo-sushant <sushant.jadhav@blueconchtech.com>
2022-09-23 14:26:02 +0530

    feat: create BuilderIO plugin

---
+++ plugins/kibo-commerce/src/plugin.jsx
@@ -0,0 +1,130 @@
+import { registerCommercePlugin } from '@builder.io/commerce-plugin-tools';
+import pkg from '../package.json';
+import { KiboCommerce } from '../utils/kibocommerce';
+
+registerCommercePlugin(
+  {
+    id: pkg.name,
+    name: 'KiboCommerce',
+    settings: [
+      {
+        name: 'apiHost',
+        type: 'string',
+        required: true,
+      },
+      {
+        name: 'authHost',
+        type: 'string',
+        required: true,
+      },
+      {
+        name: 'sharedSecret',
+        type: 'string',
+        required: true,
+      },
+      {
+        name: 'clientId',
+        type: 'string',
+        required: true,
+      },
+    ],
+    ctaText: `Connect your KiboCommerce store`,
+  },
+  settings => {
+    const apiHost = settings.get('apiHost');
+    const authHost = settings.get('authHost');
+    const sharedSecret = settings.get('sharedSecret');
+    const clientId = settings.get('clientId');
+
+    const config = { apiHost, authHost, sharedSecret, clientId };
+    const kiboClient = new KiboCommerce(config, {});
+    const PAGE_SIZE = 16;
+
+    const transformResource = resource => ({
+      ...resource,
+      id: resource?.productCode,
+      title: resource?.content?.productName,
+      handle: resource?.productCode,
+      image: {
+        src: resource?.content?.productImages[0]?.imageUrl,
+      },
+    });
+
+    const service = {
+      product: {
+        async findById(productCode) {
+          const products = await kiboClient.getItemsByProductCode(PAGE_SIZE, [productCode]);
+          const transformedProduct = transformResource(products[0]);
+
+          return transformedProduct;
+        },
+
+        async search(searchTerm) {
+          const searchOptions = {
+            query: searchTerm,
+            filter: '',
+            pageSize: PAGE_SIZE,
+            startIndex: 1,
+          };
+
+          let products = await kiboClient.performSearch(searchOptions);
+          const transformedProducts = products.items?.map(transformResource);
+
+          console.log(`---searchTerm: ${searchTerm}, res: ${JSON.stringify(transformedProducts)}`);
+          return transformedProducts;
+        },
+
+        getRequestObject(productCode) {
+          return {
+            '@type': '@builder.io/core:Request',
+            request: {
+              url: `https://api.kibocommerce.com/products/${productCode}`,
+            },
+            options: {
+              product: productCode,
+            },
+          };
+        },
+      },
+      category: {
+        async findById(productCode) {
+          const products = await kiboClient.getItemsByProductCode(PAGE_SIZE, [productCode]);
+          const transformedProduct = transformResource(products[0]);
+
+          return transformedProduct;
+        },
+
+        async search(searchTerm) {
+          const searchOptions = {
+            query: searchTerm,
+            filter: '',
+            pageSize: PAGE_SIZE,
+            startIndex: 1,
+          };
+
+          let categories = await kiboClient.performSearch(searchOptions);
+          const transformedCategories = categories.items?.map(transformResource);
+
+          console.log(
+            `---searchTerm: ${searchTerm}, res: ${JSON.stringify(transformedCategories)}`
+          );
+          return transformedCategories;
+        },
+
+        getRequestObject(categoryCode) {
+          return {
+            '@type': '@builder.io/core:Request',
+            request: {
+              url: `https://api.kibocommerce.com/category/${categoryCode}`,
+            },
+            options: {
+              category: categoryCode,
+            },
+          };
+        },
+      },
+    };
+
+    return service;
+  }
+);
---
+++ plugins/kibo-commerce/utils/kibocommerce.js
@@ -0,0 +1,181 @@
+import currency from 'currency.js';
+
+export const productSearchQuery = /* GraphQL */ `
+  query Search($query: String, $filter: String, $pageSize: Int, $startIndex: Int) {
+    productSearch(query: $query, filter: $filter, pageSize: $pageSize, startIndex: $startIndex) {
+      items {
+        productCode
+        content {
+          productName
+          productShortDescription
+          productImages {
+            imageUrl
+          }
+        }
+        price {
+          price
+        }
+      }
+      totalCount
+      pageSize
+      startIndex
+    }
+  }
+`;
+
+export class APIAuthenticationHelper {
+  _clientId;
+  _sharedSecret;
+  _authHost;
+  _cacheKey;
+  _cache;
+  authData;
+
+  constructor(clientId, sharedSecret, authHost, cacheKey, cache) {
+    this._clientId = clientId;
+    this._sharedSecret = sharedSecret;
+    this._authHost = authHost;
+    this._cacheKey = cacheKey;
+    this._cache = cache;
+    let authTicket = undefined;
+    try {
+      let serializedAuth = cache.get(cacheKey);
+      if (serializedAuth) {
+        authTicket = JSON.parse(serializedAuth);
+      }
+    } catch (error) {}
+    this.authData = authTicket;
+  }
+
+  _buildFetchOptions(data) {
+    return {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    };
+  }
+
+  _calculateTicketExpiration(kiboAuthTicket) {
+    //calculate how many milliseconds until auth expires
+    const millisecsUntilExpiration = kiboAuthTicket.expires_in * 1000;
+    kiboAuthTicket.expires_at = Date.now() + millisecsUntilExpiration;
+    return kiboAuthTicket;
+  }
+
+  async _getAuthTicket() {
+    return this.authData;
+  }
+
+  async _setAuthTicket(kiboAuthTicket) {
+    try {
+      this._cache.set(this._cacheKey, JSON.stringify(kiboAuthTicket));
+    } catch (error) {}
+    this.authData = { ...kiboAuthTicket };
+  }
+
+  async authenticate() {
+    // create oauth fetch options
+    const options = this._buildFetchOptions({
+      client_id: this._clientId,
+      client_secret: this._sharedSecret,
+      grant_type: 'client_credentials',
+    });
+    // perform authentication
+    const authTicket = await (
+      await fetch(`https://${this._authHost}/api/platform/applications/authtickets/oauth`, options)
+    ).json();
+    // set expiration time in ms on auth ticket
+    this._calculateTicketExpiration(authTicket);
+    // set authentication ticket on next server runtime object
+    await this._setAuthTicket(authTicket);
+
+    return authTicket;
+  }
+
+  async refreshTicket(kiboAuthTicket) {
+    // create oauth refresh fetch options
+    const options = this._buildFetchOptions({
+      client_id: this._clientId,
+      client_secret: this._sharedSecret,
+      grant_type: 'client_credentials',
+      refresh_token: kiboAuthTicket && kiboAuthTicket.refresh_token,
+    });
+    const refreshedTicket = await (
+      await fetch(`https://${this._authHost}/api/platform/applications/authtickets/oauth`, options)
+    ).json();
+
+    this._calculateTicketExpiration(refreshedTicket);
+
+    await this._setAuthTicket(refreshedTicket);
+
+    return refreshedTicket;
+  }
+
+  async getAccessToken() {
+    // get current Kibo API auth ticket
+
+    let authTicket = await this._getAuthTicket();
+    // if no current ticket, perform auth
+    // or if ticket expired, refresh auth
+    if (!authTicket) {
+      authTicket = await this.authenticate();
+    } else if (authTicket.expires_at < Date.now()) {
+      authTicket = await this.refreshTicket(authTicket);
+    }
+
+    return authTicket.access_token;
+  }
+}
+
+export class KiboCommerce {
+  graphQLUrl;
+  authHelper;
+  constructor({ apiHost, authHost, clientId, sharedSecret, applicationId }, cache) {
+    this.graphQLUrl = `https://${apiHost}/graphql`;
+    this.authHelper = new APIAuthenticationHelper(
+      clientId || applicationId,
+      sharedSecret,
+      authHost,
+      `${apiHost}_auth`,
+      cache
+    );
+  }
+
+  async getAccessToken() {
+    return await this.authHelper.getAccessToken();
+  }
+
+  async getHeaders() {
+    const authToken = await this.getAccessToken();
+    return {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        'Content-Type': 'application/json',
+      },
+    };
+  }
+
+  async performSearch({ query, filter, pageSize, startIndex }) {
+    const headers = await this.getHeaders();
+    const body = {
+      query: productSearchQuery,
+      variables: { query, filter, pageSize, startIndex },
+    };
+    const params = { method: 'POST', ...headers, body: JSON.stringify(body) };
+    const request = await fetch(this.graphQLUrl, params);
+    const response = await request.json();
+    return response.data.productSearch;
+  }
+
+  async getItemsByProductCode(pageSize, items = []) {
+    const filter = items.map(productCode => `productCode eq ${productCode}`).join(' or ');
+    try {
+      const result = await this.performSearch({ filter, pageSize });
+      return result.items;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
