66f8ac48ae284ee180232ba807d06a898fbb5831
shyam-builder <shyam@builder.io>
2022-09-27 18:45:21 +0530

    Fix bug where SSR was not working for data binding

--- packages/react/src/functions/safe-dynamic-require.ts
+++ packages/react/src/functions/safe-dynamic-require.ts
@@ -6,5 +6,5 @@ const noop = () => null;
 // Allow us to require things dynamically safe from webpack bundling
 
 export let safeDynamicRequire: typeof require;
-if (typeof globalThis.require === 'function') safeDynamicRequire = eval('require');
+if (Builder.isServer) safeDynamicRequire = eval('require');
 safeDynamicRequire ??= noop as any;
