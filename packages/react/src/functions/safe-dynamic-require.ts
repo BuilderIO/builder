a7b84ccf779fa4b34f45df321733e11240adc053
shyam-builder <shyam@builder.io>
2022-09-30 18:21:27 +0530

    Fix prettier

--- packages/react/src/functions/safe-dynamic-require.ts
+++ packages/react/src/functions/safe-dynamic-require.ts
@@ -5,26 +5,26 @@ import { Builder } from '@builder.io/sdk';
 const noop = () => null;
 // Allow us to require things dynamically safe from webpack bundling
 
-export let safeDynamicRequire : typeof require;
+export let safeDynamicRequire: typeof require;
 
 /*
- * The if condition below used to be 
- * 
+ * The if condition below used to be
+ *
  *     if (typeof globalThis.require === "function")
- * 
+ *
  * That broke in case Builder was running on the server (Next, SSR use-cases) where
  * globalThis.require was undefined. Avoiding use of Builder.isServer for Cloudflare worker cases
- * That said, if change it to 
- * 
+ * That said, if change it to
+ *
  * if (typeof require === 'function') {
  *   localSafeDynamicRequire = eval('require');
  * }
- * 
+ *
  * Then the TSC / rollup compiler over-optimizes and replaces the if condition with true always
  * causing it to blow up on the client side. Hence this convoluted way of doing it.
- * 
+ *
  * In Summary:
- * 
+ *
  * 1. Node -> globalThis.require does not work
  * 2. Client -> typeof require === 'function' does not work because of overoptimization by the compiler
  * 3. Cloudflare edge -> only globalThis.require works
@@ -34,4 +34,3 @@ if (typeof globalThis.require === 'function' || Builder.isServer) {
 }
 
 safeDynamicRequire ??= noop as any;
-
