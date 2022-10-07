8533d8f9c86cb5a9f86ddf982a792ab61207e7a1
Steve Sewell <steve@builder.io>
2022-10-07 14:34:01 -0700

    README

--- packages/swift/src/Shared/ContentView.swift
+++ packages/swift/src/Shared/ContentView.swift
@@ -3,42 +3,28 @@ import SwiftyJSON
 
 
 struct ContentView: View {
+    @State var content: BuilderContent? = nil
+    
     init() {
-        initContent()
-        
+        fetchContent()
     }
     
-    func initContent() {
-        testing.getContent(model: "page", apiKey: "e084484c0e0241579f01abba29d9be10", url: "/") { content in
-            self.content.value = content
+    func fetchContent() {
+        getContent(model: "page", apiKey: "e084484c0e0241579f01abba29d9be10", url: "/") { content in
+            self.content = content
         }
     }
-    
-    @ObservedObject var content = ContentTracker()
-    
-    
-    func getContent() -> BuilderContent? {
-        if content.value != nil {
-            return content.value
-        }
-        
-        return nil
-    }
-    
     
     var body: some View {
         ScrollView {
-            RenderContent(content: $content.value.wrappedValue!)
+            RenderContent(content: $content.wrappedValue!)
         }
         Button("Reload") {
-            initContent()
+            fetchContent()
         }
     }
 }
 
-final class ContentTracker: ObservableObject {
-    @Published var value: BuilderContent?;
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
