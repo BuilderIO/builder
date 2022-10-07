b49c4f766a1310f390bbbb784102b7a52dbbd99c
Steve Sewell <steve@builder.io>
2022-10-07 14:20:28 -0700

    cleanup

--- packages/swift/src/Shared/ContentView.swift
+++ packages/swift/src/Shared/ContentView.swift
@@ -1,89 +1,25 @@
 import SwiftUI
 import SwiftyJSON
-import FirebaseFirestoreSwift
-import FirebaseFirestore
 
-
-let debug = true
 
 struct ContentView: View {
-    let isEditing = true
-    
-    @State private var firestoreSubscription: ListenerRegistration?
-    
     init() {
-        if (isEditing) {
-            initFirestore()
-        } else {
-            initContent()
-        }
+        initContent()
         
     }
     
     func initContent() {
-        testing.getContent(model: "page", apiKey: "7ff1b55f7ecb4f08a012fbb2a859aced", url: "/") { content in
-            self.overrideContent.content = content
+        testing.getContent(model: "page", apiKey: "e084484c0e0241579f01abba29d9be10", url: "/") { content in
+            self.content.value = content
         }
     }
     
-    @ObservedObject var overrideContent = ContentTracker()
-    
-    @State var lastScreenshot: String? = nil;
-    
-    func initFirestore() {
-        let collectionName = "components"
-        // TODO: hardoded only for now
-        let docId = "00df1822dbdf48d18a1fdef36d98a315"
-        
-        let collection = testingApp.db.collection(collectionName)
-        let doc = collection.document(docId)
-        firestoreSubscription = doc.addSnapshotListener { documentSnapshot, error in
-            do {
-                guard let document = documentSnapshot else {
-                    print("Error fetching document: \(error!)")
-                    return
-                }
-                guard let data = try document.data(as: BuilderContent.self) else {
-                    print("Document data was empty.")
-                    return
-                }
-                
-                overrideContent.content = data
-
-                print("Got snapshot")
-
-            } catch {
-                print("Error decoding firestore data \(error)")
-            }
-        }
-
-        
-        func convertImageToBase64String (img: UIImage) -> String {
-            return img.jpegData(compressionQuality: 1)?.base64EncodedString() ?? ""
-        }
-    }
-    
-    func takeScreenshot() -> String? {
-        var screenshotImage :UIImage?
-        let layer = UIApplication.shared.keyWindow!.layer
-        let scale = UIScreen.main.scale
-        UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
-        guard let context = UIGraphicsGetCurrentContext() else {return nil}
-        layer.render(in:context)
-        screenshotImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return convertImageToBase64String(screenshotImage!)
-    }
-    
-    
-    func convertImageToBase64String (_ img: UIImage) -> String {
-        return img.jpegData(compressionQuality: 1)?.base64EncodedString() ?? ""
-    }
+    @ObservedObject var content = ContentTracker()
     
     
     func getContent() -> BuilderContent? {
-        if overrideContent.content != nil {
-            return overrideContent.content
+        if content.value != nil {
+            return content.value
         }
         
         return nil
@@ -92,31 +28,16 @@ struct ContentView: View {
     
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading) {
-                if $overrideContent.content.wrappedValue != nil {
-                    RenderContent(content: $overrideContent.content.wrappedValue!)
-                } else {
-                    let content = getContent()
-                    if content != nil {
-                        RenderContent(content: content!)
-                    } else {
-                        let _ = print("Content is nil")
-                    }
-                }
-            }
-        }.onDisappear {
-            firestoreSubscription?.remove()
+            RenderContent(content: $content.value.wrappedValue!)
         }
-        if debug && !isEditing {
-            Button("Reload") {
-                initContent()
-            }
+        Button("Reload") {
+            initContent()
         }
     }
 }
 
 final class ContentTracker: ObservableObject {
-    @Published var content: BuilderContent?;
+    @Published var value: BuilderContent?;
 }
 
 struct ContentView_Previews: PreviewProvider {
