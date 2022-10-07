7bc1bc150ec57c49d66f2cf050e533b913e928a6
Steve Sewell <steve@builder.io>
2022-10-07 10:52:05 -0700

    fix

--- packages/sdks/overrides/rsc/src/components/render-content/builder-editing.client.tsx
+++ packages/sdks/overrides/rsc/src/components/render-content/builder-editing.client.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { setupBrowserForEditing } from '../../scripts/init-editing.js';
 
 export type BuilderEditingProps = {
   model: string;
@@ -8,85 +9,6 @@ export type BuilderEditingProps = {
 };
 
 let browserSetup = false;
-
-export const setupBrowserForEditing = () => {
-  if (browserSetup) {
-    return;
-  }
-
-  browserSetup = true;
-  window.parent?.postMessage(
-    {
-      type: 'builder.sdkInfo',
-      data: {
-        target: 'reactServerComponents',
-        // TODO: compile these in
-        // type: process.env.SDK_TYPE,
-        // version: process.env.SDK_VERSION,
-        supportsPatchUpdates: false,
-        // Supports builder-model="..." attribute which is needed to
-        // scope our '+ add block' button styling
-        supportsAddBlockScoping: false,
-      },
-    },
-    '*'
-  );
-
-  window.addEventListener('message', ({ data }) => {
-    if (data) {
-      switch (data.type) {
-        case 'builder.evaluate': {
-          const text = data.data.text;
-          const args = data.data.arguments || [];
-          const id = data.data.id;
-          // tslint:disable-next-line:no-function-constructor-with-string-args
-          const fn = new Function(text);
-          let result: any;
-          let error: Error | null = null;
-          try {
-            // eslint-disable-next-line prefer-spread
-            result = fn.apply(null, args);
-          } catch (err) {
-            error = err as Error;
-          }
-
-          if (error) {
-            window.parent?.postMessage(
-              {
-                type: 'builder.evaluateError',
-                data: { id, error: error.message },
-              },
-              '*'
-            );
-          } else {
-            if (result && typeof result.then === 'function') {
-              (result as Promise<any>)
-                .then((finalResult) => {
-                  window.parent?.postMessage(
-                    {
-                      type: 'builder.evaluateResult',
-                      data: { id, result: finalResult },
-                    },
-                    '*'
-                  );
-                })
-                .catch(console.error);
-            } else {
-              window.parent?.postMessage(
-                {
-                  type: 'builder.evaluateResult',
-                  data: { result, id },
-                },
-                '*'
-              );
-            }
-          }
-          break;
-        }
-      }
-    }
-  });
-};
 
 export default function BuilderEditing(props: BuilderEditingProps) {
   useEffect(() => {
