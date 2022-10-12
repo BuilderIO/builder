234a005b331e97477eb459f4cf3a6d3a3b8a8444
Sami Jaber <jabersami@gmail.com>
2022-10-12 11:47:25 -0400

    remove extraneous styles for non-react-native SDKs

--- packages/sdks/src/functions/sanitize-styles.ts
+++
@@ -1,6 +0,0 @@
-/**
- * No-op function, sanitize on a per-framework basis by overriding.
- */
-export const sanitizeBlockStyles = (
-  styles: Partial<CSSStyleDeclaration>
-): Partial<CSSStyleDeclaration> => styles;
