4d60be6c6772d16f195857c2a76013579239fca4
Sami Jaber <jabersami@gmail.com>
2022-10-17 10:55:54 -0400

    dedupe fn

--- packages/sdks/src/functions/register-component.ts
+++ packages/sdks/src/functions/register-component.ts
@@ -1,3 +1,4 @@
+import { serializeFn } from '../blocks/util.js';
 import type { RegisteredComponent } from '../context/types.js';
 import type { ComponentInfo, Input } from '../types/components.js';
 import { fastClone } from './fast-clone.js';
@@ -30,24 +31,6 @@ export const createRegisterComponentMessage = ({
 
 const serializeValue = (value: object): any =>
   typeof value === 'function' ? serializeFn(value) : fastClone(value);
-
-/**
- * Input attributes that are functions must be converted to strings before being serialized to JSON.
- */
-// eslint-disable-next-line @typescript-eslint/ban-types
-const serializeFn = (fnValue: Function) => {
-  const fnStr = fnValue.toString().trim();
-  // we need to account for a few different fn syntaxes:
-  // 1. `function name(args) => {code}`
-  // 2. `name(args) => {code}`
-  // 3. `(args) => {}`
-  const appendFunction =
-    !fnStr.startsWith('function') && !fnStr.startsWith('(');
-
-  return `return (${
-    appendFunction ? 'function ' : ''
-  }${fnStr}).apply(this, arguments)`;
-};
 
 const prepareComponentInfoToSend = ({
   inputs,
