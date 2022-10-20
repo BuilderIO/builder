013677c0225a334b3b1172be65edb326aa3d2007
Sami Jaber <jabersami@gmail.com>
2022-10-20 17:01:50 -0400

    fix svelte config

--- examples/svelte/svelte-vite/vite.config.ts
+++ examples/svelte/svelte-vite/vite.config.ts
@@ -3,7 +3,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte({
+      emitCss: false
+    })
+  ],
   server: {
     port: 3000
   }
