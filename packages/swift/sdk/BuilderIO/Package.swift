bdda5538d18937a949c0096f471a889796d610a9
Krishna Vivek <krishna.vivek@esmagico.in>
2022-10-26 23:38:44 +0100

    Refinements

--- packages/swift/sdk/BuilderIO/Package.swift
+++ packages/swift/sdk/BuilderIO/Package.swift
@@ -13,16 +13,13 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BuilderIO",
-            dependencies: []),
-        .testTarget(
-            name: "BuilderIOTests",
-            dependencies: ["BuilderIO"]),
+            dependencies: ["SwiftyJSON"])
     ]
 )
