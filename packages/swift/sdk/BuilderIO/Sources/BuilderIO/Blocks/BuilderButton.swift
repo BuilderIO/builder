c63cb186e09d4da2c71ec074796e01aec7adbaad
Krishna Vivek <krishna.vivek@esmagico.in>
2022-10-31 04:03:17 +0000

    Added padding, background, foreground, corner radius styles to button

--- packages/swift/sdk/BuilderIO/Sources/BuilderIO/Blocks/BuilderButton.swift
+++ packages/swift/sdk/BuilderIO/Sources/BuilderIO/Blocks/BuilderButton.swift
@@ -17,26 +17,42 @@ struct BuilderButton: View {
     
     var body: some View {        
         HStack(spacing: 0) {
-            Button(getTextWithoutHtml(text)) {print(getTextWithoutHtml(text))}
-        }.frame(maxWidth: .infinity)
+            Button(getTextWithoutHtml(text)) {
+                print(getTextWithoutHtml(text))
+            }
+            .background(Color.init(hex: "3898EC"))
+            .foregroundColor(Color.white)
+            .padding(EdgeInsets(top: 15, leading: 25, bottom: 15, trailing: 25))
+            .multilineTextAlignment(.center)
+        }
+        .frame(maxWidth: .infinity)
     }
 }
 
-/*
- import SwiftUI
+@available(iOS 13.0, *)
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
 
- struct ContentView: View {
-     var body: some View {
-         VStack {
-             Text("By tapping Done, you agree to the ")
-             HStack(spacing: 0) {
-                 Button("privacy policy") {}
-                 Text(" and ")
-                 Button("terms of service") {}
-                 Text(".")
-             }
-         }
-     }
- }
-
- */
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
+}
