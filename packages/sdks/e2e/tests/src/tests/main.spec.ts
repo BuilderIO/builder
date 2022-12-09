fb47e6aad00c17eb24e752f9498497f09b16640e
Sami Jaber <jabersami@gmail.com>
2022-12-09 12:09:00 -0400

    fix test

--- packages/sdks/e2e/tests/src/tests/main.spec.ts
+++ packages/sdks/e2e/tests/src/tests/main.spec.ts
@@ -97,10 +97,21 @@ test.describe(targetContext.name, () => {
         height: '120.797px',
         'object-fit': 'contain',
       },
-      {
-        width: '1880px',
-        height: '1245px',
-      },
+      // There is currently a discrepancy, where RN will hardcode width & height props on the image when provided,
+      // whereas the web SDKs will not. Not sure which behavior is more correct.
+      ...(process.env.SDK === 'reactNative'
+        ? [
+            {
+              width: '1880px',
+              height: '1245px',
+            },
+          ]
+        : [
+            {
+              width: '1203.200px',
+              height: '796.797px',
+            },
+          ]),
     ];
 
     await expect(imageLocator).toHaveCount(expected.length);
