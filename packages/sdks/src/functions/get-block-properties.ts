0f8230687f480839db6cebf50a7090bbee20ff4d
Miško Hevery <misko@hevery.com>
2022-12-21 11:42:48 -0800

    fixup! fix: Support bindings to style attribute

--- packages/sdks/src/functions/get-block-properties.ts
+++ packages/sdks/src/functions/get-block-properties.ts
@@ -4,9 +4,22 @@ export function getBlockProperties(block: BuilderBlock) {
   return {
     ...block.properties,
     'builder-id': block.id,
-    style: block.style,
+    style: toStyleAttribute(block.style),
     class: [block.id, 'builder-block', block.class, block.properties?.class]
       .filter(Boolean)
       .join(' '),
   };
+}
+/**
+ * Svelte does not support style attribute as an object so we need to flatten it.
+ */
+function toStyleAttribute(
+  style: Partial<CSSStyleDeclaration> | undefined
+): string | undefined {
+  if (!style) return style;
+  let styleValue = '';
+  Object.keys(style).forEach((key) => {
+    styleValue += `${key}: ${(style as any)[key]};`;
+  });
+  return styleValue;
 }
