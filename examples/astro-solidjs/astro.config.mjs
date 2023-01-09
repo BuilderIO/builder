3592d2a3341aca0a62f556877e29167a457edb60
Sami Jaber <jabersami@gmail.com>
2023-01-09 17:46:59 -0400

    update snapshot

--- examples/astro-solidjs/astro.config.mjs
+++ examples/astro-solidjs/astro.config.mjs
@@ -19,7 +19,6 @@ export default defineConfig({
     optimizeDeps: {
       // sub-dependencies of our sym-linked package must be manually included here
       include: ['solid-styled-components'],
-      exclude: ['node-fetch'],
     },
     resolve: {
       // we must preserve symlinks for our sym-linked package to work properly
--- examples/svelte/sveltekit/src/apiKey.js
+++ examples/svelte/sveltekit/src/apiKey.js
@@ -1,2 +1,2 @@
 // TODO: enter your public API key
-export const BUILDER_PUBLIC_API_KEY = 'f1a790f8c3204b3b8c5c1795aeac4660'; // ggignore
+export const BUILDER_PUBLIC_API_KEY = 'fecd818f2cbd40ddb536960535f74d21'; // ggignore
