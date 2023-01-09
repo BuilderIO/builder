a18d6fc5f8ee60e4f631d9900e299c515e502863
Sami Jaber <jabersami@gmail.com>
2023-01-09 18:47:48 -0400

    setup minor release script, add .nmprc

--- packages/sdks/scripts/release-sdk.sh
+++ packages/sdks/scripts/release-sdk.sh
@@ -1,4 +1,4 @@
 # doing this for each SDK is getting tedious, and I'm lazy.
-VERSION=${2:-'patch'}    
+VERSION=${2:-'minor'}    
 echo "releasing $VERSION version of SDK $1";
 cd "output/$1" && npm run release:$VERSION && echo "released $1";
