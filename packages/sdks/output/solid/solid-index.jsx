e4d5c230d63dc52b485ddc0d19a89848e1f65fe7
Sami Jaber <jabersami@gmail.com>
2023-01-13 17:02:58 -0400

    remove solid-index hack

--- packages/sdks/output/solid/solid-index.jsx
+++
@@ -1,5 +0,0 @@
-/**
- * We need to have a `.jsx` entry point for our SolidJS SDK. To avoid enforcing this to all other frameworks,
- * we add this file and use it to re-export the index.js file from the Mitosis-generated code.
- */
-export * from './src/index.js';
