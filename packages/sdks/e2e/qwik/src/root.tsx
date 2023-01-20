537fe39c51cd746db9c500a4f8ef0f828dc542cc
Sami Jaber <jabersami@gmail.com>
2023-01-20 17:41:06 -0400

    remove

--- packages/sdks/e2e/qwik/src/root.tsx
+++ packages/sdks/e2e/qwik/src/root.tsx
@@ -1,13 +1,10 @@
 import { Main } from './main';
 
-import './global.css';
-
 export default (props: { url: string }) => {
   return (
     <html>
       <head>
         <meta charSet="utf-8" />
-        <title>Qwik Blank App</title>
       </head>
       <body>
         <Main url={props.url} />
