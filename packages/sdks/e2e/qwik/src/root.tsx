ee02341ffa4da2084634b02a27938b7d773af3d7
Sami Jaber <jabersami@gmail.com>
2023-01-20 17:58:18 -0400

    fix

--- packages/sdks/e2e/qwik/src/root.tsx
+++ packages/sdks/e2e/qwik/src/root.tsx
@@ -2,13 +2,13 @@ import { Main } from './main';
 
 export default (props: { url: string }) => {
   return (
-    <html>
+    <>
       <head>
         <meta charSet="utf-8" />
       </head>
       <body>
         <Main url={props.url} />
       </body>
-    </html>
+    </>
   );
 };
