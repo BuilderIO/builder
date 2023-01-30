c10a160fb798f91625c2b691e7dd2f58280c7f17
AJ <ancheetah@users.noreply.github.com>
2023-01-30 16:51:44 -0500

    localized component

---
+++ examples/next-js-localized/components/Heading.tsx
@@ -0,0 +1,22 @@
+import { Builder } from '@builder.io/react'
+
+export const Heading = (props: { title: string; }) => {
+
+    return(
+      <div style={{'width': '50vw'}}>
+        <h1>{props.title}</h1>
+      </div>
+    )
+  }
+  
+Builder.registerComponent(Heading, {
+name: "Heading",
+inputs: [
+    {
+    localized: true,
+    name: "title",
+    type: "text", 
+    defaultValue: 'I am a heading!'
+    },
+],
+});
--- examples/next-js-localized/pages/[[...page]].tsx
+++ examples/next-js-localized/pages/[[...page]].tsx
@@ -6,6 +6,7 @@ import Head from 'next/head'
 import builderConfig from '@config/builder'
 // loading widgets dynamically to reduce bundle size, will only be included in bundle when is used in the content
 import '@builder.io/widgets/dist/lib/builder-widgets-async'
+import '../components/Heading'
 
 builder.init(builderConfig.apiKey)
 
