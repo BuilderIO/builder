1465369af1d5358c19a77d0bfd6ba91d4575b60d
Sami Jaber <jabersami@gmail.com>
2023-02-09 16:50:12 -0400

    fix section values

--- packages/sdks/src/blocks/section/section.lite.tsx
+++ packages/sdks/src/blocks/section/section.lite.tsx
@@ -12,13 +12,9 @@ export default function SectionComponent(props: SectionProps) {
       style={{
         width: '100%',
         alignSelf: 'stretch',
-        flexGrow: '1',
+        flexGrow: 1,
         boxSizing: 'border-box',
-        maxWidth: `${
-          props.maxWidth && typeof props.maxWidth === 'number'
-            ? props.maxWidth
-            : 1200
-        }px`,
+        maxWidth: props.maxWidth || 1200,
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'stretch',
