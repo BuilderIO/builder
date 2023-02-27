cc2e708d3c30a6d59f7fc43afe69e4c53703f55b
Sami Jaber <jabersami@gmail.com>
2023-02-27 17:54:34 -0400

    remove comment

--- packages/sdks/src/blocks/text/text.lite.tsx
+++ packages/sdks/src/blocks/text/text.lite.tsx
@@ -1,18 +1,4 @@
 export default function Text(props: { text: string }) {
-  // useStyle(`
-  // .builder-text > p:first-of-type, .builder-text > .builder-paragraph:first-of-type {
-  //   margin: 0;
-  // }
-  // .builder-text > p, .builder-text > .builder-paragraph {
-  //   color: 'inherit';
-  //   line-height: 'inherit';
-  //   letter-spacing: 'inherit';
-  //   font-weight: 'inherit';
-  //   font-size: 'inherit';
-  //   text-align: 'inherit';
-  //   font-family: 'inherit';
-  // }
-  // `);
   return (
     <span
       class="builder-text"
