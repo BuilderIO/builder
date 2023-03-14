68bf22115a75ceaebb4296db7825001ffd4b2db9
Sami Jaber <jabersami@gmail.com>
2023-03-14 20:44:43 -0300

    fdsa

--- packages/sdks/src/blocks/symbol/symbol.lite.tsx
+++ packages/sdks/src/blocks/symbol/symbol.lite.tsx
@@ -90,13 +90,11 @@ export default function Symbol(props: SymbolProps) {
       className={state.className}
       dataSet={{ class: state.className }}
     >
+      {/*
+       * the `key` is used to force a re-render when the content changes (for Vue, possibly others too)
+       */}
       <RenderContent
-        key={
-          /**
-           * the `key` is used to force a re-render when the content changes (for Vue, possibly others too)
-           */
-          state.contentToUse?.id
-        }
+        key={state.contentToUse?.id}
         apiVersion={builderContext.apiVersion}
         apiKey={builderContext.apiKey!}
         context={builderContext.context}
