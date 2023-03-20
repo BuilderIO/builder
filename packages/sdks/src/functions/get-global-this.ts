cdc64581970d05918d1d19623b8108af08dd3c65
Sami Jaber <jabersami@gmail.com>
2023-03-20 15:51:45 -0300

    skip

--- packages/sdks/src/functions/get-global-this.ts
+++ packages/sdks/src/functions/get-global-this.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 export function getGlobalThis(): typeof global {
   if (typeof globalThis !== 'undefined') {
     return globalThis;
