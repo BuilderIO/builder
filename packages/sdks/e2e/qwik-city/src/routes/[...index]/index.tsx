b8df2a9cf4caca6e0a6b1a305324797d3e653259
Sami Jaber <jabersami@gmail.com>
2023-03-27 16:59:59 -0300

    support in qwikcity

--- packages/sdks/e2e/qwik-city/src/routes/[...index]/index.tsx
+++ packages/sdks/e2e/qwik-city/src/routes/[...index]/index.tsx
@@ -2,6 +2,7 @@ import { component$ } from '@builder.io/qwik';
 import { useLocation } from '@builder.io/qwik-city';
 import { RenderContent } from '@builder.io/sdk-qwik';
 import { getProps } from '@builder.io/sdks-e2e-tests';
+import { getCustomComponents } from '@builder.io/sdks-tests-custom-components/qwik';
 
 export interface MainProps {
   url: string;
@@ -11,8 +12,9 @@ export default component$(() => {
 
   const contentProps = getProps(pathname);
   console.log({ pathname, contentProps });
+  const components = getCustomComponents(pathname);
   return contentProps ? (
-    <RenderContent {...contentProps} />
+    <RenderContent {...contentProps} customComponents={components} />
   ) : (
     <div>Content Not Found</div>
   );
