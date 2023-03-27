d7c2a18ef116000614529e6a1c6e862962674023
Sami Jaber <jabersami@gmail.com>
2023-03-27 17:36:03 -0300

    add

--- packages/react-tests/nextjs/pages/[[...index]].tsx
+++ packages/react-tests/nextjs/pages/[[...index]].tsx
@@ -6,17 +6,19 @@ import type {
   GetStaticPropsContext,
   InferGetStaticPropsType,
 } from 'next';
-import { BuilderComponent, builder } from '@builder.io/react';
+import { BuilderComponent, builder, Builder } from '@builder.io/react';
 import DefaultErrorPage from 'next/error';
 import Head from 'next/head';
 import { useEffect } from 'react';
+import { getCustomComponents } from '@builder.io/sdks-tests-custom-components/output/react/src/index';
 
 builder.init(getAPIKey());
 
 type StaticProps = { index: string[] };
 
 export async function getStaticProps(x: GetStaticPropsContext<StaticProps>) {
-  return { props: getProps(x.params.index ? `/${x.params.index.join('/')}` : '/') };
+  const path = x.params.index ? `/${x.params.index.join('/')}` : '/';
+  return { props: { ...getProps(path), customComponents: getCustomComponents(path) } };
 }
 
 export function getStaticPaths(): GetStaticPathsResult<StaticProps> {
@@ -40,6 +42,7 @@ builder.canTrack = false;
 export default function Page(props: PageProps) {
   const router = useRouter();
 
+  props.customComponents.map(Builder.registerComponent(BuilderComponent, {}));
   // only enable tracking if we're not in the `/can-track-false` test route
   useEffect(() => {
     if (!router.asPath.includes('can-track-false')) {
