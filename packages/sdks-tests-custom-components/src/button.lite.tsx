15d34464b6e0d504a114c5ad72516a3ff2c9f779
Sami Jaber <jabersami@gmail.com>
2023-03-27 16:59:41 -0300

    yeet

--- packages/sdks-tests-custom-components/src/button.lite.tsx
+++ packages/sdks-tests-custom-components/src/button.lite.tsx
@@ -15,28 +15,13 @@ export default function Button(props: ButtonProps) {
     <Show
       when={props.link}
       else={
-        <button
-          css={{ all: 'unset', color: 'cyan' }}
-          class={
-            /**
-             * We have to explicitly provide `class` so that Mitosis knows to merge it with `css`.
-             */
-            props.attributes.class
-          }
-          {...props.attributes}
-        >
+        <button css={{ all: 'unset', color: 'cyan' }} {...props.attributes}>
           {props.text}
         </button>
       }
     >
       <a
         css={{ color: 'cyan' }}
-        class={
-          /**
-           * We have to explicitly provide `class` so that Mitosis knows to merge it with `css`.
-           */
-          props.attributes.class
-        }
         role="button"
         href={props.link}
         target={props.openLinkInNewTab ? '_blank' : undefined}
--- packages/sdks-tests-custom-components/src/index.ts
+++ packages/sdks-tests-custom-components/src/index.ts
@@ -1,3 +1,25 @@
 import { default as Button } from './button.lite';
+import type { RegisteredComponent } from '@builder.io/sdks/src/context/types';
 
-export const CUSTOM_COMPONENTS = [Button];
+function isBrowser(): boolean {
+  return typeof window !== 'undefined' && typeof document !== 'undefined';
+}
+
+const getPathnameFromWindow = (): string => (isBrowser() ? window.location.pathname : '');
+
+// remove trailing slash from pathname if it exists
+// unless it's the root path
+const normalizePathname = (pathname: string): string =>
+  pathname === '/' ? pathname : pathname.replace(/\/$/, '');
+
+export const getCustomComponents = (_pathname = getPathnameFromWindow()): RegisteredComponent[] => {
+  const pathname = normalizePathname(_pathname);
+
+  switch (pathname) {
+    case '/custom-core-button':
+      console.log('custom-core-button returned');
+      return [{ component: Button, name: 'Core:Button' }];
+    default:
+      return [];
+  }
+};
