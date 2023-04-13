9d7cffc0f86bbca15489e0573fd23ea41d531c90
Sami Jaber <jabersami@gmail.com>
2023-04-13 10:29:40 -0400

    parallelize for loop

--- packages/sdks/scripts/loop-command.sh
+++ packages/sdks/scripts/loop-command.sh
@@ -1,7 +1,11 @@
 # doing this for each SDK is getting tedious, and I'm lazy.
 echo "looping cmd \"$1\" over args \"$2\"";
 
+# run these loop commands in parallel
 for i in $2; do
   echo "running $1 on $i";
-  yarn run $1 $i;
+  yarn run $1 $i &
 done
+
+# wait for all the parallel commands to finish
+wait
