40056310c7d0ec8e0375b5eaf7ff62ad607f9704
Sami Jaber <jabersami@gmail.com>
2023-04-13 15:55:44 -0400

    Revert "fix"
    This reverts commit 176105fd8af0031a2ba06a68b4f4f8bd448f83a3.

--- packages/webcomponents/src/typings.d.ts
+++ packages/webcomponents/src/typings.d.ts
@@ -10,5 +10,3 @@ declare module '@builder.io/widgets/dist/preact' {
   const react = require('@builder.io/widgets');
   export = react;
 }
-
-declare module '@builder.io/shopify/dist/react/bundle.esm.js';
