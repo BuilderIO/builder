b249203a75683804982b127fed1e28f3c65da81c
Sami Jaber <jabersami@gmail.com>
2023-04-20 15:39:29 -0300

    l

--- scripts/clean-dependabot-merger.mjs
+++ scripts/clean-dependabot-merger.mjs
@@ -1,12 +1,16 @@
 #!/usr/bin/env zx
 
+import { echo } from 'zx/experimental';
+
+$.verbose = true;
+
 const query = await question('Enter a query to filter PRs by: ');
 
 const prsStr =
-  await $`gh search prs ${query} --state=open --repo=builder --owner=BuilderIO --app=dependabot --json=url,number`;
+  await $`gh search prs ${query} --state=open --repo=builder --owner=BuilderIO --app=dependabot --json=url,number,title`;
 
 /**
- * @type {Array<{url: string, number: number}>}
+ * @type {Array<{url: string, number: number, title: string}>}
  */
 const prs = JSON.parse(prsStr.stdout);
 
@@ -14,7 +18,13 @@ const prs = JSON.parse(prsStr.stdout);
 const cleanedPrs = prs.filter(pr => pr.url.includes('BuilderIO/builder/pull'));
 
 for (const pr of cleanedPrs) {
-  await $`gh pr review ${pr.number} --approve`;
-  // enable auto-merge
-  await $`gh pr merge ${pr.number} --auto --squash`;
+  echo`merging PR: ${pr.url}: ${pr.title}`;
+  try {
+    await $`gh pr review ${pr.number} --approve`;
+    // enable auto-merge
+    await $`gh pr merge ${pr.number} --auto --squash`;
+  } catch (error) {
+    echo`ERROR merging PR: ${pr.url}: ${pr.title}`;
+    echo`ERROR: ${error}`;
+  }
 }
