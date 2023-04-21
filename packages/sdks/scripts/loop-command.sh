496a228e5610d123c843086d9c78e89ab86ab238
Sami Jaber <jabersami@gmail.com>
2023-04-21 11:18:58 -0300

    Dependabot (#2173)
    * p
    
    * release
    
    * upgrade
    
    * parallelize for loop
    
    * types
    
    * logger
    
    * f
    
    * remove crashes
    
    * dpbot
    
    * g
    
    * cleaan
    
    * f
    
    * l
    
    * f
    
    * f
    
    * c
    
    * f

--- packages/sdks/scripts/loop-command.sh
+++ packages/sdks/scripts/loop-command.sh
@@ -1,6 +1,7 @@
 # doing this for each SDK is getting tedious, and I'm lazy.
 echo "looping cmd \"$1\" over args \"$2\"";
 
+# run these loop commands in parallel
 for i in $2; do
   echo "running $1 on $i";
   yarn run $1 $i;
