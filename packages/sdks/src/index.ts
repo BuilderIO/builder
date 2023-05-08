6bd5e3d6107eea942c925c5c5060f6fcf0e8964f
Sami Jaber <jabersami@gmail.com>
2023-05-08 12:47:18 -0300

    tweak exports

--- packages/sdks/src/index.ts
+++ packages/sdks/src/index.ts
@@ -1,14 +1,19 @@
 export * from './index-helpers/top-of-file.js';
-
 export * from './index-helpers/blocks-exports.js';
 
-export * from './functions/is-editing.js';
-export * from './functions/is-previewing.js';
-export * from './functions/register-component.js';
-export * from './functions/register.js';
-export * from './functions/set-editor-settings.js';
-export * from './functions/get-content/index.js';
-export * from './functions/get-builder-search-params/index.js';
+export { isEditing } from './functions/is-editing.js';
+export { isPreviewing } from './functions/is-previewing.js';
+export { createRegisterComponentMessage } from './functions/register-component.js';
+
+export { register } from './functions/register.js';
+export type { InsertMenuConfig, InsertMenuItem } from './functions/register.js';
+
+export { setEditorSettings } from './functions/set-editor-settings.js';
+export type { Settings } from './functions/set-editor-settings.js';
+
+export { getAllContent, getContent } from './functions/get-content/index.js';
+
 export { track } from './functions/track/index.js';
+
 export type { RegisteredComponent } from './context/types';
 export type { ComponentInfo } from './types/components';
