4e8221598c7a2ed1ebd202723c586f7df021fae6
Sami Jaber <jabersami@gmail.com>
2023-05-09 16:07:48 -0300

    fix

--- examples/vue/vue-2/src/main.js
+++ examples/vue/vue-2/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
-import App from './App.vue';
 
-Vue.config.productionTip = false;
+import App from './App.vue';
+import router from './router';
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app');
