bff5a48d64486080f557fcf638312cb874cee2ff
Sami Jaber <jabersami@gmail.com>
2023-05-11 20:50:37 -0400

    clean

--- packages/sdks/src/components/render-content-variants/helpers.ts
+++ packages/sdks/src/components/render-content-variants/helpers.ts
@@ -39,145 +39,23 @@ const variantScriptFn = function main(
   contentId: string,
   variants: VariantData[]
 ) {
-  console.log('hey variant script', JSON.stringify({ contentId, variants }));
+  function templateSelectorById(id: string) {
+    return `template[data-template-variant-id="${id}"]`;
+  }
 
-  const templateSelectorById = (id: string) =>
-    `template[data-template-variant-id="${id}"]`;
-
-  function removeTemplates() {
-    console.log('Removing variants');
-    /**
-     * remove each variant
-     */
+  function removeTemplatesAndScript() {
     variants.forEach((template) => {
       const el = document.querySelector(templateSelectorById(template.id));
       if (el) {
-        console.log('Removing variant', template.id);
         el.remove();
       }
     });
 
-    /**
-     * remove this script itself
-     */
     const el = document.getElementById(`variants-script-${contentId}`);
     if (el) {
-      console.log('Removing variant script');
       el.remove();
     }
   }
-
-  /**
-   * TO-DO: redundant check?
-   */
-  if (!navigator.cookieEnabled) {
-    console.log('Cookies are disabled, skipping variant script');
-    removeTemplates();
-    return;
-  }
-
-  /**
-   * TO-DO: what is this check doing?
-   * seems like a template polyfill check
-   */
-  if (typeof document.createElement('template').content === 'undefined') {
-    console.log('Template polyfill not found, skipping variant script');
-    removeTemplates();
-    return;
-  }
-
-  function getAndSetVariantId(): string {
-    function setCookie(name: string, value: string, days?: number) {
-      let expires = '';
-      if (days) {
-        const date = new Date();
-        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-        expires = '; expires=' + date.toUTCString();
-      }
-      document.cookie =
-        name +
-        '=' +
-        (value || '') +
-        expires +
-        '; path=/' +
-        '; Secure; SameSite=None';
-    }
-    function getCookie(name: string) {
-      const nameEQ = name + '=';
-      const ca = document.cookie.split(';');
-      for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0)
-          return c.substring(nameEQ.length, c.length);
-      }
-      return null;
-    }
-    const cookieName = `builder.tests.${contentId}`;
-    const variantInCookie = getCookie(cookieName);
-    const availableIDs = variants.map((vr) => vr.id).concat(contentId);
-    /**
-     * cookie already exists
-     */
-    if (variantInCookie && availableIDs.includes(variantInCookie)) {
-      return variantInCookie;
-    }
-
-    /**
-     * no cookie exists, find variant
-     */
-    let n = 0;
-    const random = Math.random();
-    for (let i = 0; i < variants.length; i++) {
-      const variant = variants[i];
-      const testRatio = variant.testRatio;
-      n += testRatio!;
-      if (random < n) {
-        setCookie(cookieName, variant.id);
-        return variant.id;
-      }
-    }
-
-    /**
-     * no variant found, assign default content
-     */
-    setCookie(cookieName, contentId);
-    return contentId;
-  }
-  const variantId = getAndSetVariantId();
-
-  if (variantId === contentId) {
-    console.log('chose default variant. Removing templates');
-    removeTemplates();
-    return;
-  }
-
-  console.log('Variant found', variantId);
-  const winningTemplate = document.querySelector<HTMLTemplateElement>(
-    templateSelectorById(variantId)
-  );
-  console.log('winningTemplate', winningTemplate);
-
-  if (!winningTemplate) {
-    console.log('No template found for variant', variantId);
-    console.log('selector: ', templateSelectorById(variantId));
-
-    /**
-     * TO-DO: what do in this case? throw? warn?
-     */
-    return;
-  }
-
-  const templatesParent = winningTemplate.parentNode!;
-  /**
-   * Create a shallow clone of the parent of all templates (and the default content)
-   */
-  const newParent = templatesParent.cloneNode(false);
-
-  /**
-   * Append only the winning variant to the new parent
-   */
-  newParent.appendChild(winningTemplate.content.firstElementChild!);
 
   /**
    * Replace the old parent with the new one.
@@ -201,10 +79,104 @@ const variantScriptFn = function main(
    *
    * Since `RenderContentVariants will replace its parent, the rest of the content will be removed.
    */
-  templatesParent.parentNode!.replaceChild(newParent, templatesParent);
+  function injectVariant() {
+    if (!navigator.cookieEnabled) {
+      return;
+    }
 
-  console.log('Variant script complete. Removing templates');
-  removeTemplates();
+    /**
+     * TO-DO: what is this check doing?
+     * seems like a template polyfill check
+     */
+    if (typeof document.createElement('template').content === 'undefined') {
+      return;
+    }
+
+    function getAndSetVariantId(): string {
+      function setCookie(name: string, value: string, days?: number) {
+        let expires = '';
+        if (days) {
+          const date = new Date();
+          date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+          expires = '; expires=' + date.toUTCString();
+        }
+        document.cookie =
+          name +
+          '=' +
+          (value || '') +
+          expires +
+          '; path=/' +
+          '; Secure; SameSite=None';
+      }
+      function getCookie(name: string) {
+        const nameEQ = name + '=';
+        const ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+          let c = ca[i];
+          while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+          if (c.indexOf(nameEQ) === 0)
+            return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+      }
+      const cookieName = `builder.tests.${contentId}`;
+      const variantInCookie = getCookie(cookieName);
+      const availableIDs = variants.map((vr) => vr.id).concat(contentId);
+      /**
+       * cookie already exists
+       */
+      if (variantInCookie && availableIDs.includes(variantInCookie)) {
+        return variantInCookie;
+      }
+
+      /**
+       * no cookie exists, find variant
+       */
+      let n = 0;
+      const random = Math.random();
+      for (let i = 0; i < variants.length; i++) {
+        const variant = variants[i];
+        const testRatio = variant.testRatio;
+        n += testRatio!;
+        if (random < n) {
+          setCookie(cookieName, variant.id);
+          return variant.id;
+        }
+      }
+
+      /**
+       * no variant found, assign default content
+       */
+      setCookie(cookieName, contentId);
+      return contentId;
+    }
+    const variantId = getAndSetVariantId();
+
+    if (variantId === contentId) {
+      return;
+    }
+
+    const winningTemplate = document.querySelector<HTMLTemplateElement>(
+      templateSelectorById(variantId)
+    );
+
+    if (!winningTemplate) {
+      /**
+       * TO-DO: what do in this case? throw? warn?
+       */
+      return;
+    }
+
+    const templatesParent = winningTemplate.parentNode!;
+    const newParent = templatesParent.cloneNode(false);
+    newParent.appendChild(winningTemplate.content.firstElementChild!);
+
+    templatesParent.parentNode!.replaceChild(newParent, templatesParent);
+  }
+
+  injectVariant();
+
+  removeTemplatesAndScript();
 };
 
 export const getVariantsScriptString = (
