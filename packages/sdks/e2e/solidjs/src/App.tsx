3f96a7210dbfc74b49fd1921c2af651d9f6aaed7
Sami Jaber <jabersami@gmail.com>
2023-06-02 11:14:22 -0300

    f

--- packages/sdks/e2e/solidjs/src/App.tsx
+++ packages/sdks/e2e/solidjs/src/App.tsx
@@ -1,6 +1,5 @@
 import type { Component } from 'solid-js';
-import { createResource } from 'solid-js';
-import { Show } from 'solid-js';
+import { createResource, Show } from 'solid-js';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { RenderContent } from '@builder.io/sdk-solid';
@@ -10,7 +9,10 @@ const App: Component = () => {
   const [props] = createResource(() => getProps());
 
   return (
-    <Show when={props} fallback={<div>Content Not Found</div>}>
+    <Show
+      when={!props.loading && !props.error}
+      fallback={<div>Content Not Found</div>}
+    >
       {() => <RenderContent {...props()} />}
     </Show>
   );
