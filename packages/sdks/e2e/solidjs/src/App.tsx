982bf132dd4e7176450be6a2218565a32fd7a4cf
Sami Jaber <jabersami@gmail.com>
2023-06-02 10:59:16 -0300

    fdas

--- packages/sdks/e2e/solidjs/src/App.tsx
+++ packages/sdks/e2e/solidjs/src/App.tsx
@@ -1,19 +1,13 @@
 import type { Component } from 'solid-js';
+import { createResource } from 'solid-js';
 import { Show } from 'solid-js';
-import { createEffect, createSignal } from 'solid-js';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { RenderContent } from '@builder.io/sdk-solid';
 import { getProps } from '@builder.io/sdks-e2e-tests';
 
 const App: Component = () => {
-  const [props, setProps] = createSignal<object | null>(null);
-
-  createEffect(() => {
-    getProps().then((content) => {
-      setProps(content);
-    });
-  });
+  const [props] = createResource(() => getProps());
 
   return (
     <Show when={props} fallback={<div>Content Not Found</div>}>
