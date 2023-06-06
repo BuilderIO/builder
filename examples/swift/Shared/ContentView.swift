bd5d36a348b38ad59b3d9e3f954729f78d49bf79
shyam-builder <shyam@builder.io>
2023-06-06 21:25:31 +0530

    Add example with Custom Components in Swift

--- examples/swift/Shared/ContentView.swift
+++ examples/swift/Shared/ContentView.swift
@@ -5,11 +5,14 @@ struct ContentView: View {
     @State var content: BuilderContent? = nil
     
     init() {
+        registerComponent(name: "HeroComponent", factory: { (options, styles) in
+            return HeroComponent(headingText: options["headingText"].stringValue, ctaText: options["ctaText"].stringValue)
+        });
         fetchContent()
     }
     
     func fetchContent() {
-        Content.getContent(model: "page", apiKey: "e084484c0e0241579f01abba29d9be10", url: "/") { content in
+        Content.getContent(model: "page", apiKey: "e084484c0e0241579f01abba29d9be10", url: "/custom-components") { content in
             self.content = content
         }
     }
