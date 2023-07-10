2e46fed4e29cd806744b3c579471908a4c4d6ebc
Sami Jaber <jabersami@gmail.com>
2023-07-10 13:55:24 -0300

    rmv examples

--- examples/next-app-dir/src/app/[[...slug]]/Foo/ClientChild.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import { useContext } from 'react'
-import FContext from './FContext'
-
-export default function ClientChild() {
-  const context = useContext(FContext)
-  return <>{context}</>
-}
