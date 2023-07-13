0996f50e7934d8157cc6e5aca4b0db8ad5101d3f
Sami Jaber <jabersami@gmail.com>
2023-07-13 11:34:34 -0300

    update spec

--- packages/sdks-tests/src/tests/ab-test.spec.ts
+++ packages/sdks-tests/src/tests/ab-test.spec.ts
@@ -2,8 +2,14 @@ import type { Browser, BrowserContext, ConsoleMessage } from '@playwright/test';
 import { expect } from '@playwright/test';
 import { findTextInPage, isRNSDK, test } from './helpers.js';
 
-const CONTENT_ID = '1d326d78efb04ce38467dd8f5160fab6';
-const VARIANT_ID = 'd50b5d04edf640f195a7c42ebdb159b2';
+const CONTENT_ID = '691abdd7105c4cf7b9609995fc1fb56c';
+const VARIANT_ID = '661775df8c2c41d6afc0aa1b5fd1dd61';
+
+const TEXTS = {
+  DEFAULT_CONTENT: 'This is the default variation!',
+  VARIANT_1: 'This is variation 1',
+  VARIANT_2: 'text only in variation 2',
+};
 
 const COOKIE_NAME = `builder.tests.${CONTENT_ID}`;
 const SELECTOR = isRNSDK ? 'div[data-builder-content-id]' : 'div[builder-content-id]';
@@ -57,8 +63,12 @@ const createContextWithCookies = async ({
 test.describe.configure({ retries: 0 });
 
 const filterHydrationmismatchMessages = (consoleMessage: ConsoleMessage) => {
-  const text = consoleMessage.text();
-  return text.includes('[Vue warn]: Hydration children mismatch');
+  const text = consoleMessage.text().toLowerCase();
+
+  const isVueHydrationMismatch =
+    text.includes('[vue warn]') && text.includes('hydration') && text.includes('mismatch');
+
+  return isVueHydrationMismatch;
 };
 
 test.describe('A/B tests', () => {
@@ -108,8 +118,9 @@ test.describe('A/B tests', () => {
 
       await page.goto('/ab-test');
 
-      await findTextInPage({ page, text: 'hello world default' });
-      await expect(page.locator(SELECTOR, { hasText: 'hello world variation 1' })).toBeHidden();
+      await findTextInPage({ page, text: TEXTS.DEFAULT_CONTENT });
+      await expect(page.locator(SELECTOR, { hasText: TEXTS.VARIANT_1 })).toBeHidden();
+      await expect(page.locator(SELECTOR, { hasText: TEXTS.VARIANT_2 })).toBeHidden();
       await expect(msgs).toEqual([]);
     });
 
@@ -155,8 +166,9 @@ test.describe('A/B tests', () => {
 
       await page.goto('/ab-test');
 
-      await findTextInPage({ page, text: 'hello world variation 1' });
-      await expect(page.locator(SELECTOR, { hasText: 'hello world default' })).toBeHidden();
+      await findTextInPage({ page, text: TEXTS.VARIANT_1 });
+      await expect(page.locator(SELECTOR, { hasText: TEXTS.DEFAULT_CONTENT })).toBeHidden();
+      await expect(page.locator(SELECTOR, { hasText: TEXTS.VARIANT_2 })).toBeHidden();
       await expect(msgs).toEqual([]);
     });
   }
