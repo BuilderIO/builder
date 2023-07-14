428591852310eb8f0a9d98502be58a965bd128d0
Sami Jaber <jabersami@gmail.com>
2023-07-14 18:54:04 -0300

    rmv qwik test

--- packages/sdks-tests/playwright.config.ts
+++ packages/sdks-tests/playwright.config.ts
@@ -18,11 +18,7 @@ const getDirName = () => {
 const WEB_SERVERS: Record<Exclude<Sdk, 'all' | 'allNew'>, PackageName[]> = {
   reactNative: ['e2e-react-native'],
   solid: ['e2e-solidjs', 'e2e-solid-start'],
-  qwik: [
-    // 'e2e-qwik',
-
-    'e2e-qwik-city',
-  ],
+  qwik: ['e2e-qwik-city'],
   react: ['e2e-nextjs-react', 'e2e-react', 'e2e-nextjs-app-dir-react'],
   vue: ['e2e-vue2', 'e2e-vue3', 'e2e-vue-nuxt3', 'e2e-vue-nuxt2'],
   svelte: ['e2e-svelte', 'e2e-sveltekit'],
