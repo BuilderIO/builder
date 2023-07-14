27ffd200a822a82ec6cfc332226ed0cee0017181
Sami Jaber <jabersami@gmail.com>
2023-07-14 13:38:12 -0300

    fix

--- packages/sdks/src/blocks/text/text.lite.tsx
+++ packages/sdks/src/blocks/text/text.lite.tsx
@@ -2,7 +2,7 @@ export default function Text(props: { text: string }) {
   return (
     <span
       class="builder-text"
-      innerHTML={props.text}
+      innerHTML={props.text.toString()}
       style={{ outline: 'none' }}
     />
   );
