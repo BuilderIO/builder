368992bc8d1f9da371ca10830795ac2a2f96b4b1
Sami Jaber <jabersami@gmail.com>
2023-07-17 17:06:19 -0300

    mov

--- packages/sdks-tests/src/specs/symbol-with-locale.ts
+++ packages/sdks-tests/src/specs/symbol-with-locale.ts
@@ -1,3 +1,84 @@
+export const DEFAULT_TEXT_SYMBOL = {
+  lastUpdatedBy: 'b0272BAHcDTw8MCeDwLmdq7Uykp2',
+  folders: [],
+  data: {
+    blocks: [
+      {
+        '@type': '@builder.io/sdk:Element',
+        '@version': 2,
+        id: 'builder-90ec911111a94f0aa3e0ee1860388d93',
+        meta: {
+          previousId: 'builder-29bab71fac7043eb921eb530a3203e5f',
+          'transformed.text': 'localized',
+          localizedTextInputs: ['text'],
+        },
+        component: {
+          name: 'Text',
+          options: {
+            text: '<span style="display: block;" class="builder-paragraph">Default text</span>',
+          },
+        },
+        responsiveStyles: {
+          large: {
+            display: 'flex',
+            flexDirection: 'column',
+            position: 'relative',
+            flexShrink: '0',
+            boxSizing: 'border-box',
+            marginTop: '20px',
+            lineHeight: 'normal',
+            height: 'auto',
+          },
+        },
+      },
+      {
+        id: 'builder-pixel-hdkrkco2lk9',
+        '@type': '@builder.io/sdk:Element',
+        tagName: 'img',
+        properties: {
+          src: 'https://cdn.builder.io/api/v1/pixel?apiKey=f1a790f8c3204b3b8c5c1795aeac4660',
+          'aria-hidden': 'true',
+          alt: '',
+          role: 'presentation',
+          width: '0',
+          height: '0',
+        },
+        responsiveStyles: {
+          large: {
+            height: '0',
+            width: '0',
+            display: 'inline-block',
+            opacity: '0',
+            overflow: 'hidden',
+            pointerEvents: 'none',
+          },
+        },
+      },
+    ],
+    state: { deviceSize: 'large', location: { path: '', query: {} } },
+  },
+  modelId: 'd661dd1d06ce43ddbe3c2fe35597e545',
+  query: [],
+  published: 'published',
+  screenshot:
+    'https://cdn.builder.io/api/v1/image/assets%2Ff1a790f8c3204b3b8c5c1795aeac4660%2F2072bf01f23f4d929d2f459fc900e61c',
+  firstPublished: 1687808895615,
+  testRatio: 1,
+  lastUpdated: 1687808953907,
+  createdDate: 1687808895615,
+  createdBy: 'b0272BAHcDTw8MCeDwLmdq7Uykp2',
+  meta: {
+    kind: 'component',
+    lastPreviewUrl:
+      'https://preview.builder.codes?model=symbol&previewing=true&apiKey=f1a790f8c3204b3b8c5c1795aeac4660&builder.space=f1a790f8c3204b3b8c5c1795aeac4660&builder.cachebust=true&builder.preview=symbol&builder.noCache=true&__builder_editing__=true&builder.overrides.symbol=e2a166f7d9544ed9ade283abf9491af3&builder.overrides.e2a166f7d9544ed9ade283abf9491af3=e2a166f7d9544ed9ade283abf9491af3&builder.options.locale=Default',
+    hasLinks: false,
+  },
+  variations: {},
+  name: 'Locale symbol',
+  id: 'e2a166f7d9544ed9ade283abf9491af3',
+  rev: 'znwd06q4jua',
+} as const;
+
 export const CONTENT = {
   lastUpdatedBy: 'b0272BAHcDTw8MCeDwLmdq7Uykp2',
   folders: [],
@@ -202,85 +283,4 @@ export const FRENCH_TEXT_SYMBOL = {
   name: 'Locale symbol',
   id: 'e2a166f7d9544ed9ade283abf9491af3',
   rev: 'upgeuaxb42',
-} as const;
-
-export const DEFAULT_TEXT_SYMBOL = {
-  lastUpdatedBy: 'b0272BAHcDTw8MCeDwLmdq7Uykp2',
-  folders: [],
-  data: {
-    blocks: [
-      {
-        '@type': '@builder.io/sdk:Element',
-        '@version': 2,
-        id: 'builder-90ec911111a94f0aa3e0ee1860388d93',
-        meta: {
-          previousId: 'builder-29bab71fac7043eb921eb530a3203e5f',
-          'transformed.text': 'localized',
-          localizedTextInputs: ['text'],
-        },
-        component: {
-          name: 'Text',
-          options: {
-            text: '<span style="display: block;" class="builder-paragraph">Default text</span>',
-          },
-        },
-        responsiveStyles: {
-          large: {
-            display: 'flex',
-            flexDirection: 'column',
-            position: 'relative',
-            flexShrink: '0',
-            boxSizing: 'border-box',
-            marginTop: '20px',
-            lineHeight: 'normal',
-            height: 'auto',
-          },
-        },
-      },
-      {
-        id: 'builder-pixel-hdkrkco2lk9',
-        '@type': '@builder.io/sdk:Element',
-        tagName: 'img',
-        properties: {
-          src: 'https://cdn.builder.io/api/v1/pixel?apiKey=f1a790f8c3204b3b8c5c1795aeac4660',
-          'aria-hidden': 'true',
-          alt: '',
-          role: 'presentation',
-          width: '0',
-          height: '0',
-        },
-        responsiveStyles: {
-          large: {
-            height: '0',
-            width: '0',
-            display: 'inline-block',
-            opacity: '0',
-            overflow: 'hidden',
-            pointerEvents: 'none',
-          },
-        },
-      },
-    ],
-    state: { deviceSize: 'large', location: { path: '', query: {} } },
-  },
-  modelId: 'd661dd1d06ce43ddbe3c2fe35597e545',
-  query: [],
-  published: 'published',
-  screenshot:
-    'https://cdn.builder.io/api/v1/image/assets%2Ff1a790f8c3204b3b8c5c1795aeac4660%2F2072bf01f23f4d929d2f459fc900e61c',
-  firstPublished: 1687808895615,
-  testRatio: 1,
-  lastUpdated: 1687808953907,
-  createdDate: 1687808895615,
-  createdBy: 'b0272BAHcDTw8MCeDwLmdq7Uykp2',
-  meta: {
-    kind: 'component',
-    lastPreviewUrl:
-      'https://preview.builder.codes?model=symbol&previewing=true&apiKey=f1a790f8c3204b3b8c5c1795aeac4660&builder.space=f1a790f8c3204b3b8c5c1795aeac4660&builder.cachebust=true&builder.preview=symbol&builder.noCache=true&__builder_editing__=true&builder.overrides.symbol=e2a166f7d9544ed9ade283abf9491af3&builder.overrides.e2a166f7d9544ed9ade283abf9491af3=e2a166f7d9544ed9ade283abf9491af3&builder.options.locale=Default',
-    hasLinks: false,
-  },
-  variations: {},
-  name: 'Locale symbol',
-  id: 'e2a166f7d9544ed9ade283abf9491af3',
-  rev: 'znwd06q4jua',
 } as const;
