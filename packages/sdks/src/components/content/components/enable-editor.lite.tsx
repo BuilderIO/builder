bd4c86744e80998257a61b14b2a89e01bf3cdc6c
Sami Jaber <jabersami@gmail.com>
2023-07-17 21:26:49 -0300

    move mergeNewContent to EnableEditor

--- packages/sdks/src/components/content/components/enable-editor.lite.tsx
+++ packages/sdks/src/components/content/components/enable-editor.lite.tsx
@@ -30,6 +30,9 @@ import type {
 import { TARGET } from '../../../constants/target.js';
 import { logger } from '../../../helpers/logger.js';
 import type { ComponentInfo } from '../../../types/components.js';
+import { getContent } from '../../../functions/get-content/index.js';
+import { isPreviewing } from '../../../functions/is-previewing.js';
+import type { BuilderContent } from '../../../types/builder-content.js';
 
 useMetadata({
   qwik: {
@@ -39,7 +42,7 @@ useMetadata({
 
 type BuilderEditorProps = Omit<
   ContentProps,
-  'customComponents' | 'content' | 'data' | 'apiVersion' | 'isSsrAbTest'
+  'customComponents' | 'data' | 'apiVersion' | 'isSsrAbTest'
 > & {
   builderContextSignal: Signal<BuilderContextInterface>;
   children?: any;
@@ -49,7 +52,66 @@ export default function EnableEditor(props: BuilderEditorProps) {
   const elementRef = useRef<HTMLDivElement>();
   const state = useStore({
     canTrackToUse: checkIsDefined(props.canTrack) ? props.canTrack : true,
+    forceReRenderCount: 0,
+    mergeNewContent(newContent: BuilderContent) {
+      props.builderContextSignal.value.content = {
+        ...props.builderContextSignal.value.content,
+        ...newContent,
+        data: {
+          ...props.builderContextSignal.value.content?.data,
+          ...newContent?.data,
+        },
+        meta: {
+          ...props.builderContextSignal.value.content?.meta,
+          ...newContent?.meta,
+          breakpoints:
+            newContent?.meta?.breakpoints ||
+            props.builderContextSignal.value.content?.meta?.breakpoints,
+        },
+      };
+    },
+    processMessage(event: MessageEvent): void {
+      const { data } = event;
+      if (data) {
+        switch (data.type) {
+          case 'builder.configureSdk': {
+            const messageContent = data.data;
+            const { breakpoints, contentId } = messageContent;
+            if (
+              !contentId ||
+              contentId !== props.builderContextSignal.value.content?.id
+            ) {
+              return;
+            }
+            if (breakpoints) {
+              state.mergeNewContent({ meta: { breakpoints } });
+            }
+            state.forceReRenderCount = state.forceReRenderCount + 1; // This is a hack to force Qwik to re-render.
+            break;
+          }
+          case 'builder.contentUpdate': {
+            const messageContent = data.data;
+            const key =
+              messageContent.key ||
+              messageContent.alias ||
+              messageContent.entry ||
+              messageContent.modelName;
 
+            const contentData = messageContent.data;
+
+            if (key === props.model) {
+              state.mergeNewContent(contentData);
+              state.forceReRenderCount = state.forceReRenderCount + 1; // This is a hack to force Qwik to re-render.
+            }
+            break;
+          }
+          case 'builder.patchUpdates': {
+            // TODO
+            break;
+          }
+        }
+      }
+    },
     evaluateJsCode() {
       // run any dynamic JS code attached to content
       const jsCode = props.builderContextSignal.value.content?.data?.jsCode;
@@ -146,6 +208,69 @@ export default function EnableEditor(props: BuilderEditorProps) {
 
   setContext(builderContext, props.builderContextSignal);
 
+  onUpdate(() => {
+    if (props.content) {
+      state.mergeNewContent(props.content);
+    }
+  }, [props.content]);
+
+  onMount(() => {
+    if (!props.apiKey) {
+      logger.error(
+        'No API key provided to `RenderContent` component. This can cause issues. Please provide an API key using the `apiKey` prop.'
+      );
+    }
+
+    if (isBrowser()) {
+      if (isEditing()) {
+        state.forceReRenderCount = state.forceReRenderCount + 1;
+        window.addEventListener('message', state.processMessage);
+      }
+
+      // override normal content in preview mode
+      if (isPreviewing()) {
+        const searchParams = new URL(location.href).searchParams;
+        const searchParamPreviewModel = searchParams.get('builder.preview');
+        const searchParamPreviewId = searchParams.get(
+          `builder.preview.${searchParamPreviewModel}`
+        );
+        const previewApiKey =
+          searchParams.get('apiKey') || searchParams.get('builder.space');
+
+        /**
+         * Make sure that:
+         * - the preview model name is the same as the one we're rendering, since there can be multiple models rendered
+         *  at the same time, e.g. header/page/footer.
+         * - the API key is the same, since we don't want to preview content from other organizations.
+         * - if there is content, that the preview ID is the same as that of the one we receive.
+         *
+         * TO-DO: should we only update the state when there is a change?
+         **/
+        if (
+          searchParamPreviewModel === props.model &&
+          previewApiKey === props.apiKey &&
+          (!props.content || searchParamPreviewId === props.content.id)
+        ) {
+          getContent({
+            model: props.model,
+            apiKey: props.apiKey,
+            apiVersion: props.builderContextSignal.value.apiVersion,
+          }).then((content) => {
+            if (content) {
+              state.mergeNewContent(content);
+            }
+          });
+        }
+      }
+    }
+  });
+
+  onUnMount(() => {
+    if (isBrowser()) {
+      window.removeEventListener('message', state.processMessage);
+    }
+  });
+
   onMount(() => {
     if (!props.apiKey) {
       logger.error(
@@ -219,6 +344,7 @@ export default function EnableEditor(props: BuilderEditorProps) {
   return (
     <Show when={props.builderContextSignal.value.content}>
       <div
+        key={state.forceReRenderCount}
         ref={elementRef}
         onClick={(event) => state.onClick(event)}
         builder-content-id={props.builderContextSignal.value.content?.id}
--- packages/sdks/src/components/content/content.lite.tsx
+++ packages/sdks/src/components/content/content.lite.tsx
@@ -7,15 +7,7 @@ import type {
 import { components } from '../../functions/register-component.js';
 import Blocks from '../blocks/blocks.lite';
 import ContentStyles from './components/styles.lite';
-import {
-  Show,
-  useStore,
-  useMetadata,
-  useState,
-  onUpdate,
-  onMount,
-  onUnMount,
-} from '@builder.io/mitosis';
+import { Show, useStore, useMetadata, useState } from '@builder.io/mitosis';
 import type { ContentProps } from './content.types.js';
 import {
   getContentInitialValue,
@@ -25,12 +17,6 @@ import { TARGET } from '../../constants/target.js';
 import { getRenderContentScriptString } from '../content-variants/helpers.js';
 import { useTarget } from '@builder.io/mitosis';
 import EnableEditor from './components/enable-editor.lite';
-import type { BuilderContent } from '../../types/builder-content.js';
-import { getContent } from '../../functions/get-content/index.js';
-import { isBrowser } from '../../functions/is-browser.js';
-import { isEditing } from '../../functions/is-editing.js';
-import { isPreviewing } from '../../functions/is-previewing.js';
-import { logger } from '../../helpers/logger.js';
 import InlinedScript from '../inlined-script.lite';
 import { wrapComponentRef } from './wrap-component-ref.js';
 
@@ -42,76 +28,15 @@ useMetadata({
 
 export default function ContentComponent(props: ContentProps) {
   const state = useStore({
-    forceReRenderCount: 0,
-    mergeNewContent(newContent: BuilderContent) {
-      builderContextSignal.value.content = {
-        ...builderContextSignal.value.content,
-        ...newContent,
-        data: {
-          ...builderContextSignal.value.content?.data,
-          ...newContent?.data,
-        },
-        meta: {
-          ...builderContextSignal.value.content?.meta,
-          ...newContent?.meta,
-          breakpoints:
-            newContent?.meta?.breakpoints ||
-            builderContextSignal.value.content?.meta?.breakpoints,
-        },
-      };
-    },
-    contentSetState: (newRootState: BuilderRenderState) => {
-      builderContextSignal.value.rootState = newRootState;
-    },
-    processMessage(event: MessageEvent): void {
-      const { data } = event;
-      if (data) {
-        switch (data.type) {
-          case 'builder.configureSdk': {
-            const messageContent = data.data;
-            const { breakpoints, contentId } = messageContent;
-            if (
-              !contentId ||
-              contentId !== builderContextSignal.value.content?.id
-            ) {
-              return;
-            }
-            if (breakpoints) {
-              state.mergeNewContent({ meta: { breakpoints } });
-            }
-            state.forceReRenderCount = state.forceReRenderCount + 1; // This is a hack to force Qwik to re-render.
-            break;
-          }
-          case 'builder.contentUpdate': {
-            const messageContent = data.data;
-            const key =
-              messageContent.key ||
-              messageContent.alias ||
-              messageContent.entry ||
-              messageContent.modelName;
-
-            const contentData = messageContent.data;
-
-            if (key === props.model) {
-              state.mergeNewContent(contentData);
-              state.forceReRenderCount = state.forceReRenderCount + 1; // This is a hack to force Qwik to re-render.
-            }
-            break;
-          }
-          case 'builder.patchUpdates': {
-            // TODO
-            break;
-          }
-        }
-      }
-    },
-
     scriptStr: getRenderContentScriptString({
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-non-null-asserted-optional-chain
       variationId: props.content?.testVariationId!,
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-non-null-asserted-optional-chain
       contentId: props.content?.id!,
     }),
+    contentSetState: (newRootState: BuilderRenderState) => {
+      builderContextSignal.value.rootState = newRootState;
+    },
 
     registeredComponents: [
       ...getDefaultRegisteredComponents(),
@@ -185,72 +110,10 @@ export default function ContentComponent(props: ContentProps) {
     { reactive: true }
   );
 
-  onUpdate(() => {
-    if (props.content) {
-      state.mergeNewContent(props.content);
-    }
-  }, [props.content]);
-
-  onMount(() => {
-    if (!props.apiKey) {
-      logger.error(
-        'No API key provided to `RenderContent` component. This can cause issues. Please provide an API key using the `apiKey` prop.'
-      );
-    }
-
-    if (isBrowser()) {
-      if (isEditing()) {
-        state.forceReRenderCount = state.forceReRenderCount + 1;
-        window.addEventListener('message', state.processMessage);
-      }
-
-      // override normal content in preview mode
-      if (isPreviewing()) {
-        const searchParams = new URL(location.href).searchParams;
-        const searchParamPreviewModel = searchParams.get('builder.preview');
-        const searchParamPreviewId = searchParams.get(
-          `builder.preview.${searchParamPreviewModel}`
-        );
-        const previewApiKey =
-          searchParams.get('apiKey') || searchParams.get('builder.space');
-
-        /**
-         * Make sure that:
-         * - the preview model name is the same as the one we're rendering, since there can be multiple models rendered
-         *  at the same time, e.g. header/page/footer.
-         * - the API key is the same, since we don't want to preview content from other organizations.
-         * - if there is content, that the preview ID is the same as that of the one we receive.
-         *
-         * TO-DO: should we only update the state when there is a change?
-         **/
-        if (
-          searchParamPreviewModel === props.model &&
-          previewApiKey === props.apiKey &&
-          (!props.content || searchParamPreviewId === props.content.id)
-        ) {
-          getContent({
-            model: props.model,
-            apiKey: props.apiKey,
-            apiVersion: props.apiVersion,
-          }).then((content) => {
-            if (content) {
-              state.mergeNewContent(content);
-            }
-          });
-        }
-      }
-    }
-  });
-
-  onUnMount(() => {
-    if (isBrowser()) {
-      window.removeEventListener('message', state.processMessage);
-    }
-  });
   return (
     <Show when={builderContextSignal.value.content}>
       <EnableEditor
-        key={state.forceReRenderCount}
+        content={props.content}
         model={props.model}
         context={props.context}
         apiKey={props.apiKey}
