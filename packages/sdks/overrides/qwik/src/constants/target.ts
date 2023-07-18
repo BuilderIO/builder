3acc4cea99a02bfed4e8a5f0046cb40a3608e741
Sami Jaber <jabersami@gmail.com>
2023-07-17 21:26:13 -0300

    targets types

--- packages/sdks/overrides/qwik/src/constants/target.ts
+++ packages/sdks/overrides/qwik/src/constants/target.ts
@@ -1,2 +1,3 @@
+type Target = import('../types/targets').Target;
 /** This file should be overriden for each framework. Ideally this would be implemented in Mitosis.  */
-export const TARGET = 'qwik' as any;
+export const TARGET = 'qwik' as Target;
--- packages/sdks/overrides/svelte/src/constants/target.ts
+++ packages/sdks/overrides/svelte/src/constants/target.ts
@@ -1,2 +1,4 @@
+type Target = import('../types/targets').Target;
+
 /** This file should be overriden for each framework. Ideally this would be implemented in Mitosis.  */
-export const TARGET = 'svelte';
+export const TARGET = 'svelte' as Target;
