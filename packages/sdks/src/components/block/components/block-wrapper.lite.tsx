9baff100a3f85291bda8c46ee84a6fe813fca003
Sami Jaber <jabersami@gmail.com>
2023-07-19 09:05:29 -0300

    f

--- packages/sdks/src/components/block/components/block-wrapper.lite.tsx
+++ packages/sdks/src/components/block/components/block-wrapper.lite.tsx
@@ -8,7 +8,7 @@ import { getBlockProperties } from '../../../functions/get-block-properties.js';
 /**
  * This import is used by the Svelte SDK. Do not remove.
  */
-// eslint-disable-next-line unused-imports/no-unused-imports
+// eslint-disable-next-line unused-imports/no-unused-imports, @typescript-eslint/no-unused-vars
 import { setAttrs } from '../../../blocks/helpers';
 
 useMetadata({
