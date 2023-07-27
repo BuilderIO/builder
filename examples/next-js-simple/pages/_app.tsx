882a43b4414911f4ba6b2e8d6f13c0bdbda5a007
Tim Garibaldi <tgaribal@gmail.com>
2023-07-27 15:41:59 -0700

    set correct api key finally

--- examples/next-js-simple/pages/_app.tsx
+++ examples/next-js-simple/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import { builder } from '@builder.io/react'
 import builderConfig from '@config/builder'
 // builder.init('e331e76e501d4e75b90664fad3b86b17');
-builder.init('fb99890e035245938d554a1286ea46da', false)
+builder.init('271bdcf584e24ca896dede7a91dfb1cb')
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
