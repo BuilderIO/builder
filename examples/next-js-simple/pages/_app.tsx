1a6a3c0f910a0c3b780efafbd621d9fac03fb1de
Tim Garibaldi <tgaribal@gmail.com>
2023-07-27 16:32:44 -0700

    try this

--- examples/next-js-simple/pages/_app.tsx
+++ examples/next-js-simple/pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from 'next/app'
 
 import { builder } from '@builder.io/react'
 import builderConfig from '@config/builder'
-// builder.init('e331e76e501d4e75b90664fad3b86b17');
 builder.init('271bdcf584e24ca896dede7a91dfb1cb')
 
 export default function MyApp({ Component, pageProps }: AppProps) {
