118a486ad5c1a3c11593ec1a7b7d8321e4545bcf
Sami Jaber <jabersami@gmail.com>
2023-07-27 11:01:37 -0300

    fix

--- packages/sdks/e2e/nextjs-react-app-dir/src/app/[[...slug]]/page.tsx
+++ packages/sdks/e2e/nextjs-react-app-dir/src/app/[[...slug]]/page.tsx
@@ -7,7 +7,7 @@ import {
 import MyTextBox from '../../components/MyTextBox/MyTextBox';
 import { componentInfo } from '../../components/MyTextBox/component-info';
 import CatFacts from '@/components/MyTextBox/CatFacts';
-import { getAllPathnames, getProps } from '@builder.io/sdks-e2e-tests';
+import { getProps } from '@builder.io/sdks-e2e-tests';
 
 interface MyPageProps {
   params: {
@@ -61,9 +61,10 @@ export default async function Page(props: MyPageProps) {
 }
 export const revalidate = 1;
 
+// TO-DO: this breaks the build.
 // Return a list of `params` to populate the [slug] dynamic segment
-export async function generateStaticParams() {
-  return getAllPathnames('gen2').map((path) => ({
-    slug: path === '/' ? null : path.split('/').filter(Boolean),
-  }));
-}
+// export async function generateStaticParams() {
+//   return getAllPathnames('gen2').map((path) => ({
+//     slug: path === '/' ? null : path.split('/').filter(Boolean),
+//   }));
+// }
