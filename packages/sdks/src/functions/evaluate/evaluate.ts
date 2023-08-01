a5d62d605095687a698b42eb2ac0e80e7e9df6ac
Sami Jaber <jabersami@gmail.com>
2023-07-31 21:08:21 -0300

    docs

--- packages/sdks/src/functions/evaluate/evaluate.ts
+++ packages/sdks/src/functions/evaluate/evaluate.ts
@@ -19,7 +19,7 @@ let runInNonNode: typeof import('./non-node-runtime.js').runInNonNode;
       runInNonNode = (await import('./non-node-runtime.js')).runInNonNode;
     } catch (err) {
       throw new Error(
-        `[Builder.io]: Error importing JS interpreter for non-node runtimes. Make sure you have js-interpreter installed.
+        `[Builder.io]: Error importing JS interpreter for non-node runtimes. Make sure you have \`js-interpreter\` installed.
         Read more here: https://github.com/BuilderIO/builder/tree/main/packages/sdks/README.md#non-nodejs-runtimes-edge-serverless
         ` + err
       );
