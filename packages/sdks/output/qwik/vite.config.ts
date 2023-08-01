a4851a8af9e524b9c76f04413c766c763058a7ba
Sami Jaber <jabersami@gmail.com>
2023-08-01 09:10:19 -0300

    ugprade qwik n vite

--- packages/sdks/output/qwik/vite.config.ts
+++ packages/sdks/output/qwik/vite.config.ts
@@ -4,16 +4,12 @@ import { qwikVite } from '@builder.io/qwik/optimizer';
 export default defineConfig(() => {
   return {
     build: {
-      target: 'es2020',
       lib: {
         entry: './src/index.ts',
         formats: ['es', 'cjs'],
         fileName: (format) => `index.qwik.${format === 'es' ? 'mjs' : 'cjs'}`,
       },
-      minify: false,
       rollupOptions: {
-        // make sure to externalize deps that shouldn't be bundled
-        // into your library
         external: ['@builder.io/qwik', 'js-interpreter'],
       },
     },
