8f2b5284f7e0290053424b01f58885e0e601922e
Sami Jaber <jabersami@gmail.com>
2023-08-02 16:23:02 -0300

    Fix: CI yarn install slowdown (#2521)
    * fix
    
    * test
    
    * try again
    
    * f
    
    * fix
    
    * update
    
    * update
    
    * add more deps
    
    * l
    
    * fix
    
    * fix

--- packages/sdks/src/blocks/text/text.lite.tsx
+++ packages/sdks/src/blocks/text/text.lite.tsx
@@ -1,9 +1,17 @@
+import { useTarget } from '@builder.io/mitosis';
+
 export default function Text(props: { text?: string }) {
   return (
     <span
       class="builder-text"
       innerHTML={props.text?.toString() || ''}
       style={{ outline: 'none' }}
+      {...useTarget({
+        reactNative: {
+          dataSet: { 'builder-text': 'true' },
+        },
+        default: {},
+      })}
     />
   );
 }
