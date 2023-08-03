067c88b9810289f45b62504afe44654561288aab
Sami Jaber <jabersami@gmail.com>
2023-08-03 16:24:22 -0300

    f

--- packages/sdks/src/functions/is-non-node-server.ts
+++ packages/sdks/src/functions/is-non-node-server.ts
@@ -4,8 +4,8 @@ import { isBrowser } from './is-browser';
  * Identifies non-node server runtimes (edge, workers, serverless, etc.)
  */
 export function isNonNodeServer(): boolean {
-  const hasNode = () =>
-    typeof process !== 'undefined' && process?.versions?.node;
+  // const hasNode = () =>
+  //   typeof process !== 'undefined' && process?.versions?.node;
 
   return !isBrowser();
 }
