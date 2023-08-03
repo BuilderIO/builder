3f4ce700407a2c1b15b4ac7afcfc20170e797b93
Sami Jaber <jabersami@gmail.com>
2023-08-03 15:41:52 -0300

    add choices

--- packages/sdks/src/functions/fetch-builder-props.ts
+++ packages/sdks/src/functions/fetch-builder-props.ts
@@ -1,28 +1,64 @@
 import { getBuilderSearchParams } from '..';
 import type { ContentVariantsProps } from '../components/content-variants/content-variants.types';
+import type { Dictionary } from '../types/typescript';
 import { fetchContent } from './get-content';
 import type { GetContentOptions } from './get-content/types';
 
-type GetBuilderPropsOptions = GetContentOptions & { url?: URL };
+type GetBuilderPropsOptions = (Omit<GetContentOptions, 'model'> & {
+  model?: string;
+}) &
+  (
+    | {
+        /**
+         * The current URL path. Used to determine the `urlPath` for targeting content.
+         *
+         * Cannot be used with `url`.
+         */
+        path: string;
+
+        /**
+         * The current URL search params. Used to parse the `searchParams` for targeting content.
+         *
+         * Cannot be used with `url`.
+         */
+        searchParams?: URLSearchParams | Dictionary<string | string[]>;
+        url?: undefined;
+      }
+    | {
+        /**
+         * The current URL. Used to determine the `urlPath` for targeting content and
+         * to parse the `searchParams` for targeting content.
+         *
+         * Cannot be used with `path` or `searchParams`.
+         */
+        url: URL;
+        path?: undefined;
+        searchParams?: undefined;
+      }
+    | {
+        url?: undefined;
+        path?: undefined;
+        searchParams?: undefined;
+      }
+  );
 
 export const fetchBuilderProps = async (
   _args: GetBuilderPropsOptions
 ): Promise<ContentVariantsProps> => {
-  const urlPath = _args.url
-    ? _args.url.pathname
-    : _args.userAttributes?.urlPath;
+  const urlPath =
+    _args.path || _args.url?.pathname || _args.userAttributes?.urlPath;
 
   const getContentArgs: GetContentOptions = {
     ..._args,
     apiKey: _args.apiKey,
-    model: _args.model,
+    model: _args.model || 'page',
     userAttributes: {
       ..._args.userAttributes,
       ...(urlPath ? { urlPath } : {}),
     },
-    options: _args.url
-      ? getBuilderSearchParams(_args.url.searchParams)
-      : _args.options,
+    options: getBuilderSearchParams(
+      _args.searchParams || _args.url?.searchParams || _args.options
+    ),
   };
 
   return {
