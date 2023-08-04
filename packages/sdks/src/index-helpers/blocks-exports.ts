0757a1fedd6d3a10239357ba4f3bc8843c4e846d
Sami Jaber <jabersami@gmail.com>
2023-08-04 09:27:26 -0300

    deprecate RenderBlocks and RenderContent

--- packages/sdks/src/index-helpers/blocks-exports.ts
+++ packages/sdks/src/index-helpers/blocks-exports.ts
@@ -2,9 +2,21 @@ export { default as Button } from '../blocks/button/button.lite';
 export { default as Columns } from '../blocks/columns/columns.lite';
 export { default as Fragment } from '../blocks/fragment/fragment.lite';
 export { default as Image } from '../blocks/image/image.lite';
-export { default as RenderBlocks } from '../components/blocks/blocks.lite';
 export { default as Section } from '../blocks/section/section.lite';
 export { default as Symbol } from '../blocks/symbol/symbol.lite';
 export { default as Text } from '../blocks/text/text.lite';
 export { default as Video } from '../blocks/video/video.lite';
-export { default as RenderContent } from '../components/content-variants/content-variants.lite';
+
+import { default as Blocks } from '../components/blocks/blocks.lite';
+import { default as Content } from '../components/content-variants/content-variants.lite';
+
+export { Blocks, Content };
+
+/**
+ * @deprecated Use `Blocks` instead.
+ */
+export const RenderBlocks = Blocks;
+/**
+ * @deprecated Use `Content` instead.
+ */
+export const RenderContent = Content;
