b3aeb462a6ead1841e6425cb7fbe7fbf896c7ec8
Sami Jaber <jabersami@gmail.com>
2023-08-07 11:00:06 -0300

    m

--- packages/sdks/e2e/qwik-city/src/routes/[...index]/index.tsx
+++ packages/sdks/e2e/qwik-city/src/routes/[...index]/index.tsx
@@ -1,18 +1,8 @@
-import * as ivm from 'isolated-vm';
 import { getProps } from '@e2e/tests';
 import { RenderContent, _processContentResult } from '@builder.io/sdk-qwik';
 import { component$ } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
-const getIsolateContext = () => {
-  // if (Builder.serverContext) {
-  //   return Builder.serverContext;
-  // }
-  // Builder.setServerContext(isolate.createContextSync());
-  const isolate = new ivm.Isolate({
-    memoryLimit: 128,
-  });
-  return isolate.createContextSync();
-};
+
 export interface MainProps {
   url: string;
 }
@@ -31,34 +21,6 @@ export const useBuilderContentLoader = routeLoader$(async (event) => {
 });
 
 export default component$(() => {
-  const isolateContext = getIsolateContext();
-  const foo = { foo: 123, bar: { bazz: 1 } };
-  isolateContext.global.setSync('foo', new ivm.ExternalCopy(foo).copyInto());
-  const resultStr = isolateContext.evalSync(
-    `
-  var newProxy = new Proxy({}, {
-    get(target, key) {
-      return target[key]
-    },
-    set(target, key, value) {
-      target[key] = value;
-      return true
-    },
-    deleteProperty(target, key) {
-      delete target[key];
-      return true
-    }
-  })
-
-  function theFunction(){
-  return foo.bar.bazz
-}
-
-theFunction()
-  `
-  );
-
-  // return <div>test {resultStr}</div>;
   const contentProps = useBuilderContentLoader();
   return contentProps.value ? (
     <RenderContent {...contentProps.value} />
