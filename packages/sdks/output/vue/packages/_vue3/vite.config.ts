2f92836f89aff985e15d833e6649b470dd58144b
Sami Jaber <jabersami@gmail.com>
2023-08-07 13:00:37 -0300

    fix vue

--- packages/sdks/output/vue/packages/_vue3/vite.config.ts
+++ packages/sdks/output/vue/packages/_vue3/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue', 'js-interpreter'],
+      external: ['vue', 'js-interpreter', 'isolated-vm'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
