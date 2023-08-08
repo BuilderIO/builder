a5d3ba553875c726b43b0ad3f16bc80a663fba04
Sami Jaber <jabersami@gmail.com>
2023-08-08 17:13:31 -0300

    fix???

--- packages/sdks/output/qwik/vite.config.ts
+++ packages/sdks/output/qwik/vite.config.ts
@@ -1,13 +1,32 @@
 import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 
-export default defineConfig(() => {
+type SdkEnv = 'node' | 'edge' | 'browser';
+
+const getEvaluatorPath = (sdkEnv: SdkEnv) => {
+  switch (sdkEnv) {
+    case 'node':
+      return './node-runtime/index.ts';
+    case 'edge':
+      return './non-node-runtime/index.ts';
+    case 'browser':
+      return './browser-runtime/index.ts';
+  }
+};
+
+export default defineConfig((a) => {
   return {
+    resolve: {
+      alias: {
+        '#code-evaluator': getEvaluatorPath(process.env.SDK_ENV as SdkEnv),
+      },
+    },
     build: {
+      outDir: ['lib', process.env.SDK_ENV].join('/'),
       lib: {
         entry: './src/index.ts',
         formats: ['es', 'cjs'],
-        fileName: (format) => `index.qwik.${format === 'es' ? 'mjs' : 'cjs'}`,
+        fileName: (format) => `index.${format === 'es' ? 'mjs' : 'cjs'}`,
       },
       rollupOptions: {
         external: ['@builder.io/qwik', 'js-interpreter', 'isolated-vm'],
