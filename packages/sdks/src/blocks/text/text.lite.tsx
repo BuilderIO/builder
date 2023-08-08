7e1c5c9213066d17003b0a48612f40f5d229ffa0
Daniel Moore <daniel@builder.io>
2023-08-08 12:09:40 -0700

    Clean up React Text component for new inline editing support.

--- packages/sdks/src/blocks/text/text.lite.tsx
+++ packages/sdks/src/blocks/text/text.lite.tsx
@@ -3,7 +3,10 @@ import { useTarget } from '@builder.io/mitosis';
 export default function Text(props: { text?: string }) {
   return (
     <span
-      class="builder-text"
+      class={
+        /* NOTE: This class name must be "builder-text" for inline editing to work in the Builder editor */
+        'builder-text'
+      }
       innerHTML={props.text?.toString() || ''}
       style={{ outline: 'none' }}
       {...useTarget({
