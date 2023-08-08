ded178482076ec3364662b48cbbeae22f018d0e9
Daniel Moore <daniel@builder.io>
2023-08-08 11:34:02 -0700

    Change element type of text components to div

--- packages/sdks/src/blocks/text/text.lite.tsx
+++ packages/sdks/src/blocks/text/text.lite.tsx
@@ -2,7 +2,7 @@ import { useTarget } from '@builder.io/mitosis';
 
 export default function Text(props: { text?: string }) {
   return (
-    <span
+    <div
       class={
         /* NOTE: This class name must be "builder-text" for inline editing to work in the Builder editor */
         'builder-text'
