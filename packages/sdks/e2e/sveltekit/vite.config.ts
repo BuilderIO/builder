601275bfae92a0ef10b428a9810c726d39168790
Sami Jaber <jabersami@gmail.com>
2023-08-10 12:24:05 -0300

    rm

--- packages/sdks/e2e/sveltekit/vite.config.ts
+++ packages/sdks/e2e/sveltekit/vite.config.ts
@@ -2,9 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ['isolated-vm'],
-  },
   resolve: {
     preserveSymlinks: true,
   },
