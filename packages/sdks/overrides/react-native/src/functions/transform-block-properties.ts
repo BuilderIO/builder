a4541a9c7afa7c67c9580a6af30bb6e59405efbc
Sami Jaber <jabersami@gmail.com>
2023-08-11 11:15:28 -0300

    fix drag-n-drop issue

--- packages/sdks/overrides/react-native/src/functions/transform-block-properties.ts
+++ packages/sdks/overrides/react-native/src/functions/transform-block-properties.ts
@@ -6,12 +6,18 @@ export function transformBlockProperties(block: BuilderBlock) {
   block.className = block.class;
   delete block.class;
 
+  const id = block['builder-id'];
+
+  if (!id && isEditing()) {
+    console.warn('No builder-id found on block', block);
+  }
+
   block.ref = (ref) => {
     if (isEditing()) {
       const el = findDOMNode(ref);
       if (el) {
-        el.setAttribute('builder-id', block.id);
-        el.classList.add(block.id);
+        el.setAttribute('builder-id', id);
+        el.classList.add(id);
       }
     }
   };
