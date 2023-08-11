10693d194dbc634f58744f4da65a8a13835ee2a2
Sami Jaber <jabersami@gmail.com>
2023-08-11 09:43:11 -0300

    f

--- examples/react-native/App.jsx
+++ examples/react-native/App.jsx
@@ -1,12 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React, { useState, useEffect } from 'react';
-import {
-  RenderContent,
-  isPreviewing,
-  getContent,
-  ContentVariants,
-} from '@builder.io/sdk-react-native';
+import { RenderContent, isPreviewing, getContent } from '@builder.io/sdk-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
