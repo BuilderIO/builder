8a2ab0597499161d8c91decdac558c4b3aae4486
Sami Jaber <jabersami@gmail.com>
2023-08-15 16:53:40 -0300

    revert nuxt2 change

--- packages/sdks/e2e/nuxt2/nuxt.config.js
+++ packages/sdks/e2e/nuxt2/nuxt.config.js
@@ -34,7 +34,7 @@ export default {
 
   build: {
     // This is needed because the package is an ESM module
-    transpile: ['@e2e/tests'],
+    transpile: ['@builder.io/sdk-vue', '@e2e/tests'],
   },
 
   // We need to import the CSS for the SDK in the Nuxt Config here
