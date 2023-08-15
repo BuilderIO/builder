3e884f7fc995edc06c641d70ea1f81909946009a
Sami Jaber <jabersami@gmail.com>
2023-08-15 15:38:27 -0300

    setup vue SDK bundling

--- packages/sdks/output/vue/packages/_vue2/vite.config.js
+++ packages/sdks/output/vue/packages/_vue2/vite.config.js
@@ -2,9 +2,16 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue2 from '@vitejs/plugin-vue2';
 import dts from 'vite-plugin-dts';
+import {
+  getEvaluatorPathAlias,
+  getSdkOutputPath,
+} from '@builder.io/sdks/output-generation';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: getEvaluatorPathAlias('input'),
+  },
   plugins: [
     vue2(),
     dts({ insertTypesEntry: true }),
@@ -12,18 +19,16 @@ export default defineConfig({
     // libCss(),
   ],
   build: {
+    outDir: getSdkOutputPath(),
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       fileName: 'sdk',
-      formats: ['cjs', 'es'],
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format === 'es' ? 'mjs' : 'cjs'}`,
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['vue', 'js-interpreter', 'isolated-vm'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           vue: 'Vue',
         },
--- packages/sdks/output/vue/packages/_vue3/vite.config.js
+++ packages/sdks/output/vue/packages/_vue3/vite.config.js
@@ -2,20 +2,29 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
+import {
+  getEvaluatorPathAlias,
+  getSdkOutputPath,
+} from '@builder.io/sdks/output-generation';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: getEvaluatorPathAlias('input'),
+  },
   plugins: [
     vue(),
-    dts({ insertTypesEntry: true }),
+    dts({}),
     // https://stackoverflow.com/a/72572426
     // libCss(),
   ],
   build: {
+    outDir: getSdkOutputPath(),
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       fileName: 'sdk',
-      formats: ['cjs', 'es'],
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format === 'es' ? 'mjs' : 'cjs'}`,
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
