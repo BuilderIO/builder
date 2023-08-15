a023b17459a6b30fa53d86ba36302561fa82f6f4
Sami Jaber <jabersami@gmail.com>
2023-08-15 09:39:10 -0300

    try

--- packages/sdks/output/nextjs/vite.config.js
+++ packages/sdks/output/nextjs/vite.config.js
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import {
   getEvaluatorPathAlias,
   getSdkOutputPath,
@@ -8,6 +9,7 @@ import {
 import * as packageJson from './package.json';
 
 const USE_CLIENT_BUNDLE_NAME = 'USE_CLIENT_BUNDLE';
+const USE_SERVER_BUNDLE_NAME = 'USE_SERVER_BUNDLE';
 
 export default defineConfig({
   resolve: {
@@ -22,24 +24,25 @@ export default defineConfig({
       fileName: (format) => `index.${format === 'es' ? 'mjs' : 'cjs'}`,
     },
     rollupOptions: {
+      // plugins: [nodeResolve()],
       external: [
         ...Object.keys(packageJson.peerDependencies).map(
           (key) => new RegExp(`node_modules/${key}`)
         ),
       ],
       output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
         manualChunks(id, { getModuleInfo }) {
           if (getModuleInfo(id).code.match(/^['"]use client['"]/)) {
             return USE_CLIENT_BUNDLE_NAME;
+          } else if (getModuleInfo(id).code.match(/^['"]use server['"]/)) {
+            return USE_SERVER_BUNDLE_NAME;
           }
         },
         banner(chunk) {
           if (chunk.name === USE_CLIENT_BUNDLE_NAME) {
             return "'use client';";
+          } else if (chunk.name === USE_SERVER_BUNDLE_NAME) {
+            return "'use server';";
           }
         },
       },
