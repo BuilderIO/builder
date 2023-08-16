c7ad025863a7a2a6ad04607469796010f7fa8b9c
Sami Jaber <jabersami@gmail.com>
2023-08-16 15:58:20 -0300

    fix

--- packages/sdks/output/qwik/vite.config.ts
+++ packages/sdks/output/qwik/vite.config.ts
@@ -15,7 +15,10 @@ export default defineConfig(() => {
       lib: {
         entry: './src/index.ts',
         formats: ['es', 'cjs'],
-        fileName: (format) => `index.${format === 'es' ? 'mjs' : 'cjs'}`,
+        /**
+         * https://github.com/BuilderIO/qwik/issues/4952
+         */
+        fileName: (format) => `index.qwik.${format === 'es' ? 'mjs' : 'cjs'}`,
       },
       rollupOptions: {
         external: ['@builder.io/qwik', 'js-interpreter', 'isolated-vm'],
