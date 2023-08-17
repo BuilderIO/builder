be15abf78790ff373931957a36a084a4187ef848
Sami Jaber <jabersami@gmail.com>
2023-08-17 10:47:08 -0300

    yeeha

--- packages/sdks/output/solid/env.d.ts
+++ packages/sdks/output/solid/env.d.ts
@@ -1,18 +1,18 @@
 declare global {
   interface ImportMeta {
     env: {
-      NODE_ENV: 'production' | 'development'
-      PROD: boolean
-      DEV: boolean
-    }
+      NODE_ENV: 'production' | 'development';
+      PROD: boolean;
+      DEV: boolean;
+    };
   }
   namespace NodeJS {
     interface ProcessEnv {
-      NODE_ENV: 'production' | 'development'
-      PROD: boolean
-      DEV: boolean
+      NODE_ENV: 'production' | 'development';
+      PROD: boolean;
+      DEV: boolean;
     }
   }
 }
 
-export {}
+export {};
