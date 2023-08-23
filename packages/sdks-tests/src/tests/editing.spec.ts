7a809aacabc721f6c85da21440601bb88214ef90
Sami Jaber <jabersami@gmail.com>
2023-08-23 15:21:09 -0300

    Docs: SDKs publishing (#2564)
    * fix qwik example
    
    * docs
    
    * publishing docs
    
    * exclude more tests
    
    * point

--- packages/sdks-tests/src/tests/editing.spec.ts
+++ packages/sdks-tests/src/tests/editing.spec.ts
@@ -41,6 +41,8 @@ test.describe('Visual Editing', () => {
         packageName === 'react-native' ||
         packageName === 'next-app-dir' ||
         packageName === 'vue3' ||
+        packageName === 'vue2' ||
+        packageName === 'sveltekit' ||
         packageName === 'nuxt3'
     );
 
@@ -80,6 +82,8 @@ test.describe('Visual Editing', () => {
         packageName === 'react-native' ||
         packageName === 'next-app-dir' ||
         packageName === 'vue3' ||
+        packageName === 'vue2' ||
+        packageName === 'sveltekit' ||
         packageName === 'nuxt3'
     );
 
