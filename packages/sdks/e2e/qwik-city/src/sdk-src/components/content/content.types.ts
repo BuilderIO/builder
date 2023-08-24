b164b7d276cf2d7e72c599fbfb40c3c64107b5bc
Sami Jaber <jabersami@gmail.com>
2023-08-24 13:29:56 -0300

    revert

--- packages/sdks/e2e/qwik-city/src/sdk-src/components/content/content.types.ts
+++
@@ -1,9 +0,0 @@
-interface InternalRenderProps {
-  /**
-   * TO-DO: improve qwik generator to not remap this name for non-HTML tags, then name it `className`
-   */
-  classNameProp: string | undefined;
-  showContent: boolean;
-  isSsrAbTest: boolean;
-}
-export type ContentProps = InternalRenderProps;
--- packages/sdks/e2e/qwik-city/src/sdk-src/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as Content } from './components/content/content';
