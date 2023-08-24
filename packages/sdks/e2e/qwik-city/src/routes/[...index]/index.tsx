2329e0b4b0519c92a319f7be2d12a240c7a7f46d
Sami Jaber <jabersami@gmail.com>
2023-08-24 13:29:17 -0300

    revert

--- packages/sdks/e2e/qwik-city/src/routes/[...index]/index.tsx
+++ packages/sdks/e2e/qwik-city/src/routes/[...index]/index.tsx
@@ -1,52 +1,33 @@
+import { getProps } from '@e2e/tests';
+import { RenderContent, _processContentResult } from '@builder.io/sdk-qwik';
 import { component$ } from '@builder.io/qwik';
-import ContentComponent from '~/sdk-src/components/content/content';
+import { routeLoader$ } from '@builder.io/qwik-city';
+
+export interface MainProps {
+  url: string;
+}
+
+export const useBuilderContentLoader = routeLoader$(async (event) => {
+  const data = await getProps({
+    pathname: event.url.pathname,
+    _processContentResult,
+  });
+
+  if (!data) {
+    event.status(404);
+  }
+  return data;
+});
 
 export default component$(() => {
+  const contentProps = useBuilderContentLoader();
   return (
-    <ContentComponent
-      content={{
-        data: {
-          title: 'Columns',
-          blocks: [
-            {
-              '@type': '@builder.io/sdk:Element',
-              id: 'builder-25c64e9c18804f46b73985264df3c41c',
-              component: {
-                name: 'Columns',
-                options: {
-                  columns: [
-                    {
-                      blocks: [
-                        {
-                          '@type': '@builder.io/sdk:Element',
-                          '@version': 2,
-                          id: 'builder-71c14581f362486eb24214d27c0c24d0',
-                          component: {
-                            name: 'Text',
-                            options: { text: '<p>text in column 1</p>' },
-                          },
-                        },
-                      ],
-                    },
-                  ],
-                },
-              },
-            },
-          ],
-        },
-        id: 'f24c6940ee5f46458369151cc9ec598c',
-      }}
-      showContent={true}
-      classNameProp={undefined}
-      data={undefined}
-      context={undefined}
-      apiVersion={undefined}
-      customComponents={undefined}
-      canTrack={undefined}
-      locale={undefined}
-      includeRefs={undefined}
-      enrich={undefined}
-      isSsrAbTest={false}
-    />
+    <>
+      {contentProps.value ? (
+        <RenderContent {...contentProps.value} />
+      ) : (
+        <div>Content Not Found</div>
+      )}
+    </>
   );
 });
