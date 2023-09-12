bdd0834f3843847f47a9423f2e767984ce627804
Aziz Abbas <aziz@builder.io>
2023-09-12 15:58:57 -0500

    fix(core): do not rely on setImmediate in nexttick on server (#2577)
    * fix(core): do not rely on setImmediate in nexttick on server
    
    * release sdk
    
    * release react sdk

--- packages/core/src/functions/next-tick.function.ts
+++ packages/core/src/functions/next-tick.function.ts
@@ -6,19 +6,13 @@ const isClient = typeof window !== 'undefined';
 
 // TODO: queue all of these in a debounceNextTick
 export function nextTick(fn: () => void) {
-  // React native
-  if (typeof setImmediate === 'function' && typeof window === 'undefined') {
-    return setImmediate(fn);
-  }
-  // TODO: should this be setImmediate instead? Forgot if that is micro or macro task
-  // TODO: detect specifically if is server
   // if (typeof process !== 'undefined' && process.nextTick) {
   //   console.log('process.nextTick?');
   //   process.nextTick(fn);
   //   return;
   // }
   // FIXME: fix the real safari issue of this randomly not working
-  if (isSafari || typeof MutationObserver === 'undefined') {
+  if (!isClient || isSafari || typeof MutationObserver === 'undefined') {
     setTimeout(fn);
     return;
   }
