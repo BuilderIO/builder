6746cf16ad74242620e99a92f7f9e4e83cf4de1f
Sami Jaber <jabersami@gmail.com>
2023-10-31 12:30:15 -0300

    fix

--- packages/sdks/scripts/version-sdks.sh
+++ packages/sdks/scripts/version-sdks.sh
@@ -4,9 +4,9 @@ DIST_TAG=$1
 SDK=$2
 
 function version {
-  echo "Bumping @builder.io/$SDK@$DIST_TAG..."
+  echo "Bumping @builder.io/$1@$DIST_TAG..."
 
-  cd "output/$SDK"
+  cd "output/$1"
 
   if [[ "$DIST_TAG" == 'dev' ]]; then
     npm version prerelease
@@ -14,20 +14,20 @@ function version {
     npm version $DIST_TAG
   fi
 
-  echo "Bumped @builder.io/$SDK@$DIST_TAG."
+  echo "Bumped @builder.io/$1@$DIST_TAG."
 }
 
 if [[ "$SDK" == 'all' ]]; then
   echo "Bumping all SDKs with tag $DIST_TAG."
   for i in nextjs svelte react-native solid qwik react vue; do
     # run these loop commands in parallel
-    version $DIST_TAG $i &
+    version $i &
   done
   # wait for all the parallel commands to finish
   wait
 else
   if [[ "$DIST_TAG" == 'dev' ]]; then
-    version $DIST_TAG $SDK
+    version $SDK
   else
     echo "Error: cannot release $VERSION version of only one SDK. You must release all SDKs at once to keep them in sync."
     echo "Error: only 'dev' versions are allowed to be released on a per-SDK basis."
