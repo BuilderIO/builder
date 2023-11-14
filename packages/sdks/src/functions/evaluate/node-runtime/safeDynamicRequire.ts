71c11637cfe3d209dddf143695f35de0441a9adf
Sami Jaber <jabersami@gmail.com>
2023-11-14 17:42:30 -0400

    doc
    f

--- packages/sdks/src/functions/evaluate/node-runtime/safeDynamicRequire.ts
+++ packages/sdks/src/functions/evaluate/node-runtime/safeDynamicRequire.ts
@@ -1,14 +1,14 @@
 const noop = () => null;
 
 /**
- * Allow us to require things dynamically safe from webpack/vite bundling.
- * We need this to be able to import `isolated-vm`, which for some reason has weird
- * build issues when imported (even directly) into certain web apps.
+ * Allows us to require node-specific code safely within bundlers.
  *
- * NOTE: this code should only ever run in `node`, and never in an edge runtime. This is guaranteed
- * by the fact that each SDK has separate bundles for each runtime.
- * However, it might still end up running in the browser if the node bundle is loaded in the
- * browser (which happens in dev mode, and certain other scenarios like Qwik's prod mode).
+ * https://github.com/laverdet/isolated-vm/issues/423
+ *
+ * NOTE: this code should only ever run in `node`, and never in an `edge` runtime.
+ * This is guaranteed by the fact that each SDK has separate bundles for each runtime.
+ * However, it might still end up running in the `browser` if the `node` bundle is loaded in the
+ * browser (which happens in many apps' dev mode, and certain other scenarios like Qwik's prod mode).
  */
 export let safeDynamicRequire: typeof require =
   noop as unknown as typeof require;
