0dc1548c591ecc42e1bae173264061c39aa2d804
Sami Jaber <jabersami@gmail.com>
2023-11-15 13:09:34 -0400

    fix nuxt3 build

--- packages/sdks/e2e/nuxt3/nuxt.config.ts
+++ packages/sdks/e2e/nuxt3/nuxt.config.ts
@@ -1,2 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  nitro: {
+    rollupConfig: {
+      external: ['@builder.io/sdk-vue'],
+    },
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ['@builder.io/sdk-vue'],
+      },
+    },
+  },
+});
