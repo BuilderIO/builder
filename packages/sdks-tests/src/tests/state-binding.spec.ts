45e7a41a310f3183f0c0307bcefce259aa3495e2
Sami Jaber <jabersami@gmail.com>
2023-11-16 13:46:46 -0400

    add back restrictions

--- packages/sdks-tests/src/tests/state-binding.spec.ts
+++ packages/sdks-tests/src/tests/state-binding.spec.ts
@@ -1,9 +1,21 @@
 import { expect } from '@playwright/test';
-import { test } from './helpers.js';
+import { isRNSDK, test } from './helpers.js';
 
 test.describe('State binding', () => {
   test.describe('inside repeater', () => {
-    test('writing to state should update binding', async ({ page }) => {
+    test('writing to state should update binding', async ({ page, packageName }) => {
+      test.skip(
+        isRNSDK ||
+          packageName === 'react-native' ||
+          packageName === 'vue2' ||
+          packageName === 'nuxt2' ||
+          packageName === 'solid' ||
+          packageName === 'solid-start' ||
+          packageName === 'svelte' ||
+          packageName === 'sveltekit' ||
+          packageName === 'next-app-dir'
+      );
+
       await page.goto('/state-binding/', { waitUntil: 'networkidle' });
       await expect(page.locator('text=initial Name')).toContainText('initial Name');
       await page.click('text=first');
