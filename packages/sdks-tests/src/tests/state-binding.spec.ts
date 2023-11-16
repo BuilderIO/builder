5d42d2a0338291720f74d9cb446266ef04d05e0a
Sami Jaber <jabersami@gmail.com>
2023-11-16 13:37:00 -0400

    try to run state binding eveyrwehre

--- packages/sdks-tests/src/tests/state-binding.spec.ts
+++ packages/sdks-tests/src/tests/state-binding.spec.ts
@@ -1,29 +1,9 @@
 import { expect } from '@playwright/test';
-import { isRNSDK, test } from './helpers.js';
+import { test } from './helpers.js';
 
 test.describe('State binding', () => {
   test.describe('inside repeater', () => {
-    test('writing to state should update binding', async ({ page, packageName }) => {
-      /**
-       * This only works in Qwik at the moment.
-       */
-      if (
-        isRNSDK ||
-        packageName === 'react-native' ||
-        packageName === 'vue2' ||
-        packageName === 'nuxt2' ||
-        packageName === 'solid' ||
-        packageName === 'solid-start' ||
-        packageName === 'svelte' ||
-        packageName === 'sveltekit' ||
-        packageName === 'next-app-dir' ||
-        packageName === 'vue3' ||
-        packageName === 'nuxt3'
-      ) {
-        test.skip();
-        return;
-      }
-
+    test('writing to state should update binding', async ({ page }) => {
       await page.goto('/state-binding/', { waitUntil: 'networkidle' });
       await expect(page.locator('text=initial Name')).toContainText('initial Name');
       await page.click('text=first');
