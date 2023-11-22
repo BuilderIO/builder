208cbdd49ccc6563444ed1560666ddb81c2e2d5f
Sami Jaber <jabersami@gmail.com>
2023-11-22 18:15:11 -0400

    yeeteth

--- examples/next-js-sdk-gen-2-experimental-app-directory/next.config.mjs
+++ examples/next-js-sdk-gen-2-experimental-app-directory/next.config.mjs
@@ -2,12 +2,6 @@
  * @type {import('next').NextConfig}
  */
 export default {
-  experimental: {
-    /**
-     * This setting is required for Builder's Visual Editor to work with your site.
-     */
-    serverActions: true,
-  },
   /**
    * This setting is required for Builder's Visual Editor to work with your site.
    */
