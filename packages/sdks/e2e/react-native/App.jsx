426af58e9b9c879fa73076e6512f6f38632ce64b
Sami Jaber <jabersami@gmail.com>
2023-11-29 11:56:34 -0400

    rm polyfill

--- packages/sdks/e2e/react-native/App.jsx
+++ packages/sdks/e2e/react-native/App.jsx
@@ -1,7 +1,3 @@
-import { setupURLPolyfill } from 'react-native-url-polyfill';
-
-setupURLPolyfill();
-
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import React, { Fragment, useEffect, useState } from 'react';
