727e8821774db3866cd012a2458b553fdc9bced6
Sami Jaber <jabersami@gmail.com>
2023-12-01 13:30:29 -0400

    fix solid start

--- packages/sdks/e2e/solid-start/src/routes/[...page].tsx
+++ packages/sdks/e2e/solid-start/src/routes/[...page].tsx
@@ -1,16 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import { RenderContent, _processContentResult } from '@builder.io/sdk-solid';
-
 import { getProps } from '@e2e/tests';
 import { Show, createResource } from 'solid-js';
-import { useLocation, useRouteData } from 'solid-start';
+import { Title, useLocation, useRouteData } from 'solid-start';
 
 export function routeData() {
   const location = useLocation();
-  const [props] = createResource(() =>
-    getProps({ pathname: location.pathname, _processContentResult })
-  );
+  const [props] = createResource(() => {
+    return getProps({ pathname: location.pathname, _processContentResult });
+  });
 
   return { props };
 }
@@ -19,8 +16,11 @@ export default function App() {
   const { props } = useRouteData<typeof routeData>();
 
   return (
-    <Show when={props} fallback={<div>Content Not Found</div>}>
-      <RenderContent {...props()} />
-    </Show>
+    <main>
+      <Title>Hello World</Title>
+      <Show when={props} fallback={<div>Content Not Found</div>}>
+        <RenderContent {...props()} />
+      </Show>
+    </main>
   );
 }
