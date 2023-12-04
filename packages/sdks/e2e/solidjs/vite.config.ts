2e4b24ac1531f94270c09101add8402af77139b7
Sami Jaber <jabersami@gmail.com>
2023-12-04 14:08:56 -0400

    f

--- packages/sdks/e2e/solidjs/vite.config.ts
+++ packages/sdks/e2e/solidjs/vite.config.ts
@@ -10,8 +10,7 @@ export default defineConfig({
     target: 'esnext',
   },
   optimizeDeps: {
-    // sub-dependencies of our sym-linked package must be manually included here
-    // include: ['solid-styled-components'],
+    exclude: ['isolated-vm'],
   },
   resolve: {
     // we must preserve symlinks for our sym-linked package to work properly
