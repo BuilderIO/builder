563e57e37b8703136dff79aa2f5a022a2d009e7d
sidmohanty11 <sidmohanty11@gmail.com>
2023-12-12 19:06:06 +0530

    fix video element discrepancy in gen2 sdk

--- packages/sdks/src/blocks/video/video.lite.tsx
+++ packages/sdks/src/blocks/video/video.lite.tsx
@@ -1,4 +1,4 @@
-import { useMetadata, useStore } from '@builder.io/mitosis';
+import { Show, useMetadata, useStore } from '@builder.io/mitosis';
 
 useMetadata({
   rsc: {
@@ -31,6 +31,8 @@ export interface VideoProps {
     | 'bottom right';
   posterImage?: string;
   lazyLoad?: boolean;
+  children?: any;
+  fitContent?: boolean;
 }
 
 export default function Video(props: VideoProps) {
@@ -46,27 +48,75 @@ export default function Video(props: VideoProps) {
     },
     get spreadProps() {
       return {
-        ...props.attributes,
         ...state.videoProps,
       };
     },
   });
   return (
-    <video
-      {...state.spreadProps}
-      preload={props.preload || 'metadata'}
-      style={{
-        width: '100%',
-        height: '100%',
-        ...props.attributes?.style,
-        objectFit: props.fit,
-        objectPosition: props.position,
-        // Hack to get object fit to work as expected and
-        // not have the video overflow
-        borderRadius: 1,
-      }}
-      src={props.video || 'no-src'}
-      poster={props.posterImage}
-    />
+    <div style={{ position: 'relative' }}>
+      <video
+        {...state.spreadProps}
+        preload={props.preload || 'metadata'}
+        style={{
+          width: '100%',
+          height: '100%',
+          ...props.attributes?.style,
+          objectFit: props.fit,
+          objectPosition: props.position,
+          // Hack to get object fit to work as expected and
+          // not have the video overflow
+          zIndex: 2,
+          borderRadius: 1,
+          ...(props.aspectRatio
+            ? {
+                position: 'absolute',
+              }
+            : null),
+        }}
+        src={props.video || 'no-src'}
+        poster={props.posterImage}
+        class="builder-video"
+      >
+        {!props.lazyLoad && <source type="video/mp4" src={props.video} />}
+      </video>
+      <Show when={props.aspectRatio && !(props.fitContent && props.children)}>
+        <div
+          style={{
+            width: '100%',
+            paddingTop: props.aspectRatio! * 100 + '%',
+            pointerEvents: 'none',
+            fontSize: '0px',
+          }}
+        />
+      </Show>
+      <Show when={props.children && props.fitContent}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'stretch',
+          }}
+        >
+          {props.children}
+        </div>
+      </Show>
+      <Show when={props.children && !props.fit}>
+        <div
+          style={{
+            pointerEvents: 'none',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'stretch',
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          {props.children}
+        </div>
+      </Show>
+    </div>
   );
 }
