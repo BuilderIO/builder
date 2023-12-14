1f71173fe965620ea04b36ae5d61030082beb463
Sami Jaber <jabersami@gmail.com>
2023-12-14 13:40:16 -0400

    f

--- packages/sdks/e2e/nextjs-pages-dir/next.config.js
+++ packages/sdks/e2e/nextjs-pages-dir/next.config.js
@@ -1,5 +1,6 @@
 const {
   withHydrationOverlay,
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
 } = require('@builder.io/react-hydration-overlay/next');
 
 /** @type {import('next').NextConfig} */
