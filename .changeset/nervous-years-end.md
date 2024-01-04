992a97e4e55eebecc4669fbaaacd4ccbd9629c75
Sami Jaber <jabersami@gmail.com>
2024-01-04 15:29:15 -0500

    Fix: SDKs: show hide if + repeats (#2895)
    ## Description

---
+++ .changeset/nervous-years-end.md
@@ -0,0 +1,9 @@
+---
+'@builder.io/sdk-qwik': patch
+'@builder.io/sdk-react': patch
+'@builder.io/sdk-react-native': patch
+'@builder.io/sdk-solid': patch
+'@builder.io/sdk-svelte': patch
+---
+
+Fix: Show & Hide If properties when combined with repeated elements
---
+++ packages/sdks-tests/src/tests/show-hide-if.spec.ts
@@ -0,0 +1,84 @@
+import { expect } from '@playwright/test';
+import { excludeTestFor, findTextInPage, test } from './helpers.js';
+import { sdk } from './sdk.js';
+
+test.describe('Show If & Hide If', () => {
+  test('works on static conditions', async ({ page }) => {
+    await page.goto('/show-hide-if');
+
+    await findTextInPage({ page, text: 'this always appears' });
+    await expect(page.locator('body')).not.toContainText('this never appears');
+  });
+
+  test('works on reactive conditions', async ({ page, packageName }) => {
+    test.fail(
+      excludeTestFor({
+        reactNative: true,
+        rsc: true,
+        solid: true,
+      })
+    );
+
+    // since these are flaky tests, we have to `.skip()` instead of `.fail()`, seeing as they might sometimes pass.
+    test.skip(
+      // TO-DO: flaky in remix
+      packageName === 'gen1-remix' ||
+        // flaky in vue3: takes too long to hydrate, causing button click not to register...
+        packageName === 'vue3' ||
+        packageName === 'nuxt3'
+    );
+
+    await page.goto('/show-hide-if');
+
+    await expect(page.getByText('even clicks')).toBeVisible();
+    await expect(page.locator('body')).not.toContainText('odd clicks');
+
+    const button = page.getByRole('button');
+    await expect(button).toBeVisible();
+    await button.click();
+
+    await expect(page.getByText('odd clicks')).toBeVisible();
+    await expect(page.locator('body')).not.toContainText('even clicks');
+  });
+  test('works with repeat elements', async ({ page, packageName }) => {
+    test.fail(excludeTestFor({ rsc: true }), 'RSC SDK has no interactivity');
+
+    // since these are flaky tests, we have to `.skip()` instead of `.fail()`, seeing as they might sometimes pass.
+    test.skip(
+      // gen1 SSR breaks here some some reason
+      packageName === 'gen1-remix' ||
+        packageName === 'gen1-next' ||
+        // flaky in vue: takes too long to hydrate, causing button click not to register...
+        sdk === 'vue2' ||
+        sdk === 'vue3'
+    );
+
+    await page.goto('/show-hide-if-repeats');
+
+    await expect(page.locator('body')).not.toContainText('zero');
+    await expect(page.locator('body')).not.toContainText('one');
+    await expect(page.locator('body')).not.toContainText('two');
+    await expect(page.locator('body')).not.toContainText('three');
+
+    await page.hover('text=button1');
+
+    await expect(page.locator('body')).not.toContainText('zero');
+    await expect(page.locator('body')).not.toContainText('one');
+    await expect(page.locator('body')).not.toContainText('two');
+    await expect(page.locator('body')).not.toContainText('three');
+
+    await page.hover('text=button2');
+
+    await expect(page.locator('body')).not.toContainText('zero');
+    await expect(page.locator('body')).not.toContainText('one');
+    await expect(page.locator('body')).toContainText('two');
+    await expect(page.locator('body')).not.toContainText('three');
+
+    await page.hover('text=button3');
+
+    await expect(page.locator('body')).not.toContainText('zero');
+    await expect(page.locator('body')).not.toContainText('one');
+    await expect(page.locator('body')).not.toContainText('two');
+    await expect(page.locator('body')).toContainText('three');
+  });
+});
