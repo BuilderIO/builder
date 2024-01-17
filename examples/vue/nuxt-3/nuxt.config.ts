5ee387ff46a3df763531115f65bad97ba7ecbf9e
Sami Jaber <jabersami@gmail.com>
2024-01-17 09:34:51 -0400

    update examples

--- examples/vue/nuxt-3/nuxt.config.ts
+++ examples/vue/nuxt-3/nuxt.config.ts
@@ -1,2 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  build: {
+    transpile: ['@builder.io/sdk-vue'],
+  },
+});
