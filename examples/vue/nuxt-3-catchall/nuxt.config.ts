e315b9fd8ccb2e22ad25585928558b0d68edbc03
Sami Jaber <jabersami@gmail.com>
2024-01-17 17:39:51 -0400

    document

--- examples/vue/nuxt-3-catchall/nuxt.config.ts
+++ examples/vue/nuxt-3-catchall/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   build: {
+    /**
+     * You need to transpile the Builder.io Vue SDK for it to work correctly.
+     */
     transpile: ['@builder.io/sdk-vue'],
   },
 });
