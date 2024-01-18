95442201e66723a7297356d2338366af4380e1b5
Sidharth Mohanty <sidmohanty11@gmail.com>
2024-01-18 20:55:32 +0530

    [fix] duplicate attributes being applied wrapper + block element (#2920)
    ## Description
    
    With the InteractiveElements we are passing all the attributes in the
    block. In this PR, we are checking if we should pass the props to the
    block itself or to the wrapper element only. This way we are fixing
    duplicate attributes being sent to the child component as well.

---
+++ packages/sdks-tests/src/specs/duplicate-attributes.ts
@@ -0,0 +1,33 @@
+export const DUPLICATE_ATTRIBUTES = {
+  data: {
+    themeId: false,
+    title: 'footer-test',
+    inputs: [],
+    blocks: [
+      {
+        '@type': '@builder.io/sdk:Element',
+        '@version': 2,
+        tagName: 'footer',
+        id: 'builder-6a8ccf9861154b7689ba9adfe4577a55',
+        meta: { previousId: 'builder-9d8032f86e844c0998e8815af7e6b110' },
+        component: { name: 'Core:Section', options: { maxWidth: 1200, lazyLoad: false } },
+        responsiveStyles: {
+          large: {
+            display: 'flex',
+            flexDirection: 'column',
+            position: 'relative',
+            flexShrink: '0',
+            boxSizing: 'border-box',
+            marginTop: '0px',
+            paddingLeft: '20px',
+            paddingRight: '20px',
+            paddingTop: '20px',
+            paddingBottom: '20px',
+            minHeight: '100px',
+            backgroundColor: 'rgba(253, 0, 0, 1)',
+          },
+        },
+      },
+    ],
+  },
+};
