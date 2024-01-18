e9272913d56769138eef81947de0c79e32bffd10
Sami Jaber <jabersami@gmail.com>
2024-01-18 17:31:47 -0400

    f

--- packages/sdks-tests/src/tests/js-code.spec.ts
+++ packages/sdks-tests/src/tests/js-code.spec.ts
@@ -3,24 +3,25 @@ import { excludeTestFor, test } from './helpers.js';
 
 test.describe('JS Code', () => {
   test('initializes correct value from JS Code block', async ({ page }) => {
-    test.fail(excludeTestFor(['vue']));
-
     await page.goto('/js-code/');
     const menuLocator = page.locator('text=Content is expanded');
     await expect(menuLocator).toBeVisible();
   });
   test('toggles value ON/OFF', async ({ page }) => {
-    test.fail(excludeTestFor(['vue', 'svelte', 'rsc', 'solid', 'reactNative', 'react']));
+    test.fail(excludeTestFor(['svelte', 'rsc', 'solid', 'reactNative', 'react']));
     await page.goto('/js-code/');
     const menuLocator = page.locator('text=Content is expanded');
     await expect(menuLocator).toBeVisible();
 
+    const btn = page.getByRole('button');
+    await expect(btn).toBeVisible();
+
     // hide
-    await page.getByRole('button').click();
+    await btn.click();
     await expect(menuLocator).toBeHidden();
 
     // show again
-    await page.getByRole('button').click();
+    await btn.click();
     await expect(menuLocator).toBeVisible();
   });
 });
