c3884e90f85ae576b3a34fc487fba4368e741c5b
Sami Jaber <jabersami@gmail.com>
2024-01-18 17:30:10 -0400

    f

--- packages/sdks-tests/src/tests/js-code.spec.ts
+++ packages/sdks-tests/src/tests/js-code.spec.ts
@@ -3,14 +3,14 @@ import { excludeTestFor, test } from './helpers.js';
 
 test.describe('JS Code', () => {
   test('initializes correct value from JS Code block', async ({ page }) => {
-    test.fail(excludeTestFor(['vue3']));
+    test.fail(excludeTestFor(['vue']));
 
     await page.goto('/js-code/');
     const menuLocator = page.locator('text=Content is expanded');
     await expect(menuLocator).toBeVisible();
   });
   test('toggles value ON/OFF', async ({ page }) => {
-    test.fail(excludeTestFor(['vue3', 'svelte', 'rsc', 'solid', 'reactNative', 'react']));
+    test.fail(excludeTestFor(['vue', 'svelte', 'rsc', 'solid', 'reactNative', 'react']));
     await page.goto('/js-code/');
     const menuLocator = page.locator('text=Content is expanded');
     await expect(menuLocator).toBeVisible();
