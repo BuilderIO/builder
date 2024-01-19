7a4ba175946231900d527685ff7b6036d219f810
Sami Jaber <jabersami@gmail.com>
2024-01-19 19:34:25 -0400

    k

--- packages/sdks/e2e/react/src/App.tsx
+++ packages/sdks/e2e/react/src/App.tsx
@@ -4,20 +4,21 @@ import { useEffect, useState } from 'react';
 
 const DataComp = (props: {
   pathname: string;
-  children: (args: { data: string }) => React.ReactNode;
+  children: (args: { data: { x: string } }) => React.ReactNode;
 }) => {
   const [data, setData] = useState('foo');
   if (props.pathname === '/external-data') {
     return (
       <div>
+        <div>External data: {data}</div>
         <button onClick={() => setData(data === 'foo' ? 'bar' : 'foo')}>
           Change value
         </button>
-        {props.children({ data })}
+        {props.children({ data: { x: data } })}
       </div>
     );
   } else {
-    return props.children({ data });
+    return props.children({ data: { x: data } });
   }
 };
 
@@ -25,6 +26,8 @@ function App() {
   const [props, setProps] = useState<any>(undefined);
 
   useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
     getProps({ _processContentResult }).then((resp) => {
       setProps(resp);
     });
