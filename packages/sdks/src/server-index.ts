e8e8b58885bdb490e7776603929c84abb3be925e
Sami Jaber <jabersami@gmail.com>
2024-01-22 14:38:35 -0400

    fix

--- packages/sdks/src/server-index.ts
+++ packages/sdks/src/server-index.ts
@@ -1,15 +1,15 @@
 export * from './index-helpers/top-of-file.js';
 
-export { ButtonProps } from './blocks/button/button.types.js';
-export { ColumnProps } from './blocks/columns/columns.types.js';
-export { FragmentProps } from './blocks/fragment/fragment.types.js';
-export { ImageProps } from './blocks/image/image.types.js';
-export { SectionProps } from './blocks/section/section.types.js';
-export { SymbolProps } from './blocks/symbol/symbol.types.js';
-export { TextProps } from './blocks/text/text.types.js';
-export { VideoProps } from './blocks/video/video.types.js';
-export { BlocksProps } from './components/blocks/blocks.types.js';
-export { ContentVariantsPrps as ContentProps } from './components/content-variants/content-variants.types.js';
+export type { ButtonProps } from './blocks/button/button.types.js';
+export type { ColumnProps } from './blocks/columns/columns.types.js';
+export type { FragmentProps } from './blocks/fragment/fragment.types.js';
+export type { ImageProps } from './blocks/image/image.types.js';
+export type { SectionProps } from './blocks/section/section.types.js';
+export type { SymbolProps } from './blocks/symbol/symbol.types.js';
+export type { TextProps } from './blocks/text/text.types.js';
+export type { VideoProps } from './blocks/video/video.types.js';
+export type { BlocksProps } from './components/blocks/blocks.types.js';
+export type { ContentVariantsPrps as ContentProps } from './components/content-variants/content-variants.types.js';
 
 export { isEditing } from './functions/is-editing.js';
 export { isPreviewing } from './functions/is-previewing.js';
