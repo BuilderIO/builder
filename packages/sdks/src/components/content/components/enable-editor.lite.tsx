87ae65da69db2862266e784d8a14cf0b194f63ab
Sami Jaber <jabersami@gmail.com>
2024-01-22 17:34:28 -0400

    do not update for props.data

--- packages/sdks/src/components/content/components/enable-editor.lite.tsx
+++ packages/sdks/src/components/content/components/enable-editor.lite.tsx
@@ -264,7 +264,7 @@ export default function EnableEditor(props: BuilderEditorProps) {
         }
       },
     });
-  }, [props.content, props.data]);
+  }, [props.content]);
 
   onUpdate(() => {
     useTarget({
