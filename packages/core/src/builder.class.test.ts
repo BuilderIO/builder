16ef928d416f8b7193c265c97bec04bd71a102ec
Daniel Moore <daniel@builder.io>
2024-01-23 09:58:13 -0800

    Added test for registering a trusted host

--- packages/core/src/builder.class.test.ts
+++ packages/core/src/builder.class.test.ts
@@ -7,4 +7,9 @@ describe('Builder', () => {
     expect(Builder.isTrustedHost('builder.io')).toBe(true);
     expect(Builder.isTrustedHost('123-review-build.beta.builder.io')).toBe(true);
   });
+
+  test('add trusted host', () => {
+    Builder.registerTrustedHost('example.com');
+    expect(Builder.isTrustedHost('example.com')).toBe(true);
+  });
 });
