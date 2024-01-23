35a16ae6456365150013a148a2594af376d3a591
Sami Jaber <jabersami@gmail.com>
2024-01-23 17:07:48 -0400

    rm

--- packages/sdks/src/components/block/components/block-wrapper.lite.tsx
+++ packages/sdks/src/components/block/components/block-wrapper.lite.tsx
@@ -27,10 +27,6 @@ type BlockWrapperProps = {
 
 /**
  * This component renders a block's wrapper HTML element (from the block's `tagName` property).
- * It reuses the exact same logic as the `InteractiveElement` component, but we need to have 2 separate components for
- * Svelte's sake, as it needs to know at compile-time whether to use:
- *  - `<svelte:element>` (for HTML element) or
- *  - `<svelte:component>` (for custom components)
  */
 export default function BlockWrapper(
   props: PropsWithChildren<BlockWrapperProps>
