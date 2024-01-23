9f85c7871cbcbfe38413e03913f2ce31844cdc20
Daniel X Moore <daniel@danielx.net>
2024-01-23 12:07:39 -0800

    stricter trusted hosts (#2943)
    No longer allow arbitrary subdomains of builder.io in trusted hosts.

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -117,9 +117,7 @@ export default {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: ['src'],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
