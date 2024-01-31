91f75fc77a4838d0f660f6affeb01e4ac48e295b
Korey Kassir <korey@builder.io>
2024-01-30 16:39:20 -0800

    feat(react): support named slots

--- packages/react/src/functions/with-children.tsx
+++ packages/react/src/functions/with-children.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import { BuilderElement } from '@builder.io/sdk';
 import { BuilderBlock } from '../components/builder-block.component';
 
+const isBuilderElement = (item: unknown): item is BuilderElement => {
+  return Boolean((item as any)?.['@type'] === '@builder.io/sdk:Element');
+};
+
 /**
  * Higher order component for passing Builder.io children as React children
  *
@@ -28,16 +32,29 @@ import { BuilderBlock } from '../components/builder-block.component';
 export const withChildren = <P extends object>(Component: React.ComponentType<P>) => {
   const HOC = React.forwardRef<any, React.PropsWithChildren<P> & { builderBlock?: BuilderElement }>(
     (props, ref) => {
+      const useProps = { ...props };
       const children =
         props.children ||
         (props.builderBlock &&
           props.builderBlock.children &&
           props.builderBlock.children.map(child => <BuilderBlock key={child.id} block={child} />));
 
+      const componentOptions = props.builderBlock?.component?.options;
+      if (!!componentOptions) {
+        Object.keys(componentOptions).forEach(key => {
+          const value = componentOptions[key];
+          const valueIsArrayOfBuilderElements =
+            Array.isArray(value) && value.every(isBuilderElement);
+          if (valueIsArrayOfBuilderElements) {
+            useProps[key] = value.map(child => <BuilderBlock key={child.id} block={child} />);
+          }
+        });
+      }
+
       return (
         // getting type errors due to `@types/react` version mismatches. Can safely ignore.
         // @ts-ignore
-        <Component {...props} ref={ref}>
+        <Component {...useProps} ref={ref}>
           {children}
         </Component>
       );
