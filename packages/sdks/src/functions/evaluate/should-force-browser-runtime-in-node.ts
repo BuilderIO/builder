ee56444bda44a0ed6208954732f3b887e1d944c3
Sami Jaber <jabersami@gmail.com>
2024-02-02 14:07:07 -0400

    g

--- packages/sdks/src/functions/evaluate/should-force-browser-runtime-in-node.ts
+++ packages/sdks/src/functions/evaluate/should-force-browser-runtime-in-node.ts
@@ -17,7 +17,7 @@ export const shouldForceBrowserRuntimeInNode = () => {
   if (isArm64 && isNode20 && !hasNoNodeSnapshotNodeOption) {
     logger.log(`Skipping usage of \`isolated-vm\` to avoid crashes in Node v20 on an arm64 machine.
     If you would like to use the \`isolated-vm\` package on this machine, please provide the \`NODE_OPTIONS=--no-node-snapshot\` config to your Node process.
-    See https://github.com/laverdet/isolated-vm/issues/424#issuecomment-1864629126 for more information.
+    See https://github.com/BuilderIO/builder/blob/main/packages/sdks/README.md#node-v20--m1-macs-apple-silicon-support for more information.
     `);
 
     return true;
