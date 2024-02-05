2bd96997fb9a9d5e46c63f1bc7aa72d01d53e124
Sami Jaber <jabersami@gmail.com>
2024-02-05 17:22:59 -0400

    ex

--- packages/sdks/e2e/react/src/App.tsx
+++ packages/sdks/e2e/react/src/App.tsx
@@ -1,6 +1,77 @@
-import { RenderContent, _processContentResult } from '@builder.io/sdk-react';
+import {
+  RenderContent,
+  _processContentResult,
+  getContent,
+} from '@builder.io/sdk-react';
+import type { BuilderContent } from '@builder.io/sdk-react/types/types/builder-content';
 import { getProps } from '@e2e/tests';
 import { useEffect, useState } from 'react';
+
+const sub = (
+  args: { model: string },
+  callback: (data: BuilderContent) => void
+) => {
+  const listener = (event: MessageEvent<any>): void => {
+    const { data } = event;
+
+    if (data) {
+      switch (data.type) {
+        case 'builder.contentUpdate': {
+          const messageContent = data.data;
+          const key =
+            messageContent.key ||
+            messageContent.alias ||
+            messageContent.entry ||
+            messageContent.modelName;
+
+          const contentData = messageContent.data;
+
+          console.log('got content update', key, contentData);
+
+          if (key === args.model) {
+            console.log('setting props', contentData);
+
+            callback(contentData);
+          }
+          break;
+        }
+      }
+    }
+  };
+  window.addEventListener('message', listener);
+
+  return () => {
+    window.removeEventListener('message', listener);
+  };
+};
+
+const REAL_API_KEY = 'f1a790f8c3204b3b8c5c1795aeac4660';
+function DataPreview() {
+  const [coffee, setProps] = useState<BuilderContent | undefined | null>(
+    undefined
+  );
+
+  useEffect(() => {
+    getContent({ apiKey: REAL_API_KEY, model: 'coffee' }).then(setProps);
+
+    const unsubscribe = sub({ model: 'coffee' }, setProps);
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  if (!coffee) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <>
+      {coffee.}
+      <div>coffee name: {coffee?.data?.name}</div>
+      <div>coffee info: {coffee?.data?.info}</div>
+    </>
+  );
+}
 
 const DataComp = (props: {
   pathname: string;
@@ -30,10 +101,14 @@ function App() {
   const [props, setProps] = useState<any>(undefined);
 
   useEffect(() => {
-    getProps({ _processContentResult }).then((resp) => {
-      setProps(resp);
-    });
+    getProps({ _processContentResult, data: 'real', getContent }).then(
+      setProps
+    );
   }, []);
+
+  if (window.location.pathname === '/preview-coffee') {
+    return <DataPreview />;
+  }
 
   return props ? (
     <DataComp pathname={window.location.pathname}>
