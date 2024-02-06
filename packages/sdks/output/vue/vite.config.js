a3b5dfafea0adf2706861f8b549af06459d39306
Sami Jaber <jabersami@gmail.com>
2024-02-06 13:51:34 -0400

    create types

--- packages/sdks/output/vue/vite.config.js
+++ packages/sdks/output/vue/vite.config.js
@@ -1,8 +1,10 @@
 import {
   getSdkEnv,
+  getSdkOutputPath,
   viteOutputGenerator,
 } from '@builder.io/sdks/output-generation/index.js';
 import vue from '@vitejs/plugin-vue';
+import { copyFileSync, renameSync } from 'fs';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
@@ -12,9 +14,13 @@ export default defineConfig({
   plugins: [
     viteOutputGenerator(),
     vue(),
-    dts({ insertTypesEntry: true }),
-    // https://stackoverflow.com/a/72572426
-    // libCss(),
+    dts({
+      afterBuild: () => {
+        const outPath = getSdkOutputPath();
+        copyFileSync(outPath + '/index.d.ts', outPath + '/index.d.mts');
+        renameSync(outPath + '/index.d.ts', outPath + '/index.d.cts');
+      },
+    }),
   ],
   build: {
     lib: {
