2adfcc9265c029577dba4fa815d5c6a26a5fec30
Sami Jaber <jabersami@gmail.com>
2024-02-06 23:12:13 -0400

    f

--- packages/sdks/src/blocks/columns/columns.lite.tsx
+++ packages/sdks/src/blocks/columns/columns.lite.tsx
@@ -224,7 +224,12 @@ export default function Columns(props: PropsWithBuilderData<ColumnProps>) {
               })}
               path={`component.options.columns.${index}.blocks`}
               parent={props.builderBlock.id}
-              styleProp={{ flexGrow: '1' }}
+              styleProp={{
+                flexGrow: useTarget<string | number>({
+                  reactNative: 1,
+                  default: '1',
+                }),
+              }}
               context={props.builderContext}
               registeredComponents={props.builderComponents}
               linkComponent={props.builderLinkComponent}
