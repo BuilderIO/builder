da4f455f83b11a094c064d3e78c4f6cbfa36b7c4
Sami Jaber <jabersami@gmail.com>
2024-02-08 11:17:22 -0400

    deprecate registerComponent()

--- packages/sdks/src/functions/register-component.ts
+++ packages/sdks/src/functions/register-component.ts
@@ -1,25 +1,6 @@
-import type { RegisteredComponent } from '../context/types.js';
 import type { ComponentInfo } from '../types/components.js';
 import type { Input } from '../types/input.js';
 import { fastClone } from './fast-clone.js';
-
-/**
- * @deprecated.  Use the `customComponents` prop in RenderContent instead to provide your custom components to the builder SDK.
- */
-export const components: RegisteredComponent[] = [];
-
-/**
- * @deprecated.  Use the `customComponents` prop in RenderContent instead to provide your custom components to the builder SDK.
- */
-export function registerComponent(component: any, info: ComponentInfo): void {
-  components.push({ component, ...info });
-
-  console.warn(
-    'registerComponent is deprecated. Use the `customComponents` prop in RenderContent instead to provide your custom components to the builder SDK.'
-  );
-
-  return component;
-}
 
 export const createRegisterComponentMessage = (info: ComponentInfo) => ({
   type: 'builder.registerComponent',
