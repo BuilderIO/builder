fe8b90f714049af874f6e0c47b084bcf2e9aedc8
sidmohanty11 <sidmohanty11@gmail.com>
2024-02-08 16:02:03 +0530

    format

--- packages/sdks-tests/src/tests/form.spec.ts
+++ packages/sdks-tests/src/tests/form.spec.ts
@@ -13,7 +13,9 @@ test.describe('Form', () => {
     expect(await page.locator('form').first().locator('input').count()).toBe(2);
     expect(await page.locator('form').first().locator('button').count()).toBe(1);
     expect(await page.locator('form').first().locator('select').count()).toBe(1);
-    expect(await page.locator('form').first().locator('select').first().locator('option').count()).toBe(3);
+    expect(
+      await page.locator('form').first().locator('select').first().locator('option').count()
+    ).toBe(3);
     expect(await page.locator('form').first().locator('button').first().innerText()).toBe('Submit');
   });
 });
