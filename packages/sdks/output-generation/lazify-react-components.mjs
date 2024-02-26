822b1f0f47d2d2f5e6c5abbeea1361917e79f472
Sami Jaber <jabersami@gmail.com>
2024-02-26 16:43:57 -0400

    create dynamic toggle export

--- packages/sdks/output-generation/lazify-react-components.mjs
+++ packages/sdks/output-generation/lazify-react-components.mjs
@@ -1,5 +1,6 @@
 import recast from 'recast';
 import typescriptParser from 'recast/parsers/typescript.js';
+import { getSdkEnv } from '.';
 
 /**
  *
@@ -14,13 +15,15 @@ const parse = (code) => recast.parse(code, { parser: typescriptParser });
  */
 export const lazyifyReactComponentsVitePlugin = () => {
   return {
-    name: 'lazify-components',
-
+    name: 'lazify-react-components',
     transform(src, id) {
+      const isBlocksExports = id.includes('blocks-exports.ts');
       const hasBlockImportOrExport =
-        id.includes('blocks-exports.ts') ||
+        isBlocksExports ||
         id.includes('builder-registered-components.ts') ||
         id.includes('extra-components.ts');
+
+      let importNames = [];
 
       if (hasBlockImportOrExport) {
         const ast = parse(src);
@@ -40,6 +43,7 @@ export const lazyifyReactComponentsVitePlugin = () => {
               ).program.body[0]
             );
 
+            importNames.push(importName);
             return false;
           },
           visitImportDeclaration(path) {
@@ -71,6 +75,57 @@ export const lazyifyReactComponentsVitePlugin = () => {
 
         const lazyfiedCode = recast.prettyPrint(ast);
 
+        if (isBlocksExports && getSdkEnv() === 'edge') {
+          const TOP_OF_FILE_MJS = `
+import React from 'react';
+
+function isBrowser() {
+  return typeof window !== 'undefined' && typeof document !== 'undefined';
+}
+
+import * as BrowserSdk from '../browser/index.mjs';
+import * as EdgeSdk from './index.mjs';
+
+${importNames
+  .map(
+    (name) =>
+      `export const ${name} = (props) => isBrowser() ? React.createElement(BrowserSdk.${name}, Object.assign({}, props)) : React.createElement(EdgeSdk.${name}, Object.assign({}, props));`
+  )
+  .join('\n')}
+        `;
+
+          this.emitFile({
+            type: 'prebuilt-chunk',
+            code: TOP_OF_FILE_MJS,
+            fileName: 'dynamic-blocks-exports.mjs',
+          });
+
+          const TOP_OF_FILE_CJS = `
+import React from 'react';
+
+function isBrowser() {
+  return typeof window !== 'undefined' && typeof document !== 'undefined';
+}
+
+import * as BrowserSdk from '../browser/index.cjs';
+import * as EdgeSdk from './index.cjs';
+
+module.exports = {
+${importNames
+  .map(
+    (name) =>
+      `${name}: (props) => isBrowser() ? React.createElement(BrowserSdk.${name}, Object.assign({}, props)) : React.createElement(EdgeSdk.${name}, Object.assign({}, props)),`
+  )
+  .join('\n')}
+};
+        `;
+
+          this.emitFile({
+            type: 'prebuilt-chunk',
+            code: TOP_OF_FILE_CJS,
+            fileName: 'dynamic-blocks-exports.cjs',
+          });
+        }
         return lazyfiedCode;
       }
 
