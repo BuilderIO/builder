26f9ab3442509e1632229fbab6ec493305475975
Sami Jaber <jabersami@gmail.com>
2024-02-27 11:27:53 -0400

    f

--- packages/sdks/overrides/react/src/components/error-boundary.tsx
+++ packages/sdks/overrides/react/src/components/error-boundary.tsx
@@ -10,7 +10,7 @@ export default class ErrorBoundary extends React.Component<
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error) {
+  static getDerivedStateFromError(_error) {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
