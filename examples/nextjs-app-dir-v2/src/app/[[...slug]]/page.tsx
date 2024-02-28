2a718f6bad87aa7f78a53fbf0bda73eddeef7164
Sami Jaber <jabersami@gmail.com>
2024-02-28 16:52:14 -0400

    Examples: React Gen 2  (#3034)
    ## Description
    
    - update react + vite example
    - add Nextjs app router example
    - add Nextjs pages router example

---
+++ examples/nextjs-app-dir-v2/src/app/[[...slug]]/page.tsx
@@ -0,0 +1,34 @@
+import { Content, fetchOneEntry, isEditing, isPreviewing } from '@builder.io/sdk-react/edge';
+
+const BUILDER_PUBLIC_API_KEY = 'f1a790f8c3204b3b8c5c1795aeac4660';
+
+interface PageProps {
+  params: {
+    slug: string[];
+  };
+  searchParams: Record<string, string>;
+}
+
+export default async function Page(props: PageProps) {
+  const urlPath = '/' + (props.params?.slug?.join('/') || '');
+
+  const content = await fetchOneEntry({
+    options: props.searchParams,
+    apiKey: BUILDER_PUBLIC_API_KEY,
+    model: 'page',
+    userAttributes: { urlPath },
+  });
+
+  const canShowContent =
+    content || isPreviewing(props.searchParams) || isEditing(props.searchParams);
+
+  if (!canShowContent) {
+    return (
+      <>
+        <h1>404</h1>
+        <p>Make sure you have your content published at builder.io.</p>
+      </>
+    );
+  }
+  return <Content content={content} apiKey={BUILDER_PUBLIC_API_KEY} model={'page'} />;
+}
---
+++ examples/nextjs-pages-dir-v2/src/pages/[[...page]].tsx
@@ -0,0 +1,78 @@
+import {
+  BuilderContent,
+  Content,
+  fetchEntries,
+  fetchOneEntry,
+  isEditing,
+  isPreviewing,
+} from '@builder.io/sdk-react/edge';
+import { GetStaticProps } from 'next';
+import DefaultErrorPage from 'next/error';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+// Replace with your Public API Key
+const BUILDER_PUBLIC_API_KEY = 'f1a790f8c3204b3b8c5c1795aeac4660';
+
+// Define a function that fetches the Builder content for a given page
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const urlPath = '/' + (Array.isArray(params?.page) ? params.page.join('/') : params?.page || '');
+
+  // Fetch the builder content for the given page
+  const page = await fetchOneEntry({
+    apiKey: BUILDER_PUBLIC_API_KEY,
+    model: 'page',
+    userAttributes: { urlPath },
+  });
+
+  return {
+    // Return the page content as props
+    props: { page },
+    // Revalidate the content every 5 seconds
+    revalidate: 5,
+  };
+};
+
+// Define a function that generates the
+// static paths for all pages in Builder
+export async function getStaticPaths() {
+  // Get a list of all pages in Builder
+  const pages = await fetchEntries({
+    apiKey: BUILDER_PUBLIC_API_KEY,
+    model: 'page',
+    // We only need the URL field
+    fields: 'data.url',
+    options: { noTargeting: true },
+  });
+
+  // Generate the static paths for all pages in Builder
+  return {
+    paths: pages.map(page => `${page.data?.url}`).filter(url => url !== '/'),
+    fallback: 'blocking',
+  };
+}
+
+// Define the Page component
+export default function Page(props: { page: BuilderContent | null }) {
+  const router = useRouter();
+
+  const canShowContent = props.page || isPreviewing(router.asPath) || isEditing(router.asPath);
+
+  // If the page content is not available
+  // and not in preview/editing mode, show a 404 error page
+  if (!canShowContent) {
+    return <DefaultErrorPage statusCode={404} />;
+  }
+
+  // If the page content is available, render
+  // the BuilderComponent with the page content
+  return (
+    <>
+      <Head>
+        <title>{props.page?.data?.title}</title>
+      </Head>
+      {/* Render the Builder page */}
+      <Content model="page" content={props.page} apiKey={BUILDER_PUBLIC_API_KEY} />
+    </>
+  );
+}
