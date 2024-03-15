9fb925145d9d331cc5f6a81b453a5ad25d379c87
Sami Jaber <jabersami@gmail.com>
2024-03-15 12:00:14 -0300

    f

--- packages/sdks/src/functions/evaluate/evaluate.ts
+++ packages/sdks/src/functions/evaluate/evaluate.ts
@@ -3,27 +3,27 @@ import { chooseBrowserOrServerEval } from './choose-eval.js';
 import type { EvaluatorArgs, ExecutorArgs } from './helpers.js';
 import { getBuilderGlobals, parseCode } from './helpers.js';
 
-type CacheValue = NonNullable<unknown>;
+type EvalValue = NonNullable<unknown>;
 
 class EvalCache {
   static cacheLimit = 20;
-  static cache = new Map<string, CacheValue | undefined>();
+  static cache = new Map<string, EvalValue | undefined>();
 
   static getCacheKey(args: ExecutorArgs) {
     return JSON.stringify(args);
   }
 
-  static getCachedValue(args: ExecutorArgs) {
-    const cachedVal = EvalCache.cache.get(EvalCache.getCacheKey(args));
+  static getCachedValue(key: string) {
+    const cachedVal = EvalCache.cache.get(key);
 
     return cachedVal;
   }
 
-  static setCachedValue(args: ExecutorArgs, value: CacheValue) {
+  static setCachedValue(key: string, value: EvalValue) {
     if (EvalCache.cache.size > 20) {
       EvalCache.cache.delete(EvalCache.cache.keys().next().value);
     }
-    EvalCache.cache.set(EvalCache.getCacheKey(args), value);
+    EvalCache.cache.set(key, value);
   }
 }
 
@@ -35,10 +35,10 @@ export function evaluate({
   rootSetState,
   event,
   isExpression = true,
-}: EvaluatorArgs): any {
+}: EvaluatorArgs): EvalValue | undefined {
   if (code === '') {
     logger.warn('Skipping evaluation of empty code block.');
-    return;
+    return undefined;
   }
 
   const args: ExecutorArgs = {
@@ -51,14 +51,16 @@ export function evaluate({
     localState,
   };
 
-  const cachedValue = EvalCache.getCachedValue(args);
+  const cacheKey = EvalCache.getCacheKey(args);
+
+  const cachedValue = EvalCache.getCachedValue(cacheKey);
   if (cachedValue) {
     return cachedValue;
   }
 
   try {
     const newEval = chooseBrowserOrServerEval(args);
-    EvalCache.setCachedValue(args, newEval);
+    EvalCache.setCachedValue(cacheKey, newEval);
     return newEval;
   } catch (e: any) {
     logger.error('Failed code evaluation: ' + e.message, { code });
