10c0922ba9b30cb1ce0c6c78a34c0e4f45cd3001
Sami Jaber <jabersami@gmail.com>
2024-03-15 12:05:00 -0300

    f

--- packages/sdks/src/functions/evaluate/evaluate.ts
+++ packages/sdks/src/functions/evaluate/evaluate.ts
@@ -3,11 +3,11 @@ import { chooseBrowserOrServerEval } from './choose-eval.js';
 import type { EvaluatorArgs, ExecutorArgs } from './helpers.js';
 import { getBuilderGlobals, parseCode } from './helpers.js';
 
-type EvalValue = NonNullable<unknown>;
+type EvalValue = unknown;
 
 class EvalCache {
   static cacheLimit = 20;
-  static cache = new Map<string, EvalValue | undefined>();
+  static cache = new Map<string, { value: EvalValue } | undefined>();
 
   static getCacheKey(args: ExecutorArgs) {
     return JSON.stringify(args);
@@ -23,7 +23,7 @@ class EvalCache {
     if (EvalCache.cache.size > 20) {
       EvalCache.cache.delete(EvalCache.cache.keys().next().value);
     }
-    EvalCache.cache.set(key, value);
+    EvalCache.cache.set(key, { value });
   }
 }
 
@@ -35,7 +35,7 @@ export function evaluate({
   rootSetState,
   event,
   isExpression = true,
-}: EvaluatorArgs): EvalValue | undefined {
+}: EvaluatorArgs): EvalValue {
   if (code === '') {
     logger.warn('Skipping evaluation of empty code block.');
     return undefined;
@@ -55,7 +55,7 @@ export function evaluate({
 
   const cachedValue = EvalCache.getCachedValue(cacheKey);
   if (cachedValue) {
-    return cachedValue;
+    return cachedValue.value;
   }
 
   try {
