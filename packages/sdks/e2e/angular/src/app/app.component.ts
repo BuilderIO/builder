ee5917962fc46da4d41b95db951cf06127679d9a
sidmohanty11 <sidmohanty11@gmail.com>
2024-03-22 15:29:21 +0530

    initial setup linking package

---
+++ packages/sdks/e2e/angular/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <content-variants></content-variants>
+  `,
+})
+export class AppComponent {
+  title = 'angular';
+}
