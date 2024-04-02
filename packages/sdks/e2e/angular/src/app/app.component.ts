5f416d95e3cb6c10d8d9577d6c0f2018c122d72b
sidmohanty11 <sidmohanty11@gmail.com>
2024-04-02 20:14:55 +0530

    getProps setup

--- packages/sdks/e2e/angular/src/app/app.component.ts
+++ packages/sdks/e2e/angular/src/app/app.component.ts
@@ -1,9 +1,63 @@
 import { Component } from '@angular/core';
+import {
+  _processContentResult,
+  fetchOneEntry,
+  getBuilderSearchParams,
+} from '@builder.io/sdk-angular';
+import { getProps } from '@e2e/tests';
+
+interface BuilderProps {
+  apiVersion: string;
+  canTrack?: boolean;
+  trustedHosts?: undefined;
+  apiKey: string;
+  model: string;
+  content: any;
+}
 
 @Component({
   selector: 'app-root',
-  template: ` <content-variants></content-variants> `,
+  template: `
+    <content
+      [model]="model"
+      [content]="content"
+      [apiKey]="apiKey"
+      [trustedHosts]="trustedHosts"
+      [canTrack]="canTrack"
+      [apiVersion]="apiVersion"
+    ></content>
+  `,
 })
 export class AppComponent {
   title = 'angular';
+  apiVersion: BuilderProps['apiVersion'] = 'v3';
+  canTrack: BuilderProps['canTrack'];
+  trustedHosts: BuilderProps['trustedHosts'];
+  apiKey: BuilderProps['apiKey'] = 'abcd';
+  model: BuilderProps['model'] = 'page';
+  content: BuilderProps['content'];
+
+  async ngOnInit() {
+    const urlPath = '/' + (window.location.pathname || '');
+
+    const builderProps = await getProps({
+      pathname: urlPath,
+      _processContentResult,
+      options: getBuilderSearchParams(
+        new URLSearchParams(window.location.search)
+      ),
+      fetchOneEntry,
+    });
+
+    if (!builderProps) {
+      return;
+    }
+
+    this.content = builderProps.content;
+    this.canTrack = builderProps.canTrack;
+    this.trustedHosts = builderProps.trustedHosts;
+    this.apiKey = builderProps.apiKey;
+    this.model = builderProps.model;
+    this.apiVersion = builderProps.apiVersion;
+  }
 }
