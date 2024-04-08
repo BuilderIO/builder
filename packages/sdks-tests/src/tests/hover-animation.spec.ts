82617b7f029c57692bcb1e8f923e59bb0bc455bf
sidmohanty11 <sidmohanty11@gmail.com>
2024-04-08 14:20:48 +0530

    fmt

--- packages/sdks-tests/src/tests/hover-animation.spec.ts
+++ packages/sdks-tests/src/tests/hover-animation.spec.ts
@@ -8,12 +8,12 @@ test.describe('Hover animations', () => {
 
     const button = isOldReactSDK ? page.locator('span') : page.locator('button');
 
-    const initialColor = await button.evaluate((el) => getComputedStyle(el).backgroundColor);
+    const initialColor = await button.evaluate(el => getComputedStyle(el).backgroundColor);
 
     await button.hover();
     await page.waitForTimeout(1000);
 
-    const hoveredColor = await button.evaluate((el) => getComputedStyle(el).backgroundColor);
+    const hoveredColor = await button.evaluate(el => getComputedStyle(el).backgroundColor);
 
     expect(initialColor).toContain('rgb(0, 0, 0)');
     expect(hoveredColor).not.toContain(initialColor);
