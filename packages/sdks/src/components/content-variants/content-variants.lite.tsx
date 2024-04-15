110213ce1b231141ab38a49c8111bb264a344beb
Sami Jaber <jabersami@gmail.com>
2024-04-15 11:41:46 -0300

    f

--- packages/sdks/src/components/content-variants/content-variants.lite.tsx
+++ packages/sdks/src/components/content-variants/content-variants.lite.tsx
@@ -1,13 +1,6 @@
-import { onMount, useMetadata, useStore, useTarget } from '@builder.io/mitosis';
-import { handleABTestingSync } from '../../helpers/ab-tests.js';
-import { getDefaultCanTrack } from '../../helpers/canTrack.js';
+import { useMetadata } from '@builder.io/mitosis';
 import ContentComponent from '../content/content.lite.jsx';
 import type { ContentVariantsPrps } from './content-variants.types.js';
-import {
-  checkShouldRenderVariants,
-  getUpdateCookieAndStylesScript,
-  getVariants,
-} from './helpers.js';
 
 useMetadata({
   rsc: {
@@ -26,102 +19,106 @@ type VariantsProviderProps = ContentVariantsPrps & {
 };
 
 export default function ContentVariants(props: VariantsProviderProps) {
-  onMount(() => {
-    /**
-     * We unmount the non-winning variants post-hydration in Vue.
-     */
-    useTarget({
-      vue3: () => {
-        state.shouldRenderVariants = false;
-      },
-      solid: () => {
-        state.shouldRenderVariants = false;
-      },
-      svelte: () => {
-        state.shouldRenderVariants = false;
-      },
-    });
-  });
+  // onMount(() => {
+  //   /**
+  //    * We unmount the non-winning variants post-hydration in Vue.
+  //    */
+  //   useTarget({
+  //     vue3: () => {
+  //       state.shouldRenderVariants = false;
+  //     },
+  //     solid: () => {
+  //       state.shouldRenderVariants = false;
+  //     },
+  //     svelte: () => {
+  //       state.shouldRenderVariants = false;
+  //     },
+  //   });
+  // });
 
-  const state = useStore({
-    shouldRenderVariants: checkShouldRenderVariants({
-      canTrack: getDefaultCanTrack(props.canTrack),
-      content: props.content,
-    }),
-    get updateCookieAndStylesScriptStr() {
-      return getUpdateCookieAndStylesScript(
-        getVariants(props.content).map((value) => ({
-          id: value.testVariationId!,
-          testRatio: value.testRatio,
-        })),
-        props.content?.id || ''
-      );
-    },
+  // const state = useStore({
+  //   shouldRenderVariants: checkShouldRenderVariants({
+  //     canTrack: getDefaultCanTrack(props.canTrack),
+  //     content: props.content,
+  //   }),
+  //   get updateCookieAndStylesScriptStr() {
+  //     return getUpdateCookieAndStylesScript(
+  //       getVariants(props.content).map((value) => ({
+  //         id: value.testVariationId!,
+  //         testRatio: value.testRatio,
+  //       })),
+  //       props.content?.id || ''
+  //     );
+  //   },
 
-    get hideVariantsStyleString() {
-      return getVariants(props.content)
-        .map((value) => `.variant-${value.testVariationId} { display: none; } `)
-        .join('');
-    },
-    get defaultContent() {
-      return state.shouldRenderVariants
-        ? { ...props.content, testVariationId: props.content?.id }
-        : handleABTestingSync({
-            item: props.content,
-            canTrack: getDefaultCanTrack(props.canTrack),
-          });
-    },
-  });
+  //   get hideVariantsStyleString() {
+  //     return getVariants(props.content)
+  //       .map((value) => `.variant-${value.testVariationId} { display: none; } `)
+  //       .join('');
+  //   },
+  //   get defaultContent() {
+  //     return state.shouldRenderVariants
+  //       ? { ...props.content, testVariationId: props.content?.id }
+  //       : handleABTestingSync({
+  //           item: props.content,
+  //           canTrack: getDefaultCanTrack(props.canTrack),
+  //         });
+  //   },
+  // });
 
-  return (
-    <>
-      {/* <Show when={!props.__isNestedRender && TARGET !== 'reactNative'}>
-        <InlinedScript scriptStr={getScriptString()} />
-      </Show>
-      <Show when={state.shouldRenderVariants}>
-        <InlinedStyles
-          id={`variants-styles-${props.content?.id}`}
-          styles={state.hideVariantsStyleString}
-        />
-        // Sets A/B test cookie for all `RenderContent` to read
-        <InlinedScript scriptStr={state.updateCookieAndStylesScriptStr} />
-
-        <For each={getVariants(props.content)}>
-          {(variant) => (
-            <ContentComponent
-              key={variant.testVariationId}
-              content={variant}
-              showContent={false}
-              model={props.model}
-              data={props.data}
-              context={props.context}
-              apiKey={props.apiKey}
-              apiVersion={props.apiVersion}
-              customComponents={props.customComponents}
-              canTrack={props.canTrack}
-              locale={props.locale}
-              includeRefs={props.includeRefs}
-              enrich={props.enrich}
-              isSsrAbTest={state.shouldRenderVariants}
-            />
-          )}
-        </For>
-      </Show> */}
-      <ContentComponent
-        content={state.defaultContent}
-        showContent
-        model={props.model}
-        data={props.data}
-        context={props.context}
-        apiKey={props.apiKey}
-        apiVersion={props.apiVersion}
-        customComponents={props.customComponents}
-        canTrack={props.canTrack}
-        locale={props.locale}
-        includeRefs={props.includeRefs}
-        enrich={props.enrich}
-        isSsrAbTest={state.shouldRenderVariants}
+  // <>
+  {
+    /* <Show when={!props.__isNestedRender && TARGET !== 'reactNative'}>
+      <InlinedScript scriptStr={getScriptString()} />
+    </Show>
+    <Show when={state.shouldRenderVariants}>
+      <InlinedStyles
+        id={`variants-styles-${props.content?.id}`}
+        styles={state.hideVariantsStyleString}
       />
-    </>
+      // Sets A/B test cookie for all `RenderContent` to read
+      <InlinedScript scriptStr={state.updateCookieAndStylesScriptStr} />
+
+      <For each={getVariants(props.content)}>
+        {(variant) => (
+          <ContentComponent
+            key={variant.testVariationId}
+            content={variant}
+            showContent={false}
+            model={props.model}
+            data={props.data}
+            context={props.context}
+            apiKey={props.apiKey}
+            apiVersion={props.apiVersion}
+            customComponents={props.customComponents}
+            canTrack={props.canTrack}
+            locale={props.locale}
+            includeRefs={props.includeRefs}
+            enrich={props.enrich}
+            isSsrAbTest={state.shouldRenderVariants}
+          />
+        )}
+      </For>
+    </Show> */
+  }
+  return (
+    <ContentComponent
+      content={props.content}
+      showContent
+      model={props.model}
+      data={props.data}
+      context={props.context}
+      apiKey={props.apiKey}
+      apiVersion={props.apiVersion}
+      customComponents={props.customComponents}
+      canTrack={props.canTrack}
+      locale={props.locale}
+      includeRefs={props.includeRefs}
+      enrich={props.enrich}
+      isSsrAbTest={false}
+    />
   );
+  {
+    /* </> */
+  }
 }
