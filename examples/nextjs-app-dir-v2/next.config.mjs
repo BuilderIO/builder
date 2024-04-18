9383e6d9f1f1ceba70b1e7d172520a7ce49b52c1
Sami Jaber <jabersami@gmail.com>
2024-04-18 18:49:31 -0300

    fix

--- examples/nextjs-app-dir-v2/next.config.mjs
+++ examples/nextjs-app-dir-v2/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['isolated-vm'],
+  },
+};
 
 export default nextConfig;
