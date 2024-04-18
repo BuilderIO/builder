20529abc85ce8e9dc360f010d04c512b7a441249
Sami Jaber <jabersami@gmail.com>
2024-04-18 18:55:02 -0300

    lint

--- examples/remix-gen2/vite.config.ts
+++ examples/remix-gen2/vite.config.ts
@@ -1,8 +1,8 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
-import { defineConfig } from "vite";
+import { vitePlugin as remix } from '@remix-run/dev';
+import { installGlobals } from '@remix-run/node';
 import { vercelPreset } from '@vercel/remix/vite';
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 installGlobals();
 
