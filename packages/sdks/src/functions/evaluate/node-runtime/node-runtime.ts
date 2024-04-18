ea1ffc07b51f88f37919d529da18e7778ff13f90
Sami Jaber <jabersami@gmail.com>
2024-04-18 17:43:03 -0300

    f

--- packages/sdks/src/functions/evaluate/node-runtime/node-runtime.ts
+++ packages/sdks/src/functions/evaluate/node-runtime/node-runtime.ts
@@ -101,7 +101,7 @@ const getIvm = (): typeof import('isolated-vm') => {
     importing and calling \`initializeNodeRuntime()\` from "@builder.io/sdk-react/node/init". This must be done in
     a server-only execution path within your application.
 
-    Please see the documentation for more information: https://builder.io/c/docs/integration-tips
+    Please see the documentation for more information: https://builder.io/c/docs/integration-tips#enabling-data-bindings-in-node-environments
     `
   );
 };
