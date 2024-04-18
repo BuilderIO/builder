a2ce79bcaeaf47430d46d9f04299ed74ac869565
Sami Jaber <jabersami@gmail.com>
2024-04-18 19:04:31 -0300

    Examples: upgrade react gen2  (#3213)
    ## Description
    
    Add a short description of what changes you made, why you made them, and
    any other context that you think might be helpful for someone to better
    understand what is contained in this pull request. This sort of
    information is useful for people reviewing the code, as well as anyone
    from the future trying to understand why changes were made or why a bug
    started happening.
    
    _Screenshot_
    If relevant, add a screenshot or two of the changes you made.

--- examples/remix-gen2/app/routes/($slug)._index.tsx
+++ examples/remix-gen2/app/routes/($slug)._index.tsx
@@ -1,28 +1,26 @@
 // ($slug)._index.tsx
 import {
+  Content,
   fetchOneEntry,
+  getBuilderSearchParams,
   isEditing,
   isPreviewing,
-  getBuilderSearchParams,
-  Content,
-} from "@builder.io/sdk-react";
-import type { LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { fetch as webFetch } from "@remix-run/web-fetch";
+} from '@builder.io/sdk-react';
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { fetch as webFetch } from '@remix-run/web-fetch';
 
-const apiKey = "f1a790f8c3204b3b8c5c1795aeac4660"; // Replace with your actual API key
+const apiKey = 'f1a790f8c3204b3b8c5c1795aeac4660'; // Replace with your actual API key
 
 export const loader = async ({ params, request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
-  const urlPath = `/${params["slug"] || ""}`;
+  const urlPath = `/${params['slug'] || ''}`;
 
-  const { initializeNodeRuntime } = await import(
-    "@builder.io/sdk-react/node/init"
-  );
+  const { initializeNodeRuntime } = await import('@builder.io/sdk-react/node/init');
   await initializeNodeRuntime();
 
   const page = await fetchOneEntry({
-    model: "page",
+    model: 'page',
     apiKey: apiKey,
     options: getBuilderSearchParams(url.searchParams),
     userAttributes: { urlPath },
@@ -32,9 +30,9 @@ export const loader = async ({ params, request }: LoaderFunctionArgs) => {
   const isEditingOrPreviewing = isEditing() || isPreviewing();
 
   if (!page && !isEditingOrPreviewing) {
-    throw new Response("Page Not Found", {
+    throw new Response('Page Not Found', {
       status: 404,
-      statusText: "Page not found in Builder.io",
+      statusText: 'Page not found in Builder.io',
     });
   }
 
