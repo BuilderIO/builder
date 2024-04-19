c660d88a3f7123eeb61a1a93ccf2a90f66b2a3e1
Sami Jaber <jabersami@gmail.com>
2024-04-19 13:50:17 -0300

    f
    f
    
    f

--- packages/sdks/src/functions/is-editing.ts
+++ packages/sdks/src/functions/is-editing.ts
@@ -6,6 +6,7 @@ export function isEditing(search?: Search) {
   return (
     isIframe() &&
     (TARGET === 'reactNative' ||
+      // accessing window.location.search is safe here because `isIframe()` is only `true` if we're in a browser.
       getSearchString(search || window.location.search).indexOf(
         'builder.frameEditing='
       ) !== -1)
