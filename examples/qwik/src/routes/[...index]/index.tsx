e7a6c3c592f3fcde44d82fef2109c86d0882e2f9
Sami Jaber <jabersami@gmail.com>
2024-04-22 11:54:31 -0300

    f

--- examples/qwik/src/routes/[...index]/index.tsx
+++ examples/qwik/src/routes/[...index]/index.tsx
@@ -1,7 +1,7 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 import { routeLoader$ } from '@builder.io/qwik-city';
-import { getContent, RenderContent, getBuilderSearchParams } from '@builder.io/sdk-qwik';
+import { fetchOneEntry, Content, getBuilderSearchParams } from '@builder.io/sdk-qwik';
 import { CUSTOM_COMPONENTS } from '../../components/builder-registry';
 
 // This page is a catch-all for all routes that don't have a pre-defined route.
@@ -9,14 +9,14 @@ import { CUSTOM_COMPONENTS } from '../../components/builder-registry';
 
 // Use the `useBuilderContent` route loader to get your content from Builder.
 // `routeLoader$()` takes an async function to fetch content
-// from Builder with using `getContent()`.
+// from Builder with using `fetchOneEntry()`.
 export const useBuilderContent = routeLoader$(async ({ url, error }) => {
   const isPreviewing = url.searchParams.has('builder.preview');
 
   // Fetch Builder.io Visual CMS content using the Qwik SDK.
   // The public API key is set in the .env file at the root
   // https://www.builder.io/c/docs/using-your-api-key
-  const builderContent = await getContent({
+  const builderContent = await fetchOneEntry({
     model: 'page',
     apiKey: import.meta.env.PUBLIC_BUILDER_API_KEY,
     options: getBuilderSearchParams(url.searchParams),
@@ -38,10 +38,10 @@ export const useBuilderContent = routeLoader$(async ({ url, error }) => {
 export default component$(() => {
   const builderContent = useBuilderContent();
 
-  // RenderContent component uses the `content` prop to render
+  // Content component uses the `content` prop to render
   // the page, specified by the API Key, at the current URL path.
   return (
-    <RenderContent
+    <Content
       model="page"
       content={builderContent.value}
       apiKey={import.meta.env.PUBLIC_BUILDER_API_KEY}
