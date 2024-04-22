c900cb35d930e87ae0b500fd5a446eb71975373f
Sami Jaber <jabersami@gmail.com>
2024-04-22 13:09:48 -0300

    lint

--- examples/qwik/src/components/header/header.tsx
+++ examples/qwik/src/components/header/header.tsx
@@ -1,6 +1,6 @@
-import { component$ } from "@builder.io/qwik";
-import { QwikLogo } from "../icons/qwik";
-import styles from "./header.module.css";
+import { component$ } from '@builder.io/qwik';
+import { QwikLogo } from '../icons/qwik';
+import styles from './header.module.css';
 
 export default component$(() => {
   return (
@@ -13,26 +13,17 @@ export default component$(() => {
         </div>
         <ul>
           <li>
-            <a
-              href="https://qwik.dev/docs/components/overview/"
-              target="_blank"
-            >
+            <a href="https://qwik.dev/docs/components/overview/" target="_blank">
               Docs
             </a>
           </li>
           <li>
-            <a
-              href="https://qwik.dev/examples/introduction/hello-world/"
-              target="_blank"
-            >
+            <a href="https://qwik.dev/examples/introduction/hello-world/" target="_blank">
               Examples
             </a>
           </li>
           <li>
-            <a
-              href="https://qwik.dev/tutorial/welcome/overview/"
-              target="_blank"
-            >
+            <a href="https://qwik.dev/tutorial/welcome/overview/" target="_blank">
               Tutorials
             </a>
           </li>
--- examples/qwik/src/components/router-head/router-head.tsx
+++ examples/qwik/src/components/router-head/router-head.tsx
@@ -1,5 +1,5 @@
-import { component$ } from "@builder.io/qwik";
-import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
+import { component$ } from '@builder.io/qwik';
+import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
 
 /**
  * The RouterHead component is placed inside of the document `<head>` element.
@@ -16,15 +16,15 @@ export const RouterHead = component$(() => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 
-      {head.meta.map((m) => (
+      {head.meta.map(m => (
         <meta key={m.key} {...m} />
       ))}
 
-      {head.links.map((l) => (
+      {head.links.map(l => (
         <link key={l.key} {...l} />
       ))}
 
-      {head.styles.map((s) => (
+      {head.styles.map(s => (
         <style key={s.key} {...s.props} dangerouslySetInnerHTML={s.style} />
       ))}
     </>
