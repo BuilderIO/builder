be1bbf47c59d6ce15812d7eb01802e1db9fb89f0
Sami Jaber <jabersami@gmail.com>
2024-05-02 19:05:26 -0300

    f

--- examples/svelte/sveltekit/vite.config.js
+++ examples/svelte/sveltekit/vite.config.js
@@ -6,8 +6,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
 	plugins: [sveltekit()],
 	optimizeDeps: {
-		// `isolated-vm` must be excluded from the pre-bundling optimization because
-		// it cannot (and shouldn't) be bundled at all.
+		/**
+		 * `isolated-vm` is an SDK dependency that must be excluded from the
+		 * pre-bundling optimization because it cannot (and shouldn't) be bundled at all.
+		 */
 		exclude: ['isolated-vm']
 	}
 };
