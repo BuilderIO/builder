a20ee920b0e0557041bfba309c3e532e874c4788
Sami Jaber <jabersami@gmail.com>
2024-05-02 18:17:21 -0300

    up

--- examples/svelte/sveltekit/vite.config.js
+++ examples/svelte/sveltekit/vite.config.js
@@ -1,7 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
+/**
+ * @type {import('vite').UserConfig}
+ */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		// `isolated-vm` must be excluded from the pre-bundling optimization because
+		// it cannot (and shouldn't) be bundled at all.
+		exclude: ['isolated-vm']
+	}
 };
 
 export default config;
