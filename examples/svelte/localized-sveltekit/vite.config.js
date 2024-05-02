a48f85ed9c7f0926668ca2c250f7dcb58b080ef0
Sami Jaber <jabersami@gmail.com>
2024-05-02 19:10:43 -0300

    Fix: svelte SDK examples (#3263)
    ## Description
    
    - update to latest SDK
    - fix sveltekit example

--- examples/svelte/localized-sveltekit/vite.config.js
+++ examples/svelte/localized-sveltekit/vite.config.js
@@ -1,7 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		/**
+		 * `isolated-vm` is an SDK dependency that must be excluded from the
+		 * pre-bundling optimization because it cannot (and shouldn't) be bundled at all.
+		 */
+		exclude: ['isolated-vm']
+	}
 };
 
 export default config;
--- examples/svelte/sveltekit/vite.config.js
+++ examples/svelte/sveltekit/vite.config.js
@@ -1,7 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
+/**
+ * @type {import('vite').UserConfig}
+ */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		/**
+		 * `isolated-vm` is an SDK dependency that must be excluded from the
+		 * pre-bundling optimization because it cannot (and shouldn't) be bundled at all.
+		 */
+		exclude: ['isolated-vm']
+	}
 };
 
 export default config;
