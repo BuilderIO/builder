143180d296c9cb190dafcadb64b4862e531eda5e
sidmohanty11 <sidmohanty11@gmail.com>
2024-05-07 15:57:11 +0530

    only for react and angular

--- packages/sdks-tests/src/tests/custom-components.spec.ts
+++ packages/sdks-tests/src/tests/custom-components.spec.ts
@@ -1,9 +1,14 @@
 import { expect } from '@playwright/test';
-import { test } from './helpers/index.js';
+import { excludeTestFor, test } from './helpers/index.js';
 
 test.describe('Custom components', () => {
-  test('correctly renders custom component', async ({ page, packageName }) => {
-    test.skip(packageName === 'next-app-dir-client');
+  test('correctly renders custom component', async ({ page }) => {
+    test.skip(
+      !excludeTestFor({
+        angular: true,
+        react: true,
+      })
+    );
     await page.goto('/custom-components');
     const helloWorldText = page.locator('text=hello World').first();
     await expect(helloWorldText).toBeVisible();
