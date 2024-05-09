6c653242ce4363f4272cad478d408fb57f24334e
sidmohanty11 <sidmohanty11@gmail.com>
2024-05-09 12:46:09 +0530

    mark passing test

--- packages/sdks-tests/src/tests/block-styles.spec.ts
+++ packages/sdks-tests/src/tests/block-styles.spec.ts
@@ -1,9 +1,8 @@
 import { expect } from '@playwright/test';
-import { excludeTestFor, getClassSelector, test } from './helpers/index.js';
+import { getClassSelector, test } from './helpers/index.js';
 
 test.describe('Block Styles', () => {
   test('check that block styles load', async ({ page, sdk }) => {
-    test.fail(excludeTestFor({ angular: true }, sdk), 'Angular Gen2 SDK not implemented.');
     await page.goto('/columns');
     const blocksWrapper = await page.locator(getClassSelector('builder-blocks', sdk)).first();
     await expect(blocksWrapper).toHaveCSS('display', 'flex');
