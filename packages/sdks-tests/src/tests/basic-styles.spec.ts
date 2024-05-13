672de0940f69c603874b175f9e764770b5a46c37
sidmohanty11 <sidmohanty11@gmail.com>
2024-05-13 18:34:19 +0530

    fail rn

--- packages/sdks-tests/src/tests/basic-styles.spec.ts
+++ packages/sdks-tests/src/tests/basic-styles.spec.ts
@@ -12,7 +12,7 @@ test.describe('Basic styles', () => {
     test.skip(packageName === 'react-native');
     await page.goto('/basic-styles');
 
-    const button = await page.waitForSelector('button');
+    const button = await page.waitForSelector('button', { timeout: 10000 });
 
     const buttonWidth = await button.evaluate(el => el.getBoundingClientRect().width);
     expect(buttonWidth).toBe(1264);
