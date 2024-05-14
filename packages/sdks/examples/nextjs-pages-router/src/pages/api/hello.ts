41678c218ea1d6f7812ef20a395f52e939982e10
sidmohanty11 <sidmohanty11@gmail.com>
2024-05-14 22:13:03 +0530

    lint

--- packages/sdks/examples/nextjs-pages-router/src/pages/api/hello.ts
+++ packages/sdks/examples/nextjs-pages-router/src/pages/api/hello.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
   name: string;
@@ -7,7 +7,7 @@ type Data = {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ name: 'John Doe' });
 }
--- packages/sdks/examples/nuxt/nuxt.config.ts
+++ packages/sdks/examples/nuxt/nuxt.config.ts
@@ -1,4 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+});
