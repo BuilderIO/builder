3aaeaeb06c80b6361dfba8de38452fb86e75396e
Sami Jaber <jabersami@gmail.com>
2024-05-15 13:38:46 -0300

    f

--- packages/sdks/snippets/vue/src/router/index.ts
+++ packages/sdks/snippets/vue/src/router/index.ts
@@ -1,11 +1,11 @@
-import BuilderContent from '@/views/BuilderContent.vue';
+import QuickStart from '@/views/QuickStart.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     // will match everything and put it under `route.params.pathMatch`
-    { path: '/:pathMatch(.*)*', name: 'NotFound', component: BuilderContent },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: QuickStart },
   ],
 });
 
