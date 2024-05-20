d05a67f671d82adf34c94fc7a7317d2b53c543c2
sidmohanty11 <sidmohanty11@gmail.com>
2024-05-20 18:49:50 +0530

    fix ci

--- packages/sdks-tests/src/e2e-tests/block-styles.spec.ts
+++ packages/sdks-tests/src/e2e-tests/block-styles.spec.ts
@@ -1,8 +1,9 @@
 import { expect } from '@playwright/test';
-import { getClassSelector, test } from '../helpers/index.js';
+import { checkIsRN, getClassSelector, test } from '../helpers/index.js';
 
 test.describe('Block Styles', () => {
   test('check that block styles load', async ({ page, sdk }) => {
+    test.skip(checkIsRN(sdk));
     await page.goto('/columns');
     const blocksWrapper = await page.locator(getClassSelector('builder-blocks', sdk)).first();
     await expect(blocksWrapper).toHaveCSS('display', 'flex');
