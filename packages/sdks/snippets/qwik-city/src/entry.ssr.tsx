5f20b2ea03118354dcd60435fc32d3d595ab74cc
sidmohanty11 <sidmohanty11@gmail.com>
2024-05-20 12:37:26 +0530

    lint fix

--- packages/sdks/snippets/qwik-city/src/entry.ssr.tsx
+++ packages/sdks/snippets/qwik-city/src/entry.ssr.tsx
@@ -13,9 +13,9 @@
 import {
   renderToStream,
   type RenderToStreamOptions,
-} from "@builder.io/qwik/server";
-import { manifest } from "@qwik-client-manifest";
-import Root from "./root";
+} from '@builder.io/qwik/server';
+import { manifest } from '@qwik-client-manifest';
+import Root from './root';
 
 export default function (opts: RenderToStreamOptions) {
   return renderToStream(<Root />, {
@@ -23,7 +23,7 @@ export default function (opts: RenderToStreamOptions) {
     ...opts,
     // Use container attributes to set attributes on the html tag.
     containerAttributes: {
-      lang: "en-us",
+      lang: 'en-us',
       ...opts.containerAttributes,
     },
     serverData: {
