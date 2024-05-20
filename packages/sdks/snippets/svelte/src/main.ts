b8dd1e8d5ca508bb2ab385e1447f5528c1d1107c
sidmohanty11 <sidmohanty11@gmail.com>
2024-05-20 18:09:07 +0530

    add ci

--- packages/sdks/snippets/svelte/src/main.ts
+++ packages/sdks/snippets/svelte/src/main.ts
@@ -1,8 +1,8 @@
-import './app.css'
-import App from './App.svelte'
+import './app.css';
+import App from './App.svelte';
 
 const app = new App({
   target: document.getElementById('app')!,
-})
+});
 
-export default app
+export default app;
