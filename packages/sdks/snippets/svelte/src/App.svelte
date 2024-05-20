b93baca636fe5e1a2225a2b2d64e351558f48516
sidmohanty11 <sidmohanty11@gmail.com>
2024-05-20 18:07:15 +0530

    svelte

---
+++ packages/sdks/snippets/svelte/src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { Content, _processContentResult, fetchOneEntry } from '@builder.io/sdk-svelte';
+
+  let props = null;
+  let apiKey = 'f1a790f8c3204b3b8c5c1795aeac4660';
+  let model = 'page';
+
+  const fetch = async () => {
+    const content = await fetchOneEntry({
+      apiKey,
+      model,
+      userAttributes: {
+        url: location.pathname
+      }
+    });
+    props = {
+      content,
+      model,
+      apiKey
+    }
+  };
+
+  fetch();
+</script>
+
+<svelte:head>
+  <title>Home</title>
+</svelte:head>
+
+<main>
+  {#if props}
+    <Content {...props} />
+  {:else}
+    Content Not Found
+  {/if}
+</main>
