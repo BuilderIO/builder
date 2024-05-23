8523828f7afd2d797dde67ebec887edc1caa6aac
sidmohanty11 <sidmohanty11@gmail.com>
2024-05-23 22:50:09 +0530

    all tries

--- packages/sdks-tests/src/snippet-tests/quickstart.spec.ts
+++ packages/sdks-tests/src/snippet-tests/quickstart.spec.ts
@@ -21,11 +21,40 @@ test.describe('Quickstart', () => {
 
     const links = page.locator('a');
 
-    const columnsLink = await links.filter({
-      hasText: 'Columns (with images) ',
-    });
+    const columnsLink = await links
+      .filter({
+        hasText: 'Columns (with images) ',
+      })
+      .first();
 
     await columnsLink.click();
     await findTextInPage({ page, text: 'Stack at tablet' });
+  });
+  test('loads homepage and navigates to columns 2nd try', async ({ page }) => {
+    await page.goto('/');
+
+    const link = page.locator('a', { hasText: 'Columns (with images) ' });
+
+    await link.click();
+
+    await findTextInPage({ page, text: 'Stack at tablet' });
+  });
+  test('loads homepage and navigates to columns 3rd try', async ({ page }) => {
+    await page.goto('/');
+
+    await page.click('a:has-text("Columns (with images) ")');
+
+    await page.waitForLoadState('networkidle');
+
+    await findTextInPage({ page, text: 'Stack at tablet' });
+  });
+  test('loads homepage and navigates to columns 4th try', async ({ page }) => {
+    await page.goto('/');
+
+    await page.click('a:has-text("Columns (with images) ")');
+
+    await page.waitForLoadState('networkidle');
+
+    expect(await page.locator('text=Stack at tablet').isVisible()).toBeTruthy();
   });
 });
