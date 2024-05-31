e18cfd350b36b1225314cbe21b552993d76b785e
Sami Jaber <jabersami@gmail.com>
2024-05-31 18:06:16 -0300

    f

--- packages/sdks-tests/playwright.config.ts
+++ packages/sdks-tests/playwright.config.ts
@@ -37,7 +37,8 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  retries: 0,
+  /* Allow retrying snippet tests because they're not deterministic */
+  retries: testType === 'snippet' ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
