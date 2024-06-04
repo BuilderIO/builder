11d8912a4b035121fecf6e225ee4a0700af9d654
Sami Jaber <jabersami@gmail.com>
2024-06-04 12:15:10 -0300

    f

--- packages/sdks/e2e/remix/app/root.tsx
+++ packages/sdks/e2e/remix/app/root.tsx
@@ -4,7 +4,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
