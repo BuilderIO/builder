af5e3af90da41d1a4974533302af33a585a809ef
Sami Jaber <jabersami@gmail.com>
2024-06-05 22:19:41 -0300

    f

--- packages/sdks-tests/src/e2e-tests/dynamic-content.spec.ts
+++ packages/sdks-tests/src/e2e-tests/dynamic-content.spec.ts
@@ -2,8 +2,7 @@ import { expect } from '@playwright/test';
 import { excludeTestFor, findTextInPage, test } from '../helpers/index.js';
 
 test.describe('Dynamic Content', () => {
-  test('Data Bindings', async ({ page, sdk, packageName }) => {
-    test.fail(packageName === 'hydrogen');
+  test('Data Bindings', async ({ page, sdk }) => {
     test.skip(
       excludeTestFor({ angular: true }, sdk),
       'Data bindings are not working in Angular -- loads forever so skipping for now'
