ce68173dc742488af0a57c2703f2c182316c5498
Sami Jaber <jabersami@gmail.com>
2024-06-25 16:20:40 -0300

    f

--- packages/sdks/e2e/react-native/app/[...slug].tsx
+++ packages/sdks/e2e/react-native/app/[...slug].tsx
@@ -1,52 +1,43 @@
 import {
   Content,
+  _processContentResult,
   fetchOneEntry,
-  isPreviewing,
 } from '@builder.io/sdk-react-native';
+import { getProps } from '@sdk/tests';
 import { useLocalSearchParams } from 'expo-router';
 import { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 
-const BUILDER_API_KEY = 'f1a790f8c3204b3b8c5c1795aeac4660'; // ggignore
-
 export default function HomeScreen() {
-  const [content, setContent] = useState<any>(undefined);
-
+  const [props, setProps] = useState<any>(undefined);
   const { slug } = useLocalSearchParams<{ slug: string }>();
 
   useEffect(() => {
-    fetchOneEntry({
-      model: 'page',
-      apiKey: BUILDER_API_KEY,
-      userAttributes: {
-        urlPath: slug ? `/${slug}` : '/',
-      },
-    })
-      .then((content) => {
-        if (content) {
-          setContent(content);
-        }
-      })
-      .catch((err) => {
-        console.error(
-          'something went wrong while fetching Builder Content: ',
-          err
-        );
-      });
+    getProps({
+      pathname: slug ? `/${slug}` : '/',
+      _processContentResult,
+      fetchOneEntry,
+    }).then((resp) => {
+      setProps(resp);
+    });
   }, []);
 
-  const shouldRenderBuilderContent = content || isPreviewing();
-
   return (
-    <View>
-      {shouldRenderBuilderContent ? (
+    <View
+      style={{
+        // mimick body stylesheets from the web
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      {props ? (
         <View
           style={{
             display: 'flex',
             flexDirection: 'column',
           }}
         >
-          <Content apiKey={BUILDER_API_KEY} model="page" content={content} />
+          <Content {...props} />
         </View>
       ) : (
         <Text>Not Found.</Text>
