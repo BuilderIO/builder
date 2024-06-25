68407597a61c66415e6ea718a4b762f1cc1d3eff
Sami Jaber <jabersami@gmail.com>
2024-06-25 15:40:04 -0300

    f

--- packages/sdks/e2e/react-native/app/_layout.tsx
+++ packages/sdks/e2e/react-native/app/_layout.tsx
@@ -1,28 +1,12 @@
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
     <ThemeProvider value={DefaultTheme}>
       <Stack>
