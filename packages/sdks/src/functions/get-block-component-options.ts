9ec429d6448a3336428e232463659e49b0032261
sidmohanty11 <sidmohanty11@gmail.com>
2024-06-25 11:36:22 +0530

    initial impl

--- packages/sdks/src/functions/get-block-component-options.ts
+++ packages/sdks/src/functions/get-block-component-options.ts
@@ -4,9 +4,5 @@ export function getBlockComponentOptions(block: BuilderBlock) {
   return {
     ...block.component?.options,
     ...(block as any).options,
-    /**
-     * Our built-in components frequently make use of the block, so we provide all of it under `builderBlock`
-     */
-    builderBlock: block,
   };
 }
