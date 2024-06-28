e6e8c419c12bb27964bcad7473a6988c4b59070c
Sami Jaber <jabersami@gmail.com>
2024-06-28 13:10:44 -0300

    u

--- packages/sdks/snippets/react-native/app/_layout.tsx
+++ packages/sdks/snippets/react-native/app/_layout.tsx
@@ -1,10 +1,5 @@
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import 'react-native-reanimated';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   return (
