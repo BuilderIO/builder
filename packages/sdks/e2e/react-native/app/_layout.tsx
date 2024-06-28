f7fde4a2e6483f1aedce87c9cb21ce3572ac2fed
Sami Jaber <jabersami@gmail.com>
2024-06-28 11:49:25 -0300

    rm dep

--- packages/sdks/e2e/react-native/app/_layout.tsx
+++ packages/sdks/e2e/react-native/app/_layout.tsx
@@ -1,6 +1,5 @@
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import 'react-native-reanimated';
 
 export default function RootLayout() {
   return (
