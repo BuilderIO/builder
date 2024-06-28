be40fa590c8ec7378f018242ce41a9b14682ffaf
Sami Jaber <jabersami@gmail.com>
2024-06-28 15:14:09 -0300

    f

--- examples/vue/nuxt-2/nuxt.config.js
+++ examples/vue/nuxt-2/nuxt.config.js
@@ -4,11 +4,7 @@
 export default {
   components: true,
 
-  buildModules: [
-    '@nuxt/components',
-    // Commented out since it currently does not work
-    // '@builder.io/sdk-vue/nuxt'
-  ],
+  buildModules: ['@nuxt/components', '@builder.io/sdk-vue/nuxt'],
 
   build: {
     extend(config, ctx) {
@@ -19,9 +15,6 @@ export default {
     // This is needed because the package is an ESM module
     transpile: ['@builder.io/sdk-vue'],
   },
-
-  // We need to import the CSS for the SDK in the Nuxt Config here
-  css: ['@builder.io/sdk-vue/vue2/css'],
 
   server: { port: 3001 },
 }
