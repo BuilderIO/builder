c7879984e5c17ac0e75f8a11a87bfe256f3f0a98
Sami Jaber <jabersami@gmail.com>
2024-07-05 16:33:42 -0300

    Fix: React Native SDK (#3345)
    ## Description
    
    Handle multiple compatibility issues with Expo 50+
    
    - async storage: handle SSR/static rendering
    - async storage: add `regenerator-runtime` polyfill
    - include source in build as per
    https://callstack.github.io/react-native-builder-bob/build#source
    - update e2e and snippets to use Expo 50+
    
    
    TODO:
    
    - [x] fix react-native-render-html `defaultProps` usage
    - [x] fix development builds
    - [x] test development builds
    - [ ] add detox e2e test

---
+++ .changeset/nice-toys-pretend.md
@@ -0,0 +1,6 @@
+---
+'@builder.io/sdk-react-native': patch
+---
+
+Fix: replace `react-native-render-html` with `@builder.io/react-native-render-html` to remove `defaultProps` errors.
+See https://github.com/meliorence/react-native-render-html/issues/661
--- packages/sdks/snippets/react-native/metro.config.js
+++ packages/sdks/snippets/react-native/metro.config.js
@@ -1,18 +1,53 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const exclusionList = require('metro-config/src/defaults/exclusionList');
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require('path');
+
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+const rootNodeModules = path.join(
+  __dirname,
+  '..',
+  '..',
+  '..',
+  '..',
+  'node_modules'
+);
+
+// We override this config because expo will automatically include every folder in the yarn monorepo,
+// which is vastly overkill and slows down the build.
+config.watchFolders = [
+  // root node_modules
+  rootNodeModules,
+
+  // packages/node_modules
+  path.join(__dirname, '..', '..', '..', 'node_modules'),
+
+  // packages/sdks
+  path.join(__dirname, '..', '..', '..', 'sdks', 'node_modules'),
+
+  // packages/sdks/output/react-native
+  path.join(__dirname, '..', '..', 'output', 'react-native'),
+
+  // this project
+  path.join(__dirname),
+];
+
+const initialBlockList = Array.isArray(config.resolver.blockList)
+  ? config.resolver.blockList
+  : [config.resolver.blockList];
 
 /**
- * we need to manually exclude dists and nx caches because metro scans them and
- * the `package.json` within will collide with those of the real packages.
- * ref: https://metrobundler.dev/docs/configuration/#blocklist
+ * We need to exclude the react, react-native and react-dom versions coming
+ * from the rest of the monorepo, so that we only use the versions that are part of this expo app.
  */
-module.exports = {
-  resolver: {
-    blacklistRE: exclusionList([
-      /.*\/react\/dist\/.*/,
-      /.*\/core\/dist\/.*/,
-      /.*\/webcomponents\/dist\/.*/,
-      /.*\.nx-cache.*/,
-    ]),
-  },
-};
+const excludedRootPkgs = new RegExp(
+  rootNodeModules + '/(react|react-native|react-dom)/'
+);
+
+config.resolver.blockList = [...initialBlockList, excludedRootPkgs];
+
+module.exports = config;
