43ddcedd1aebb9f4b0370823575b76b57a13bab9
sidmohanty11 <sidmohanty11@gmail.com>
2024-07-10 15:08:46 +0530

    fix script

--- packages/sdks/output/angular/scripts/multi-build.js
+++ packages/sdks/output/angular/scripts/multi-build.js
@@ -55,7 +55,7 @@ const getFolderName = () => {
   }
 };
 
-content = content.replace(/placeholder-runtime/g, `./${getFolderName()}/index`);
+content = content.replace(/'placeholder-runtime'/g, `'./${getFolderName()}/index'`);
 
 fs.writeFileSync(chooseEvalFile, content, 'utf-8');
 
@@ -70,6 +70,6 @@ if (fs.existsSync(packageJsonPath)) {
 }
 
 // revert back the choose-eval.ts file
-content = content.replace(`./${getFolderName()}/index`, 'placeholder-runtime');
+content = content.replace(`'./${getFolderName()}/index'`, "'placeholder-runtime'");
 
 fs.writeFileSync(chooseEvalFile, content, 'utf-8');
