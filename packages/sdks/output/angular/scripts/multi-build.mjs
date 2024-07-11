27c2175fe29fb272dd587f982d1ecc4c120d3b36
Sidharth Mohanty <sidmohanty11@gmail.com>
2024-07-11 20:53:29 +0530

    multi-build support for angular gen2 sdk (#3398)
    ## Description
    
    Creates two builds (node and browser) and exports them from `/lib`
    
    Jira
    https://builder-io.atlassian.net/browse/ENG-5583
    
    _Screenshot_
    If relevant, add a screenshot or two of the changes you made.

---
+++ packages/sdks/output/angular/scripts/multi-build.mjs
@@ -0,0 +1,97 @@
+import babel from '@babel/core';
+import tsPlugin from '@babel/plugin-syntax-typescript';
+import {
+  getEvaluatorPathAlias,
+  getSdkOutputPath,
+} from '@builder.io/sdks/output-generation/index.js';
+import { execSync } from 'child_process';
+import fs from 'fs';
+import glob from 'glob';
+import path from 'path';
+import prettier from 'prettier';
+
+/**
+ * @typedef {import('@babel/core').PluginObj} babel.PluginObj
+ * @typedef {babel.PluginObj} InlinePlugin
+ */
+
+const ngPackageJsonPath = path.resolve('ng-package.json');
+const ngPackageJson = JSON.parse(fs.readFileSync(ngPackageJsonPath, 'utf-8'));
+
+const outputPath = getSdkOutputPath();
+
+// Set the destination path to the correct output path for specific SDK_ENV
+ngPackageJson.dest = outputPath;
+
+fs.writeFileSync(
+  ngPackageJsonPath,
+  // we needed to prettify the JSON to make sure it matches the linting rules
+  await prettier.format(JSON.stringify(ngPackageJson, null, 2), {
+    parser: 'json',
+  }),
+  'utf-8'
+);
+
+const foundFiles = glob.sync('src/**/*/choose-eval.*');
+
+if (foundFiles.length !== 1) {
+  throw new Error(
+    'Expected exactly one file named `choose-eval.ts` containing "placeholder-runtime" import.'
+  );
+}
+
+const chooseEvalFile = foundFiles[0];
+
+// Helper function to transform the file
+const transformFile = (filePath, replaceValue, revert = false) => {
+  let hasReplacedImport = false;
+  const result = babel.transformFileSync(filePath, {
+    plugins: [
+      tsPlugin,
+      /** @returns {InlinePlugin} */
+      () => {
+        return {
+          visitor: {
+            ImportDeclaration(path) {
+              const currentValue = revert
+                ? replaceValue
+                : 'placeholder-runtime';
+              const newValue = revert ? 'placeholder-runtime' : replaceValue;
+              if (path.node.source.value === currentValue) {
+                path.node.source.value = newValue;
+                hasReplacedImport = true;
+              }
+            },
+          },
+        };
+      },
+    ],
+  });
+
+  if (!hasReplacedImport) {
+    throw new Error(
+      `Expected to replace import with value ${replaceValue} in ${filePath} but no such import found.`
+    );
+  }
+
+  fs.writeFileSync(filePath, result.code, 'utf-8');
+};
+
+// Transform the choose-eval.ts file to replace 'placeholder-runtime'
+// to appropriate runtime value based on the SDK_ENV
+const alias = getEvaluatorPathAlias();
+const folderName = alias['placeholder-runtime'];
+transformFile(chooseEvalFile, folderName);
+
+// Run the Angular build command
+execSync(`ng build --project sdk-angular`, { stdio: 'inherit' });
+
+// Remove any `package.json` generated inside the `lib/*` folder
+const packageJsonPath = path.resolve(outputPath, 'package.json');
+
+if (fs.existsSync(packageJsonPath)) {
+  fs.unlinkSync(packageJsonPath);
+}
+
+// Revert the choose-eval.ts file to its original state i.e, "placeholder-runtime"
+transformFile(chooseEvalFile, folderName, true);
