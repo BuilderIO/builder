2628fd927b2fc12a021f5597b2468e64f6a2fee3
Sami Jaber <jabersami@gmail.com>
2024-07-12 15:26:19 -0300

    rely on PAT token instead

--- scripts/lint-and-commit.sh
+++ scripts/lint-and-commit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "$PRIVATE_KEY" --import >gpg
-echo "$PUBLIC_KEY" --import >gpg
-git config --global user.email "sami+builderbot@builder.io"
-git config --global user.name "builderio-bot"
-git config --global user.signingkey $KEY_ID
-git config --global commit.gpgsign true
+# echo "$PRIVATE_KEY" --import >gpg
+# echo "$PUBLIC_KEY" --import >gpg
+# git config --global user.email "sami+builderbot@builder.io"
+# git config --global user.name "builderio-bot"
+# git config --global user.signingkey $KEY_ID
+# git config --global commit.gpgsign true
 echo foo >bar.txt
 git add .
 git commit -S -m "This commit is signed!"
