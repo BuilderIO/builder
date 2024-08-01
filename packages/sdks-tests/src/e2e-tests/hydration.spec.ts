b4b87b2dcb7330793b2dfa0aabe8273df9630c9e
sidmohanty11 <sidmohanty11@gmail.com>
2024-08-01 21:18:29 +0530

    skip test

--- packages/sdks-tests/src/e2e-tests/hydration.spec.ts
+++ packages/sdks-tests/src/e2e-tests/hydration.spec.ts
@@ -23,7 +23,7 @@ test.describe('Hydration', () => {
   });
 
   test('No mismatch on A/B test content', async ({ page, packageName }) => {
-    test.fail(packageName === 'angular-ssr');
+    test.skip(packageName === 'angular-ssr', 'Angular SSR does not support A/B tests');
     test.fail(
       packageName === 'nextjs-sdk-next-app',
       "NextJS SDK currently does not support SSR'd A/B tests. Losing variants are stripped from SSR'd Content too late."
