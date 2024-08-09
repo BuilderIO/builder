5bbbb1a4dd507ed031f2af0ea2aef69c2535b24c
Steve Sewell <steve@builder.io>
2024-08-09 08:03:26 -0700

    major perf improvements

--- packages/sdks/src/functions/get-processed-block.ts
+++ packages/sdks/src/functions/get-processed-block.ts
@@ -1,9 +1,34 @@
 import type { BuilderContextInterface } from '../context/types.js';
+import { omit } from '../helpers/omit.js';
 import type { BuilderBlock } from '../types/builder-block.js';
 import { evaluate } from './evaluate/index.js';
-import { fastClone } from './fast-clone.js';
 import { set } from './set.js';
 import { transformBlock } from './transform-block.js';
+
+// Deep clone a block but without cloning any child blocks
+export function deepCloneWithConditions<T = any>(obj: T): T {
+  if (obj === null || typeof obj !== 'object') {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map((item: any) => deepCloneWithConditions(item)) as T;
+  }
+
+  if ((obj as any)['@type'] === '@builder.io/sdk:Element') {
+    return obj;
+  }
+
+  const clonedObj: any = {};
+
+  for (const key in obj) {
+    if (key !== 'meta' && Object.prototype.hasOwnProperty.call(obj, key)) {
+      clonedObj[key] = deepCloneWithConditions(obj[key]);
+    }
+  }
+
+  return clonedObj;
+}
 
 const evaluateBindings = ({
   block,
@@ -20,11 +45,14 @@ const evaluateBindings = ({
   if (!block.bindings) {
     return block;
   }
-  const copy = fastClone(block);
+  // TODO: don't fast clone this! Any
+  const copy = deepCloneWithConditions(omit(block, 'children', 'meta'));
   const copied = {
     ...copy,
     properties: { ...copy.properties },
     actions: { ...copy.actions },
+    children: block.children,
+    meta: block.meta,
   };
 
   for (const binding in block.bindings) {
@@ -35,12 +63,12 @@ const evaluateBindings = ({
       rootState,
       rootSetState,
       context,
-      enableCache: true,
+      enableCache: typeof window === 'undefined',
     });
     set(copied, binding, value);
   }
 
-  return copied;
+  return copied as BuilderBlock;
 };
 
 export function getProcessedBlock({
