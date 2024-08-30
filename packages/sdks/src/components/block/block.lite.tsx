22d86bf04ad392b2a7550bcad86de08fc1903961
Sami Jaber <jabersami@gmail.com>
2024-08-30 16:25:31 -0300

    f

--- packages/sdks/src/components/block/block.lite.tsx
+++ packages/sdks/src/components/block/block.lite.tsx
@@ -69,18 +69,6 @@ export default function Block(props: BlockProps) {
       });
     },
     get processedBlock(): BuilderBlock {
-      // Simple agnostic memoization for the processed block
-      // This is used to avoid re-processing the block on every render
-      // We need to make this a property on an object so setState() isn't
-      // called causing infinite rerenders e.g. in React
-      // _processedBlock: { value: null as BuilderBlock | null, update: false },
-      // if (
-      //   state._processedBlock.value &&
-      //   !state._processedBlock.update &&
-      //   !isPreviewing()
-      // ) {
-      //   return state._processedBlock.value;
-      // }
       const blockToUse = props.block.repeat?.collection
         ? props.block
         : getProcessedBlock({
@@ -91,9 +79,6 @@ export default function Block(props: BlockProps) {
             context: props.context.value.context,
             shouldEvaluateBindings: true,
           });
-
-      // state._processedBlock.value = blockToUse;
-      // state._processedBlock.update = false;
 
       return blockToUse;
     },
@@ -187,31 +172,6 @@ export default function Block(props: BlockProps) {
       };
     },
   });
-
-  /**
-   * This trick forces the component to re-compute the `processedBlock` on every update.
-   */
-  // onUpdate(() => {
-  //   useTarget({
-  //     svelte: () => {},
-  //     vue: () => {},
-  //     angular: () => {},
-  //     qwik: () => {},
-  //     solid: () => {},
-  //     default: () => {
-  //       state._processedBlock.update = true;
-  //     },
-  //   });
-  // });
-
-  // /**
-  //  * For frameworks that use signals/stores (e.g. Svelte), we need to
-  //  * track changes on the `block` prop to force the component to re-compute
-  //  * the `processedBlock`
-  //  */
-  // onUpdate(() => {
-  //   state._processedBlock.update = true;
-  // }, [props.block]);
 
   onMount(() => {
     useTarget({
