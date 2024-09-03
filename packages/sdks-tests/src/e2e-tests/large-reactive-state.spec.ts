c9b335bd284d40ef2159734f9b482c581f586b6d
Sami Jaber <jabersami@gmail.com>
2024-09-03 17:54:53 -0300

    f

--- packages/sdks-tests/src/e2e-tests/large-reactive-state.spec.ts
+++ packages/sdks-tests/src/e2e-tests/large-reactive-state.spec.ts
@@ -2,29 +2,10 @@ import { expect } from '@playwright/test';
 import { excludeTestFor, test } from '../helpers/index.js';
 
 test.describe('Large Reactive State', () => {
-  test('shows default value', async ({ page }) => {
+  test('renders entire page correctly', async ({ page }) => {
     await page.goto('/large-reactive-state');
 
     await expect(page.getByText('0', { exact: true })).toBeVisible();
-  });
-
-  test('increments value correctly', async ({ page, packageName, sdk }) => {
-    test.fail(excludeTestFor({ rsc: true }, sdk));
-    test.fail(packageName === 'nextjs-sdk-next-app');
-
-    await page.goto('/large-reactive-state');
-
-    await expect(page.getByText('0', { exact: true })).toBeVisible();
-
-    await page.getByText('Increment Number').click();
-
-    await expect(page.getByText('1', { exact: true })).toBeVisible();
-  });
-
-  test('renders all dummy text blocks with correct bindings', async ({ page }) => {
-    await page.goto('/large-reactive-state');
-
-    // Check if the last dummy text block is rendered
     await expect(page.getByText('Dummy text block 1000')).toBeVisible();
   });
 
