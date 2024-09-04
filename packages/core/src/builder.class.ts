b927b870e93fb5770656552e0b757f9806026eeb
Clyde Mendonca <clydemendonca@Clydes-MacBook-Pro.local>
2024-09-04 15:34:08 +0530

    feat[core][Builder]: ENG-6337 remove unrequired files

--- packages/core/src/builder.class.ts
+++ packages/core/src/builder.class.ts
@@ -22,12 +22,6 @@ import hash from 'hash-sum';
 import { toError } from './functions/to-error';
 import { emptyUrl, UrlLike } from './url';
 import { DEFAULT_API_VERSION, ApiVersion } from './types/api-version';
-import {
-  flatten,
-  flattenMongoQuery,
-  getBuilderSearchParamsFromWindow,
-  normalizeSearchParams,
-} from './builder.helper';
 
 export type Url = any;
 
