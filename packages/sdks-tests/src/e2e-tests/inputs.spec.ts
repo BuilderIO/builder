091d6a586b40ec336d226c11d37b2966b2b6cd33
sidmohanty11 <sidmohanty11@gmail.com>
2024-09-10 14:34:03 +0530

    move tests around

--- packages/sdks-tests/src/e2e-tests/inputs.spec.ts
+++ packages/sdks-tests/src/e2e-tests/inputs.spec.ts
@@ -7,18 +7,3 @@ test.describe('Inputs', () => {
     await expect(page.locator('text=init value')).toBeVisible();
   });
 });
-
-test.describe('Symbols with bound content inputs', () => {
-  test('repeat Symbols with content bound to content inputs should render correctly', async ({
-    page,
-  }) => {
-    // here data prop is { products: [{ header: 'title1' }, { header: 'title2' }, { header: 'title3' }] }
-    await page.goto('/symbol-with-repeat-input-binding');
-
-    const promises = [];
-    for (let i = 1; i <= 3; i++) {
-      promises.push(expect(page.locator(`text=title${i}`)).toBeVisible());
-    }
-    await Promise.all(promises);
-  });
-});
