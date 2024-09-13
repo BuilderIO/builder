207ae81591b0e60a24bcc13e3c82f84b01a5014f
Clyde Mendonca <clydemendonca@Clydes-MacBook-Pro.local>
2024-09-13 14:06:55 +0530

    feat[react-sdk-next-app][blocks]: ENG-6614 code refactoring

--- packages/sdks/e2e/react-sdk-next-app/src/app/[[...slug]]/page.tsx
+++ packages/sdks/e2e/react-sdk-next-app/src/app/[[...slug]]/page.tsx
@@ -7,6 +7,48 @@ import {
 } from '@builder.io/sdk-react';
 import { getProps } from '@sdk/tests';
 
+const builderBlockWithClassNameCustomComponent = {
+  name: 'BuilderBlockWithClassName',
+  component: BuilderBlockWithClassName,
+  isRSC: true,
+  shouldReceiveBuilderProps: {
+    builderBlock: true,
+    builderContext: true,
+    builderComponents: true,
+  },
+  inputs: [
+    {
+      name: 'content',
+      type: 'uiBlocks',
+      defaultValue: [
+        {
+          '@type': '@builder.io/sdk:Element',
+          '@version': 2,
+          id: 'builder-c6e179528dee4e62b337cf3f85d6496f',
+          component: {
+            name: 'Text',
+            options: {
+              text: 'Enter some text...',
+            },
+          },
+          responsiveStyles: {
+            large: {
+              display: 'flex',
+              flexDirection: 'column',
+              position: 'relative',
+              flexShrink: '0',
+              boxSizing: 'border-box',
+              marginTop: '20px',
+              lineHeight: 'normal',
+              height: 'auto',
+            },
+          },
+        },
+      ],
+    },
+  ],
+};
+
 interface PageProps {
   params: {
     slug: string[];
@@ -34,54 +76,9 @@ export default async function Page(props: PageProps) {
     );
   }
 
-  return (
-    <Content
-      {...builderProps}
-      customComponents={[
-        {
-          name: 'BuilderBlockWithClassName',
-          component: BuilderBlockWithClassName,
-          isRSC: true,
-          shouldReceiveBuilderProps: {
-            builderBlock: true,
-            builderContext: true,
-            builderComponents: true,
-          },
-          inputs: [
-            {
-              name: 'content',
-              type: 'uiBlocks',
-              defaultValue: [
-                {
-                  '@type': '@builder.io/sdk:Element',
-                  '@version': 2,
-                  id: 'builder-c6e179528dee4e62b337cf3f85d6496f',
-                  component: {
-                    name: 'Text',
-                    options: {
-                      text: 'Enter some text...',
-                    },
-                  },
-                  responsiveStyles: {
-                    large: {
-                      display: 'flex',
-                      flexDirection: 'column',
-                      position: 'relative',
-                      flexShrink: '0',
-                      boxSizing: 'border-box',
-                      marginTop: '20px',
-                      lineHeight: 'normal',
-                      height: 'auto',
-                    },
-                  },
-                },
-              ],
-            },
-          ],
-        },
-      ]}
-    />
-  );
+  builderProps.customComponents = [builderBlockWithClassNameCustomComponent];
+
+  return <Content {...builderProps} />;
 }
 
 export const revalidate = 4;
