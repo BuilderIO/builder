7edcc564585645222ce8f47bc7de64361dea9f28
Clyde Mendonca <clydemendonca@Clydes-MacBook-Pro.local>
2024-09-17 16:29:50 +0530

    feat[react-native][blocks]: ENG-6614 add className for blocks for react-native

--- packages/sdks-tests/src/e2e-tests/blocks-class-name.spec.ts
+++ packages/sdks-tests/src/e2e-tests/blocks-class-name.spec.ts
@@ -2,8 +2,13 @@ import { expect } from '@playwright/test';
 import { excludeGen1, test } from '../helpers/index.js';
 
 test.describe('Blocks className', () => {
-  test('should be present on element with class builder-blocks', async ({ page, sdk }) => {
+  test('should be present on element with class builder-blocks', async ({
+    page,
+    sdk,
+    packageName,
+  }) => {
     test.skip(excludeGen1(sdk));
+    test.skip(packageName === 'react-native');
 
     await page.goto('/blocks-class-name', { waitUntil: 'networkidle' });
     const countOfBuilderBlocksWithClassName = await page
