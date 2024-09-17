fa8e6baee4c9634ff47920cdf50b6f5efc3d9373
Clyde Mendonca <clydemendonca@Clydes-MacBook-Pro.local>
2024-09-17 10:04:35 +0530

    feat[blocks][className]: ENG-6614 remove only for test.describe

--- packages/sdks-tests/src/e2e-tests/blocks-class-name.spec.ts
+++ packages/sdks-tests/src/e2e-tests/blocks-class-name.spec.ts
@@ -1,13 +1,9 @@
 import { expect } from '@playwright/test';
-import { excludeGen1, excludeTestFor, test } from '../helpers/index.js';
+import { excludeGen1, test } from '../helpers/index.js';
 
 test.describe('Blocks className', () => {
-  test('call proxy API only once - in page', async ({ page, sdk }) => {
+  test('should be present on element with class builder-blocks', async ({ page, sdk }) => {
     test.skip(excludeGen1(sdk));
-    test.fail(
-      excludeTestFor({ angular: true }, sdk),
-      'Angular class/className mistmatch for blocks-wrapper'
-    );
 
     await page.goto('/blocks-class-name', { waitUntil: 'networkidle' });
     const countOfBuilderBlocksWithClassName = await page
