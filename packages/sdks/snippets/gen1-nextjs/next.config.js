b911cfa57f89a6beb8d066e9d8360c5aac6f2d2f
sidmohanty11 <sidmohanty11@gmail.com>
2024-09-26 15:58:04 +0530

    fix

--- packages/sdks/snippets/gen1-nextjs/next.config.js
+++ packages/sdks/snippets/gen1-nextjs/next.config.js
@@ -1,7 +1,20 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     externalDir: true,
+  },
+  /**
+   * `useIsPreviewing` uses a different react version while linking so here are resolving it
+   * this is not required when using the SDK directly
+   */
+  webpack: (config) => {
+    config.resolve.alias['react'] = path.resolve(
+      __dirname,
+      './node_modules/react'
+    );
+    return config;
   },
 };
 
