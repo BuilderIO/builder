12cefc0d2a56a5485469f8dc7f5956432e643dd9
Uttej Venkata Sastry <uttej@builder.io>
2024-09-27 13:32:53 +0530

    refactor

--- packages/sdks/snippets/angular/src/app/blog-article/blog-article.component.ts
+++ packages/sdks/snippets/angular/src/app/blog-article/blog-article.component.ts
@@ -1,34 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Content, fetchOneEntry } from '@builder.io/sdk-angular';
+import {
+  Content,
+  fetchOneEntry,
+  type BuilderContent,
+} from '@builder.io/sdk-angular';
 
 @Component({
   selector: 'app-blog-article',
   standalone: true,
   imports: [Content, CommonModule],
   template: `<div *ngIf="article" class="content">
-    <h1>{{ article.data.title }}</h1>
-    <p>{{ article.data.blurb }}</p>
-    <img [src]="article.data.image" alt="" />
-    <content [model]="model" [content]="article"></content>
+    <h1>{{ article.data?.['title'] }}</h1>
+    <p>{{ article.data?.['blurb'] }}</p>
+    <img [src]="article.data?.['image']" alt="" />
+    <builder-content [model]="model" [content]="article"></builder-content>
   </div> `,
 })
 export class BlogArticleComponent {
-  article: any;
+  article!: BuilderContent | null;
   model = 'blog-article';
 
-  constructor() {
-  }
-
   async ngOnInit() {
-    const handle = 'new-product-line';
-    const article = await fetchOneEntry({
-      model: 'blog-article',
+    this.article = await fetchOneEntry({
+      model: this.model,
       apiKey: 'ee9f13b4981e489a9a1209887695ef2b',
       query: {
-        'data.handle': handle,
+        'data.handle': 'new-product-line',
       },
     });
-    this.article = article;
   }
 }
