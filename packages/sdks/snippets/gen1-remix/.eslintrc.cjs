b69b553f76b42eed4ef1ce8fea4301da55f5a406
sidmohanty11 <sidmohanty11@gmail.com>
2024-09-30 15:30:34 +0530

    try disabling

--- packages/sdks/snippets/gen1-remix/.eslintrc.cjs
+++ packages/sdks/snippets/gen1-remix/.eslintrc.cjs
@@ -23,7 +23,9 @@ module.exports = {
 
   // Base config
   extends: ['eslint:recommended'],
-
+  rules: {
+    'import/no-unresolved': ['error', { ignore: ['@builder.io/react'] }],
+  },
   overrides: [
     // React
     {
