747c0f0596d177e93c6619771fc4ab8f8b02aa87
sidmohanty11 <sidmohanty11@gmail.com>
2024-09-30 15:18:01 +0530

    fix

--- packages/sdks/snippets/gen1-remix/vite.config.ts
+++ packages/sdks/snippets/gen1-remix/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  ssr: {
+    noExternal: ['@builder.io/react'],
+  },
   plugins: [
     remix({
       future: {
