aad5c1b4d51511486812ea5c236244c95175bfda
KonstantinChabanenko <kostiantyn.chabanenko@deptagency.com>
2024-10-02 10:42:05 -0700

    Added custom typograpghy formats to the RTE

--- plugins/custom-rich-text/src/plugin.tsx
+++ plugins/custom-rich-text/src/plugin.tsx
@@ -1,12 +1,37 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
+import React, { useRef, useEffect } from 'react';
 import { Builder } from '@builder.io/sdk';
-import ReactQuill from 'react-quill';
+import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import './styles/global.css';
+import Typography, { AllowedTags } from './formats/typography';
+
+Quill.register(Typography, true);
+
+const getStringifiedFormat = (tag: AllowedTags, className: string): string =>
+  JSON.stringify({ tag, className });
 
 const modules = {
   toolbar: [
-    [{ header: [1, 2, false] }],
+    [
+      {
+        typography: [
+          getStringifiedFormat('P', ''),
+          getStringifiedFormat('H1', ''),
+          getStringifiedFormat('H2', ''),
+          getStringifiedFormat('H3', ''),
+          getStringifiedFormat('H4', ''),
+          getStringifiedFormat('H5', ''),
+          getStringifiedFormat('H6', ''),
+          getStringifiedFormat('P', 'small'),
+          getStringifiedFormat('P', 'medium'),
+          getStringifiedFormat('P', 'large'),
+          getStringifiedFormat('SPAN', 'small'),
+          getStringifiedFormat('SPAN', 'large')
+        ]
+      }
+    ],
     ['bold', 'italic', 'underline', 'strike', 'blockquote'],
     [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
     ['link', 'image'],
@@ -15,7 +40,7 @@ const modules = {
 };
 
 const formats = [
-  'header',
+  'typography',
   'bold',
   'italic',
   'underline',
@@ -34,8 +59,22 @@ interface TextProps {
 }
 
 function RichTextEditor(props: TextProps) {
+  const quillRef = useRef<ReactQuill>(null!);
+
+  useEffect(() => {
+    const quill  = quillRef.current.getEditor(); // Access the Quill instance
+
+    // Set default content with a specific format
+    const delta = quill.clipboard.convert('<p>Enter text...</p>');
+    quill.setContents(delta);
+
+    // Optionally, set a default format
+    // quill.format('typography', getStringifiedFormat('P', 'medium'));
+  }, []);
+
   return (
     <ReactQuill
+      ref={quillRef}
       theme="snow"
       value={props.value}
       onChange={props.onChange}
