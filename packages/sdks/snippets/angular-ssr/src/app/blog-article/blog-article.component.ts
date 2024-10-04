a965c8810b5ad04f1b4c6b12a59d35c6682812f2
Uttej Venkata Sastry <uttej@builder.io>
2024-10-05 00:15:50 +0530

    housekeeping

--- packages/sdks/snippets/angular-ssr/src/app/blog-article/blog-article.component.ts
+++ packages/sdks/snippets/angular-ssr/src/app/blog-article/blog-article.component.ts
@@ -7,16 +7,7 @@ import { Content, type BuilderContent } from '@builder.io/sdk-angular';
   selector: 'app-blog-article',
   standalone: true,
   imports: [Content, CommonModule],
-  template: `<div *ngIf="article" class="content">
-    <h1>{{ article.data?.title }}</h1>
-    <p>{{ article.data?.['blurb'] }}</p>
-    <img [src]="article.data?.['image']" alt="" />
-    <builder-content
-      [model]="model"
-      [content]="article"
-      [apiKey]="apiKey"
-    ></builder-content>
-  </div> `,
+  templateUrl: './blog-article.component.html',
 })
 export class BlogArticleComponent {
   article: BuilderContent | null = null;
