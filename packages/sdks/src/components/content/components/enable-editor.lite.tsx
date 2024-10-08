3e1fece2ca5f6d136a1bcdcf10a18bb18d6747d0
Sami Jaber <me@sami.website>
2024-10-08 14:03:51 -0400

    Revert "Feat/register component devtools (#3619)"
    This reverts commit a15696e79b829b150eecc2b8842f5e3ffdc733a8.

--- packages/sdks/src/components/content/components/enable-editor.lite.tsx
+++ packages/sdks/src/components/content/components/enable-editor.lite.tsx
@@ -20,7 +20,7 @@ import { fetch } from '../../../functions/get-fetch.js';
 import { isBrowser } from '../../../functions/is-browser.js';
 import { isEditing } from '../../../functions/is-editing.js';
 import { isPreviewing } from '../../../functions/is-previewing.js';
-import { sendRegisterComponentMessage } from '../../../functions/register-component.js';
+import { createRegisterComponentMessage } from '../../../functions/register-component.js';
 import { _track } from '../../../functions/track/index.js';
 import { getInteractionPropertiesForEvent } from '../../../functions/track/interaction.js';
 import { getDefaultCanTrack } from '../../../helpers/canTrack.js';
@@ -291,7 +291,8 @@ export default function EnableEditor(props: BuilderEditorProps) {
       Object.values<ComponentInfo>(
         props.builderContextSignal.value.componentInfos
       ).forEach((registeredComponent) => {
-        sendRegisterComponentMessage(registeredComponent);
+        const message = createRegisterComponentMessage(registeredComponent);
+        window.parent?.postMessage(message, '*');
       });
       window.addEventListener(
         'builder:component:stateChangeListenerActivated',
