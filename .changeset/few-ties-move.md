a15696e79b829b150eecc2b8842f5e3ffdc733a8
Sami Jaber <jabersami@gmail.com>
2024-10-08 15:02:39 -0300

    Feat/register component devtools (#3619)
    ## Description
    
    Add a short description of what changes you made, why you made them, and
    any other context that you think might be helpful for someone to better
    understand what is contained in this pull request. This sort of
    information is useful for people reviewing the code, as well as anyone
    from the future trying to understand why changes were made or why a bug
    started happening.
    
    _Screenshot_
    If relevant, add a screenshot or two of the changes you made.

---
+++ .changeset/few-ties-move.md
@@ -0,0 +1,12 @@
+---
+'@builder.io/sdk-vue': patch
+'@builder.io/sdk-angular': patch
+'@builder.io/sdk-react-nextjs': patch
+'@builder.io/sdk-qwik': patch
+'@builder.io/sdk-react': patch
+'@builder.io/sdk-react-native': patch
+'@builder.io/sdk-solid': patch
+'@builder.io/sdk-svelte': patch
+---
+
+misc: add internal `registerComponentFromDevTools` helpers for devtools.
--- packages/sdks/src/functions/register-component.ts
+++ packages/sdks/src/functions/register-component.ts
@@ -1,9 +1,24 @@
+import type { RegisteredComponent } from '../context/types.js';
 import type { ComponentInfo } from '../types/components.js';
 
 export const createRegisterComponentMessage = (info: ComponentInfo) => ({
   type: 'builder.registerComponent',
   data: serializeIncludingFunctions(info),
 });
+
+export const sendRegisterComponentMessage = (info: ComponentInfo) => {
+  window.parent?.postMessage(createRegisterComponentMessage(info), '*');
+};
+
+/**
+ * @private
+ * @description This is a private internal function that is used to register a component from the Builder DevTools.
+ * Do not use this function directly.
+ */
+export const registerComponentFromDevTools = (info: RegisteredComponent) => {
+  const { component: _, ...rest } = info;
+  sendRegisterComponentMessage(rest);
+};
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 const serializeFn = (fnValue: Function) => {
