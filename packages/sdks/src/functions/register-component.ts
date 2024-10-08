cbece4a4ef039348e0094694483310969f7e78ef
Sami Jaber <jabersami@gmail.com>
2024-10-08 15:04:09 -0300

    Revert "Feat/register component devtools" (#3620)
    Reverts BuilderIO/builder#3619
    
    
    I'm not sure anymore that this is actually needed

--- packages/sdks/src/functions/register-component.ts
+++ packages/sdks/src/functions/register-component.ts
@@ -1,24 +1,9 @@
-import type { RegisteredComponent } from '../context/types.js';
 import type { ComponentInfo } from '../types/components.js';
 
 export const createRegisterComponentMessage = (info: ComponentInfo) => ({
   type: 'builder.registerComponent',
   data: serializeIncludingFunctions(info),
 });
-
-export const sendRegisterComponentMessage = (info: ComponentInfo) => {
-  window.parent?.postMessage(createRegisterComponentMessage(info), '*');
-};
-
-/**
- * @private
- * @description This is a private internal function that is used to register a component from the Builder DevTools.
- * Do not use this function directly.
- */
-export const registerComponentFromDevTools = (info: RegisteredComponent) => {
-  const { component: _, ...rest } = info;
-  sendRegisterComponentMessage(rest);
-};
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 const serializeFn = (fnValue: Function) => {
