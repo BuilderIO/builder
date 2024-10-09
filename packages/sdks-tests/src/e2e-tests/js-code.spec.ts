ec86e01a8d962950bd6fc55f0c35f3ae6af8904c
Sami Jaber <jabersami@gmail.com>
2024-10-09 18:23:13 -0300

    f

--- packages/sdks-tests/src/e2e-tests/js-code.spec.ts
+++ packages/sdks-tests/src/e2e-tests/js-code.spec.ts
@@ -1,14 +1,23 @@
 import { expect } from '@playwright/test';
-import { excludeTestFor, test } from '../helpers/index.js';
+import { excludeTestFor, isSSRFramework, test } from '../helpers/index.js';
 
 test.describe('JS Code', () => {
-  test('initializes correct value from JS Code block', async ({ page }) => {
+  test('runs code', async ({ page }) => {
     await page.goto('/js-code/');
     const menuLocator = page.locator('text=jsCode text');
     await expect(menuLocator).toBeVisible();
   });
-
-  test('code inside Browser.isBrowser should work', async ({ page, sdk }) => {
+  test('runs code in SSR (JS disabled)', async ({ browser, packageName }) => {
+    test.fail(!isSSRFramework(packageName));
+    const context = await browser.newContext({
+      javaScriptEnabled: false,
+    });
+    const page = await context.newPage();
+    await page.goto('/js-code/');
+    const menuLocator = page.locator('text=jsCode text');
+    await expect(menuLocator).toBeVisible();
+  });
+  test('runs code inside Browser.isBrowser', async ({ page, sdk }) => {
     // doesn't work for these as they are SSR and there is no hydration step
     // so the code is not run on the client (Builder.isBrowser) block isn't executed
     test.fail(excludeTestFor(['qwik', 'rsc'], sdk));
