1e6eea6db50874f132c064e9e0ddcd71a9110caa
Clyde Mendonca <clydemendonca@Clydes-MacBook-Pro.local>
2024-10-09 16:15:11 +0530

    feat[core][Builder]: ENG-6337 change to tests

--- packages/sdks-tests/src/e2e-tests/hit-query-api.spec.ts
+++ packages/sdks-tests/src/e2e-tests/hit-query-api.spec.ts
@@ -4,15 +4,6 @@ import { excludeGen1, excludeTestFor, test } from '../helpers/index.js';
 test.describe('Get Query', () => {
   test('call query API only once - in page', async ({ page, packageName, sdk }) => {
     test.skip(!excludeGen1(sdk));
-    test.fail(
-      packageName === 'nextjs-sdk-next-app',
-      'editor tests not supported in nextjs-sdk-next-app'
-    );
-    test.fail(
-      excludeTestFor({ qwik: true }, sdk),
-      'error setting httpRequest response or making API call altogether.'
-    );
-    test.skip(excludeTestFor({ vue: true }, sdk), 'Vue flakiness issues');
 
     const urlMatch = /https:\/\/cdn\.builder\.io\/api\/v3\/query/;
 
