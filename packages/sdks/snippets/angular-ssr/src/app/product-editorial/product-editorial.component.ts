75932387584ea30cb5c126dff303733c838867d8
Uttej Venkata Sastry <uttej@builder.io>
2024-10-11 00:05:05 +0530

    angular ssr editorial complete

--- packages/sdks/snippets/angular-ssr/src/app/product-editorial/product-editorial.component.ts
+++ packages/sdks/snippets/angular-ssr/src/app/product-editorial/product-editorial.component.ts
@@ -1,10 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {
-  Content,
-  fetchOneEntry,
-  type BuilderContent,
-} from '@builder.io/sdk-angular';
+import { ActivatedRoute } from '@angular/router';
+import { BuilderContent, Content } from '@builder.io/sdk-angular';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductInfoComponent } from './product-info/product-info.component';
@@ -22,7 +19,7 @@ import { ProductInfoComponent } from './product-info/product-info.component';
   template: `
     <app-header />
 
-    <!-- Product info -->
+    <!-- Product info  -->
     <app-product-info [product]="product" />
 
     <builder-content
@@ -37,28 +34,13 @@ import { ProductInfoComponent } from './product-info/product-info.component';
 export class ProductEditorialComponent {
   product: any;
   editorial: BuilderContent | null = null;
-  productId?: string;
 
-  async ngOnInit() {
-    this.productId = window.location.pathname.split('/').pop() || '';
-    if (this.productId) {
-      await this.fetchProductAndEditorial();
-    }
-  }
+  constructor(private route: ActivatedRoute) {}
 
-  private async fetchProductAndEditorial() {
-    // Fetch product data from external API or your own CMS
-    this.product = await fetch(
-      `https://fakestoreapi.com/products/${this.productId}`
-    ).then((res) => res.json());
-
-    // Fetch editorial content from Builder.io
-    this.editorial = await fetchOneEntry({
-      apiKey: 'ee9f13b4981e489a9a1209887695ef2b',
-      model: 'product-editorial',
-      userAttributes: {
-        urlPath: window.location.pathname || '/',
-      },
+  ngOnInit() {
+    this.route.data.subscribe((data: any) => {
+      this.product = data.productData.product;
+      this.editorial = data.productData.editorial;
     });
   }
 }
