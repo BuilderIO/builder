269db7b4aa96462930bcf6a361612abbc823388e
Sami Jaber <jabersami@gmail.com>
2024-10-18 19:00:10 -0300

    Fix: SDKs onInit (#3622)
    ## Description

--- packages/sdks/src/functions/evaluate/browser-runtime/browser.ts
+++ packages/sdks/src/functions/evaluate/browser-runtime/browser.ts
@@ -1,6 +1,5 @@
-import type { BuilderRenderState } from '../../../context/types.js';
 import type { ExecutorArgs } from '../helpers.js';
-import { getFunctionArguments } from '../helpers.js';
+import { flattenState, getFunctionArguments } from '../helpers.js';
 
 export const runInBrowser = ({
   code,
@@ -22,49 +21,3 @@ export const runInBrowser = ({
     ...functionArgs.map(([, value]) => value)
   );
 };
-
-export function flattenState({
-  rootState,
-  localState,
-  rootSetState,
-}: {
-  rootState: Record<string | symbol, any>;
-  localState: Record<string | symbol, any> | undefined;
-  rootSetState: ((rootState: BuilderRenderState) => void) | undefined;
-}): BuilderRenderState {
-  return new Proxy(rootState, {
-    get: (target, prop) => {
-      if (localState && prop in localState) {
-        return localState[prop];
-      }
-
-      const val = target[prop];
-      if (typeof val === 'object' && val !== null) {
-        return flattenState({
-          rootState: val,
-          localState: undefined,
-          rootSetState: rootSetState
-            ? (subState) => {
-                target[prop] = subState;
-                rootSetState(target);
-              }
-            : undefined,
-        });
-      }
-
-      return val;
-    },
-    set: (target, prop, value) => {
-      if (localState && prop in localState) {
-        throw new Error(
-          'Writing to local state is not allowed as it is read-only.'
-        );
-      }
-
-      target[prop] = value;
-
-      rootSetState?.(target);
-      return true;
-    },
-  });
-}
