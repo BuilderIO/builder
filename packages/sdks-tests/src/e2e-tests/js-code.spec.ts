a6b65a369abe31bd15298f115359b8e75b019013
Sami Jaber <jabersami@gmail.com>
2024-10-21 11:47:12 -0300

    f

--- packages/sdks-tests/src/e2e-tests/js-code.spec.ts
+++ packages/sdks-tests/src/e2e-tests/js-code.spec.ts
@@ -35,6 +35,17 @@ test.describe('JS Code', () => {
     const menuLocator = page.locator('text=jsCode text');
     await expect(menuLocator).toBeVisible();
   });
+  test('runs code in SSR (JS disabled)', async ({ browser, packageName }) => {
+    test.fail(!isSSRFramework(packageName));
+
+    const context = await browser.newContext({
+      javaScriptEnabled: false,
+    });
+    const page = await context.newPage();
+    await page.goto('/js-code/');
+    const menuLocator = page.locator('text=jsCode text');
+    await expect(menuLocator).toBeVisible();
+  });
   test('runs code inside Browser.isBrowser', async ({ page, sdk }) => {
     // doesn't work for these as they are SSR frameworks without a hydration step.
     // therefore jsCode blocks are not run on the client at all
@@ -42,49 +53,30 @@ test.describe('JS Code', () => {
 
     const msgPromise = page.waitForEvent('console', msg => msg.text().includes('hello world'));
 
-    test('runs code in SSR (JS disabled)', async ({ browser, packageName }) => {
-      test.fail(!isSSRFramework(packageName));
+    await page.goto('/js-content-is-browser');
+    await msgPromise;
+  });
 
-      const context = await browser.newContext({
-        javaScriptEnabled: false,
-      });
-      const page = await context.newPage();
-      await page.goto('/js-code/');
-      const menuLocator = page.locator('text=jsCode text');
-      await expect(menuLocator).toBeVisible();
-    });
-    test('runs code inside Browser.isBrowser', async ({ page, sdk }) => {
-      // doesn't work for these as they are SSR frameworks without a hydration step.
-      // therefore jsCode blocks are not run on the client at all
-      test.skip(excludeTestFor(['qwik', 'rsc'], sdk));
+  test('runs code inside Browser.isBrowser (after client-side navigation)', async ({
+    page,
+    sdk,
+  }) => {
+    // this isn't really client-side navigation in most frameworks
+    // except for sveltekit I think which automatically hijacks anchor tags
 
-      const msgPromise = page.waitForEvent('console', msg => msg.text().includes('hello world'));
+    // therefore this test is not applicable to these frameworks which will ssr the page
+    test.skip(excludeTestFor(['qwik', 'rsc'], sdk));
 
-      await page.goto('/js-content-is-browser');
-      await msgPromise;
-    });
+    await page.goto('/');
+    const msgPromise = page.waitForEvent('console', msg => msg.text().includes('hello world'));
 
-    test('runs code inside Browser.isBrowser (after client-side navigation)', async ({
-      page,
-      sdk,
-    }) => {
-      // this isn't really client-side navigation in most frameworks
-      // except for sveltekit I think which automatically hijacks anchor tags
+    const links = page.locator('a');
 
-      // therefore this test is not applicable to these frameworks which will ssr the page
-      test.skip(excludeTestFor(['qwik', 'rsc'], sdk));
+    const jsCodeLink = await links.filter({ hasText: 'JS isBrowser code' });
 
-      await page.goto('/');
-      const msgPromise = page.waitForEvent('console', msg => msg.text().includes('hello world'));
+    await expect(jsCodeLink).toHaveCount(1);
 
-      const links = page.locator('a');
-
-      const jsCodeLink = await links.filter({ hasText: 'JS isBrowser code' });
-
-      await expect(jsCodeLink).toHaveCount(1);
-
-      await jsCodeLink.click();
-      await msgPromise;
-    });
+    await jsCodeLink.click();
+    await msgPromise;
   });
 });
