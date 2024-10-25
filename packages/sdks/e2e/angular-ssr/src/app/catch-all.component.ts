c8c09db1adc514b4477b37d3de49dd46d69c2356
Clyde Mendonca <clydemendonca@Clydes-MacBook-Pro.local>
2024-10-25 12:49:14 +0530

    feat[gen2][apiBaseUrl] ENG-7147 change to angular, angular-ssr and hydrogen e2e code to make tests pass

--- packages/sdks/e2e/angular-ssr/src/app/catch-all.component.ts
+++ packages/sdks/e2e/angular-ssr/src/app/catch-all.component.ts
@@ -14,6 +14,7 @@ interface BuilderProps {
   model: string;
   content: any;
   data?: any;
+  apiHost?: string;
 }
 
 @Component({
@@ -28,6 +29,7 @@ interface BuilderProps {
         [canTrack]="canTrack"
         [customComponents]="customComponents"
         [data]="data"
+        [apiHost]="apiHost"
       ></builder-content>
     </ng-container>
 
@@ -43,6 +45,7 @@ export class CatchAllComponent {
   model: BuilderProps['model'] = 'page';
   content: BuilderProps['content'];
   data: BuilderProps['data'];
+  apiHost: BuilderProps['apiHost'];
 
   customComponents: RegisteredComponent[] = [
     {
@@ -109,10 +112,12 @@ export class CatchAllComponent {
 
   ngOnInit() {
     this.activatedRoute.data.subscribe((data: any) => {
+      console.log('data', data);
       this.content = data.content?.content;
       this.canTrack = data.content?.canTrack;
       this.trustedHosts = data.content?.trustedHosts;
       this.data = data.content?.data;
+      this.apiHost = data.content?.apiHost;
     });
   }
 }
