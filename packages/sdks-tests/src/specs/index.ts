44293a1faafb78871afd903b03d8901084eafdc7
Sami Jaber <jabersami@gmail.com>
2024-10-31 18:41:55 -0300

    🏃

--- packages/sdks-tests/src/specs/index.ts
+++ packages/sdks-tests/src/specs/index.ts
@@ -161,7 +161,11 @@ const apiVersionPathToProp = {
 
 export type Path = keyof typeof PAGES;
 
-const GEN1_ONLY_PATHNAMES: Path[] = ['/api-version-v1', '/personalization-container'];
+const GEN1_ONLY_PATHNAMES: Path[] = [
+  '/api-version-v1',
+  '/personalization-container',
+  '/override-base-url',
+];
 const GEN2_ONLY_PATHNAMES: Path[] = [];
 
 export const getAllPathnames = (target: 'gen1' | 'gen2'): string[] => {
