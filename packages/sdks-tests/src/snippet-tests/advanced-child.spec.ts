45cea1536ebe41848236f4a53be074a2cfb9c4e6
Uttej V S K <uttejvsk@hotmail.com>
2024-11-01 10:32:32 -0500

    EDU-297: Angular Gen 2 SSR - Advanced child sub components - Custom Components (#3701)
    ## Description
    
    This is the Angular Gen 2 SSR version of the Advanced Child
    sub-components PR raised in #3700.
    
    @samijaber Please review
    
    FYI, This branch is created on top of the previous PR #3700, so it contains
    previous PR changes as well.
    
    ---------
    
    Authored-by: Uttej Venkata Sastry <uttej@builder.io>

--- packages/sdks-tests/src/snippet-tests/advanced-child.spec.ts
+++ packages/sdks-tests/src/snippet-tests/advanced-child.spec.ts
@@ -6,7 +6,8 @@ test.describe('Editable Regions in Custom Components', () => {
     page,
     packageName,
   }) => {
-    test.skip(!['react', 'angular'].includes(packageName));
+
+    test.skip(!['react', 'angular', 'angular-ssr'].includes(packageName));
 
     await page.goto('/advanced-child');
 
@@ -14,7 +15,9 @@ test.describe('Editable Regions in Custom Components', () => {
   });
 
   test('Display two buttons with label Tab 1 and Tab 2', async ({ page, packageName }) => {
-    test.skip(!['react', 'angular'].includes(packageName));
+
+    test.skip(!['react', 'angular', 'angular-ssr'].includes(packageName));
+
 
     await page.goto('/advanced-child');
 
@@ -36,7 +39,8 @@ test.describe('Editable Regions in Custom Components', () => {
   });
 
   test('Display content for the clicked tab and hide the other', async ({ page, packageName }) => {
-    test.skip(!['react', 'angular'].includes(packageName));
+    test.skip(!['react', 'angular', 'angular-ssr'].includes(packageName));
+
 
     await page.goto('/advanced-child');
 
