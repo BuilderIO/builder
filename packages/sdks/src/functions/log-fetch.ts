9afea7717db3c50c07e2a57722f328d1d95b109d
Clyde Mendonca <clydemendonca@Clydes-MacBook-Pro.local>
2024-11-06 21:27:06 +0530

    feat[gen2][react] ENG-7239 resolved angular tests error

--- packages/sdks/src/functions/log-fetch.ts
+++ packages/sdks/src/functions/log-fetch.ts
@@ -1,7 +1,9 @@
 import { logger } from '../helpers/logger.js';
 
 export function logFetch(url: string) {
-  if (String(process.env.DEBUG) == 'true') {
-    logger.log(url);
+  if (typeof process !== 'undefined' && process.env?.DEBUG) {
+    if (String(process.env.DEBUG) == 'true') {
+      logger.log(url);
+    }
   }
 }
