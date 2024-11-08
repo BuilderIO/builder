152eff8528bcc96ad61e612f59ce8dcb5ba3110a
Uttej V S K <uttejvsk@hotmail.com>
2024-11-08 13:29:33 -0600

    Refactor: React Gen 2 SDK- Custom Components (#3723)
    This is similar to the previous PR I raised, which incorporated Steve's
    feedback on removing unnecessary divs and styles to make the code
    cleaner and minimal.
    
    Merging the previous PR would allow the test cases to pass, as that
    contains all the necessary changes for React
    
    ---------
    
    Authored-by: Uttej Venkata Sastry <uttej@builder.io>

--- packages/sdks/snippets/react/src/components/CustomTabs.tsx
+++ packages/sdks/snippets/react/src/components/CustomTabs.tsx
@@ -1,8 +1,3 @@
-/**
- * https://www.builder.io/c/docs/custom-components-children
- * src/components/CustomTabs.tsx
- */
-
 import {
   Blocks,
   BuilderBlock,
@@ -10,66 +5,36 @@ import {
 } from '@builder.io/sdk-react';
 import { useState } from 'react';
 
-type Tab = {
-  tabName: string;
-  children: BuilderBlock[];
-};
 interface TabProps {
-  tabList: Tab[];
+  tabList?: { tabName: string; blocks: BuilderBlock[] }[];
   builderBlock: BuilderBlock;
 }
 
-const CustomTabs = (props: TabProps) => {
+export const CustomTabs = ({ tabList, builderBlock }: TabProps) => {
   const [activeTab, setActiveTab] = useState(0);
 
-  return (
-    <div>
-      <h2>Custom Component with editable regions</h2>
+  if (!tabList?.length) return null;
 
-      <div>
-        <div>
-          {/* 
-            The tabList[] prop is an array that represents the tabs.
-            Each tab has a tabName, which is displayed as the button label,
-            and children, which are the content blocks rendered inside the tab.
-            This comes from the inputs declared inside your custom component.
-            For more details: https://www.builder.io/c/docs/custom-components-input-types
-          */}
-          {props.tabList?.map((tab, index) => (
-            <button
-              key={index}
-              className={activeTab === index ? 'active' : ''}
-              onClick={() => setActiveTab(index)}
-            >
-              {tab.tabName}
-            </button>
-          ))}
-        </div>
+  return (
+    <>
+      <div className="tab-buttons">
+        {tabList.map((tab, index) => (
+          <button
+            key={index}
+            className={`tab-button ${activeTab === index ? 'active' : ''}`}
+            onClick={() => setActiveTab(index)}
+          >
+            {tab.tabName}
+          </button>
+        ))}
       </div>
 
-      {props.tabList && props.tabList.length !== 0 && (
-        <div>
-          {props.tabList.map((tab, index) => (
-            <div
-              key={index}
-              style={{ display: activeTab === index ? 'block' : 'none' }}
-            >
-              {/** 
-                The Blocks component from Builder.io dynamically renders the content inside the tab.
-                - `parent` is the ID of the Builder's parent div, ensuring correct content rendering.
-                - `path` defines where to find the content for this Tab
-                - `blocks` contains the actual content to be displayed inside the tab.
-              */}
-              <Blocks
-                parent={props.builderBlock?.id}
-                path={`component.options.tabList.${index}.children`}
-                blocks={tab.children}
-              />
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+      <Blocks
+        parent={builderBlock?.id}
+        path={`tabList.${activeTab}.blocks`}
+        blocks={tabList[activeTab].blocks}
+      />
+    </>
   );
 };
 
@@ -77,52 +42,24 @@ export const customTabsInfo: RegisteredComponent = {
   component: CustomTabs,
   name: 'TabFields',
   shouldReceiveBuilderProps: {
-    /** enabling this causes the SDK to pass the `builderBlock` prop down to the component */
     builderBlock: true,
+    builderComponents: true,
+    builderContext: true,
   },
   inputs: [
     {
       name: 'tabList',
       type: 'list',
-
       subFields: [
         {
           name: 'tabName',
           type: 'string',
         },
         {
-          name: 'children',
+          name: 'blocks',
           type: 'uiBlocks',
           hideFromUI: true,
-          defaultValue: [
-            {
-              '@type': '@builder.io/sdk:Element',
-              component: {
-                name: 'Text',
-
-                options: {
-                  text: 'This is editable block within the builder editor',
-                },
-              },
-              responsiveStyles: {
-                large: {
-                  display: 'flex',
-                  flexDirection: 'column',
-                  position: 'relative',
-                  flexShrink: '0',
-                  boxSizing: 'border-box',
-                  marginTop: '8px',
-                  lineHeight: 'normal',
-                  height: '200px',
-                  textAlign: 'left',
-                  minHeight: '200px',
-                },
-                small: {
-                  height: '200px',
-                },
-              },
-            },
-          ],
+          defaultValue: [],
         },
       ],
     },
