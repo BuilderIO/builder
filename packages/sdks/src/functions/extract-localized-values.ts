a84f31fb3e82cf4e3ed897fbdeff2a46fef217d1
sidmohanty11 <sidmohanty11@gmail.com>
2024-12-03 16:11:34 +0530

    fix

--- packages/sdks/src/functions/extract-localized-values.ts
+++ packages/sdks/src/functions/extract-localized-values.ts
@@ -17,7 +17,7 @@ function containsLocalizedValues(data: Record<string, any>) {
 
 function extractLocalizedValues(data: Record<string, any>, locale: string) {
   if (!data || !Object.getOwnPropertyNames(data).length) {
-    return;
+    return {};
   }
   const extractedResult: Record<string, any> = {};
 
@@ -36,21 +36,21 @@ export function resolveLocalizedValues(
   if (
     block.component?.options &&
     containsLocalizedValues(block.component?.options) &&
-    locale
+    !locale
   ) {
-    block.component.options = extractLocalizedValues(
-      block.component.options,
-      locale
+    console.warn(
+      '[Builder.io] In order to use localized fields in Builder, you must pass a locale prop to the BuilderComponent or to options object while fetching the content to resolve localized fields. Learn more: https://www.builder.io/c/docs/localization-inline#targeting-and-inline-localization'
     );
   }
 
   if (
     block.component?.options &&
     containsLocalizedValues(block.component?.options) &&
-    !locale
+    locale
   ) {
-    console.warn(
-      '[Builder.io] In order to use localized fields in Builder, you must pass a locale prop to the BuilderComponent or to options object while fetching the content to resolve localized fields. Learn more: https://www.builder.io/c/docs/localization-inline#targeting-and-inline-localization'
+    block.component.options = extractLocalizedValues(
+      block.component.options,
+      locale
     );
   }
 
