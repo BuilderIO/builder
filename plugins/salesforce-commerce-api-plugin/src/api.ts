28ff97e59cd333bddf48f6155f63ee771c47e304
anaghav2023 <124548794+anaghav2023@users.noreply.github.com>
2024-12-09 22:18:56 +0530

    renaming

--- plugins/salesforce-commerce-api-plugin/src/api.ts
+++ plugins/salesforce-commerce-api-plugin/src/api.ts
@@ -93,7 +93,7 @@ export class Api {
   async validateConfig(){
        const response = await this.request(`validate-config`);
        if(response.errors){
-        throw 'We failed to authenticate your access to Salesforce Commerce Cloud B2C api. Please review all plugin fields and make sure they are correct.';
+        throw 'We failed to authenticate your access to Salesforce Commerce Cloud B2C API. Please review all plugin fields and make sure they are correct.';
        }
   }
 
