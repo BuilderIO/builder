b21b288edf3725e0a12893c9df78f4d20576cb6e
Uttej V S K <uttejvsk@hotmail.com>
2024-12-09 12:41:12 -0600

    EDU-159: Remix Gen1 - Advanced child sub components (#3750)
    ## Description
    
    This PR introduces remix gen 1 example for Advanced child sub-components
    in the custom components page in the docs.
    
    ticket: https://builder-io.atlassian.net/browse/EDU-324
    @samijaber Please review
    
    ---------
    
    Authored-by: Uttej Venkata Sastry <uttej@builder.io>

--- packages/sdks-tests/src/helpers/index.ts
+++ packages/sdks-tests/src/helpers/index.ts
@@ -193,6 +193,18 @@ export const getBuilderSessionIdCookie = async ({ context }: { context: BrowserC
   return builderSessionCookie;
 };
 
+export async function testClickAndVerifyVisibility(
+  page: Page,
+  buttonText: string,
+  contentText: string
+) {
+  await page.click(`button:has-text("${buttonText}")`);
+  const content = await page.waitForSelector(`div:has-text("${contentText}")`, {
+    state: 'visible',
+  });
+  return content.isVisible();
+}
+
 export const checkIfIsHydrationErrorMessage = (_text: string) => {
   const text = _text.toLowerCase();
   const isVueHydrationMismatch =
--- packages/sdks-tests/src/snippet-tests/custom-child.spec.ts
+++ packages/sdks-tests/src/snippet-tests/custom-child.spec.ts
@@ -3,32 +3,25 @@ import { test } from '../helpers/index.js';
 
 test.describe('Div with Hero class, and text', () => {
   test('should render the page without 404', async ({ page, packageName }) => {
-    test.skip(!['react', 'angular', 'angular-ssr'].includes(packageName));
+    test.skip(!['react', 'angular', 'angular-ssr', 'gen1-remix'].includes(packageName));
 
     const response = await page.goto('/custom-child');
     expect(response?.status()).toBeLessThan(400);
   });
 
   test('should verify builder-block with specific text', async ({ page, packageName }) => {
-    test.skip(!['react', 'angular', 'angular-ssr'].includes(packageName));
+    test.skip(!['react', 'angular', 'angular-ssr', 'gen1-remix'].includes(packageName));
 
     await page.goto('/custom-child');
 
+    await page.waitForLoadState('networkidle');
     const builderBlock = page.locator('div.builder-block').first();
     await expect(builderBlock).toBeVisible();
 
-    const childDivs = builderBlock.locator('div');
+    const column1Text = page.locator("text=This is your component's text");
+    await expect(column1Text).toBeVisible();
 
-    const inlineStyledDiv = childDivs.nth(0);
-    await expect(inlineStyledDiv).toBeVisible();
-
-    const inlineText = await inlineStyledDiv.textContent();
-    expect(inlineText?.trim()).toBe("This is your component's text");
-
-    const builderTextDiv = childDivs.nth(1);
-    await expect(builderTextDiv).toBeVisible();
-
-    const builderText = await builderTextDiv.textContent();
-    expect(builderText?.trim()).toBe('This is Builder text');
+    const column2Text = page.locator('text=This is Builder text');
+    await expect(column2Text).toBeVisible();
   });
 });
