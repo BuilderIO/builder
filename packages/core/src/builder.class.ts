2a14d16fa299629e4c7e3699c40ee2c4cd29ccea
Clyde Mendonca <clydemendonca@Clydes-MacBook-Pro.local>
2024-12-13 12:03:18 +0530

    fix[gen1][core] ENG-7760 change serializing of custom components function

--- packages/core/src/builder.class.ts
+++ packages/core/src/builder.class.ts
@@ -1104,9 +1104,15 @@ export class Builder {
       // 2. `name(args) => {code}`
       // 3. `(args) => {}`
       // 4. `args => {}`
+      // 5. `async name(args) => {code}`
+      // 6. `async (args) => {}`
+      // 7. `async args => {}`
       const isArrowWithoutParens = /^[a-zA-Z0-9_]+\s*=>/i.test(fnStr);
       const appendFunction =
-        !fnStr.startsWith('function') && !fnStr.startsWith('(') && !isArrowWithoutParens;
+        !fnStr.startsWith('function') &&
+        !fnStr.startsWith('async') &&
+        !fnStr.startsWith('(') &&
+        !isArrowWithoutParens;
 
       return `return (${appendFunction ? 'function ' : ''}${fnStr}).apply(this, arguments)`;
     };
