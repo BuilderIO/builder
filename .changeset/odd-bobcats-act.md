2fc9fc5ab5ef96dc618d950d8a8fb45652eb690a
Clyde Mendonca <clyde@builder.io>
2024-12-16 23:48:12 +0530

    fix[gen1][gen2] ENG-7760 onChange does not fire when an `async` function is passed to it (#3782)
    ## Description
    
    onChange does not fire when an `async` function is passed to it
    
    **Jira ticket**
    https://builder-io.atlassian.net/browse/ENG-7760
    
    **Loom**
    Gen1: https://www.loom.com/share/b6012b2b37f54944ab94074c264f3bb6
    Gen2: https://www.loom.com/share/ca3d895ddf5d4b69b13eab341003dde1
    
    ---------
    
    Co-authored-by: Clyde Mendonca <clydemendonca@Clydes-MacBook-Pro.local>
    Co-authored-by: Sidharth Mohanty <sidmohanty11@gmail.com>

---
+++ .changeset/odd-bobcats-act.md
@@ -0,0 +1,14 @@
+---
+"@builder.io/sdk": patch
+"@builder.io/react": patch
+"@builder.io/sdk-angular": patch
+"@builder.io/sdk-react-nextjs": patch
+"@builder.io/sdk-qwik": patch
+"@builder.io/sdk-react": patch
+"@builder.io/sdk-react-native": patch
+"@builder.io/sdk-solid": patch
+"@builder.io/sdk-svelte": patch
+"@builder.io/sdk-vue": patch
+---
+
+Fix: `onChange` functions passed to builder inputs can now receive async functions
--- packages/core/src/builder.class.test.ts
+++ packages/core/src/builder.class.test.ts
@@ -120,6 +120,129 @@ describe('serializeIncludingFunctions', () => {
     expect(typeof result.inputs[2].onChange).toBe('string');
     expect(result.inputs[2].onChange).toContain('v.toLowerCase()');
   });
+
+  test('serializes arrow functions with non-parenthesized args in inputs', () => {
+    // Using eval and template literal to prevent TypeScript from adding parens
+    const fn = eval(`(${`e => !0 === e.get("isABTest")`})`);
+    const input = {
+      name: 'onChangeComponent',
+      inputs: [
+        {
+          name: 'number',
+          type: 'number',
+          // @ts-expect-error required for this test
+          onChange: value => value * 2,
+          showIf: fn,
+        },
+      ],
+    };
+
+    const result = Builder['serializeIncludingFunctions'](input);
+
+    expect(typeof result.inputs[0].onChange).toBe('string');
+    expect(result.inputs[0].onChange).toBe(`return ((value) => value * 2).apply(this, arguments)`);
+  });
+
+  test('serializes functions with parenthesized args in inputs', () => {
+    // Using eval and template literal to prevent TypeScript from adding parens
+    const fn = eval(`(${`e => !0 === e.get("isABTest")`})`);
+    const input = {
+      name: 'onChangeComponent',
+      inputs: [
+        {
+          name: 'number',
+          type: 'number',
+          onChange: function (value: number) {
+            return value * 2;
+          },
+          showIf: fn,
+        },
+      ],
+    };
+
+    const result = Builder['serializeIncludingFunctions'](input);
+
+    expect(typeof result.inputs[0].onChange).toBe('string');
+    expect(result.inputs[0].onChange).toBe(
+      `return (function(value) {
+            return value * 2;
+          }).apply(this, arguments)`
+    );
+  });
+
+  test('serializes async functions with parenthesized args in inputs', () => {
+    // Using eval and template literal to prevent TypeScript from adding parens
+    const fn = eval(`(${`e => !0 === e.get("isABTest")`})`);
+    const input = {
+      name: 'AsyncOnChangeComponent',
+      inputs: [
+        {
+          name: 'number',
+          type: 'number',
+          onChange: async function (value: number) {
+            return value * 2;
+          },
+          showIf: fn,
+        },
+      ],
+    };
+
+    const result = Builder['serializeIncludingFunctions'](input);
+
+    expect(typeof result.inputs[0].onChange).toBe('string');
+    expect(result.inputs[0].onChange).toBe(
+      `return (async function(value) {
+            return value * 2;
+          }).apply(this, arguments)`
+    );
+  });
+
+  test('serializes async arrow functions with parenthesized args in inputs', () => {
+    // Using eval and template literal to prevent TypeScript from adding parens
+    const fn = eval(`(${`e => !0 === e.get("isABTest")`})`);
+    const input = {
+      name: 'AsyncOnChangeComponent',
+      inputs: [
+        {
+          name: 'number',
+          type: 'number',
+          onChange: async (value: number) => value * 2,
+          showIf: fn,
+        },
+      ],
+    };
+
+    const result = Builder['serializeIncludingFunctions'](input);
+
+    expect(typeof result.inputs[0].onChange).toBe('string');
+    expect(result.inputs[0].onChange).toBe(
+      'return (async (value) => value * 2).apply(this, arguments)'
+    );
+  });
+
+  test('serializes async arrow functions without parenthesized args in inputs', () => {
+    // Using eval and template literal to prevent TypeScript from adding parens
+    const fn = eval(`(${`e => !0 === e.get("isABTest")`})`);
+    const input = {
+      name: 'AsyncOnChangeComponent',
+      inputs: [
+        {
+          name: 'number',
+          type: 'number',
+          // @ts-expect-error
+          onChange: async value => value * 2,
+          showIf: fn,
+        },
+      ],
+    };
+
+    const result = Builder['serializeIncludingFunctions'](input);
+
+    expect(typeof result.inputs[0].onChange).toBe('string');
+    expect(result.inputs[0].onChange).toBe(
+      'return (async (value) => value * 2).apply(this, arguments)'
+    );
+  });
 });
 
 describe('prepareComponentSpecToSend', () => {
