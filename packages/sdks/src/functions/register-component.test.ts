c5b583e688b35e4c22674c7e56bd7a1a8bb864b2
Clyde Mendonca <clyde@builder.io>
2024-12-16 23:48:12 +0530

    fix[gen1][gen2] ENG-7760 onChange does not fire when an `async` function is passed to it (#3782)
    ## Description
    
    onChange does not fire when an `async` function is passed to it
    
    **Jira ticket**
    https://builder-io.atlassian.net/browse/ENG-7760
    
    **Loom**
    Gen1: https://www.loom.com/share/b6012b2b37f54944ab94074c264f3bb6
    Gen2: https://www.loom.com/share/ca3d895ddf5d4b69b13eab341003dde1
    
    ---------
    
    Co-authored-by: Clyde Mendonca <clydemendonca@Clydes-MacBook-Pro.local>
    Co-authored-by: Sidharth Mohanty <sidmohanty11@gmail.com>

--- packages/sdks/src/functions/register-component.test.ts
+++ packages/sdks/src/functions/register-component.test.ts
@@ -42,6 +42,165 @@ describe('Component Registration and Serialization', () => {
     expect(result.hooks.onChange).toContain('return value.toUpperCase();');
   });
 
+  test('serializeComponentInfo handles arrow functions without parenthesis correctly', () => {
+    const mockComponentInfo: ComponentInfo = {
+      name: 'TestComponent',
+      inputs: [
+        {
+          name: 'testInput',
+          type: 'string',
+          // eslint-disable-next-line
+          // @ts-expect-error required for passing tests
+          // prettier-ignore
+          onChange: value => value.toUpperCase(),
+        },
+      ],
+      // Add other required fields as necessary
+    };
+
+    const result = serializeIncludingFunctions(mockComponentInfo);
+
+    expect(result).toMatchSnapshot();
+    expect(result.name).toBe('TestComponent');
+    expect(result.inputs).toEqual([
+      {
+        name: 'testInput',
+        type: 'string',
+        onChange: `return ((value) => value.toUpperCase()).apply(this, arguments)`,
+      },
+    ]);
+  });
+
+  test('serializeComponentInfo handles functions with parenthesis correctly', () => {
+    const mockComponentInfo: ComponentInfo = {
+      name: 'TestComponent',
+      inputs: [
+        {
+          name: 'testInput',
+          type: 'string',
+          // eslint-disable-next-line
+          onChange: function (value) {
+            // @ts-expect-error required for passing tests
+            return value.toUpperCase();
+          },
+        },
+      ],
+      // Add other required fields as necessary
+    };
+
+    const result = serializeIncludingFunctions(mockComponentInfo);
+
+    expect(result).toMatchSnapshot();
+    expect(result.name).toBe('TestComponent');
+    expect(result.inputs).toEqual([
+      {
+        name: 'testInput',
+        type: 'string',
+        onChange: `return (function(value) {
+            return value.toUpperCase();
+          }).apply(this, arguments)`,
+      },
+    ]);
+  });
+
+  test('serializeComponentInfo handles async arrow functions without parenthesis correctly', () => {
+    const mockComponentInfo: ComponentInfo = {
+      name: 'TestComponent',
+      inputs: [
+        {
+          name: 'testInput',
+          type: 'string',
+          // eslint-disable-next-line
+          onChange: async (value) => {
+            // @ts-expect-error required for passing tests
+            return value.toUpperCase();
+          },
+        },
+      ],
+      // Add other required fields as necessary
+    };
+
+    const result = serializeIncludingFunctions(mockComponentInfo);
+
+    expect(result).toMatchSnapshot();
+    expect(result.name).toBe('TestComponent');
+    expect(result.inputs).toEqual([
+      {
+        name: 'testInput',
+        type: 'string',
+        onChange: `return (async (value) => {
+            return value.toUpperCase();
+          }).apply(this, arguments)`,
+      },
+    ]);
+  });
+
+  test('serializeComponentInfo handles async functions correctly', () => {
+    const mockComponentInfo: ComponentInfo = {
+      name: 'TestComponent',
+      inputs: [
+        {
+          name: 'testInput',
+          type: 'string',
+          // eslint-disable-next-line
+          // @ts-expect-error required for passing tests
+          // eslint-disable-next-line object-shorthand
+          onChange: async function (value: string) {
+            return value.toUpperCase();
+          },
+        },
+      ],
+      // Add other required fields as necessary
+    };
+
+    const result = serializeIncludingFunctions(mockComponentInfo);
+
+    expect(result).toMatchSnapshot();
+    expect(result.name).toBe('TestComponent');
+    expect(result.inputs).toEqual([
+      {
+        name: 'testInput',
+        type: 'string',
+        onChange: `return (async function(value) {
+            return value.toUpperCase();
+          }).apply(this, arguments)`,
+      },
+    ]);
+  });
+
+  test('serializeComponentInfo handles async arrow functions without parenthesis correctly', () => {
+    const mockComponentInfo: ComponentInfo = {
+      name: 'TestComponent',
+      inputs: [
+        {
+          name: 'testInput',
+          type: 'string',
+          // eslint-disable-next-line
+          // @ts-expect-error required for passing tests
+          // eslint-disable-next-line object-shorthand
+          onChange: async (value) => {
+            return value;
+          },
+        },
+      ],
+      // Add other required fields as necessary
+    };
+
+    const result = serializeIncludingFunctions(mockComponentInfo);
+
+    expect(result).toMatchSnapshot();
+    expect(result.name).toBe('TestComponent');
+    expect(result.inputs).toEqual([
+      {
+        name: 'testInput',
+        type: 'string',
+        onChange: `return (async (value) => {
+            return value;
+          }).apply(this, arguments)`,
+      },
+    ]);
+  });
+
   test('serializeComponentInfo handles arrow functions', () => {
     const mockComponentInfo: ComponentInfo = {
       name: 'ArrowComponent',
