42a258bcc9adb2cc6ea40e1c69db9d2b1e20a877
Clyde Mendonca <clydemendonca@Clydes-MacBook-Pro.local>
2024-12-18 10:50:49 +0530

    fix[gen1][core] ENG-7841 change to tests

--- packages/core/src/builder.class.ts
+++ packages/core/src/builder.class.ts
@@ -2594,6 +2594,10 @@ export class Builder {
         queryParams.staleCacheSeconds = options.staleCacheSeconds;
       }
 
+      if (this.apiEndpoint === 'content') {
+        queryParams.includeRefs = true;
+      }
+
       const properties: (keyof GetContentOptions)[] = [
         'prerender',
         'extractCss',
@@ -2605,6 +2609,7 @@ export class Builder {
         'entry',
         'rev',
         'static',
+        'includeRefs',
       ];
 
       for (const key of properties) {
@@ -2640,9 +2645,6 @@ export class Builder {
     const isApiCallForCodegenOrQuery = isApiCallForCodegen || this.apiEndpoint === 'query';
 
     if (this.apiEndpoint === 'content') {
-      queryParams.noTraverse = queue[0].limit !== 1;
-      queryParams.includeRefs = true;
-
       if (queue[0].query) {
         const flattened = this.flattenMongoQuery({ query: queue[0].query });
         for (const key in flattened) {
