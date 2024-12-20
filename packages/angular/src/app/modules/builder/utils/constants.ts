84d8d0d78d184f51b52fac5848e2dde9d5f2a398
Sami Jaber <jabersami@gmail.com>
2024-12-20 17:15:23 -0400

    Feat: WC & Angular Gen1 SDK info (#3796)
    ## Description
    
    - update WC and Angular gen1 to latest core SDK
    - make them send `sdkInfo`, including current version number
    
    https://builder-io.atlassian.net/browse/ENG-7783

--- packages/angular/src/app/modules/builder/utils/constants.ts
+++ packages/angular/src/app/modules/builder/utils/constants.ts
@@ -1,3 +1,3 @@
 // TODO remove hardcoded version, maybe a release tag?
-export const ANGULAR_LATEST_VERSION = '1.3.51';
+export const ANGULAR_LATEST_VERSION = '2.0.0';
 export const SCRIPT_ID = 'builder-wc-script';
