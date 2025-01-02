4f2b8f274118beda77db497ba27f8a846dcbce5a
sidmohanty11 <sidmohanty11@gmail.com>
2025-01-02 16:07:12 +0530

    fix

--- packages/sdks/e2e/angular-19-ssr/src/app/app.config.ts
+++ packages/sdks/e2e/angular-19-ssr/src/app/app.config.ts
@@ -4,7 +4,7 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import {
   provideClientHydration,
   withEventReplay,
@@ -16,6 +16,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
   ],
 };
