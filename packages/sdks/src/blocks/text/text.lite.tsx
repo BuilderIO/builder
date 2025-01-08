1f4aaaf7ce5bbe403fdc169273bfd1812e90af35
Sami Jaber <jabersami@gmail.com>
2025-01-08 11:42:45 -0400

    🏃

--- packages/sdks/src/blocks/text/text.lite.tsx
+++ packages/sdks/src/blocks/text/text.lite.tsx
@@ -7,7 +7,7 @@ export default function Text(props: TextProps) {
         /* NOTE: This class name must be "builder-text" for inline editing to work in the Builder editor */
         'builder-text'
       }
-      innerHTML={props.text}
+      innerHTML={props.text?.toString() || ''}
       style={{ outline: 'none' }}
     />
   );
