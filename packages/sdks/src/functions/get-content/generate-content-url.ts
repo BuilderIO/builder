e0dc757b5d48e51262226ba666377c3b649d085a
Clyde Mendonca <clyde@builder.io>
2025-01-13 11:49:15 +0530

    fix[gen2][builderStudio] ENG-7646 add Builder Studio support for Gen-2 SDK (#3794)
    ## Description
    
    Builder Components not rendering in Studio Tab on Gen 2 SDK
    
    **Jira Ticket**
    https://builder-io.atlassian.net/browse/ENG-7646
    
    **Jira Ticket for Gen1 References**
    https://builder-io.atlassian.net/browse/ENG-4980
    
    **Loom**
    https://www.loom.com/share/063d068f6be84b54adb027b03ec14793
    
    **Loom where current changes will not give desired result**
    https://www.loom.com/share/6628e8caa98f4af2905abb2c6c0d4646
    
    ---------
    
    Co-authored-by: Clyde Mendonca <clydemendonca@Clydes-MacBook-Pro.local>

--- packages/sdks/src/functions/get-content/generate-content-url.ts
+++ packages/sdks/src/functions/get-content/generate-content-url.ts
@@ -1,7 +1,12 @@
-import { flatten, flattenMongoQuery } from '../../helpers/flatten.js';
+import {
+  flatten,
+  flattenMongoQuery,
+  unflatten,
+} from '../../helpers/flatten.js';
 import { normalizeSearchParams } from '../../helpers/search/search.js';
 import { DEFAULT_API_VERSION } from '../../types/api-version.js';
 import { getBuilderSearchParamsFromWindow } from '../get-builder-search-params/index.js';
+import { isBrowser } from '../is-browser.js';
 import type { GetContentOptions } from './types.js';
 
 const isPositiveNumber = (thing: unknown) =>
@@ -49,7 +54,7 @@ export const generateContentUrl = (options: GetContentOptions): URL => {
   url.searchParams.set('includeRefs', String(true));
 
   const finalLocale = locale || userAttributes?.locale;
-  let finalUserAttributes = userAttributes;
+  let finalUserAttributes: Record<string, any> = userAttributes || {};
 
   if (finalLocale) {
     url.searchParams.set('locale', finalLocale);
@@ -96,12 +101,17 @@ export const generateContentUrl = (options: GetContentOptions): URL => {
     ...normalizeSearchParams(options.options || {}),
   };
 
+  finalUserAttributes = {
+    ...finalUserAttributes,
+    ...getUserAttributesAsJSON(queryOptions),
+  };
+
   const flattened = flatten(queryOptions);
   for (const key in flattened) {
     url.searchParams.set(key, String(flattened[key]));
   }
 
-  if (finalUserAttributes) {
+  if (Object.keys(finalUserAttributes).length > 0) {
     url.searchParams.set('userAttributes', JSON.stringify(finalUserAttributes));
   }
   if (query) {
@@ -111,4 +121,28 @@ export const generateContentUrl = (options: GetContentOptions): URL => {
     }
   }
   return url;
+};
+
+const getUserAttributesFromQueryOptions = (queryOptions: any) => {
+  const newUserAttributes: any = {};
+  for (const key in queryOptions) {
+    if (key.startsWith('userAttributes.')) {
+      newUserAttributes[key] = queryOptions[key];
+      delete queryOptions[key];
+    }
+  }
+  return newUserAttributes;
+};
+
+const getUserAttributesAsJSON = (queryOptions: any) => {
+  if (isBrowser() && queryOptions['preview'] === 'BUILDER_STUDIO') {
+    queryOptions['userAttributes.urlPath'] = window.location.pathname;
+    queryOptions['userAttributes.host'] = window.location.host;
+
+    const queryOptionsForUserAttributes =
+      getUserAttributesFromQueryOptions(queryOptions);
+    const { userAttributes } = unflatten(queryOptionsForUserAttributes);
+    return userAttributes;
+  }
+  return {};
 };
