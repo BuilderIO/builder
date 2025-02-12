95b0a14a6d21e2d3335d9dc341e99a17a130cf04
Uttej Venkata Sastry <uttej@builder.io>
2025-02-11 18:49:06 -0600

    .

--- packages/sdks-tests/src/snippet-tests/live-previewing.spec.ts
+++ packages/sdks-tests/src/snippet-tests/live-previewing.spec.ts
@@ -21,13 +21,6 @@ test.describe('LivePreviewBlogData Component', () => {
     await expect(blogPreview).toContainText('Blog Title: Welcome to Builder.io');
     await expect(blogPreview).toContainText('Authored by: John Doe');
     await expect(blogPreview).toContainText('Handle: john_doe');
-
-    const expectedDate = new Date().toDateString().split(' ').slice(0, 4).join(' ');
-
-    console.log('expectedDate', expectedDate);
-
-    const actualText = await blogPreview.textContent();
-
-    expect(actualText).toContain(`Published date: ${expectedDate}`);
+    await expect(blogPreview).toContainText('Published date: Tue Feb 11 2025');
   });
 });
