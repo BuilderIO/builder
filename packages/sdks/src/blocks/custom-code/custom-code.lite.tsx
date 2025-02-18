3978246bc1c1c15b88f9ae5effbeef310146095b
sidmohanty11 <sidmohanty11@gmail.com>
2025-02-18 15:17:20 +0530

    fix

--- packages/sdks/src/blocks/custom-code/custom-code.lite.tsx
+++ packages/sdks/src/blocks/custom-code/custom-code.lite.tsx
@@ -1,6 +1,5 @@
 import {
   onMount,
-  onUpdate,
   useMetadata,
   useRef,
   useStore,
@@ -24,68 +23,59 @@ export default function CustomCode(props: CustomCodeProps) {
   const state = useStore({
     scriptsInserted: [] as string[],
     scriptsRun: [] as string[],
-    isHydrated: false,
-    runScripts: () => {
-      if (
-        !elementRef ||
-        !elementRef?.getElementsByTagName ||
-        typeof window === 'undefined' ||
-        !state.isHydrated
-      ) {
-        return;
-      }
-
-      const scripts = elementRef.getElementsByTagName('script');
-      for (let i = 0; i < scripts.length; i++) {
-        const script = scripts[i];
-        if (script.src) {
-          if (state.scriptsInserted.includes(script.src)) {
-            continue;
-          }
-          state.scriptsInserted.push(script.src);
-          const newScript = document.createElement('script');
-          newScript.async = true;
-          newScript.src = script.src;
-          document.head.appendChild(newScript);
-        } else if (
-          !script.type ||
-          [
-            'text/javascript',
-            'application/javascript',
-            'application/ecmascript',
-          ].includes(script.type)
-        ) {
-          if (state.scriptsRun.includes(script.innerText)) {
-            continue;
-          }
-          try {
-            useTarget({
-              angular: () => {
-                requestAnimationFrame(() => {
-                  state.scriptsRun.push(script.innerText);
-                  new Function(script.innerText)();
-                });
-              },
-              default: () => {
-                state.scriptsRun.push(script.innerText);
-                new Function(script.innerText)();
-              },
-            });
-          } catch (error) {
-            console.warn('`CustomCode`: Error running script:', error);
-          }
-        }
-      }
-    },
   });
 
   onMount(() => {
-    state.isHydrated = true;
-  });
+    if (
+      !elementRef ||
+      !elementRef?.getElementsByTagName ||
+      typeof window === 'undefined'
+    ) {
+      return;
+    }
 
-  onUpdate(() => {
-    state.runScripts();
-  }, [props.code, state.isHydrated]);
+    const scripts = elementRef.getElementsByTagName('script');
+    for (let i = 0; i < scripts.length; i++) {
+      const script = scripts[i];
+      if (script.src) {
+        if (state.scriptsInserted.includes(script.src)) {
+          continue;
+        }
+        state.scriptsInserted.push(script.src);
+        const newScript = document.createElement('script');
+        newScript.async = true;
+        newScript.src = script.src;
+        document.head.appendChild(newScript);
+      } else if (
+        !script.type ||
+        [
+          'text/javascript',
+          'application/javascript',
+          'application/ecmascript',
+        ].includes(script.type)
+      ) {
+        if (state.scriptsRun.includes(script.innerText)) {
+          continue;
+        }
+        try {
+          useTarget({
+            angular: () => {
+              requestAnimationFrame(() => {
+                state.scriptsRun.push(script.innerText);
+                new Function(script.innerText)();
+              });
+            },
+            default: () => {
+              state.scriptsRun.push(script.innerText);
+              new Function(script.innerText)();
+            },
+          });
+        } catch (error) {
+          console.warn('`CustomCode`: Error running script:', error);
+        }
+      }
+    }
+  });
 
   return (
     <div
