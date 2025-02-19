eea0b1af1d0cec15b259a515c86a3f03f03aa4d2
Uttej Venkata Sastry <uttej@builder.io>
2025-02-19 16:50:13 -0600

    angular PR comments

--- packages/sdks/snippets/angular-16/src/app/live-preview/live-preview.component.ts
+++ packages/sdks/snippets/angular-16/src/app/live-preview/live-preview.component.ts
@@ -4,50 +4,49 @@ import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import type { BuilderContent } from '@builder.io/sdk-angular';
 import { fetchOneEntry, subscribeToEditor } from '@builder.io/sdk-angular';
+
 @Component({
   standalone: true,
   selector: 'app-live-preview',
   imports: [CommonModule],
-  template: `<div *ngIf="loading">Loading Data...</div>
+  template: `
+    <div *ngIf="loading" class="loading-message">Loading Data...</div>
+
     <div *ngIf="!loading && content" class="blog-data-preview">
       <div>Blog Title: {{ content.data?.title }}</div>
       <div>Authored by: {{ content.data?.['author'] }}</div>
       <div>Handle: {{ content.data?.['handle'] }}</div>
       <div>
         Published date:
-        {{
-          content.data?.['publishedDate']?.split(' ')?.slice(0, 4)?.join(' ')
-        }}
+        {{ getFormattedDate(content.data?.['publishedDate']) }}
       </div>
     </div>
-    <div *ngIf="error" class="error-message">{{ error }}</div>`,
+
+    <div *ngIf="!loading && !content" class="no-data-message">No Data.</div>
+  `,
 })
 export class LivePreviewComponent implements OnInit, OnDestroy {
   content: BuilderContent | null = null;
-  loading = true;
-  error: string | null = null;
+  loading = false;
 
   private unsubscribeFn?: () => void;
   private router = inject(Router);
 
+  getFormattedDate(dateString: string | null): string {
+    return dateString ? new Date(dateString).toDateString() : '';
+  }
+
   ngOnInit(): void {
+    this.loading = true;
     fetchOneEntry({
       model: 'blog-data',
       apiKey: 'ee9f13b4981e489a9a1209887695ef2b',
-      userAttributes: {
-        urlPath: this.router.url,
-      },
+      userAttributes: { urlPath: this.router.url },
     })
       .then((data) => {
-        this.content = data;
-        this.loading = false;
+        this.content = data || null;
       })
-      .catch((err) => {
-        console.error(
-          'Something went wrong while fetching Builder content:',
-          err
-        );
-        this.error = 'Failed to load content. Please try again later.';
+      .finally(() => {
         this.loading = false;
       });
 
