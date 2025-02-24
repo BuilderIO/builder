887c6e0b3a18b792695ae44e770c6f26eecd3b06
Sidharth Mohanty <sidmohanty11@gmail.com>
2025-02-24 12:07:27 +0530

    fix[gen2-sdks]: updating code doesn't reflect changes in VE and angular sdk nativeElement not found (#3908)
    ## Description
    
    Fixes CustomCode block:
    - realtime script/code updates reflect changes in the Visual Editor
    
    Fixes Angular CustomCode and Embed nativeElement not found issue
    
    Jira
    https://builder-io.atlassian.net/browse/ENG-8354
    https://builder-io.atlassian.net/browse/ENG-8353
    
    _Screenshot_
    If relevant, add a screenshot or two of the changes you made.

--- packages/sdks/src/blocks/custom-code/custom-code.lite.tsx
+++ packages/sdks/src/blocks/custom-code/custom-code.lite.tsx
@@ -1,8 +1,20 @@
-import { onMount, useMetadata, useRef, useStore } from '@builder.io/mitosis';
+import {
+  onMount,
+  onUpdate,
+  useMetadata,
+  useRef,
+  useStore,
+  useTarget,
+} from '@builder.io/mitosis';
+import { isEditing } from '../../functions/is-editing.js';
+import { logger } from '../../helpers/logger.js';
 
 useMetadata({
   rsc: {
     componentType: 'client',
+  },
+  angular: {
+    changeDetection: 'OnPush',
   },
 });
 
@@ -17,46 +29,85 @@ export default function CustomCode(props: CustomCodeProps) {
   const state = useStore({
     scriptsInserted: [] as string[],
     scriptsRun: [] as string[],
+    runScripts: () => {
+      if (
+        !elementRef ||
+        !elementRef?.getElementsByTagName ||
+        typeof window === 'undefined'
+      ) {
+        return;
+      }
+
+      const scripts = elementRef.getElementsByTagName('script');
+      for (let i = 0; i < scripts.length; i++) {
+        const script = scripts[i];
+        if (script.src) {
+          if (state.scriptsInserted.includes(script.src)) {
+            continue;
+          }
+          state.scriptsInserted.push(script.src);
+          const newScript = document.createElement('script');
+          newScript.async = true;
+          newScript.src = script.src;
+          document.head.appendChild(newScript);
+        } else if (
+          !script.type ||
+          [
+            'text/javascript',
+            'application/javascript',
+            'application/ecmascript',
+          ].includes(script.type)
+        ) {
+          if (state.scriptsRun.includes(script.innerText)) {
+            continue;
+          }
+          try {
+            useTarget({
+              angular: () => {
+                requestAnimationFrame(() => {
+                  state.scriptsRun.push(script.innerText);
+                  new Function(script.innerText)();
+                });
+              },
+              default: () => {
+                state.scriptsRun.push(script.innerText);
+                new Function(script.innerText)();
+              },
+            });
+          } catch (error) {
+            logger.warn(
+              '[BUILDER.IO] `CustomCode`: Error running script:',
+              error
+            );
+          }
+        }
+      }
+    },
   });
 
   onMount(() => {
-    // TODO: Move this function to standalone one in '@builder.io/utils'
-    if (!elementRef?.getElementsByTagName || typeof window === 'undefined') {
-      return;
-    }
-
-    const scripts = elementRef.getElementsByTagName('script');
-    for (let i = 0; i < scripts.length; i++) {
-      const script = scripts[i];
-      if (script.src) {
-        if (state.scriptsInserted.includes(script.src)) {
-          continue;
-        }
-        state.scriptsInserted.push(script.src);
-        const newScript = document.createElement('script');
-        newScript.async = true;
-        newScript.src = script.src;
-        document.head.appendChild(newScript);
-      } else if (
-        !script.type ||
-        [
-          'text/javascript',
-          'application/javascript',
-          'application/ecmascript',
-        ].includes(script.type)
-      ) {
-        if (state.scriptsRun.includes(script.innerText)) {
-          continue;
-        }
-        try {
-          state.scriptsRun.push(script.innerText);
-          new Function(script.innerText)();
-        } catch (error) {
-          console.warn('`CustomCode`: Error running script:', error);
-        }
-      }
-    }
+    state.runScripts();
   });
+
+  onUpdate(() => {
+    if (isEditing()) {
+      useTarget({
+        svelte: () => {
+          setTimeout(() => {
+            state.runScripts();
+          }, 0);
+        },
+        vue: () => {
+          setTimeout(() => {
+            state.runScripts();
+          }, 0);
+        },
+        default: () => {
+          state.runScripts();
+        },
+      });
+    }
+  }, [props.code]);
 
   return (
     <div
