b07bdcc9a1ed62a81a961f40a7a35963e9ce9b4c
Uttej V S K <uttejvsk@hotmail.com>
2025-02-28 16:18:05 -0600

    angular and angular-ssr, svelte and sveltekit refactor

--- packages/sdks/snippets/svelte/src/components/LivePreview.svelte
+++ packages/sdks/snippets/svelte/src/components/LivePreview.svelte
@@ -1,31 +1,11 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { fetchOneEntry, subscribeToEditor } from '@builder.io/sdk-svelte';
+  import { subscribeToEditor } from '@builder.io/sdk-svelte';
   import type { BuilderContent } from '@builder.io/sdk-svelte';
 
   let content: BuilderContent | null = null;
-  let isLoading = true;
-  
-  let urlPath = window.location.pathname;
 
   onMount(() => {
-    fetchOneEntry({
-      model: 'blog-data',
-      apiKey: 'ee9f13b4981e489a9a1209887695ef2b',
-      userAttributes: {
-        urlPath,
-      },
-    })
-      .then((data) => {
-        content = data;
-      })
-      .catch((err) => {
-        console.error('Error fetching Builder content:', err);
-      })
-      .finally(() => {
-        isLoading = false;
-      });
-
     let unsubscribe = subscribeToEditor({
       model: 'blog-data',
       apiKey: 'ee9f13b4981e489a9a1209887695ef2b',
@@ -37,10 +17,6 @@
     return () => unsubscribe();
   });
 </script>
-
-{#if isLoading && !content}
-  <div>Loading...</div>
-{/if}
 
 {#if content}
   <div class="blog-data-preview">
