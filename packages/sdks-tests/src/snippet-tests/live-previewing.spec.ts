67021f6c2a580da22822b053bab867859f7cc634
Uttej V S K <uttejvsk@hotmail.com>
2025-03-04 00:05:08 +0530

    EDU-459: Qwik - live preview (#3936)
    ## Description
    
    This is similar to Angular, Vue, React, and Svelte PRs raised earlier.
    
    @samijaber This is the last PR in the ticket:
    https://builder-io.atlassian.net/browse/EDU-459
    
    Please review this.
    
    ---------
    Authored-by: Uttej Venkata Sastry <uttej@builder.io>

--- packages/sdks-tests/src/snippet-tests/live-previewing.spec.ts
+++ packages/sdks-tests/src/snippet-tests/live-previewing.spec.ts
@@ -3,42 +3,13 @@ import { test } from '../helpers/index.js';
 import { launchEmbedderAndWaitForSdk, sendPatchOrUpdateMessage } from '../helpers/visual-editor.js';
 
 test.describe('LivePreviewBlogData Component', () => {
-  test('should render the page without 404', async ({ page, packageName }) => {
-    test.skip(
-      !['react', 'vue', 'nuxt', 'angular-16', 'angular-16-ssr', 'sveltekit', 'svelte'].includes(
-        packageName
-      )
-    );
-
-    const response = await page.goto('/live-preview');
-    expect(response?.status()).toBeLessThan(400);
-  });
-
-  test('should display blog details correctly', async ({ page, packageName }) => {
-    test.skip(
-      !['react', 'vue', 'nuxt', 'angular-16', 'angular-16-ssr', 'sveltekit', 'svelte'].includes(
-        packageName
-      )
-    );
-
-    await page.goto('/live-preview');
-
-    const blogPreview = page.locator('.blog-data-preview');
-    await expect(blogPreview).toBeVisible();
-
-    //assert the blog details coming from builder data model
-    await expect(blogPreview).toContainText('Blog Title: Welcome to Builder.io');
-    await expect(blogPreview).toContainText('Authored by: John Doe');
-    await expect(blogPreview).toContainText('Handle: john_doe');
-  });
-
   test.describe('Live Preview blog data - Visual Editor', () => {
     test('enables live previewing and editing', async ({ page, basePort, sdk, packageName }) => {
       test.skip(
         packageName === 'nextjs-sdk-next-app' ||
           packageName === 'gen1-next14-pages' ||
           packageName === 'gen1-remix' ||
-          !['react', 'vue', 'nuxt', 'angular-16', 'angular-16-ssr', 'sveltekit', 'svelte'].includes(
+          !['react', 'vue', 'nuxt', 'angular-16', 'angular-16-ssr', 'sveltekit', 'svelte', 'qwik-city'].includes(
             packageName
           ),
         'Skipping test: incompatible package or framework.'
@@ -53,6 +24,16 @@ test.describe('LivePreviewBlogData Component', () => {
           handle: 'john_doe',
         },
       };
+
+      await sendPatchOrUpdateMessage({
+        page,
+        content: INITIAL_CONTENT,
+        model: 'blog-data',
+        sdk,
+        updateFn: content => content,
+        path: '/live-preview',
+      });
+
       await expect(
         page.frameLocator('iframe').getByText(`Blog Title: ${INITIAL_CONTENT.data.title}`)
       ).toBeVisible();
