a7a027d89c3dfab5091e2de2bedb963916317fc3
Shubham <shubham@Shubhams-MacBook-Pro.local>
2025-03-03 18:30:36 +0530

    removed extra custom components

--- examples/angular-gen2/src/app/catch-all.component.ts
+++ examples/angular-gen2/src/app/catch-all.component.ts
@@ -1,16 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { HttpClient, HttpResponse } from '@angular/common/http';
 import {
   Content,
   _processContentResult,
   fetchOneEntry,
   getBuilderSearchParams,
-  type RegisteredComponent,
   type BuilderContent,
 } from '@builder.io/sdk-angular';
-import { firstValueFrom, retry } from 'rxjs';
-import { customComponents } from '../../../../packages/sdks/e2e/angular-19-ssr/src/app/custom-components';
+
+
 import { environment } from '../environments/environment';
 
 interface BuilderProps {
@@ -34,7 +32,6 @@ interface BuilderProps {
         [model]="model"
         [content]="content"
         [apiKey]="apiKey"
-        [customComponents]="customComponents"
       ></builder-content>
     } @else {
       <div>404 - Content not found</div>
@@ -46,29 +43,9 @@ export class CatchAllComponent {
     model = "page";
     apiKey = environment.builderApiKey;
     content: BuilderContent | null = null;
-    customComponents: RegisteredComponent[] = customComponents;
 
     private route = inject(ActivatedRoute);
     private router = inject(Router);
-    private http = inject(HttpClient);
-
-    httpClientFetch=async (url: string, options: RequestInit) => {
-        return firstValueFrom(
-          this.http.request<any>(options.method || 'GET', url, {
-            body: options.body,
-            headers: options.headers as any,
-            ...options,
-            observe: 'response', 
-            responseType: 'json',
-          })
-        ).then((response: HttpResponse<any>) => {
-          return {
-            ok: response.status >= 200 && response.status < 300,
-            status: response.status,
-            json: () => Promise.resolve(response.body),
-          };
-        });
-      };
 
   async ngOnInit() {
     const urlPath = this.router.url.split('?')[0] || '';
@@ -80,17 +57,16 @@ export class CatchAllComponent {
     });
 
     const builderContent = await fetchOneEntry({
-        model: 'page',
-        apiKey: this.apiKey,
-        options: getBuilderSearchParams(searchParams),
-        userAttributes: { urlPath },
-        fetch: this.httpClientFetch,
-      });
-  
-      if (!builderContent) {
-        return;
-      }
-  
-      this.content = builderContent;
+      model: 'page',
+      apiKey: this.apiKey,
+      options: getBuilderSearchParams(searchParams),
+      userAttributes: { urlPath },
+    });
+
+    if (!builderContent) {
+      return;
+    }
+
+    this.content = builderContent;
   }
 }
