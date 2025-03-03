3d6aa46a7bb77cee3dd9796bdf68e0af329d15d7
Yash Wadhia <yashwadhia@Yashs-MacBook-Pro.local>
2025-03-03 12:54:48 +0530

    fix: allowed dynamic test cases for all the sdks. checking href value

--- packages/sdks-tests/src/e2e-tests/dynamic-button.spec.ts
+++ packages/sdks-tests/src/e2e-tests/dynamic-button.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from '@playwright/test';
-import { test } from '../helpers/index.js';
+import { checkIsRN, test } from '../helpers/index.js';
 import {
   cloneContent,
   launchEmbedderAndWaitForSdk,
@@ -9,7 +9,6 @@ import { DYNAMIC_BUTTON } from '../specs/dynamic-button.js';
 
 test.describe('Dynamic Button', () => {
   test('should render a button', async ({ page, sdk, basePort }) => {
-    test.skip(sdk !== 'angular');
 
     await launchEmbedderAndWaitForSdk({
       path: '/dynamic-button',
@@ -18,9 +17,10 @@ test.describe('Dynamic Button', () => {
       sdk,
     });
 
-    const buttonLocator = page
-      .frameLocator('iframe')
-      .locator('[builder-id="builder-b53d1cc2bcbb481b869207fdd97ee1db"]');
+    const buttonLocator = checkIsRN(sdk) ? page.frameLocator('iframe').locator('button') : page
+    .frameLocator('iframe')
+    .locator('[builder-id="builder-b53d1cc2bcbb481b869207fdd97ee1db"]');
+    
     await expect(buttonLocator).toHaveText('Click me!');
     const newContent = cloneContent(DYNAMIC_BUTTON);
 
@@ -46,10 +46,12 @@ test.describe('Dynamic Button', () => {
       model: 'page',
     });
 
-    const updatedButtonLocator = page
+    const updatedButtonLocator = checkIsRN(sdk) ? page.frameLocator('iframe').locator('a') : page
       .frameLocator('iframe')
       .locator('[builder-id="builder-b53d1cc2bcbb481b869207fdd97ee1db"]');
 
     await expect(updatedButtonLocator).toBeVisible();
+    
+    await expect(updatedButtonLocator).toHaveAttribute('href', '#go');
   });
 });
