a4938ce8edd5a342c8d1b18b470ee28f50a9fa8a
Wes Reid <bwreid@gmail.com>
2025-03-04 08:03:53 -0800

    Add GetContentOptions for Gen 1 React

--- packages/sdks/snippets/gen1-react/src/routes/query-cheatsheet/queries/and.tsx
+++ packages/sdks/snippets/gen1-react/src/routes/query-cheatsheet/queries/and.tsx
@@ -1,6 +1,7 @@
 import { builder } from '@builder.io/react';
+import type { GetContentOptions } from '@builder.io/sdk';
 
-export const getProduct = (options: { [key: string]: any }) =>
+export const getProduct = (options: GetContentOptions) =>
   builder.get('product', {
     query: {
       'data.price': {
--- packages/sdks/snippets/gen1-react/src/routes/query-cheatsheet/queries/ne.tsx
+++ packages/sdks/snippets/gen1-react/src/routes/query-cheatsheet/queries/ne.tsx
@@ -1,6 +1,7 @@
 import { builder } from '@builder.io/react';
+import type { GetContentOptions } from '@builder.io/sdk';
 
-export const getProduct = (options: { [key: string]: any }) =>
+export const getProduct = (options: GetContentOptions) =>
   builder.get('contact-record', {
     query: { 'data.department': { $ne: 'Human Resources' } },
     ...options,
