3b2e9fc042aa84dcf86868e4428f132311e9b47b
Uttej V S K <uttejvsk@hotmail.com>
2025-03-05 14:18:01 -0600

    remove console.log

--- packages/sdks/snippets/sveltekit/src/routes/product/category/[handle]/+page.server.js
+++ packages/sdks/snippets/sveltekit/src/routes/product/category/[handle]/+page.server.js
@@ -2,14 +2,13 @@ import { fetchOneEntry } from '@builder.io/sdk-svelte';
 
 export const load = async ({ params }) => {
   const handle = params.handle; 
- 
-  const data = await fetchOneEntry({
+
+  const productDetails = await fetchOneEntry({
     model: 'product-details',
     apiKey: 'ee9f13b4981e489a9a1209887695ef2b',
     query: {
       'data.handle': handle,
     },
   });
-  console.log('data', data);
-  return { productDetails: data };
+  return { productDetails };
 };
