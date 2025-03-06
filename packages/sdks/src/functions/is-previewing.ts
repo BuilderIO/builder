9f6d0c8bf49a5292dbe805021b2e71bf96fd8b50
Sami Jaber <jabersami@gmail.com>
2025-03-06 15:41:34 -0400

    👹

--- packages/sdks/src/functions/is-previewing.ts
+++ packages/sdks/src/functions/is-previewing.ts
@@ -9,21 +9,22 @@ import { isBrowser } from './is-browser.js';
  * When called on the client, it uses `window.location.search`.
  * When called on the server (SSR), it requires a `search` argument.
  *
- * @param _search - The search parameters to check. Can be a URL string (containing the search parameters), `URLSearchParams`, or a key-value object containing the search parameters.
+ * @param search - The search parameters to check. Can be a URL string (containing the search parameters), `URLSearchParams`, or a key-value object containing the search parameters.
  * @returns `true` if the current page is being previewed, `false` otherwise.
  */
-export function isPreviewing(_search?: Search) {
-  const search = _search || (isBrowser() ? window.location.search : undefined);
+export function isPreviewing(search?: Search) {
+  const searchToUse =
+    search || (isBrowser() ? window.location.search : undefined);
 
   /**
    * If this function is called on the server without an explicit `search` argument,
    * then it can't check if the user is previewing, and will return `false`.
    */
-  if (!search) {
+  if (!searchToUse) {
     return false;
   }
 
-  const normalizedSearch = getSearchString(search);
+  const normalizedSearch = getSearchString(searchToUse);
 
   return Boolean(normalizedSearch.indexOf('builder.preview=') !== -1);
 }
