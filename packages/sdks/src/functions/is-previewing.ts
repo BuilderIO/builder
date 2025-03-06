e07909e8dad40170015252f1b7ad83cf30367030
Sami Jaber <jabersami@gmail.com>
2025-03-06 15:39:24 -0400

    👹

--- packages/sdks/src/functions/is-previewing.ts
+++ packages/sdks/src/functions/is-previewing.ts
@@ -2,6 +2,16 @@ import type { Search } from '../helpers/search/search.js';
 import { getSearchString } from '../helpers/search/search.js';
 import { isBrowser } from './is-browser.js';
 
+/**
+ * Checks the current URL's search parameters to see if it is attempting to
+ * preview Builder Content.
+ *
+ * When called on the client, it uses `window.location.search`.
+ * When called on the server (SSR), it requires a `search` argument.
+ *
+ * @param _search - The search parameters to check. Can be a URL string (containing the search parameters), `URLSearchParams`, or a key-value object containing the search parameters.
+ * @returns `true` if the current page is being previewed, `false` otherwise.
+ */
 export function isPreviewing(_search?: Search) {
   const search = _search || (isBrowser() ? window.location.search : undefined);
 
