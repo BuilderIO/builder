e0d6b6a670d90047b557f6a6d0fa50b2934e5ffb
Uttej V S K <uttejvsk@hotmail.com>
2025-03-17 16:53:51 -0500

    fix typos

--- packages/sdks/snippets/vue/src/views/blueprints/ProductDetails.vue
+++ packages/sdks/snippets/vue/src/views/blueprints/ProductDetails.vue
@@ -3,27 +3,29 @@
       <p>Loading product details...</p>
     </div>
     <div class="product-details-page" v-else>
-      <h1>{{ productDetails.data?.['name'] }}</h1>
+      <h1>{{ productDetails.data.name }}</h1>
       <img
-        :src="productDetails.data?.['image']"
-        :alt="productDetails.data?.['name']"
+        :src="productDetails.data.image"
+        :alt="productDetails.data.name"
       />
-      <p>{{ productDetails.data?.['collection'].value.data.copy }}</p>
-      <p>Price: {{ productDetails.data?.['collection'].value.data.price }}</p>
+      <p>{{ productDetails.data.collection.value.data.copy }}</p>
+      <p>Price: {{ productDetails.data.collection.value.data.price }}</p>
     </div>
   </template>
   
   <script setup lang="ts">
   import { ref, onMounted } from 'vue'
-  import { fetchOneEntry, typeBuilderContent } from '@builder.io/sdk-vue'
+  import { fetchOneEntry, type BuilderContent } from '@builder.io/sdk-vue'
+  import { useRoute } from 'vue-router'
   
+  const route = useRoute()
   const productDetails = ref<BuilderContent | null>(null);
   
   onMounted( () => {
     fetchOneEntry({
         model: 'product-details',
         apiKey: 'ee9f13b4981e489a9a1209887695ef2b',
-        query: { 'data.handle': 'jacket' },
+        query: { 'data.handle': route.params.handle },
       }).then((data) => {
         productDetails.value = data
       })
