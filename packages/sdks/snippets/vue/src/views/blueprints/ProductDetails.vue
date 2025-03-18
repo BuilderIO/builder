116660ec841caa007b0983d55cb43219668ea6d4
UVSK <uttejvsk@hotmail.com>
2025-03-18 20:32:58 +0530

    EDU-135: Product details for all the frameworks (#3948)
    ## Description
    
    This PR adds the code for Product Details for all the frameworks.
    
    @samijaber This is part 2 of our new strategy to use two PRs, one for a
    single framework to talk about changes and another to replicate the
    feedback and finish off the ticket.
    
    Part 1 PR: https://github.com/BuilderIO/builder/pull/3946

---
+++ packages/sdks/snippets/vue/src/views/blueprints/ProductDetails.vue
@@ -0,0 +1,34 @@
+<template>
+    <div v-if="!productDetails?.data">
+      <p>Loading product details...</p>
+    </div>
+    <div class="product-details-page" v-else>
+      <h1>{{ productDetails.data.name }}</h1>
+      <img
+        :src="productDetails.data.image"
+        :alt="productDetails.data.name"
+      />
+      <p>{{ productDetails.data.collection.value.data.copy }}</p>
+      <p>Price: {{ productDetails.data.collection.value.data.price }}</p>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref, onMounted } from 'vue'
+  import { fetchOneEntry, type BuilderContent } from '@builder.io/sdk-vue'
+  import { useRoute } from 'vue-router'
+  
+  const route = useRoute()
+  const productDetails = ref<BuilderContent | null>(null);
+  
+  onMounted( () => {
+    fetchOneEntry({
+        model: 'product-details',
+        apiKey: 'ee9f13b4981e489a9a1209887695ef2b',
+        query: { 'data.handle': route.params.handle },
+      }).then((data) => {
+        productDetails.value = data
+      })
+  })
+  </script>
+  
