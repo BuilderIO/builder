d101c80539e3204f01585a1bc1362863820bd5f6
Uttej V S K <uttejvsk@hotmail.com>
2025-04-09 17:26:48 -0500

    .

--- packages/sdks-tests/src/snippet-tests/blueprints-navlinks.spec.ts
+++ packages/sdks-tests/src/snippet-tests/blueprints-navlinks.spec.ts
@@ -3,7 +3,7 @@ import { test } from '../helpers/index.js';
 
 test.describe('Nav Bar Components', () => {
   test.beforeEach(async ({ page, packageName }) => {
-    test.skip(!['angular-16', 'angular-16-ssr'].includes(packageName));
+    test.skip(!['angular-16', 'angular-16-ssr', 'vue'].includes(packageName));
     // Visit the page where NavBarComponent is rendered
     await page.goto('/landing-page');
   });
