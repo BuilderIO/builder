d77474feccf2ac947ef8c6133c6a91403e9ea2c1
Uttej V S K <uttejvsk@hotmail.com>
2025-04-09 16:01:56 -0500

    fix

--- packages/sdks/snippets/react/src/routes/blueprints/NavLinks.tsx
+++ packages/sdks/snippets/react/src/routes/blueprints/NavLinks.tsx
@@ -2,15 +2,13 @@ import { fetchOneEntry, type BuilderContent } from '@builder.io/sdk-react';
 import { useEffect, useState } from 'react';
 import { NavBar } from '../../components/NavBar';
 
-const API_KEY = 'ee9f13b4981e489a9a1209887695ef2b';
-
 export function NavLinks() {
   const [links, setLinks] = useState<BuilderContent | null>(null);
 
   useEffect(() => {
     fetchOneEntry({
       model: 'navigation-links',
-      apiKey: API_KEY,
+      apiKey: 'ee9f13b4981e489a9a1209887695ef2b',
     }).then((data) => {
       setLinks(data);
     });
