473e18fcd22d4018f7e026f81017f2a5708cb52a
Uttej V S K <uttejvsk@hotmail.com>
2025-04-09 20:00:25 -0500

    nuxt

--- packages/sdks-tests/src/snippet-tests/blueprints-navlinks.spec.ts
+++ packages/sdks-tests/src/snippet-tests/blueprints-navlinks.spec.ts
@@ -3,7 +3,29 @@ import { test } from '../helpers/index.js';
 
 test.describe('Nav Bar Components', () => {
   test.beforeEach(async ({ page, packageName }) => {
-    test.skip(!['angular-16', 'angular-16-ssr', 'vue'].includes(packageName));
+    test.skip(
+      [
+        'react-native-74',
+        'react-native-76-fabric',
+        'solid',
+        'solid-start',
+        'remix',
+        'react-sdk-next-15-app',
+        'nextjs-sdk-next-app',
+        'react',
+        'angular-19-ssr',
+        'gen1-react',
+        'gen1-remix',
+        'gen1-next14-pages',
+        'gen1-next15-app',
+        'svelte',
+        'sveltekit',
+        'react-sdk-14-app',
+        'react-sdk-next-pages',
+        'hydrogen',
+        'qwik-city',
+      ].includes(packageName)
+    );
     // Visit the page where NavBarComponent is rendered
     await page.goto('/landing-page');
   });
@@ -26,7 +48,7 @@ test.describe('Nav Bar Components', () => {
     });
 
     test('should contain NavLinksComponent', async ({ page }) => {
-      const navLinksComponent = page.getByRole('navigation');
+      const navLinksComponent = page.getByRole('list');
       await expect(navLinksComponent).toBeVisible();
     });
   });
---
+++ packages/sdks/snippets/nuxt/pages/landing-page/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import { useAsyncData } from '#app';
+  import NavBar from '@/components/NavBar.vue';
+  import { fetchOneEntry } from '@builder.io/sdk-vue';
+  
+  const { data: links } = useAsyncData('nav-links', () =>
+     fetchOneEntry({
+      model: 'navigation-links',
+      apiKey: 'ee9f13b4981e489a9a1209887695ef2b'
+    })
+  );
+</script>
+
+<template>
+    <nav>
+      <h1>Acme company</h1>
+      <!-- NavBar component with links from Builder.io -->
+      <NavBar :links="links" />
+      <!-- Auth buttons -->
+      <div class="auth-buttons">
+        <button>Login</button>
+        <button>Register</button>
+      </div>
+    </nav>
+  </template>
+
+  
