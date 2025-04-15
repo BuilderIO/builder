710f3b546fc75cb40dfad9160239f59d5bf7ea65
Uttej V S K <uttejvsk@hotmail.com>
2025-04-15 10:30:04 -0500

    variables for reusability

--- packages/sdks/snippets/react/src/routes/blueprints/Hero.tsx
+++ packages/sdks/snippets/react/src/routes/blueprints/Hero.tsx
@@ -1,13 +1,16 @@
 import { BuilderContent, Content, fetchOneEntry } from '@builder.io/sdk-react';
 import { useEffect, useState } from 'react';
 
+const BUILDER_API_KEY = 'ee9f13b4981e489a9a1209887695ef2b';
+const MODEL_NAME = 'collection-hero';
+
 export default function ProductHero() {
   const [productHero, setProductHero] = useState<BuilderContent | null>(null);
 
   useEffect(() => {
     fetchOneEntry({
-      model: 'collection-hero',
-      apiKey: 'ee9f13b4981e489a9a1209887695ef2b',
+      model: MODEL_NAME,
+      apiKey: BUILDER_API_KEY,
       userAttributes: { urlPath: window.location.pathname },
     }).then((data) => {
       setProductHero(data);
@@ -16,18 +19,13 @@ export default function ProductHero() {
 
   return (
     <>
-      {/* Your nav goes here */}
-
-      {/* Render the Hero content if available */}
       {productHero && (
         <Content
           content={productHero}
-          model="collection-hero"
-          apiKey="ee9f13b4981e489a9a1209887695ef2b"
+          model={MODEL_NAME}
+          apiKey={BUILDER_API_KEY}
         />
       )}
-
-      {/* The rest of your page goes here */}
     </>
   );
 }
