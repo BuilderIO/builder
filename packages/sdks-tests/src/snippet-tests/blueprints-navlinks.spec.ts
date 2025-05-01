ac7e06cf6991f0413d241c31719feaee13437bc8
Uttej V S K <uttejvsk@hotmail.com>
2025-05-01 10:55:40 -0500

    fix to test case

--- packages/sdks-tests/src/snippet-tests/blueprints-navlinks.spec.ts
+++ packages/sdks-tests/src/snippet-tests/blueprints-navlinks.spec.ts
@@ -3,7 +3,22 @@ import { test } from '../helpers/index.js';
 
 test.describe('Nav Bar Components', () => {
   test.beforeEach(async ({ page, packageName }) => {
-    test.skip(!['angular-16', 'angular-16-ssr', 'react'].includes(packageName));
+    test.skip(
+      [
+        'react-native-74',
+        'react-native-76-fabric',
+        'solid',
+        'solid-start',
+        'remix',
+        'react-sdk-next-15-app',
+        'nextjs-sdk-next-app',
+        'angular-19-ssr',
+        'gen1-react',
+        'gen1-remix',
+        'gen1-next14-pages',
+        'gen1-next15-app',
+      ].includes(packageName)
+    );
     // Visit the page where NavBarComponent is rendered
     await page.goto('/landing-page');
   });
