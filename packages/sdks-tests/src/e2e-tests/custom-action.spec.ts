3360c2f1833e9dc3f2f340227803e4a176f3e33e
Shubham <shubham@Shubhams-MacBook-Pro.local>
2025-05-06 22:30:59 +0530

    updated test for react-vite

--- packages/sdks-tests/src/e2e-tests/custom-action.spec.ts
+++ packages/sdks-tests/src/e2e-tests/custom-action.spec.ts
@@ -1,11 +1,10 @@
-import { test, excludeGen2 } from '../helpers/index.js';
+import { test } from '../helpers/index.js';
 import { launchEmbedderAndWaitForSdk } from '../helpers/visual-editor.js';
 
 const CUSTOM_ACTION_LOADED_MESSAGE = 'BUILDER_EVENT: builder.registerAction';
 
-test.describe('Custom actions', () => {
+test.describe.only('Custom actions', () => {
   test('correctly captures registering of custom action', async ({ page, basePort, sdk }) => {
-    test.skip(excludeGen2(sdk));
 
     const customActionMsgPromise = page.waitForEvent('console', msg =>
       msg.text().includes(CUSTOM_ACTION_LOADED_MESSAGE)
