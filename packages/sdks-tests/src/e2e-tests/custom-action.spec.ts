9c79a23c3e00e721098673b7f443c20695fba5bf
Shubham <shubham@Shubhams-MacBook-Pro.local>
2025-05-07 12:15:18 +0530

    updated custom test

--- packages/sdks-tests/src/e2e-tests/custom-action.spec.ts
+++ packages/sdks-tests/src/e2e-tests/custom-action.spec.ts
@@ -4,7 +4,18 @@ import { launchEmbedderAndWaitForSdk } from '../helpers/visual-editor.js';
 const CUSTOM_ACTION_LOADED_MESSAGE = 'BUILDER_EVENT: builder.registerAction';
 
 test.describe.only('Custom actions', () => {
-  test('correctly captures registering of custom action', async ({ page, basePort, sdk }) => {
+  test('correctly captures registering of custom action', async ({ page, basePort, packageName, sdk }) => {
+    test.skip([
+      'solid',
+      'solid-start',
+      'react-sdk-next-pages',
+      'remix','react-sdk-next-14-app',
+      'react-sdk-next-15-app',
+      'nextjs-sdk-next-app',
+      'vue',
+      'svelte',
+      'sveltekit'
+    ].includes(packageName));
 
     const customActionMsgPromise = page.waitForEvent('console', msg =>
       msg.text().includes(CUSTOM_ACTION_LOADED_MESSAGE)
