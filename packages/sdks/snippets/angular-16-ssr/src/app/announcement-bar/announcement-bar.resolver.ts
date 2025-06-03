f0c25f21f8fa2a725b6325a5c7288a9eff0a45dc
Uttej V S K <uttejvsk@hotmail.com>
2025-06-03 18:01:37 -0500

    angular 16 init

--- packages/sdks/snippets/angular-16-ssr/src/app/announcement-bar/announcement-bar.resolver.ts
+++ packages/sdks/snippets/angular-16-ssr/src/app/announcement-bar/announcement-bar.resolver.ts
@@ -1,12 +1,12 @@
 import type { ActivatedRouteSnapshot, ResolveFn } from '@angular/router';
 import { BuilderContent, fetchOneEntry } from '@builder.io/sdk-angular';
 
-export const announcementBarResolver: ResolveFn<BuilderContent | null> = (
+export const announcementBarResolver: ResolveFn<BuilderContent | null> = async (
   route: ActivatedRouteSnapshot
 ) => {
   const urlPath = `/${route.url.join('/')}`;
 
-  return fetchOneEntry({
+  return await fetchOneEntry({
     apiKey: 'ee9f13b4981e489a9a1209887695ef2b',
     model: 'announcement-bar',
     userAttributes: {
--- packages/sdks/snippets/angular-16-ssr/src/app/catch-all/catch-all.component.ts
+++ packages/sdks/snippets/angular-16-ssr/src/app/catch-all/catch-all.component.ts
@@ -6,14 +6,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Content, type BuilderContent } from '@builder.io/sdk-angular';
+import {
+  Content,
+  isPreviewing,
+  type BuilderContent,
+} from '@builder.io/sdk-angular';
 
 @Component({
   selector: 'app-catchall',
   standalone: true,
   imports: [Content, CommonModule],
   template: `
-    <ng-container *ngIf="content; else notFound">
+    <ng-container *ngIf="canShowContent; else notFound">
       <builder-content
         [model]="model"
         [content]="content"
@@ -21,21 +25,22 @@ import { Content, type BuilderContent } from '@builder.io/sdk-angular';
       ></builder-content>
     </ng-container>
 
-    <ng-template #notFound>
-      <div>404 - Content not found</div>
-    </ng-template>
+    <ng-template #notFound> 404 </ng-template>
   `,
 })
 export class CatchAllComponent {
   apiKey = 'ee9f13b4981e489a9a1209887695ef2b';
   model = 'page';
   content: BuilderContent | null = null;
+  canShowContent = false;
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
     this.activatedRoute.data.subscribe((data: any) => {
       this.content = data.content;
+      const searchParams = this.activatedRoute.snapshot.queryParams;
+      this.canShowContent = !!this.content || isPreviewing(searchParams);
     });
   }
 }
