6c94acd410394b87dc84f79e322872d310ef426b
Uttej V S K <uttejvsk@hotmail.com>
2025-06-03 16:03:35 -0500

    add support for nextjs with getBuilderSearchParams

--- packages/sdks/snippets/nextjs-sdk-next-app/app/[[...slug]]/page.tsx
+++ packages/sdks/snippets/nextjs-sdk-next-app/app/[[...slug]]/page.tsx
@@ -6,6 +6,7 @@
 import {
   Content,
   fetchOneEntry,
+  getBuilderSearchParams,
   isPreviewing,
 } from '@builder.io/sdk-react-nextjs';
 
@@ -25,6 +26,7 @@ export default async function Page(props: PageProps) {
     apiKey: PUBLIC_API_KEY,
     model: 'page',
     userAttributes: { urlPath },
+    options: getBuilderSearchParams(props.searchParams),
   });
 
   const canShowContent = content || isPreviewing(props.searchParams);
