511ab1644afa48ef08c72d01ed796d12e51895d5
Uttej V S K <uttejvsk@hotmail.com>
2025-06-24 19:58:17 -0500

    nextjs fixed as per feedback

--- packages/sdks/snippets/react-sdk-next-14-app/app/(blueprints)/(hero)/[slug]/page.tsx
+++ packages/sdks/snippets/react-sdk-next-14-app/app/(blueprints)/(hero)/[slug]/page.tsx
@@ -1,11 +1,14 @@
 import { Content, fetchOneEntry, isPreviewing } from '@builder.io/sdk-react';
-import { headers } from 'next/headers';
 
 const model = 'collection-hero';
 const apiKey = 'ee9f13b4981e489a9a1209887695ef2b';
 
-export default async function ProductHeroPage() {
-  const pathname = headers().get('x-pathname') as string;
+export default async function ProductHeroPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const pathname = `/${params.slug}`;
 
   const productHero = await fetchOneEntry({
     model,
