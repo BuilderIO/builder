f017feb50fb504a2820ffcbfc96322fe6598ea60
Midhun A Darvin <midhunadarvin@gmail.com>
2025-11-20 23:31:42 +0530

    remove angular-20-ssr

--- packages/sdks-tests/src/e2e-tests/custom-components.spec.ts
+++ packages/sdks-tests/src/e2e-tests/custom-components.spec.ts
@@ -50,7 +50,7 @@ test.describe('Custom components', () => {
 
   test('children content are ssred', async ({ browser, packageName }) => {
     test.skip(
-      !['angular-19-ssr', 'angular-17-ssr', 'angular-20-ssr'].includes(packageName),
+      !['angular-19-ssr', 'angular-17-ssr'].includes(packageName),
       'Only run this for Angular SSR and Angular 17,19,20 SSR'
     );
 
--- packages/sdks/e2e/angular-20-ssr/src/app/custom-route-reuse-strategy.ts
+++
@@ -1,7 +0,0 @@
-import { BaseRouteReuseStrategy } from "@angular/router";
-
-export class CustomRouteReuseStrategy extends BaseRouteReuseStrategy {
-  override shouldReuseRoute() {
-    return false;
-  }
-}
