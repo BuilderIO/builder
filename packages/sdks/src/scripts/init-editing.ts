9214940895a9c48b20a156ee4d8ddf4d69e315d1
Sridhar Maskeri <shridharmaskeri@gmail.com>
2026-02-27 13:52:13 +0530

    reverted unwanted change

--- packages/sdks/src/scripts/init-editing.ts
+++ packages/sdks/src/scripts/init-editing.ts
@@ -32,7 +32,6 @@ export const setupBrowserForEditing = (options: {
           apiKey: options.apiKey,
           supportsXSmallBreakpoint: TARGET === 'reactNative' ? false : true,
           blockLevelPersonalization: true,
-          supportsGlobalSymbols: true,
         },
       },
       '*'
